41e8aa64febd7cc7e6ccd55b2b9d493fd38f10f2
Dmitry_Lebedev1 <Dmitry_Lebedev1@epam.com>
2018-06-21 19:20:48 +0300

    Minor fixes Parallelism mode fixes, method >> class

--- jdi-light-template/src/test/java/org/mytests/tests/example/SimpleTestsInit.java
+++ jdi-light-template/src/test/java/org/mytests/tests/example/SimpleTestsInit.java
@@ -2,9 +2,9 @@ package org.mytests.tests.example;
 
 import com.epam.jdi.light.driver.WebDriverFactory;
 import org.mytests.uiobjects.example.site.JDIExampleSite;
-import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
 
 import static com.epam.jdi.light.common.ActionProcessor.ERROR_THROWN;
 import static com.epam.jdi.light.elements.init.PageFactory.initElements;
@@ -12,18 +12,19 @@ import static com.epam.jdi.light.logger.LogLevels.STEP;
 import static com.epam.jdi.light.settings.WebSettings.logger;
 
 public class SimpleTestsInit {
-    @BeforeSuite(alwaysRun = true)
+    @BeforeClass(alwaysRun = true)
     public static void setUp() {
         logger.setLogLevel(STEP);
         initElements(JDIExampleSite.class);
         logger.info("Run Tests");
     }
+
     @BeforeMethod
     public void before() {
         ERROR_THROWN = false;
     }
 
-    @AfterSuite(alwaysRun = true)
+    @AfterClass(alwaysRun = true)
     public void teardown() {
         WebDriverFactory.close();
     }
