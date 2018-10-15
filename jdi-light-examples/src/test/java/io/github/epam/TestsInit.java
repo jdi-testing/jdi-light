152d7e57cc23cdab331e66752973e88629374849
Oksana Cherniavskaia <oksana_cherniavskaia@epam.com>
2018-10-15 18:32:44 +0300

    Work on killing Mac Os Driver.

--- jdi-light-examples/src/test/java/io/github/epam/TestsInit.java
+++ jdi-light-examples/src/test/java/io/github/epam/TestsInit.java
@@ -1,6 +1,8 @@
 package io.github.epam;
 
 import com.epam.jdi.light.driver.WebDriverFactory;
+import com.epam.jdi.light.driver.WebDriverUtils;
+import com.epam.jdi.light.settings.WebSettings;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
@@ -12,14 +14,22 @@ import static io.github.epam.EpamGithubSite.homePage;
 public class TestsInit {
     @BeforeSuite(alwaysRun = true)
     public static void setUp() {
+      /*  if (WebSettings.KILL_BROWSER.toLowerCase().contains("before")) {
+            WebDriverUtils.killAllBrowsersStartedBySelenium();
+        }*/
         logger.setLogLevel(STEP);
         initElements(EpamGithubSite.class);
         homePage.open();
         logger.toLog("Run Tests");
+
     }
 
     @AfterSuite(alwaysRun = true)
     public static void tearDown() {
+
+      /*  if (WebSettings.KILL_BROWSER.toLowerCase().contains("after")) {
+            WebDriverUtils.killAllBrowsersStartedBySelenium();
+        }*/
         WebDriverFactory.close();
     }
 }
