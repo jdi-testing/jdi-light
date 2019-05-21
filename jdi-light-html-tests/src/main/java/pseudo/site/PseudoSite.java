b73855b9a00b5521cbc210df4c0c5bae0b47a023
vladislav_murashov <vladislav_murashov@epam.com>
2019-05-21 15:24:58 +0300

    task 132 actualization

--- jdi-light-html-tests/src/main/java/pseudo/site/PseudoSite.java
+++ jdi-light-html-tests/src/main/java/pseudo/site/PseudoSite.java
@@ -3,8 +3,9 @@ package pseudo.site;
 import com.epam.jdi.light.elements.pageobjects.annotations.Title;
 import com.epam.jdi.light.elements.pageobjects.annotations.Url;
 import com.epam.jdi.light.elements.pageobjects.annotations.simple.UI;
-import pseudo.site.pages.Header;
+import pseudo.site.pages.*;
 import pseudo.site.section.*;
+
 
 public class PseudoSite {
     public static Header pseudoHeader;
@@ -14,4 +15,18 @@ public class PseudoSite {
     @UI(".extendedSectionUI")
     public static ExtendedSection extendedSectionUI;
     public static ExtendedSection extendedSection;
+
+    @Url("pagewithurl.com")
+    public static PageWithURL pageWithURL;
+
+    @Url("/pagewithurl.com")
+    public static PageWithURL slashPageWithURL;
+
+    @Title("Page with Title")
+    public static PageWithTitle pageWithTitle;
+
+    @Url("pagewithboth.com") @Title("Page with both")
+    public static PageWithBoth pageWithBoth;
+
+    public static PageWithoutBoth pageWithoutBoth;
 }
--- jdi-light-html-tests/src/main/java/pseudo/site/pages/PageWithURL.java
+++ jdi-light-html-tests/src/main/java/pseudo/site/pages/PageWithURL.java
@@ -1,7 +1,5 @@
 package pseudo.site.pages;
 
 import com.epam.jdi.light.elements.composite.WebPage;
-import com.epam.jdi.light.elements.pageobjects.annotations.Url;
 
-@Url("www.pagewithurl.com")
 public class PageWithURL extends WebPage {}
