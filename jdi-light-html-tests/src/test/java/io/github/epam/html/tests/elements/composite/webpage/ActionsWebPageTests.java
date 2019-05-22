b068142baa1c028c0dd43f982a9026e43cdd51b8
vladislav_murashov <vladislav_murashov@epam.com>
2019-05-22 12:54:09 +0300

    task 132 annotations tests are done

--- jdi-light-html-tests/src/test/java/io/github/epam/html/tests/elements/composite/webpage/ActionsWebPageTests.java
+++ jdi-light-html-tests/src/test/java/io/github/epam/html/tests/elements/composite/webpage/ActionsWebPageTests.java
@@ -1,5 +1,6 @@
 package io.github.epam.html.tests.elements.composite.webpage;
 
+import com.epam.jdi.light.elements.composite.WebPage;
 import io.github.epam.TestsInit;
 import org.testng.annotations.Test;
 import pseudo.site.dataproviders.WebPage.WebPageDataProvider;
@@ -8,7 +9,7 @@ import pseudo.site.dataproviders.WebPage.WebPageDataProvider;
 public class ActionsWebPageTests extends TestsInit {
 
     @Test(dataProvider = "actionsWebPageDataProvider", dataProviderClass = WebPageDataProvider.class)
-    public void actionsWebPageTest() {
+    public void actionsWebPageTest(WebPage webPage, String expected) {
 
     }
 }
