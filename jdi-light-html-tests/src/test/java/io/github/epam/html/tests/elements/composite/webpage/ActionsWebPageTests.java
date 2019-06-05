520f05f90dcd36033a16fae2fe752486edfecc72
vladislav_murashov <vladislav_murashov@epam.com>
2019-06-05 14:21:59 +0300

    camel case package renaming

--- jdi-light-html-tests/src/test/java/io/github/epam/html/tests/elements/composite/webpage/ActionsWebPageTests.java
+++ jdi-light-html-tests/src/test/java/io/github/epam/html/tests/elements/composite/webpage/ActionsWebPageTests.java
@@ -3,7 +3,7 @@ package io.github.epam.html.tests.elements.composite.webpage;
 import com.epam.jdi.light.elements.composite.WebPage;
 import io.github.epam.TestsInit;
 import org.testng.annotations.Test;
-import pseudo.site.dataproviders.WebPage.WebPageDataProvider;
+import pseudo.site.dataproviders.webpage.WebPageDataProvider;
 
 
 public class ActionsWebPageTests extends TestsInit {
