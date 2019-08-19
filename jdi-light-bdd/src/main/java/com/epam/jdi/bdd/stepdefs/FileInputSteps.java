a220546d4dfec57d2ca654a86ade73b5b5a5e24f
Vitalii Balitckii <Vitalii_Balitckii@epam.com>
2019-08-19 13:26:13 +0300

    updated 'Feature: File Input element functionality'

--- jdi-light-bdd/src/main/java/com/epam/jdi/bdd/stepdefs/FileInputSteps.java
+++ jdi-light-bdd/src/main/java/com/epam/jdi/bdd/stepdefs/FileInputSteps.java
@@ -1,7 +1,6 @@
 package com.epam.jdi.bdd.stepdefs;
 
 import com.epam.jdi.light.ui.html.common.FileInput;
-import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -10,23 +9,13 @@ import static com.epam.jdi.light.driver.get.DriverData.PROJECT_PATH;
 import static com.epam.jdi.light.elements.composite.WebPage.ELEMENTS;
 import static com.epam.jdi.tools.PathUtils.mergePath;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Assert;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 
 public class FileInputSteps {
-    @Then("^\"([^\"]*)\" file input element label text equals \"([^\"]*)\"$")
-    public void fileInputElementHasLabelText(String elementName, String labelText) {
-        FileInput fileInput = (FileInput) ELEMENTS.get(elementName).get(0);
-        fileInput.label().is().text(labelText);
-    }
-
-    @And("^\"([^\"]*)\" file input element label text contains \"([^\"]*)\"$")
-    public void fileInputElementLabelTextContains(String elementName, String labelText) {
-        FileInput fileInput = (FileInput) ELEMENTS.get(elementName).get(0);
-        fileInput.label().is().text(containsString(labelText));
-    }
-
     @When("^I upload file \"([^\"]*)\" by \"([^\"]*)\" file input element$")
     public void iUploadFileByFileInputElement(String pathToFile, String elementName) {
         FileInput fileInput = (FileInput) ELEMENTS.get(elementName).get(0);
@@ -44,10 +33,22 @@ public class FileInputSteps {
         }
     }
 
+    @Then("^\"([^\"]*)\" file input element label text equals \"([^\"]*)\"$")
+    public void fileInputElementHasLabelText(String elementName, String labelText) {
+        FileInput fileInput = (FileInput) ELEMENTS.get(elementName).get(0);
+        fileInput.label().is().text(labelText);
+    }
+
+    @And("^\"([^\"]*)\" file input element label text contains \"([^\"]*)\"$")
+    public void fileInputElementLabelTextContains(String elementName, String labelText) {
+        FileInput fileInput = (FileInput) ELEMENTS.get(elementName).get(0);
+        fileInput.label().is().text(containsString(labelText));
+    }
+
     @Then("^\"([^\"]*)\" file input element text equals \"([^\"]*)\"$")
     public void fileInputElementTextEquals(String elementName, String text) {
         FileInput fileInput = (FileInput) ELEMENTS.get(elementName).get(0);
-        assertEquals(fileInput.getText(), text);
+        fileInput.is().text(is(text));
     }
 
     @Then("^\"([^\"]*)\" file input element text contains \"([^\"]*)\"$")
@@ -57,15 +58,14 @@ public class FileInputSteps {
     }
 
     @And("^\"([^\"]*)\" file input element value equals \"([^\"]*)\"$")
-    public void fileInputElementValueEquals(String elementName, String value) throws Throwable {
+    public void fileInputElementValueEquals(String elementName, String value) {
         FileInput fileInput = (FileInput) ELEMENTS.get(elementName).get(0);
         assertEquals(fileInput.value(), value);
     }
 
     @And("^\"([^\"]*)\" file input element value contains \"([^\"]*)\"$")
-    public void fileInputElementValueContains(String elementName, String value) throws Throwable {
+    public void fileInputElementValueContains(String elementName, String value) {
         FileInput fileInput = (FileInput) ELEMENTS.get(elementName).get(0);
-        // contains
-//        assertEquals(fileInput.value(), value);
+        MatcherAssert.assertThat(fileInput.value(), CoreMatchers.containsString(value));
     }
 }
