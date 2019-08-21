e39bd914b6a3180ccd2d59b8fd7ccf591780ca94
Denis Ovchinnikov <Denis_Ovchinnikov@epam.com>
2019-08-21 16:53:29 +0300

    Remove is step, change description for assertThat step, rework getColorPicker func

--- jdi-light-bdd-tests/src/test/resources/features/colorPicker.feature
+++ jdi-light-bdd-tests/src/test/resources/features/colorPicker.feature
@@ -10,11 +10,6 @@ Feature: ColorPicker
     When I set "Color Picker" to "#ffd7a6" color
     Then "Color Picker" color equals to "#ffd7a6"
 
-  Scenario: Color picker is() validation test
-    Given I open "Html5 Page"
-    And "Color Picker" was set to "#ffd7a6" color
-    Then is validation for "Color Picker" is successful for color "#ffd7a6"
-
   Scenario: Color picker label assertion test
     Given I open "Html5 Page"
     Then "Color Picker" label text equals to "Select a color"
@@ -22,7 +17,7 @@ Feature: ColorPicker
   Scenario: Color picker assertThat validation test
     Given I open "Html5 Page"
     And "Color Picker" was set to "#ffd7a6" color
-    Then assertThat for "Color Picker" is successful for color "#ffd7a6"
+    Then "Color Picker" color is "#ffd7a6"
 
   Scenario: baseValidation
     Given I open "Html5 Page"
--- jdi-light-bdd/src/main/java/com/epam/jdi/bdd/stepdefs/ColorPickerSteps.java
+++ jdi-light-bdd/src/main/java/com/epam/jdi/bdd/stepdefs/ColorPickerSteps.java
@@ -5,17 +5,13 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-import static com.epam.jdi.light.common.Exceptions.exception;
-import static com.epam.jdi.light.elements.composite.WebPage.ELEMENTS;
-import static org.testng.Assert.*;
+import static com.epam.jdi.bdd.Utils.getUI;
 
 public class ColorPickerSteps {
     private ColorPicker colorPicker;
 
     private ColorPicker getColorPicker(String name) {
-        if (ELEMENTS.has(name))
-            return (ColorPicker) ELEMENTS.get(name).get(0);
-        throw exception("Can't find %s element", name);
+        return (ColorPicker) getUI(name, ColorPicker.class);
     }
 
     @Given("^\"([^\"]*)\" was set to \"([^\"]*)\" color$")
@@ -29,14 +25,7 @@ public class ColorPickerSteps {
         getColorPicker(colorPickerName).is().color(expectedColor);
     }
 
-    @Then("^is validation for \"([^\"]*)\" is successful for color \"([^\"]*)\"$")
-    public void isValidation(String colorPickerName, String color) {
-        colorPicker = getColorPicker(colorPickerName);
-        colorPicker.is().enabled();
-        colorPicker.is().color(color);
-    }
-
-    @Then("^assertThat for \"([^\"]*)\" is successful for color \"([^\"]*)\"$")
+    @Then("^\"([^\"]*)\" color is \"([^\"]*)\"$")
     public void assertThatColor(String colorPickerName, String color) {
         getColorPicker(colorPickerName).assertThat().color(color);
     }
