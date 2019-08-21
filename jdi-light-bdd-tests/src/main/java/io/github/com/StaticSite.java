8c4080fecc66d2dd76cf373c258bf1f679defca3
RomanIovlev <Roman_Iovlev@epam.com>
2019-08-21 12:03:45 +0300

    add css improve get element utils method

--- jdi-light-bdd-tests/src/main/java/io/github/com/StaticSite.java
+++ jdi-light-bdd-tests/src/main/java/io/github/com/StaticSite.java
@@ -27,10 +27,7 @@ public class StaticSite {
 	public static MetalAndColorsPage metalAndColorsPage;
 	@Url("/contacts.html") @Title("Contact Form")
 	public static ContactFormPage contactFormPage;
-	@Url(value = "/html5/%s", template = "/html5/+*", validate = CONTAINS)
-	public static HtmlElementsPage htmlElementsPage2;
 	@Url("/html5.html") public static HtmlElementsPage html5Page;
-	@Url("/suspend-html-page.html") public static HtmlElementsPage suspendHtml5Page;
 	@Url("/dates.html") @Title("Dates")
 	public static DatesPage datesPage;
 	@Url("/user-table.html") @Title("User Table")
