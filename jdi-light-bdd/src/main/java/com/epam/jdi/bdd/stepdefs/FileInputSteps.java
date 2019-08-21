6ebd7e55dec19d83a358d510982c062c617d6162
Vitalii Balitckii <Vitalii_Balitckii@epam.com>
2019-08-21 16:52:34 +0300

    deleted unnecessary imports

--- jdi-light-bdd/src/main/java/com/epam/jdi/bdd/stepdefs/FileInputSteps.java
+++ jdi-light-bdd/src/main/java/com/epam/jdi/bdd/stepdefs/FileInputSteps.java
@@ -3,14 +3,11 @@ package com.epam.jdi.bdd.stepdefs;
 import com.epam.jdi.bdd.Utils;
 import com.epam.jdi.light.ui.html.common.FileInput;
 import cucumber.api.java.en.And;
-import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 import static com.epam.jdi.light.driver.get.DriverData.PROJECT_PATH;
 import static com.epam.jdi.light.elements.composite.WebPage.ELEMENTS;
 import static com.epam.jdi.tools.PathUtils.mergePath;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
 
 import org.hamcrest.CoreMatchers;
