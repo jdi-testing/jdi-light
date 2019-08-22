7bea7339a67dc1de99ee67a24e94636b0d486bea
Yulia_Litvin <iulia.litwin@yandex.ru>
2019-08-22 15:50:25 +0300

    remove commented lines

--- jdi-light-bdd/src/main/java/com/epam/jdi/bdd/stepdefs/DateTimeSelectorSteps.java
+++ jdi-light-bdd/src/main/java/com/epam/jdi/bdd/stepdefs/DateTimeSelectorSteps.java
@@ -12,21 +12,18 @@ public class DateTimeSelectorSteps {
     @Then("^\"([^\"]*)\" min equals to \"([^\"]*)\"$")
     public void attributeMinEquals(String name, String date) {
         DateTimeSelector dateTimeSelector = (DateTimeSelector)getUI(name);
-      //  DateTimeSelector dateTimeSelector = (DateTimeSelector) ELEMENTS.get(element_name).get(0);
         assertEquals(dateTimeSelector.min(), date);
     }
 
     @Then("^\"([^\"]*)\" max equals to \"([^\"]*)\"$")
     public void attributeMaxEquals(String name, String date) {
         DateTimeSelector dateTimeSelector = (DateTimeSelector)getUI(name);
-       // DateTimeSelector dateTimeSelector = (DateTimeSelector) ELEMENTS.get(element_name).get(0);
         assertEquals(dateTimeSelector.max(), date);
     }
 
     @When("^(?:I |)Set date \"([^\"]*)\" in \"([^\"]*)\"$")
     public void iSetDate(String date, String name) {
         DateTimeSelector dateTimeSelector = (DateTimeSelector)getUI(name);
-       // DateTimeSelector dateTimeSelector = (DateTimeSelector) ELEMENTS.get(element_name).get(0);
         dateTimeSelector.setDateTime(date);
     }
 
