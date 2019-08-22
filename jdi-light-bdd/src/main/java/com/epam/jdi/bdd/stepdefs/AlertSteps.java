5591afbe151f939c7115fa03bba902cc4bb22a25
ChepelVE <Viktor_Chepel@epam.com>
2019-08-22 11:39:07 +0300

    Alert steps and feature

--- jdi-light-bdd/src/main/java/com/epam/jdi/bdd/stepdefs/AlertSteps.java
+++ jdi-light-bdd/src/main/java/com/epam/jdi/bdd/stepdefs/AlertSteps.java
@@ -1,15 +1,27 @@
 package com.epam.jdi.bdd.stepdefs;
 
+import static com.epam.jdi.light.elements.base.Alerts.acceptAlert;
+import static com.epam.jdi.light.elements.base.Alerts.dismissAlert;
+import static com.epam.jdi.light.elements.base.Alerts.validateAlert;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.matchesPattern;
+
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-import static com.epam.jdi.light.elements.base.Alerts.getAlertText;
-import static com.epam.jdi.light.elements.base.Alerts.validateAlert;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 public class AlertSteps {
+	
+	@When("^(?:I |)Accept alert")
+	public void accept() {
+		acceptAlert();
+	}
+	
+	@When("^(?:I |)Dismiss alert")
+	public void dismiss() {
+		dismissAlert();
+	}
+	
 	@Then("^Alert text equals to \"([^\"]*)\"$")
     public void alertTextEquals(String alertText) {
 		validateAlert(is(alertText));
@@ -17,11 +29,11 @@ public class AlertSteps {
 	
 	@Then("^Alert text contains \"([^\"]*)\"$")
     public void alertTextContains(String alertText) {
-    	assertTrue(getAlertText().contains(alertText));
+    	validateAlert(containsString(alertText));
     }
 	
-	@When("^Accept alert")
-	public void acceptAlert() {
-		acceptAlert();
-	}
+	@Then("^Alert text match to \"([^\"]*)\"$")
+    public void alertTextMatches(String alertText) {
+    	validateAlert(matchesPattern(alertText));
+    }
 }
