93d98c8b66cf53cc37a221f09bd96769a0877e49
Yulia_Litvin <iulia.litwin@yandex.ru>
2019-08-29 18:27:50 +0300

    use toIntArray in DropDownSteps multidropdown ended

--- jdi-light-bdd/src/main/java/com/epam/jdi/bdd/stepdefs/MultiDropDownSteps.java
+++ jdi-light-bdd/src/main/java/com/epam/jdi/bdd/stepdefs/MultiDropDownSteps.java
@@ -7,6 +7,7 @@ import cucumber.api.java.en.When;
 import java.util.List;
 
 import static com.epam.jdi.bdd.Utils.getUI;
+import static com.epam.jdi.tools.LinqUtils.toIntArray;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.core.IsIterableContaining.hasItem;
@@ -30,10 +31,8 @@ public class MultiDropDownSteps {
     @When("^(?:I |)check in the \"([^\"]*)\" values by number:$")
     public void iCheckInTheValuesByNumber(String name, List<Integer> values) {
         MultiSelector multiDropdown = (MultiSelector) getUI(name);
-        int[] numbers = values.stream().mapToInt(Integer::intValue).toArray();
-        multiDropdown.check(numbers);
+        multiDropdown.check(toIntArray(values));
     }
-
 
     @When("^(?:I |)check value \"([^\"]*)\" in the \"([^\"]*)\"$")
     public void iCheckValueInThe(String value, String name) {
@@ -58,6 +57,7 @@ public class MultiDropDownSteps {
         MultiSelector multiDropdown = getUI(name, MultiSelector.class);
         multiDropdown.is().values(hasItem(value));
     }
+
     @Then("^the \"([^\"]*)\" has disabled item \"([^\"]*)\"$")
     public void theHasDisabledItem(String name, String value) {
         MultiSelector multiDropdown = getUI(name, MultiSelector.class);
