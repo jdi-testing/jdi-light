27671142a8f94d72e7a9eda635de3e7e905aa337
Alexandra Kozhevnikova <ms.asck@yandex.ru>
2019-08-29 17:46:37 +0300

    - add cucumber radio test - add method getText in HTMLRADIOGROUP

---
+++ jdi-light-bdd-tests/src/test/resources/features/Radio.feature
@@ -0,0 +1,33 @@
+@radio
+
+Feature: Radio
+
+  Scenario: Select
+    Given I open "Html5 Page" page
+    And I Select "Blue" field from "Html5 Page.Colors"
+    Then the "Html5 Page.Colors" text equals to "Blue"
+    When I select the radio button with "1" index from "Html5 Page.Colors"
+    Then the "Html5 Page.Colors" text equals to "Red"
+
+  Scenario: Consist of
+    Given I open "Html5 Page" page
+    Then the "Html5 Page.Colors" consists of next values:
+      | Red | Green | Blue | Yellow |
+
+  Scenario: Validate
+    Given I open "Html5 Page" page
+    And I Select "Blue" field from "Html5 Page.Colors"
+    Then the "Html5 Page.Colors" text equals to "Blue"
+    And the "Html5 Page.Colors" contains "Yellow" radio button
+    And the "Html5 Page.Colors" contains "Yellow" disabled radio button
+    And the "Html5 Page.Colors" does not contain "Yellow" enabled radio button
+    And the "Html5 Page.Colors" contains next enabled values:
+      | Green | Blue |
+
+
+
+
+
+
+
+
---
+++ jdi-light-bdd/src/main/java/com/epam/jdi/bdd/stepdefs/RadioSteps.java
@@ -0,0 +1,59 @@
+package com.epam.jdi.bdd.stepdefs;
+
+import com.epam.jdi.light.ui.html.complex.RadioButtons;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.core.IsIterableContaining.hasItem;
+import static org.testng.Assert.assertEquals;
+import static com.epam.jdi.bdd.Utils.getUI;
+
+public class RadioSteps {
+
+    @When("^(?:I |)select the radio button with \"([^\"]*)\" index from \"([^\"]*)\"$")
+    public void selectRadioIndex(int index, String name) {
+        RadioButtons el = getUI(name, RadioButtons.class);
+        el.select(index);
+    }
+
+    //#region Then
+    @Then("^the \"([^\"]*)\" consists of next values:$")
+    public void theConsistOfNextValues(String name, List<String> values) {
+        RadioButtons el = getUI(name, RadioButtons.class);
+        assertEquals(el.values(), values);
+    }
+
+
+    @Then("^the \"([^\"]*)\" contains \"([^\"]*)\" radio button$")
+    public void theContainsRadioButton(String name, String value) {
+        RadioButtons el = getUI(name, RadioButtons.class);
+        el.is().values(hasItem(value));
+    }
+
+
+    @Then("^the \"([^\"]*)\" contains \"([^\"]*)\" disabled radio button$")
+    public void theContainsDisabledRadioButton(String name, String value) {
+        RadioButtons el = getUI(name, RadioButtons.class);
+        el.is().disabled(hasItem(value));
+    }
+
+    @Then("^the \"([^\"]*)\" does not contain \"([^\"]*)\" enabled radio button$")
+    public void theDoesNotContainEnabledRadioButton(String name, String value) {
+        RadioButtons el = getUI(name, RadioButtons.class);
+        el.is().enabled(not(hasItem(value)));
+    }
+
+
+    @Then("^the \"([^\"]*)\" contains next enabled values:$")
+    public void theContainsNextValues(String name, List<String> values) {
+        RadioButtons el = getUI(name, RadioButtons.class);
+        for (String string : values) {
+            el.is().enabled(hasItem(string));
+        }
+    }
+    //#endregion
+}
+
+
