9ebe384bc8c8e0b5c0d0ee8ac6f0fc41e3ac2d7e
ChepelVE <Viktor_Chepel@epam.com>
2019-08-29 11:32:35 +0300

    add getForm

--- jdi-light-bdd/src/main/java/com/epam/jdi/bdd/Utils.java
+++ jdi-light-bdd/src/main/java/com/epam/jdi/bdd/Utils.java
@@ -1,18 +1,19 @@
 package com.epam.jdi.bdd;
 
-import com.epam.jdi.light.elements.base.BaseUIElement;
-import com.epam.jdi.light.elements.base.JDIBase;
-import com.epam.jdi.light.elements.complex.JList;
-import com.epam.jdi.light.elements.composite.WebPage;
-import com.epam.jdi.tools.LinqUtils;
-
-import java.util.List;
-
 import static com.epam.jdi.light.common.Exceptions.exception;
 import static com.epam.jdi.light.elements.composite.WebPage.ELEMENTS;
 import static com.epam.jdi.light.elements.composite.WebPage.getCurrentPage;
 import static com.epam.jdi.tools.LinqUtils.first;
 import static com.epam.jdi.tools.ReflectionUtils.isClass;
+
+import java.util.List;
+
+import com.epam.jdi.light.elements.base.BaseUIElement;
+import com.epam.jdi.light.elements.base.JDIBase;
+import com.epam.jdi.light.elements.complex.JList;
+import com.epam.jdi.light.elements.composite.Form;
+import com.epam.jdi.light.elements.composite.WebPage;
+import com.epam.jdi.tools.LinqUtils;
 
 /**
  * Created by Dmitry_Lebedev1 on 1/13/2016.
@@ -71,6 +72,14 @@ public final class Utils {
                     });
             if (element != null)
                 return element;
+        }
+        throw exception("Can't find %s element", name);
+    }
+    
+    public static Form getForm(String name) {
+        if (ELEMENTS.has(name)) {
+            List<Object> elements = ELEMENTS.get(name);
+            return (Form) elements.get(0);
         }
         throw exception("Can't find %s element", name);
     }
