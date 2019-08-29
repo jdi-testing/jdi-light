7331818ac925a4086ede20699675bb903d31ead5
Igor_Gaiduk <Igor_Gaiduk@epam.com>
2019-08-29 16:41:30 +0300

    Completed table element functionality

--- jdi-light-bdd-tests/src/test/resources/features/Table.feature
+++ jdi-light-bdd-tests/src/test/resources/features/Table.feature
@@ -1,5 +1,6 @@
-Feature: Checkbox
 @table
+Feature: Table
+
   Scenario: Get label text test
     Given I open "Users Page"
     Then the "Users Table" table columns count equals "4"
@@ -9,3 +10,45 @@ Feature: Checkbox
       | Type        |
       | User        |
       | Description |
+
+  Scenario: Preview test
+    Given I open "Users Page"
+    Then the "Users Table" table preview equals values:
+      | NumberTypeUserDescription                                     |
+      | 1AdminUserManagerRomanWolverineVip                            |
+      | 2AdminUserManagerSergeyIvanSpiderManVip                       |
+      | 3AdminUserManagerVladzimirPunisherVip                         |
+      | 4AdminUserManagerHelenBennettCaptainAmericasomedescriptionVip |
+      | 5AdminUserManagerYoshiTannamuriCyclopesomedescriptionVip      |
+      | 6AdminUserManagerGiovanniRovelliHulksomedescriptionVip        |
+
+  Scenario: Common matchers test
+    Given I open "Users Page"
+    Then the "Users Table" table has size "6"
+    And the "Users Table" table has size greater than "3"
+    And the "Users Table" table has size less or equal to "6"
+    And the "Users Table" table is not empty
+
+  Scenario: Row matcher test
+    Given I open "Users Page"
+    Then the "Users Table" table has row that contains value "Ivan" in column "User"
+
+  Scenario: Rows all test
+    Given I open "Users Page"
+    Then the "Users Table" table all rows contain value "Vip" in column "Description"
+
+  Scenario: No rows test
+    Given I open "Users Page"
+    Then the "Users Table" table has no rows which contain value "Vip" in column "Description"
+
+  Scenario: At least test
+    Given I open "Users Page"
+    Then the "Users Table" table has at least "3" rows which contain value " " in column "User"
+
+  Scenario: Exact matcher test
+    Given I open "Users Page"
+    Then the "Users Table" table has exact "2" rows which contain value "R" in column "User"
+
+  Scenario: Raw data exact matcher test
+    Given I open "Users Page"
+    Then the "Users Table" table has exact "1" rows which have value "Roman" in column "User"
--- jdi-light-bdd/src/main/java/com/epam/jdi/bdd/stepdefs/TableSteps.java
+++ jdi-light-bdd/src/main/java/com/epam/jdi/bdd/stepdefs/TableSteps.java
@@ -2,10 +2,16 @@ package com.epam.jdi.bdd.stepdefs;
 
 import com.epam.jdi.light.elements.complex.table.Table;
 import cucumber.api.java.en.Then;
+import org.testng.Assert;
 
 import java.util.List;
 
 import static com.epam.jdi.bdd.Utils.getUIComplex;
+import static com.epam.jdi.light.elements.complex.table.Column.inColumn;
+import static com.epam.jdi.light.elements.complex.table.TableMatcher.containsValue;
+import static com.epam.jdi.light.elements.complex.table.TableMatcher.hasValue;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -13,19 +19,86 @@ public class TableSteps {
     @Then("^the \"([^\"]*)\" table columns count equals \"([^\"]*)\"$")
     public void assertColumnsCount(String elementName, int columns) {
         Table table = getUIComplex(elementName, Table.class);
-        assertEquals(columns, table.size());
+        assertEquals(table.size(), columns);
     }
 
     @Then("^the \"([^\"]*)\" table rows count equals \"([^\"]*)\"$")
     public void assertRowsCount(String elementName, int rows) {
         Table table = getUIComplex(elementName, Table.class);
-        assertEquals(rows, table.rows().size());
+        assertEquals(table.rows().size(), rows);
     }
 
     @Then("^the \"([^\"]*)\" table header has items:$")
     public void assertHasItems(String elementName, List<String> values) {
         Table table = getUIComplex(elementName, Table.class);
-        System.out.println(table.header());
         assertTrue(table.header().containsAll(values));
+    }
+
+    @Then("^the \"([^\"]*)\" table preview equals values:$")
+    public void assertEqualsValues(String elementName, List<String> values) {
+        Table table = getUIComplex(elementName, Table.class);
+        String tableValues = String.join(", ", table.preview().replaceAll(" ", ""));
+        String expectedValue = String.join(", ", values).replaceAll(", ", "");
+        Assert.assertEquals(tableValues, expectedValue);
+    }
+
+    @Then("^the \"([^\"]*)\" table is not empty$")
+    public void assertNotEmpty(String elementName) {
+        Table table = getUIComplex(elementName, Table.class);
+        table.isNotEmpty();
+    }
+
+    @Then("^the \"([^\"]*)\" table has size \"([^\"]*)\"$")
+    public void assertSize(String elementName, int size) {
+        Table table = getUIComplex(elementName, Table.class);
+        table.has().size(size);
+    }
+
+    @Then("^the \"([^\"]*)\" table has size greater than \"([^\"]*)\"$")
+    public void assertSizeGreaterThan(String elementName, int size) {
+        Table table = getUIComplex(elementName, Table.class);
+        table.assertThat().size(greaterThan(size));
+    }
+
+    @Then("^the \"([^\"]*)\" table has size less or equal to \"([^\"]*)\"$")
+    public void assertSizeLessOrEqualTo(String elementName, int size) {
+        Table table = getUIComplex(elementName, Table.class);
+        table.assertThat().size(lessThanOrEqualTo(size));
+    }
+
+    @Then("^the \"([^\"]*)\" table has row that contains value \"([^\"]*)\" in column \"([^\"]*)\"$")
+    public void assertHasRowContainValue(String elementName, String value, String column) {
+        Table table = getUIComplex(elementName, Table.class);
+        table.has().rowThat(containsValue(value, inColumn(column)));
+    }
+
+    @Then("^the \"([^\"]*)\" table all rows contain value \"([^\"]*)\" in column \"([^\"]*)\"$")
+    public void assertAllRowsContainValue(String elementName, String value, String column) {
+        Table table = getUIComplex(elementName, Table.class);
+        table.assertThat().all().rows(containsValue(value, inColumn(column)));
+    }
+
+    @Then("^the \"([^\"]*)\" table has no rows which contain value \"([^\"]*)\" in column \"([^\"]*)\"$")
+    public void assertNoRowsContainValue(String elementName, String value, String column) {
+        Table table = getUIComplex(elementName, Table.class);
+        table.assertThat().no().rows(containsValue(value, inColumn(column)));
+    }
+
+    @Then("^the \"([^\"]*)\" table has at least \"([^\"]*)\" rows which contain value \"([^\"]*)\" in column \"([^\"]*)\"$")
+    public void assertAtLeastRowsContainValue(String elementName, int rows, String value, String column) {
+        Table table = getUIComplex(elementName, Table.class);
+        table.assertThat().atLeast(rows).rows(containsValue(value, inColumn(column)));
+    }
+
+    @Then("^the \"([^\"]*)\" table has exact \"([^\"]*)\" rows which contain value \"([^\"]*)\" in column \"([^\"]*)\"$")
+    public void assertExactRowsContainValue(String elementName, int rows, String value, String column) {
+        Table table = getUIComplex(elementName, Table.class);
+        table.assertThat().exact(rows).rows(containsValue(value, inColumn(column)));
+    }
+
+    @Then("^the \"([^\"]*)\" table has exact \"([^\"]*)\" rows which have value \"([^\"]*)\" in column \"([^\"]*)\"$")
+    public void assertExactRowsHaveValue(String elementName, int rows, String value, String column) {
+        Table table = getUIComplex(elementName, Table.class);
+        table.assertThat().exact(rows).rows(hasValue(value, inColumn(column)));
     }
 }
