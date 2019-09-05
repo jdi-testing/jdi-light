14e0b07445e481e2ed91ab814ba247515c65e5e7
Roman_Iovlev <romanyister@gmail.com>
2019-09-05 12:10:14 +0300

    fix steps

--- jdi-light-bdd/src/main/java/com/epam/jdi/bdd/JDILocatorSteps.java
+++ jdi-light-bdd/src/main/java/com/epam/jdi/bdd/JDILocatorSteps.java
@@ -1,30 +1,14 @@
 package com.epam.jdi.bdd;
 
-import com.epam.jdi.light.elements.base.BaseUIElement;
-import com.epam.jdi.light.elements.base.JDIBase;
 import com.epam.jdi.light.elements.composite.WebPage;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.openqa.selenium.WebElement;
 
 import static com.epam.jdi.light.ui.html.HtmlFactory.$;
 import static org.hamcrest.Matchers.matchesPattern;
 
 public class JDILocatorSteps {
-    private static boolean sLoggedIn = false;
-
-    @Given("^(?:I |)logged in with name \"([^\"]*)\" and password \"([^\"]*)\"$")
-    public void iLoggedIn(String name, String password) {
-        if (!sLoggedIn) {
-            WebPage.openUrl("https://jdi-testing.github.io/jdi-light/");
-            $("img#user-icon").click();
-            $("form #name").sendKeys(name);
-            $("form #password").sendKeys(password);
-            $("form [type=submit]").click();
-            sLoggedIn = true;
-        }
-    }
 
     @Given("^(?:I |)open page by url \"([^\"]*)\"$")
     public void openPageByUrl(String url) {
@@ -33,127 +17,106 @@ public class JDILocatorSteps {
 
     @When("^(?:I |)Click on \"([^\"]*)\" element$")
     public void click(String locator) {
-        WebElement el = $(locator);
-        el.click();
+        $(locator).click();
     }
 
     @When("^(?:I |)Click with JS on \"([^\"]*)\" element$")
     public void jsClick(String locator) {
-        BaseUIElement el = $(locator);
-        el.jsClick();
+        $(locator).jsClick();
     }
 
     @Then("^the \"([^\"]*)\" element text equals to \"([^\"]*)\"$")
     public void textEquals(String locator, String value) {
-        JDIBase el = $(locator);
-        el.is().text(value);
+        $(locator).is().text(value);
     }
 
     @When("^(?:I |)send keys \"([^\"]*)\" to \"([^\"]*)\" element$")
     public void sendKeys(String value, String locator) {
-        BaseUIElement el = $(locator);
-        el.sendKeys(value);
+        $(locator).sendKeys(value);
     }
 
     @Then("^the \"([^\"]*)\" element text matches to \"([^\"]*)\"$")
     public void textMatchRegex(String locator, String regex) {
-        JDIBase el = $(locator);
-        el.is().text(matchesPattern(regex));
+        $(locator).is().text(matchesPattern(regex));
     }
 
     @When("^(?:I |)check \"([^\"]*)\" element$")
     public void check(String locator) {
-        BaseUIElement el = $(locator);
-        el.check();
+        $(locator).check();
     }
 
     @When("^(?:I |)uncheck \"([^\"]*)\" element$")
     public void uncheck(String locator) {
-        BaseUIElement el = $(locator);
-        el.uncheck();
+        $(locator).uncheck();
     }
 
     @Then("^the \"([^\"]*)\" element is selected$")
     public void isSelected(String locator) {
-        JDIBase el = $(locator);
-        el.is().selected();
+        $(locator).is().selected();
     }
 
     @Then("^the \"([^\"]*)\" element is deselected$")
     public void isDeselected(String locator) {
-        JDIBase el = $(locator);
-        el.is().deselected();
+        $(locator).is().deselected();
     }
 
     @When("^(?:I |)clear \"([^\"]*)\" element$")
     public void clear(String locator) {
-        BaseUIElement el = $(locator);
-        el.clear();
+        $(locator).clear();
     }
 
     @Then("^the \"([^\"]*)\" element css \"([^\"]*)\" equals to \"([^\"]*)\"$")
     public void cssEquals(String locator, String attrName, String cssValue) {
-        JDIBase el = $(locator);
-        el.is().css(attrName, cssValue);
+        $(locator).is().css(attrName, cssValue);
     }
 
     @When("^(?:I |)input \"([^\"]*)\" in \"([^\"]*)\" element$")
     public void input(String value, String locator) {
-        BaseUIElement el = $(locator);
-        el.input(value);
+        $(locator).input(value);
     }
 
     @Then("^the \"([^\"]*)\" element placeholder equals to \"([^\"]*)\"$")
     public void placeholderEquals(String locator, String placeholder) {
-        JDIBase el = $(locator);
-        el.is().attr("placeholder", placeholder);
+        $(locator).is().attr("placeholder", placeholder);
     }
 
     @Then("^the \"([^\"]*)\" element attribute \"([^\"]*)\" equals to \"([^\"]*)\"$")
     public void attributeEquals(String locator, String attrName, String attrValue) {
-        JDIBase el = $(locator);
-        el.is().attr(attrName, attrValue);
+        $(locator).is().attr(attrName, attrValue);
     }
 
     @Then("^the \"([^\"]*)\" element is enabled$")
     public void isEnabled(String locator) {
-        JDIBase el = $(locator);
-        el.is().enabled();
+        $(locator).is().enabled();
     }
 
     @Then("^the \"([^\"]*)\" element is disabled$")
     public void isDisabled(String locator) {
-        JDIBase el = $(locator);
-        el.is().disabled();
+        $(locator).is().disabled();
     }
 
     @Then("^the \"([^\"]*)\" element is hidden$")
     public void isHidden(String locator) {
-        JDIBase el = $(locator);
-        el.is().hidden();
+        $(locator).is().hidden();
     }
 
     @Then("^the \"([^\"]*)\" element does not appear$")
     public void isNotAppear(String locator) {
-        JDIBase el = $(locator);
-        el.is().notAppear();
+        $(locator).is().notAppear();
     }
 
     @Then("^the \"([^\"]*)\" element is displayed$")
     public void isDisplayed(String locator) {
-        JDIBase el = $(locator);
-        el.is().displayed();
+        $(locator).is().displayed();
     }
 
     @When("^(?:I |)set text \"([^\"]*)\" in \"([^\"]*)\" element$")
     public void setText(String value, String locator) {
-        JDIBase el = $(locator);
-        el.setText(value);
+        $(locator).setText(value);
     }
 
     @When("^(?:I |)select \"([^\"]*)\" field from \"([^\"]*)\" element$")
     public void select(String value, String locator) {
-        BaseUIElement el = $(locator);
-        el.select(value);
+        $(locator).select(value);
     }
 }
