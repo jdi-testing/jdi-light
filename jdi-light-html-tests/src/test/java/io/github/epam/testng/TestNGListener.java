b2ffb8f37d06786ba5eaf9ff93fceb5619250939
Katenka Vasilkova <katenka.vasilkova@gmail.com>
2019-09-12 23:27:28 +0300

    task 550 - implement screenshoting alerts on test failing

--- jdi-light-html-tests/src/test/java/io/github/epam/testng/TestNGListener.java
+++ jdi-light-html-tests/src/test/java/io/github/epam/testng/TestNGListener.java
@@ -5,7 +5,10 @@ package io.github.epam.testng;
  * Email: roman.iovlev.jdi@gmail.com; Skype: roman.iovlev
  */
 
+import com.epam.jdi.light.driver.ScreenshotMaker;
 import com.epam.jdi.tools.Safe;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
 import org.testng.IInvokedMethod;
 import org.testng.IInvokedMethodListener;
 import org.testng.ITestResult;
@@ -16,13 +19,22 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import static com.epam.jdi.light.actions.ActionProcessor.provideExtraInfoOnFail;
+import static com.epam.jdi.light.driver.WebDriverFactory.getDriver;
 import static com.epam.jdi.light.settings.WebSettings.TEST_NAME;
 import static com.epam.jdi.light.settings.WebSettings.logger;
+import static java.lang.String.format;
 import static java.lang.System.currentTimeMillis;
 
 public class TestNGListener implements IInvokedMethodListener {
 
     private Safe<Long> start = new Safe<>(0L);
+
+    private Safe<WebDriver.Window> window = new Safe<>(getDriver().manage().window());
+
+    private Safe<Integer> x = new Safe<>(window.get().getPosition().x);
+    private Safe<Integer> y = new Safe<>(window.get().getPosition().y);
+    private Safe<Integer> width = new Safe<>(window.get().getSize().width);
+    private Safe<Integer> height = new Safe<>(window.get().getSize().height);
 
     @Override
     public void beforeInvocation(IInvokedMethod method, ITestResult iTestResult) {
@@ -45,8 +57,19 @@ public class TestNGListener implements IInvokedMethodListener {
             if (result.equals("FAILED")) {
                 String methodName = method.getTestMethod().toString();
                 String shortMethodName = methodName.substring(0, methodName.indexOf("("));
-                provideExtraInfoOnFail(shortMethodName + "(..)");
-                logger.error("ERROR: " + iTestResult.getThrowable().getMessage());
+
+                try {
+                    getDriver().switchTo().alert();
+                    new ScreenshotMaker().takeScreenshotByRobot(
+                            x.get(), y.get(), width.get(), height.get(),
+                            format("Method_%s_failed_at_", shortMethodName + "(..)"), "dd-MM-yyyy_HH-mm-ss");
+                    getDriver().wait(1000000000);
+                } catch (NoAlertPresentException ex) {
+                    provideExtraInfoOnFail(shortMethodName + "(..)");
+                } catch (Exception ignore) {
+                } finally {
+                    logger.error("ERROR: " + iTestResult.getThrowable().getMessage());
+                }
             }
         }
     }
