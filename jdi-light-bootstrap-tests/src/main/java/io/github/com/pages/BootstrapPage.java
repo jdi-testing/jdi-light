5e4ab858c06563f339d8018fa577efccc71276ae
Yulia_Litvin <iulia.litwin@yandex.ru>
2019-09-13 17:21:20 +0300

    input group section input + text

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/pages/BootstrapPage.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/pages/BootstrapPage.java
@@ -1,12 +1,15 @@
 package io.github.com.pages;
 
 import com.epam.jdi.light.elements.composite.WebPage;
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.Css;
 import com.epam.jdi.light.ui.bootstrap.elements.common.Button;
+import io.github.com.sections.InputGroupInputWithText;
+
 
 public class BootstrapPage extends WebPage {
     //region Simple Elements
     public static Button redButton, doubleButton;
-
+    @Css("#input-group-username") public static InputGroupInputWithText inputGroupUsername;
     //endregion
 
     //region Complex Elements
