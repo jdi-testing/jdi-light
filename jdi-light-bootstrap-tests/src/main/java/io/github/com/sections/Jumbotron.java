bd60ed230c817f3b4a4bd284a202e913b4deaaf3
Igor_Gaiduk <Igor_Gaiduk@epam.com>
2019-09-17 17:06:04 +0300

    Added link to jumbotron element to bootstrap documentation

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/Jumbotron.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/Jumbotron.java
@@ -6,6 +6,8 @@ import com.epam.jdi.light.elements.pageobjects.annotations.locators.Css;
 import com.epam.jdi.light.ui.bootstrap.elements.common.Button;
 import com.epam.jdi.light.ui.bootstrap.elements.common.Text;
 
+// Implements Jumbotron
+// https://getbootstrap.com/docs/4.0/components/jumbotron/
 public class Jumbotron extends Section implements IsJumbotron {
     @Css(".display-4") public Text title;
     @Css(".lead") public Text description;
