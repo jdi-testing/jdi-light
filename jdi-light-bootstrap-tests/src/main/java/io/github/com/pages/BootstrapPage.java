82b03382597b4032cbcbb3eb68513e158aca088e
Amelina, Natalia <Natalia_Amelina@epam.com>
2019-09-17 11:34:06 +0300

    [517]: Buttons group - change to update locators

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/pages/BootstrapPage.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/pages/BootstrapPage.java
@@ -1,11 +1,20 @@
 package io.github.com.pages;
 
 import com.epam.jdi.light.elements.composite.WebPage;
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.Css;
 import com.epam.jdi.light.ui.bootstrap.elements.common.Button;
 
 public class BootstrapPage extends WebPage {
     //region Simple Elements
     public static Button redButton, doubleButton;
+
+    @Css(".btn-group .btn-danger")
+    public static Button redButtonGroup;
+
+    @Css(".btn-group .btn-success:not(.dropdown-toggle)")
+    public static Button greenButtonGroup;
+
+    //public static Dropdown dropdownButtonGroup;
 
     //endregion
 
