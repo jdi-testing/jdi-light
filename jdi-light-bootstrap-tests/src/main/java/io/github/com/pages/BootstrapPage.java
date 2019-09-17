f22ad1bcd09c6a219d5b572e338b119e03026637
Dmitrii_Pavlov <delnote@gmail.com>
2019-09-17 13:53:51 +0300

    Alert Button added to Alert.

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/pages/BootstrapPage.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/pages/BootstrapPage.java
@@ -4,17 +4,21 @@ import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.Css;
 import com.epam.jdi.light.ui.bootstrap.elements.common.Alert;
 import com.epam.jdi.light.ui.bootstrap.elements.common.Button;
+import com.epam.jdi.light.ui.bootstrap.elements.complex.ButtonWithDropdown;
 
 public class BootstrapPage extends WebPage {
     //region Simple Elements
     public static Button redButton, doubleButton;
     @Css("#simple-alert") public static Alert simpleAlert;
     @Css("#dismissible-alert") public static Alert dismissibleAlert;
-    @Css("#dismissible-alert-close-button") public static Button alertButton;
-
+    @Css("#dismissible-alert-close-button") public static Alert alertButton;
     //endregion
 
     //region Complex Elements
+
+    @Css("#button-with-dropdown") public static ButtonWithDropdown dropdownMenu;
+    @Css("#button-with-dropdown input") public static ButtonWithDropdown textInputArea;
+
 
     //endregion
 
