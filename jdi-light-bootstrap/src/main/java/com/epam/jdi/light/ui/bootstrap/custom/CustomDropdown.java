9b6d0609c4f7dbac9b2a6e59c2e216840e4c0c39
dexguitar <stinzane@gmail.com>
2019-09-17 12:48:35 +0300

    Inheriting Collapse from DropdownExpand now

--- jdi-light-bootstrap/src/main/java/com/epam/jdi/light/ui/bootstrap/custom/CustomDropdown.java
+++
@@ -1,9 +0,0 @@
-package com.epam.jdi.light.ui.bootstrap.custom;
-
-import com.epam.jdi.light.elements.complex.dropdown.DropdownExpand;
-
-public class CustomDropdown extends DropdownExpand {
-    public CustomDropdown() {
-        expandLocator = "*[data-toggle=collapse]";
-    }
-}
--- jdi-light-bootstrap/src/main/java/com/epam/jdi/light/ui/bootstrap/elements/complex/Collapse.java
+++ jdi-light-bootstrap/src/main/java/com/epam/jdi/light/ui/bootstrap/elements/complex/Collapse.java
@@ -1,8 +1,6 @@
 package com.epam.jdi.light.ui.bootstrap.elements.complex;
 
-import com.epam.jdi.light.ui.bootstrap.custom.CustomDropdown;
-
-public class Collapse extends CustomDropdown {
+public class Collapse extends DropdownExpand {
 
 
 }
