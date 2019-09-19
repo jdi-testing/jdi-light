9d56bfe5f9f9ad2f5c59fd3d2b4e4b57513a1a08
OlgaIvanova777 <olga_ivanova@epam.com>
2019-09-19 17:45:56 +0300

    Merge branch 'bootstrap' into task_515_Implement-boostrap-element-Breadcrumb
    # Conflicts:
    #	jdi-light-bootstrap-tests/src/main/java/io/github/com/pages/BootstrapPage.java

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/pages/BootstrapPage.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/pages/BootstrapPage.java
@@ -2,35 +2,25 @@ package io.github.com.pages;
 
 import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.light.elements.composite.WebPage;
-import com.epam.jdi.light.elements.pageobjects.annotations.locators.Css;
-import com.epam.jdi.light.ui.bootstrap.elements.common.Breadcrumb;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.ui.bootstrap.elements.common.Alert;
+import com.epam.jdi.light.ui.bootstrap.elements.common.Breadcrumb;
 import com.epam.jdi.light.ui.bootstrap.elements.common.Button;
 import io.github.com.sections.*;
-
 
 public class BootstrapPage extends WebPage {
     //region Simple Elements
     public static UIElement inputGroupWrap,inputGroupNowrap;
     public static Button redButton, doubleButton, disabledButton;
-
-    @Css("#jumbotron-fluid") public static Jumbotron jumbotronFluid;
-    @Css("#jumbotron") public static Jumbotron jumbotron;
-
-    @Css("#breadcrumb") public static Breadcrumb breadcrumb;
-
-    @Css("#input-group-username") public static InputGroupInputWithText inputGroupUsername;
-    @Css("#input-group-checkbox") public static InputGroupInputWithCheckBox inputGroupCheckBox;
-    @Css("#input-group-radio") public static InputGroupInputWithRadio inputGroupRadio;
     @UI("#simple-alert") public static Alert simpleAlert;
     @UI("#dismissible-alert") public static Alert dismissibleAlert;
     @UI("#jumbotron-fluid") public static Jumbotron jumbotronFluid;
     @UI("#jumbotron") public static Jumbotron jumbotron;
+    @UI("#breadcrumb") public static Breadcrumb breadcrumb;
     //endregion
 
     //region Complex Elements
-    @Css("#card-example") public static CardExample cardExample;
+    //endregion
 
     //region Composite Elements
     @UI("#input-group-username") public static InputGroupInputWithText inputGroupUsername;
--- jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/inputGroup/InputGroupWrapping.java
+++ jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/inputGroup/InputGroupWrapping.java
@@ -5,7 +5,8 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static io.github.com.StaticSite.bsPage;
-import static io.github.com.pages.BootstrapPage.*;
+import static io.github.com.pages.BootstrapPage.inputGroupNowrap;
+import static io.github.com.pages.BootstrapPage.inputGroupWrap;
 import static io.github.epam.states.States.shouldBeLoggedIn;
 import static org.testng.Assert.assertFalse;
 
