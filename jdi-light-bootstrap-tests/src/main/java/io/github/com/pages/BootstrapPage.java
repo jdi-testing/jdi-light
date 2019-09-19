5f833e4b37ae5cc3ff4952d436ac11418b06db6c
ChepelVE <Viktor_Chepel@epam.com>
2019-09-19 16:28:36 +0300

    change css to UI

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/pages/BootstrapPage.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/pages/BootstrapPage.java
@@ -1,7 +1,7 @@
 package io.github.com.pages;
 
 import com.epam.jdi.light.elements.composite.WebPage;
-import com.epam.jdi.light.elements.pageobjects.annotations.locators.Css;
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.ui.bootstrap.elements.common.Button;
 import com.epam.jdi.light.ui.bootstrap.elements.complex.Carousel;
 
@@ -14,23 +14,23 @@ import io.github.com.sections.Jumbotron;
 public class BootstrapPage extends WebPage {
     //region Simple Elements
     public static Button redButton, doubleButton, disabledButton;
-    @Css("#jumbotron-fluid") public static Jumbotron jumbotronFluid;
-    @Css("#jumbotron") public static Jumbotron jumbotron;
+    @UI("#jumbotron-fluid") public static Jumbotron jumbotronFluid;
+    @UI("#jumbotron") public static Jumbotron jumbotron;
     //endregion
 
     //region Complex Elements
-    @Css("#carousel-example-slides-only") public static Carousel carouselWithSlidesOnly;
-    @Css("#carousel-example-controls") public static Carousel carouselWithControls;
-    @Css("#carousel-example-indicators") public static Carousel carouselWithIndicators;
-    @Css("#carousel-example-captions") public static Carousel carouselWithCaptions;
-    @Css("#carousel-example-fade") public static Carousel carouselWithFadeTransition;
-    @Css("#carousel-example-interval") public static Carousel carouselWithCustomInterval;
+    @UI("#carousel-example-slides-only") public static Carousel carouselWithSlidesOnly;
+    @UI("#carousel-example-controls") public static Carousel carouselWithControls;
+    @UI("#carousel-example-indicators") public static Carousel carouselWithIndicators;
+    @UI("#carousel-example-captions") public static Carousel carouselWithCaptions;
+    @UI("#carousel-example-fade") public static Carousel carouselWithFadeTransition;
+    @UI("#carousel-example-interval") public static Carousel carouselWithCustomInterval;
     //endregion
     
     //region Composite Elements
-    @Css("#input-group-username") public static InputGroupInputWithText inputGroupUsername;
-    @Css("#input-group-checkbox") public static InputGroupInputWithCheckBox inputGroupCheckBox;
-    @Css("#input-group-radio") public static InputGroupInputWithRadio inputGroupRadio;
-    @Css("#card-example") public static CardExample cardExample;
+    @UI("#input-group-username") public static InputGroupInputWithText inputGroupUsername;
+    @UI("#input-group-checkbox") public static InputGroupInputWithCheckBox inputGroupCheckBox;
+    @UI("#input-group-radio") public static InputGroupInputWithRadio inputGroupRadio;
+    @UI("#card-example") public static CardExample cardExample;
     //endregion
 }
