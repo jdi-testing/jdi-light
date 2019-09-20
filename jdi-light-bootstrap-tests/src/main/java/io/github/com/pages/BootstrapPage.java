e84a061e1078ae6934e241da819ee0e632ae6bf7
Aleksandra Kozhevnikova <ms.asck@yandex.ru>
2019-09-20 17:21:41 +0300

    media obctec update

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/pages/BootstrapPage.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/pages/BootstrapPage.java
@@ -1,13 +1,13 @@
 package io.github.com.pages;
 
 import com.epam.jdi.light.elements.common.UIElement;
+import com.epam.jdi.light.elements.complex.JList;
 import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.JDropdown;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.ui.bootstrap.elements.common.Alert;
 import com.epam.jdi.light.ui.bootstrap.elements.common.Button;
 import com.epam.jdi.light.ui.bootstrap.elements.complex.Collapse;
-
 import io.github.com.sections.*;
 
 public class BootstrapPage extends WebPage {
