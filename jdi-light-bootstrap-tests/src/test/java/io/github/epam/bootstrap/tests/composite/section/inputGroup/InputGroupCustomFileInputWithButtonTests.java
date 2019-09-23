79026c418dbe6baab693540376db5f9cd15966b4
OlgaIvanova777 <olga_ivanova@epam.com>
2019-09-23 14:59:19 +0300

    add InputGroupCustomFileInputWithButtonTests

---
+++ jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/inputGroup/InputGroupCustomFileInputWithButtonTests.java
@@ -0,0 +1,8 @@
+package io.github.epam.bootstrap.tests.composite.section.inputGroup;
+
+import io.github.epam.TestsInit;
+
+public class InputGroupCustomFileInputWithButtonTests extends TestsInit {
+
+
+}
