98a4640f60e8e01c8a7a40e0ae8a5b7d6241957f
Artem <chilly.artem@gmail.com>
2019-09-24 17:55:15 +0300

    created a separate class for Card

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/CardGroup.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/CardGroup.java
@@ -2,9 +2,9 @@ package io.github.com.sections;
 
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 
-public class CardGroup extends Card {
+public class CardGroup extends CardWithinCardGroup {
 
-    @UI(".card:nth-of-type(1)") public Card card1;
-    @UI(".card:nth-of-type(2)") public Card card2;
+    @UI(".card:nth-of-type(1)") public CardWithinCardGroup card1;
+    @UI(".card:nth-of-type(2)") public CardWithinCardGroup card2;
 
 }
--- jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/CardWithinCardGroup.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/CardWithinCardGroup.java
@@ -1,11 +1,11 @@
 package io.github.com.sections;
 
-import com.epam.jdi.light.elements.composite.Section;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.ui.bootstrap.elements.common.Image;
 import com.epam.jdi.light.ui.bootstrap.elements.common.Text;
+import com.epam.jdi.light.ui.bootstrap.elements.complex.Card;
 
-public class Card extends Section {
+public class CardWithinCardGroup extends Card {
 
     @UI(".card-title") public Text title;
     @UI(".card-text:nth-of-type(1)") public Text mainText;
