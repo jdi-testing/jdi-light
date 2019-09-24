d3def34fbcdcef67122daf1bbc44b90287d9695c
dexguitar <stinzane@gmail.com>
2019-09-24 16:36:35 +0300

    CardNavTests additions

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/CardNavigation.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/CardNavigation.java
@@ -1,10 +1,13 @@
 package io.github.com.sections;
 
+import com.epam.jdi.light.elements.common.Label;
 import com.epam.jdi.light.elements.complex.Menu;
 import com.epam.jdi.light.elements.composite.Section;
 import com.epam.jdi.light.elements.pageobjects.annotations.Title;
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.Css;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.ui.bootstrap.elements.common.Button;
+import com.epam.jdi.light.ui.bootstrap.elements.common.Link;
 import com.epam.jdi.light.ui.bootstrap.elements.common.Text;
 
 public class CardNavigation extends Section {
@@ -12,12 +15,19 @@ public class CardNavigation extends Section {
     @UI(".nav")
     public Menu menu;
 
+    @UI(".nav-link active")
+    public Link activeLink;
+    @UI(".nav-link")
+    public Link jdiLink;
+    @UI(".nav-link disabled")
+    public Link disabledLink;
+
     @Title
     @UI("h5")
     public Text title;
 
     @UI(".card-text")
-    public Text text;
+    public Text subtitle;
 
     @UI("button")
     public Button button;
--- jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/card/CardNavigationTests.java
+++ jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/card/CardNavigationTests.java
@@ -1,12 +1,16 @@
 package io.github.epam.bootstrap.tests.composite.section.card;
 
+import com.epam.jdi.light.elements.common.Label;
 import io.github.epam.TestsInit;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import static io.github.com.StaticSite.bsPage;
 import static io.github.com.pages.BootstrapPage.cardNavigation;
+import static io.github.com.pages.BootstrapPage.cardWithSubtitlesAndLinks;
 import static io.github.epam.states.States.shouldBeLoggedIn;
+import static org.testng.Assert.assertEquals;
 
 /**
  * Created by Zaur Bibilov on 24.09.2019
@@ -16,6 +20,17 @@ import static io.github.epam.states.States.shouldBeLoggedIn;
 
 public class CardNavigationTests extends TestsInit {
 
+    // Texts to check
+    String titleText = "SPECIAL TITLE TREATMENT";
+    String subtitleText = "With supporting text below as a natural lead-in to additional content.";
+    String buttonText = "Click Me!";
+    String activeLinkText = "Active";
+    String jdiLinkText = "JDI";
+    String disabledLinkText = "Disabled";
+    String activeLinkRef = "javascript: void()";
+    String jdiLinkRef = "https://github.com/jdi-testing/jdi-light";
+    String disabledLinkRef = "javascript: void()";
+
     @BeforeMethod
     public void before() {
         shouldBeLoggedIn();
@@ -23,9 +38,54 @@ public class CardNavigationTests extends TestsInit {
     }
 
     @Test
-    public void navLinkClickTest() {
-        cardNavigation.menu.select("Active");
-        cardNavigation.menu.select("JDI");
+    public void getTitleTextTest() {
+        assertEquals(cardNavigation.title.getText(), titleText);
     }
+
+    @Test
+    public void getDescriptionTextTest() {
+        assertEquals(cardNavigation.subtitle.getText(), subtitleText);
+    }
+
+    @Test
+    public void getButtonTextTest() {
+        assertEquals(cardNavigation.button.getText(), buttonText);
+    }
+
+    @Test
+    public void getLinkTextTest() {
+        cardNavigation.menu.highlight();
+        assertEquals(cardNavigation.activeLink.getText(), activeLinkText);
+    }
+
+//    @Test
+//    public void getLinkValueTest() {
+//        cardNavigation.menu.highlight();
+//        assertEquals(cardNavigation.activeLink.getValue(), activeLinkText);
+//        assertEquals(cardNavigation.jdiLink.getValue(), jdiLinkText);
+//        assertEquals(cardNavigation.disabledLink.getValue(), disabledLinkText);
+//    }
+
+//    @Test
+//    public void getLinkRefTest() {
+//        cardNavigation.menu.highlight();
+//        assertEquals(cardNavigation.activeLink.ref(), activeLinkRef);
+//        assertEquals(cardNavigation.jdiLink.ref(), jdiLinkRef);
+//        assertEquals(cardNavigation.disabledLink.ref(), disabledLinkRef);
+//    }
+
+//    @Test
+//    public void getLinkUrlTest() {
+//        cardNavigation.menu.highlight();
+//        assertEquals(cardNavigation.activeLink.url().getPath(), activeLinkRef);
+//        assertEquals(cardNavigation.jdiLink.ref(), jdiLinkRef);
+//        assertEquals(cardNavigation.disabledLink.ref(), disabledLinkRef);
+//    }
+
+//    @Test
+//    public void navLinkClickTest() {
+//        cardNavigation.menu.select("Active");
+//        cardNavigation.menu.select("JDI");
+//    }
 
 }
