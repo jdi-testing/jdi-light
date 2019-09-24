079764f799de622f55894baa2408018859ad22cc
ChepelVE <Viktor_Chepel@epam.com>
2019-09-24 16:44:30 +0300

    fix issues after review

--- jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/complex/CarouselTests.java
+++ jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/complex/CarouselTests.java
@@ -19,14 +19,14 @@ import com.epam.jdi.light.elements.composite.WebPage;
 import io.github.epam.TestsInit;
 
 public class CarouselTests extends TestsInit {
-	
+
 	@BeforeMethod
 	public void before() {
 		shouldBeLoggedIn();
 		bsPage.shouldBeOpened();
 		refresh();
 	}
-	
+
 	private String nextText = "Next";
 	private String prevText = "Previous";
 	private String firstSlideText = "First slide";
@@ -35,116 +35,112 @@ public class CarouselTests extends TestsInit {
 	private String firstSlideFullText = firstSlideText + "\n" + prevText + "\n" + nextText;
 	private String secondSlideFullText = secondSlideText + "\n" + prevText + "\n" + nextText;
 	private String thirdSlideFullText = thirdSlideText + "\n" + prevText + "\n" + nextText;
-	
-	private String firstSlideWithLabelText = firstSlideText + "\n" +
-	                                 "FIRST SLIDE LABEL\n" +
-			                         "Nulla vitae elit libero, a pharetra augue mollis interdum.\n" + 
-	                                 prevText + "\n" + nextText;
-	private String secondSlideWithLabelText = secondSlideText + "\n" + 
-			                          "SECOND SLIDE LABEL\n" + 
-			                          "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" + 
-			                          prevText + "\n" + nextText;
-	private String thirdSlideWithLabelText = thirdSlideText + "\n" + 
-			                          "THIRD SLIDE LABEL\n" + 
-			                          "Praesent commodo cursus magna, vel scelerisque nisl consectetur.\n" + 
-			                          prevText + "\n" + nextText;
 
-	//Carousel slides only tests
+	private String firstSlideWithLabelText = firstSlideText + "\n" + "FIRST SLIDE LABEL\n"
+			+ "Nulla vitae elit libero, a pharetra augue mollis interdum.\n" + prevText + "\n" + nextText;
+	private String secondSlideWithLabelText = secondSlideText + "\n" + "SECOND SLIDE LABEL\n"
+			+ "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" + prevText + "\n" + nextText;
+	private String thirdSlideWithLabelText = thirdSlideText + "\n" + "THIRD SLIDE LABEL\n"
+			+ "Praesent commodo cursus magna, vel scelerisque nisl consectetur.\n" + prevText + "\n" + nextText;
+	
+	private int interval = 5;
+	private int customInterval = 1;
+
+	// Carousel slides only tests
 	@Test
 	public void getSlidesTextTest() {
 		carouselWithSlidesOnly.is().text(firstSlideText);
 		carouselWithSlidesOnly.is().text(secondSlideText);
 	}
-	
-	//Carousel with controls tests
+
+	// Carousel with controls tests
 	@Test
 	public void prevTest() {
-		carouselWithControls.prev();	
+		carouselWithControls.prev();
 		carouselWithControls.is().text(secondSlideFullText);
-		
-		carouselWithControls.prev();	
+
+		carouselWithControls.prev();
 		carouselWithControls.is().text(firstSlideFullText);
-		
+
 		carouselWithControls.prevControl().is().text(prevText);
 	}
-	
+
 	@Test
 	public void nextTest() {
-		carouselWithControls.next();	
+		carouselWithControls.next();
 		carouselWithControls.is().text(firstSlideFullText);
-		
-		carouselWithControls.next();	
+
+		carouselWithControls.next();
 		carouselWithControls.is().text(secondSlideFullText);
-		
+
 		carouselWithControls.nextControl().is().text(nextText);
 	}
-	
-	//Carousel with indicators tests
+
+	// Carousel with indicators tests
 	@Test
 	public void selectTest() {
-		carouselWithIndicators.select(1);	
+		carouselWithIndicators.select(1);
 		carouselWithIndicators.is().text(firstSlideFullText);
 		carouselWithIndicators.indicators().get(0).is().selected();
 		carouselWithIndicators.indicators().get(1).is().deselected();
-		
-		carouselWithIndicators.select(3);	
+
+		carouselWithIndicators.select(3);
 		carouselWithIndicators.is().text(thirdSlideFullText);
 		carouselWithIndicators.indicators().get(2).is().selected();
 		carouselWithIndicators.indicators().get(0).is().deselected();
-		
-		carouselWithIndicators.select(2);	
+
+		carouselWithIndicators.select(2);
 		carouselWithIndicators.is().text(secondSlideFullText);
 		carouselWithIndicators.indicators().get(1).is().selected();
 		carouselWithIndicators.indicators().get(2).is().deselected();
-    }
-	
-	//Carousel with captions tests
+	}
+
+	// Carousel with captions tests
 	@Test
 	public void captionTest() {
 		carouselWithCaptions.select(1);
 		carouselWithCaptions.is().text(firstSlideWithLabelText);
-		
+
 		carouselWithCaptions.select(2);
 		carouselWithCaptions.is().text(secondSlideWithLabelText);
 
 		carouselWithCaptions.select(3);
 		carouselWithCaptions.is().text(thirdSlideWithLabelText);
 	}
-	
+
 	// Carousel fade tests
 	@Test
 	public void fadePrevTest() {
-		carouselWithFadeTransition.prev();	
+		carouselWithFadeTransition.prev();
 		carouselWithFadeTransition.is().text(thirdSlideFullText);
-		
-		carouselWithFadeTransition.prev();	
+
+		carouselWithFadeTransition.prev();
 		carouselWithFadeTransition.is().text(secondSlideFullText);
-		
+
 		carouselWithFadeTransition.prevControl().is().text(prevText);
 	}
-	
+
 	@Test
 	public void fadeNextTest() {
-		carouselWithFadeTransition.next();	
+		carouselWithFadeTransition.next();
 		carouselWithFadeTransition.is().text(secondSlideFullText);
-		
-		carouselWithFadeTransition.next();	
+
+		carouselWithFadeTransition.next();
 		carouselWithFadeTransition.is().text(thirdSlideFullText);
-		
+
 		carouselWithFadeTransition.nextControl().is().text(nextText);
 	}
-	
+
 	// Carousel with interval tests
 	@Test
 	public void intervalTest() {
 		WebPage.reload();
-		durationMoreThan(5, 
-				() -> carouselWithCustomInterval.is().text(secondSlideFullText));
-		durationMoreThan(carouselWithCustomInterval.interval()/1000, 
-				() -> carouselWithCustomInterval.is().text(thirdSlideFullText));
-		durationMoreThan(carouselWithCustomInterval.interval()/1000, 
-				() -> carouselWithCustomInterval.is().text(firstSlideFullText));
-		durationMoreThan(carouselWithCustomInterval.interval()/1000, 
-				() -> carouselWithCustomInterval.is().text(secondSlideFullText));
+		durationMoreThan(interval, () -> carouselWithCustomInterval.is().text(secondSlideFullText));
+		customInterval = carouselWithCustomInterval.interval() / 1000;
+		durationMoreThan(customInterval, () -> carouselWithCustomInterval.is().text(thirdSlideFullText));
+		customInterval = carouselWithCustomInterval.interval() / 1000;
+		durationMoreThan(customInterval, () -> carouselWithCustomInterval.is().text(firstSlideFullText));
+		customInterval = carouselWithCustomInterval.interval() / 1000;
+		durationMoreThan(customInterval, () -> carouselWithCustomInterval.is().text(secondSlideFullText));
 	}
 }
--- jdi-light-bootstrap/src/main/java/com/epam/jdi/light/ui/bootstrap/elements/complex/Carousel.java
+++ jdi-light-bootstrap/src/main/java/com/epam/jdi/light/ui/bootstrap/elements/complex/Carousel.java
@@ -11,55 +11,56 @@ import com.epam.jdi.light.elements.complex.WebList;
 import com.epam.jdi.light.elements.interfaces.common.IsButton;
 import com.epam.jdi.light.elements.interfaces.common.IsText;
 
-public class Carousel extends UIBaseElement<TextAssert> implements IsText, IsButton{
-	public String getText() { return text(TEXT); }
-	
-    public String currentSlideLocator = "//div[contains(@class,'carousel-item active')]";
-	
-    @JDIAction("Get current slide in '{name}'")
-	public UIElement currentSlide() {
-        return linked(currentSlideLocator, "current");
-    }
-	
+public class Carousel extends UIBaseElement<TextAssert> implements IsText, IsButton {
+	public String currentSlideLocator = "//div[contains(@class,'carousel-item active')]";
 	public String nextLocator = ".carousel-control-next";
-	
+	public String prevLocator = ".carousel-control-prev";
+	public String indicatorsLocator = "li";
+
+	public String getText() {
+		return text(TEXT);
+	}
+
+	@JDIAction("Get current slide in '{name}'")
+	public UIElement currentSlide() {
+		return linked(currentSlideLocator, "current");
+	}
+
 	public UIElement nextControl() {
-        return linked(nextLocator, "next");
-    }
-	
+		return linked(nextLocator, "next");
+	}
+
 	@JDIAction("Click 'next control' in '{name}'")
 	public void next() {
 		nextControl().click();
 	}
-	
-	public String prevLocator = ".carousel-control-prev";
-    
+
 	public UIElement prevControl() {
-        return linked(prevLocator, "prev");
-    }
-	
+		return linked(prevLocator, "prev");
+	}
+
 	@JDIAction("Click 'prev control' in '{name}'")
 	public void prev() {
 		prevControl().click();
 	}
-	
-	public String indicatorsLocator = "li";
-		
-    public WebList indicators() {
-        return linkedList(indicatorsLocator, "indicators");
-    }
-    
-    public void select(int index) {
-        if (index < 1)
-            throw exception("Can't get element with index '%s'. Index should be 1 or more", index);
-        indicators().select(index - 1);
-    }
-    
-    @JDIAction("Current slide interval in '{name}'")
-    public int interval() {
-    	return Integer.valueOf(currentSlide().getAttribute("data-interval"));
-    }
-    
-    @Override
-    public TextAssert is() { return new TextAssert().set(this); }
+
+	public WebList indicators() {
+		return linkedList(indicatorsLocator, "indicators");
+	}
+
+	public void select(int index) {
+		if (index < 1)
+			throw exception("Can't get element with index '%s'. Index should be 1 or more", index);
+		indicators().select(index - 1);
+	}
+
+	@JDIAction("Current slide interval in '{name}'")
+	public int interval() {
+		return Integer.valueOf(currentSlide().getAttribute("data-interval"));
+	}
+
+	@Override
+	public TextAssert is() {
+		return new TextAssert().set(this);
+	}
 }
