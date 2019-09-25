17e396f0ee3e67ba92ad35f76e4034be5376e021
Dmitrii_Pavlov <delnote@gmail.com>
2019-09-25 15:37:33 +0300

    List Groups in process.

--- jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/listGroup/LinksTests.java
+++ jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/listGroup/LinksTests.java
@@ -5,10 +5,18 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import static com.epam.jdi.light.elements.common.WindowsManager.closeWindow;
+import static com.epam.jdi.light.elements.common.WindowsManager.switchToNewWindow;
+import static com.epam.jdi.light.elements.composite.WebPage.getTitle;
 import static io.github.com.StaticSite.bsPage;
-import static io.github.com.pages.BootstrapPage.listGroupActiveItems;
 import static io.github.com.pages.BootstrapPage.listGroupLinks;
 import static io.github.epam.states.States.shouldBeLoggedIn;
+import static org.testng.Assert.assertEquals;
+
+/**
+ * Created by Dmitrii Pavlov on 25.09.2019
+ * Email: delnote@gmail.com; Skype: Dmitrii Pavlov
+ */
 
 public class LinksTests extends TestsInit {
 
@@ -24,11 +32,22 @@ public class LinksTests extends TestsInit {
     String text4 = "Porta ac consectetur ac";
     String text5 = "Vestibulum at eros";
     String listClass = "list-group-item list-group-item-action";
+    String pageTitle1 = "JDI";
+    String pageTitle2 = "JDI Testing tools · GitHub";
+    String pageTitle3 = "Home Page";
+    String pageTitle4 = "List group · Bootstrap";
 
     @DataProvider
     public Object[][] listData() {
         return new Object[][]{
                 {1, text1}, {2, text2}, {3, text3}, {4, text4}, {5, text5}
+        };
+    }
+
+    @DataProvider
+    public Object[][] clickValidate() {
+        return new Object[][]{
+                {1, pageTitle1}, {2, pageTitle2}, {3, pageTitle3}, {4, pageTitle4}
         };
     }
 
@@ -42,13 +61,24 @@ public class LinksTests extends TestsInit {
                 .hasClass("list-group");
         listGroupLinks.listGroup.get(1).is()
                 .hasClass(listClass + " active");
-        listGroupLinks.listGroup.get(5).enabled();
-        listGroupLinks.listGroup.get(5).isEnabled();
-//                .hasClass(listClass + " disabled");
+        listGroupLinks.listGroup.get(5).is()
+                .hasClass(listClass + " disabled");
     }
 
     @Test(dataProvider = "listData")
     public void listGroupTextTests(int num, String text) {
         listGroupLinks.listGroup.get(num).is().text(text);
+    }
+
+    @Test (dataProvider = "clickValidate")
+    public void linkClickableTests(int index, String pageTitle) {
+        listGroupLinks.listGroup.get(index).click();
+        newWindowTitleCheck(pageTitle);
+    }
+
+    public void newWindowTitleCheck(String pageTitle) {
+        switchToNewWindow();
+        assertEquals(getTitle(), pageTitle);
+        closeWindow();
     }
 }
