94912d9aa482d01da0e4c3e239a964897b20a338
Igor_Gaiduk <Igor_Gaiduk@epam.com>
2019-09-25 14:40:38 +0300

    Fixed test in framework. Reverted test to the previous version after fix.

--- jdi-light-bootstrap/src/main/java/com/epam/jdi/light/ui/bootstrap/elements/complex/RadioButtons.java
+++ jdi-light-bootstrap/src/main/java/com/epam/jdi/light/ui/bootstrap/elements/complex/RadioButtons.java
@@ -4,12 +4,12 @@ import com.epam.jdi.light.asserts.generic.UISelectAssert;
 import com.epam.jdi.light.elements.base.UIListBase;
 import com.epam.jdi.light.elements.complex.WebList;
 
-import static com.epam.jdi.light.common.TextTypes.LABEL;
+import static com.epam.jdi.light.common.TextTypes.INNER;
 
 public class RadioButtons extends UIListBase<UISelectAssert> {
     @Override
     public WebList list() {
-        return super.list().setUIElementName(LABEL);
+        return super.list().setUIElementName(INNER);
     }
     public RadioButtons() {
         base().setLocator("input[type=radio]");
