4ae4c501e9113e776e4bb0a923ed2adc30fff385
OlgaIvanova777 <olga_ivanova@epam.com>
2019-09-26 20:06:33 +0300

    improve code quality

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/CustomFileInputWithButton.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/CustomFileInputWithButton.java
@@ -4,13 +4,12 @@ import com.epam.jdi.light.elements.common.Label;
 import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.light.elements.composite.Section;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
-import com.epam.jdi.light.ui.bootstrap.elements.common.FileInput;
+import com.epam.jdi.light.ui.bootstrap.elements.common.IG_FileInput;
 
 public class CustomFileInputWithButton extends Section {
 
-    @UI(".input-group-prepend") public Label uploadTextBefore;
-    @UI(".input-group-append") public Label uploadTextAfter;
+    @UI(".input-group-text") public Label uploadText;
 
-    @UI(".custom-file-input") public FileInput inputFiled;
+    @UI("#inputGroupFile03")public IG_FileInput inputField;
     @UI(".custom-file-label") public UIElement labelText;
 }
