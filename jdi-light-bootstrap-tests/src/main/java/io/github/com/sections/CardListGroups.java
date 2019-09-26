959854aa19420cde9fc639619704565f431395f5
Aleksandr Sudiarov <aleksandr_sudiarov@epam.com>
2019-09-26 19:51:51 +0300

    Card list groups tests finished

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/CardListGroups.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/CardListGroups.java
@@ -3,12 +3,12 @@ package io.github.com.sections;
 import com.epam.jdi.light.elements.common.Label;
 import com.epam.jdi.light.elements.composite.Section;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
-import com.epam.jdi.light.ui.bootstrap.elements.common.Text;
+import org.openqa.selenium.WebElement;
 
 import java.util.List;
 
 public class CardListGroups extends Section {
 
     @UI(".card-header") public Label cardHeader;
-    @UI(".list-group-item") public List<Text> listGroups;
+    @UI(".list-group-item") public List<WebElement> listGroups;
 }
