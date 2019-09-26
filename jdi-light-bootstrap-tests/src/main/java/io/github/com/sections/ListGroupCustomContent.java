9db9dabc194e7a4aed33d95bd34e1dd5f2725fd2
Dmitrii_Pavlov <delnote@gmail.com>
2019-09-26 11:27:31 +0300

    List Groups in process.

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/ListGroupCustomContent.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/ListGroupCustomContent.java
@@ -7,5 +7,10 @@ import com.epam.jdi.light.ui.bootstrap.elements.complex.ListGroup;
 public class ListGroupCustomContent extends Section {
 
     @UI("a") public ListGroup listGroup;
+    @UI("a div h5") public ListGroup header;
+    @UI("a div small") public ListGroup dateText;
+    @UI("a p") public ListGroup mainText;
+    @UI("a small") public ListGroup footer;
+    @UI("a div") public ListGroup container;
 
 }
--- jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/listGroup/CustomContentTests.java
+++ jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/listGroup/CustomContentTests.java
@@ -1,10 +1,16 @@
 package io.github.epam.bootstrap.tests.composite.section.listGroup;
 
+import com.epam.jdi.light.ui.bootstrap.elements.complex.ListGroup;
 import io.github.epam.TestsInit;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import static io.github.com.StaticSite.bsPage;
+import static io.github.com.pages.BootstrapPage.listGroupCustomContent;
 import static io.github.epam.states.States.shouldBeLoggedIn;
+import static org.hamcrest.CoreMatchers.containsStringIgnoringCase;
+import static org.hamcrest.CoreMatchers.is;
 
 public class CustomContentTests extends TestsInit {
 
@@ -14,6 +20,61 @@ public class CustomContentTests extends TestsInit {
         bsPage.shouldBeOpened();
     }
 
-    
+    String link1 = "https://jdi-testing.github.io/jdi-light/index.html";
+    String Link2 = "https://github.com/jdi-testing";
+    String link3 = "https://github.com/jdi-docs";
+    String header1 = "List group item heading one";
+    String header2 = "List group item heading two";
+    String header3 = "List group item heading three";
+    String dateText = "3 days ago";
+    String mainText1 = "Some simple text for first section of custom list group.";
+    String mainText2 = "Some simple text for second section of custom list group.";
+    String mainText3 = "Some simple text for third section of custom list group.";
+    String footer1 = "JDI website";
+    String footer2 = "JDI - testing tool";
+    String footer3 = "JDI Docs";
 
+
+    @DataProvider
+    public Object[][] listData() {
+        return new Object[][]{
+                {1, link1, header1, dateText, mainText1, footer1},
+                {2, Link2, header2, dateText, mainText2, footer2},
+                {3, link3, header3, dateText, mainText3, footer3}
+        };
+    }
+
+    @Test
+    public void isValidationTests() {
+        listGroupCustomContent.listGroup.is().size(3);
+        listGroupCustomContent.container.is().size(3);
+        listGroupCustomContent.header.is().size(3);
+        listGroupCustomContent.dateText.is().size(3);
+        listGroupCustomContent.mainText.is().size(3);
+    }
+
+    @Test (dataProvider = "listData")
+    public void tests(int index, String link, String header, String dateText, String mainText, String footer) {
+        listGroupCustomContent.listGroup.get(index)
+                .is().core()
+                .hasClass("list-group-item list-group-item-action")
+                .attr("href", is(link));
+        listGroupCustomContent.container.get(index)
+                .is().core()
+                .hasClass("d-flex w-100 justify-content-between");
+        listGroupCustomContent.header.get(index)
+                .is().core()
+                .hasClass("mb-1")
+                .text(is(containsStringIgnoringCase(header)));
+        listGroupCustomContent.dateText.get(index)
+                .is().core()
+                .text(is(dateText));
+        listGroupCustomContent.mainText.get(index)
+                .is().core()
+                .hasClass("mb-1")
+                .text(is(mainText));
+        listGroupCustomContent.footer.get(index)
+                .is().core()
+                .text(is(footer));
+    }
 }
