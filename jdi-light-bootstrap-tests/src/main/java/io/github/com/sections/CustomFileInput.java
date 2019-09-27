fc3b660c90bc7ac4e27b60eebf259f7bb1b7f282
OlgaIvanova777 <olga_ivanova@epam.com>
2019-09-27 18:51:19 +0300

    add @FindBy for CustomFileInput and CustomFileInputWithButton

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/CustomFileInput.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/CustomFileInput.java
@@ -2,12 +2,17 @@ package io.github.com.sections;
 
 import com.epam.jdi.light.elements.common.Label;
 import com.epam.jdi.light.elements.composite.Section;
+import com.epam.jdi.light.elements.pageobjects.annotations.FindBy;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.ui.bootstrap.elements.common.FileInput;
 
 public class CustomFileInput extends Section {
-
+    //@FindBy(css = ".input-group-text")
     @UI(".input-group-text") public Label upload;
+
+    //@FindBy(css = ".custom-file-input")
     @UI(".custom-file-input") public FileInput input;
+
+    //@FindBy(css = ".custom-file-label")
     @UI(".custom-file-label") public Label label;
 }
