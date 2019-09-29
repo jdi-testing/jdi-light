02262c5348633a5ef3488ee489028e61aee44202
dexguitar <stinzane@gmail.com>
2019-09-29 17:25:39 +0300

    Created Mockito tests for checking recently modified UIElement's enabled() method

---
+++ jdi-light/src/test/java/EnabledElementMockTests.java
@@ -0,0 +1,37 @@
+import mocks.CardNavigationMock;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+public class EnabledElementMockTests {
+
+    CardNavigationMock cardNavigationMock = null;
+
+    @BeforeMethod
+    public void init() {
+        cardNavigationMock = mock(CardNavigationMock.class);
+    }
+
+    @Test
+    public void hasClassTest() {
+        when(cardNavigationMock.hasClassCheck()).thenReturn(true);
+        assertTrue(cardNavigationMock.hasClassCheck());
+    }
+
+    @Test
+    public void hasClassAndAttributeTest() {
+        when(cardNavigationMock.hasClassAndAttributeCheck()).thenReturn(false);
+        assertFalse(cardNavigationMock.hasClassAndAttributeCheck());
+    }
+
+    @Test
+    public void getWebElementCheck() {
+        when(cardNavigationMock.getWebElementCheck()).thenReturn(true);
+        assertTrue(cardNavigationMock.getWebElementCheck());
+    }
+
+}
