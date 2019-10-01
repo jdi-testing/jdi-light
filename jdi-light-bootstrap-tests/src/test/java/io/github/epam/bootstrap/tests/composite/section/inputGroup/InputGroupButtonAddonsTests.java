90de37afe5eebbf0e2b811ee7364e4f125439782
Vsevolod_Adrianov <Vsevolod_Adrianov@epam.com>
2019-10-01 19:10:01 +0300

    use @UI

--- jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/inputGroup/InputGroupButtonAddonsTests.java
+++ jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/inputGroup/InputGroupButtonAddonsTests.java
@@ -7,13 +7,14 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static com.epam.jdi.light.driver.WebDriverFactory.getDriver;
+import static com.epam.jdi.light.elements.init.UIFactory.$;
 import static io.github.com.StaticSite.bsPage;
 import static io.github.com.pages.BootstrapPage.*;
 import static io.github.epam.states.States.shouldBeLoggedIn;
 
 public class InputGroupButtonAddonsTests extends TestsInit {
     private static final String JS_SCROLL_TO_ELEMENT = "arguments[0].scrollIntoView(true);";
-    private static final String HEADER_RIGHT_PART = "//h4[contains(., 'Input group - Button addons')]";
+    private static final String HEADER_RIGHT_PART = "//h4[.='Input group - Button addons']";
     private String text = "XA-XA",
        placeholder_text = "Recipient's username";
 
@@ -27,33 +28,33 @@ public class InputGroupButtonAddonsTests extends TestsInit {
 // Button addons tests
     @Test
     public void checkButtonAddon1Test() {
-        inputGroupButtonAddons1.input1.input(text);
-        inputGroupButtonAddons1.button1.click();
-        inputGroupButtonAddons1.input1.assertThat().text(text);
+        inputGroupButtonAddons1.input.input(text);
+        inputGroupButtonAddons1.button.click();
+        inputGroupButtonAddons1.input.assertThat().text(text);
     }
 
     @Test
     public void checkButtonAddon2Test() {
-        inputGroupButtonAddons2.input2.input(text);
-        inputGroupButtonAddons2.button2.click();
-        inputGroupButtonAddons2.input2.input(placeholder_text);
-        inputGroupButtonAddons2.input2.assertThat().text(placeholder_text);
+        inputGroupButtonAddons2.input.input(text);
+        inputGroupButtonAddons2.button.click();
+        inputGroupButtonAddons2.input.input(placeholder_text);
+        inputGroupButtonAddons2.input.assertThat().text(placeholder_text);
     }
 
     @Test
     public void checkButtonAddon3Test() {
-        inputGroupButtonAddons3.input3.input(text);
-        inputGroupButtonAddons3.anyButtons.get(1).click();
-        inputGroupButtonAddons3.anyButtons.get(2).click();
-        inputGroupButtonAddons3.input3.assertThat().text(text);
+        inputGroupButtonAddons3.inputField.input(text);
+        inputGroupButtonAddons3.listButtons.get(1).click();
+        inputGroupButtonAddons3.listButtons.get(2).click();
+        inputGroupButtonAddons3.inputField.assertThat().text(text);
     }
 
     @Test
     public void checkButtonAddon4Test() {
-        inputGroupButtonAddons4.input4.input(text);
-        inputGroupButtonAddons4.nextButtons.get(1).click();
-        inputGroupButtonAddons4.input4.input(placeholder_text);
-        inputGroupButtonAddons4.nextButtons.get(2).click();
-        inputGroupButtonAddons4.input4.assertThat().text(placeholder_text);
+        inputGroupButtonAddons4.inputField.input(text);
+        inputGroupButtonAddons4.listButtons.get(1).click();
+        inputGroupButtonAddons4.inputField.input(placeholder_text);
+        inputGroupButtonAddons4.listButtons.get(2).click();
+        inputGroupButtonAddons4.inputField.assertThat().text(placeholder_text);
     }
 }
