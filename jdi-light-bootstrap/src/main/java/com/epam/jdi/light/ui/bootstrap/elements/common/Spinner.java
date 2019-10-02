7dd06ae7c3503cde34229e5e61d6871a83b7fff6
Yulia_Litvin <iulia.litwin@yandex.ru>
2019-10-02 14:27:48 +0300

    spinner extends from UI element , not from section

--- jdi-light-bootstrap/src/main/java/com/epam/jdi/light/ui/bootstrap/elements/common/Spinner.java
+++ jdi-light-bootstrap/src/main/java/com/epam/jdi/light/ui/bootstrap/elements/common/Spinner.java
@@ -1,12 +1,12 @@
 package com.epam.jdi.light.ui.bootstrap.elements.common;
 
-import com.epam.jdi.light.elements.composite.Section;
+import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.tools.Timer;
 
-public class Spinner extends Section {
+public class Spinner extends UIElement {
     public Spinner disappearAfter(int timeoutSec) {
         assertThat().displayed();
-        new Timer(timeoutSec * 1000).wait(() -> this.core().isHidden());
+        new Timer(timeoutSec * 1000).wait(() -> core().isHidden());
         assertThat().hidden();
         return this;
     }
