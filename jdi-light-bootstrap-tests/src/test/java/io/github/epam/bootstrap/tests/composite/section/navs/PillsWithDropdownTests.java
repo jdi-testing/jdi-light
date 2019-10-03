2e6d9a2b33d0be0bbfaf827894c0221111897200
Dmitrii_Pavlov <delnote@gmail.com>
2019-10-03 09:57:55 +0300

    NAVS refactored.

--- jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/navs/PillsWithDropdownTests.java
+++ jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/navs/PillsWithDropdownTests.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 
 import static io.github.com.StaticSite.bsPage;
 import static io.github.com.pages.BootstrapPage.navsPillsWithDropdown;
-import static io.github.epam.bootstrap.tests.composite.section.navs.ClickVarification.newWindowTitleCheck;
+import static io.github.epam.bootstrap.tests.composite.section.navs.ClickVerification.newWindowTitleCheck;
 import static io.github.epam.states.States.shouldBeLoggedIn;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
