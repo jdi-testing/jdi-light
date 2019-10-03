5d60dda9c37a783602be08f7f420832771ec6edd
ArKhmeln <aakhmelinin@gmail.com>
2019-10-03 13:49:33 +0300

    conflict resolving

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/pages/BootstrapPage.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/pages/BootstrapPage.java
@@ -42,6 +42,12 @@ public class BootstrapPage extends WebPage {
     @UI("#tooltipWithHTML") public static Tooltip tooltipWithHTML;
     @UI("#wrapperForDisabledButton") public static Tooltip wrapperForDisabledButton;
     @UI("#tooltipOnDisabledButton") public static Tooltip tooltipOnDisabledButton;
+    @UI("#progress-bar-base-width-0") public static Progress progressBaseProgress0;
+    @UI("#progress-bar-base-width-0 .progress-bar") public static Progress progressBaseWidth0;
+    @UI("#progress-bar-base-width-25 .progress-bar") public static Progress progressBaseWidth25;
+    @UI("#progress-bar-base-width-50 .progress-bar") public static Progress progressBaseWidth50;
+    @UI("#progress-bar-base-width-75 .progress-bar") public static Progress progressBaseWidth75;
+    @UI("#progress-bar-base-width-100 .progress-bar") public static Progress progressBaseWidth100;
     @UI("#progress-with-labels") public static Progress progressWithLabels;
     @UI("#button-show-spinner-border") public static Button buttonSpinnerBorder;
     @UI("#spinner-border") public static Spinner spinnerBorder;
