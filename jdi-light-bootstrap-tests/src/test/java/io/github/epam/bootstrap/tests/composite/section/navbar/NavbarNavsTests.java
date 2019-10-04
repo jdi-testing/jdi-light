13c67789fb6b6cbe66cf81dbe70dc3a68cd1c2cb
Aleksandra Kozhevnikova <ms.asck@yandex.ru>
2019-10-04 16:02:41 +0300

    navbar nav

--- jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/navbar/NavbarNavsTests.java
+++ jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/navbar/NavbarNavsTests.java
@@ -6,8 +6,8 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static io.github.com.StaticSite.bsPage;
-import static io.github.com.pages.BootstrapPage.*;
-import static io.github.com.pages.BootstrapPage.progressWithLabels;
+import static io.github.com.pages.BootstrapPage.navbarNavWithDisabled;
+import static io.github.com.pages.BootstrapPage.navbarNavWithDropdown;
 import static io.github.epam.bootstrap.tests.BaseValidations.baseValidation;
 import static io.github.epam.states.States.shouldBeLoggedIn;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -31,10 +31,10 @@ public class NavbarNavsTests extends TestsInit {
 
     @Test
     public void isValidationTest() {
-        progressWithLabels.is().displayed();
-        progressWithLabels.is().enabled();
-        assertThat(progressWithLabels.core().css("font-size"), is("14px"));
-        progressWithLabels.assertThat().displayed()
+        navbarNavWithDisabled.is().displayed();
+        navbarNavWithDisabled.is().enabled();
+        assertThat(navbarNavWithDisabled.core().css("font-size"), is("14px"));
+        navbarNavWithDisabled.assertThat().displayed()
                 .core()
                 .css("font-size", is("14px"))
                 .cssClass("navbar")
