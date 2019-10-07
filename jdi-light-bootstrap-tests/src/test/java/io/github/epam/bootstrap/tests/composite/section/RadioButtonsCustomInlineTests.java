77c618736cedce181dac788d246377d5c024dc2e
Dmitrii_Pavlov <delnote@gmail.com>
2019-10-07 17:56:22 +0300

    Custom Radio inline done.

--- jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/RadioButtonsCustomInlineTests.java
+++ jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/RadioButtonsCustomInlineTests.java
@@ -5,13 +5,19 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static io.github.com.StaticSite.bsPage;
+import static io.github.com.pages.BootstrapPage.radioButtonsCustomInline;
 import static io.github.epam.states.States.shouldBeLoggedIn;
 import static org.hamcrest.Matchers.is;
 
+/**
+ * Created by Dmitrii Pavlov on 07.10.2019
+ * Email: delnote@gmail.com; Skype: Dmitrii Pavlov
+ */
+
 public class RadioButtonsCustomInlineTests extends TestsInit {
 
-    private String label1 = "Toggle this custom radio";
-    private String label2 = "Or toggle this other custom radio";
+    private String label1 = "Toggle this\ncustom radio";
+    private String label2 = "Or toggle this\ncustom radio";
 
     @BeforeMethod
     public void before() {
@@ -21,74 +27,74 @@ public class RadioButtonsCustomInlineTests extends TestsInit {
 
     @Test
     public void baseInitTest() {
-        radioButtonCustom.radioButton.is()
+        radioButtonsCustomInline.radioButton.is()
                 .size(2);
-        radioButtonCustom.radio1.is()
+        radioButtonsCustomInline.radio1.is()
                 .deselected();
-        radioButtonCustom.radio2.is()
+        radioButtonsCustomInline.radio2.is()
                 .deselected();
-        radioButtonCustom.radio1Label.is()
+        radioButtonsCustomInline.radio1Label.is()
                 .text(is(label1));
-        radioButtonCustom.radio2Label.is()
+        radioButtonsCustomInline.radio2Label.is()
                 .text(is(label2));
     }
 
     @Test
     public void baseInitByIndexTest() {
-        radioButtonCustom.radioButton.get(1).is()
+        radioButtonsCustomInline.radioButton.get(1).is()
                 .deselected();
-        radioButtonCustom.radioButton.get(2).is()
+        radioButtonsCustomInline.radioButton.get(2).is()
                 .deselected();
     }
 
     @Test
     public void radioButtonByIndexTests() {
-        radioButtonCustom.radioButton.select(2);
-        radioButtonCustom.radio2.is()
+        radioButtonsCustomInline.radioButton.select(2);
+        radioButtonsCustomInline.radio2.is()
                 .selected();
-        radioButtonCustom.radio1.is()
+        radioButtonsCustomInline.radio1.is()
                 .deselected();
-        radioButtonCustom.radioButton.select(1);
-        radioButtonCustom.radio1.is()
+        radioButtonsCustomInline.radioButton.select(1);
+        radioButtonsCustomInline.radio1.is()
                 .selected();
-        radioButtonCustom.radio2.is()
+        radioButtonsCustomInline.radio2.is()
                 .deselected();
     }
 
     @Test
     public void radioButtonByLabelTests() {
-        radioButtonCustom.radio2Label.click();
-        radioButtonCustom.radio2.is()
+        radioButtonsCustomInline.radio2Label.click();
+        radioButtonsCustomInline.radio2.is()
                 .selected();
-        radioButtonCustom.radio1.is()
+        radioButtonsCustomInline.radio1.is()
                 .deselected();
-        radioButtonCustom.radio1Label.click();
-        radioButtonCustom.radio1.is()
+        radioButtonsCustomInline.radio1Label.click();
+        radioButtonsCustomInline.radio1.is()
                 .selected();
-        radioButtonCustom.radio2.is()
+        radioButtonsCustomInline.radio2.is()
                 .deselected();
     }
 
     @Test
     public void radioOneIsValidationTests() {
-        radioButtonCustom.radio1.is()
+        radioButtonsCustomInline.radio1.is()
                 .hidden()
                 .enabled()
                 .core()
                 .attr("type", "radio")
-                .attr("name", "customRadio")
+                .attr("name", "customRadioInline1")
                 .hasClass("custom-control-input")
                 .tag(is("input"));
     }
 
     @Test
     public void radioTwoIsValidationTests() {
-        radioButtonCustom.radio2.is()
+        radioButtonsCustomInline.radio2.is()
                 .hidden()
                 .enabled()
                 .core()
                 .attr("type", "radio")
-                .attr("name", "customRadio")
+                .attr("name", "customRadioInline1")
                 .hasClass("custom-control-input")
                 .tag(is("input"));
     }
