3fde367acd8e7e5d47dc2216075a624a82aacb4e
OlgaIvanova777 <olga_ivanova@epam.com>
2019-10-07 16:00:08 +0300

    add ScrollspyWithNestedNavTests, NestedNav, ScrollspyWithNestedNav

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/pages/BootstrapPage.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/pages/BootstrapPage.java
@@ -210,6 +210,8 @@ public class BootstrapPage extends WebPage {
     @UI(".card-columns") public static CardColumns cardColumns;
     @UI("#list-example a") public static ListGroup listGroupForScrollspy;
     @UI("#list-example~div") public static ScrollspyWithListGroup scrollspyWithListGroup;
+    @UI("#navbar-example3") public static NestedNav nestedNav;
+    @UI("#list-example~div") public static ScrollspyWithNestedNav scrollspyWithNestedNav;
     @UI("body") public static Popover popover;
     @UI("body") public static CheckboxesDefaultInline checkboxesDefaultInline;
     @UI("body") public static RadioButtonsDefaultInline radioButtonsDefaultInline;
--- jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/ScrollspyWithListGroup.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/ScrollspyWithListGroup.java
@@ -2,7 +2,6 @@ package io.github.com.sections;
 
 import com.epam.jdi.light.elements.composite.Section;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
-import com.epam.jdi.light.ui.bootstrap.elements.common.Text;
 import com.epam.jdi.light.ui.bootstrap.elements.complex.ListGroup;
 
 public class ScrollspyWithListGroup  extends Section {
