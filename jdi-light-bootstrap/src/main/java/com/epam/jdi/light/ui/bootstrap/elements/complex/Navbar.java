1c63dc96ae02e8ad3a4ce2b94fcdb3d95e1ea7e2
Aleksandra Kozhevnikova <ms.asck@yandex.ru>
2019-10-08 18:11:52 +0300

    add navbar nav

--- jdi-light-bootstrap/src/main/java/com/epam/jdi/light/ui/bootstrap/elements/complex/Navbar.java
+++ jdi-light-bootstrap/src/main/java/com/epam/jdi/light/ui/bootstrap/elements/complex/Navbar.java
@@ -9,10 +9,8 @@ import com.epam.jdi.light.ui.bootstrap.elements.common.Link;
 
 public class Navbar extends Section {
 
-    @UI("//*[contains(@class, 'nav-item')]")
-    public WebList navbarLinks;
-    @UI(".navbar-brand")
-    public Link navbarBrand;
+    @UI("//*[contains(@class, 'nav-item')]") public WebList navbarLinks;
+    @UI(".navbar-brand") public Link navbarBrand;
     @JDropdown(expand = ".navbar-toggler-icon", value = ".navbar-nav", list = "a")
     public Collapse togglerButton;
     @UI("//*[contains(@class, 'nav-link dropdown-toggle')]")
