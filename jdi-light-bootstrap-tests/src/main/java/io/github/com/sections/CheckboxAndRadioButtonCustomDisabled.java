27c41d868b8346831b2354f8a1f346bb9bd7667b
Dmitrii_Pavlov <delnote@gmail.com>
2019-10-08 10:43:53 +0300

    Custom Radio and checkbox disabled done.

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/CheckboxAndRadioButtonCustomDisabled.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/CheckboxAndRadioButtonCustomDisabled.java
@@ -1,5 +1,6 @@
 package io.github.com.sections;
 
+import com.epam.jdi.light.elements.common.Label;
 import com.epam.jdi.light.elements.composite.Section;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.ui.bootstrap.elements.common.Checkbox;
@@ -15,5 +16,7 @@ public class CheckboxAndRadioButtonCustomDisabled extends Section {
     public RadioButton radioButtonContainer;
     @UI("#customRadioDisabled2")
     public RadioButton radioButton;
+    @UI("label[for='customRadioDisabled2']")
+    public Label radio1Label;
 
 }
--- jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/CheckboxAndRadioButtonCustomDisabledTests.java
+++ jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/CheckboxAndRadioButtonCustomDisabledTests.java
@@ -1,6 +1,7 @@
 package io.github.epam.bootstrap.tests.composite.section;
 
 import io.github.epam.TestsInit;
+import org.hamcrest.Matchers;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -16,6 +17,8 @@ import static org.hamcrest.CoreMatchers.is;
  */
 
 public class CheckboxAndRadioButtonCustomDisabledTests extends TestsInit {
+
+    private String label1 = "Toggle this custom radio";
 
     @BeforeMethod
     public void before() {
@@ -52,7 +55,39 @@ public class CheckboxAndRadioButtonCustomDisabledTests extends TestsInit {
     }
 
     @Test
-    public void clickableTests() {
+    public void radioButtonIsValidationTests() {
+        checkboxAndRadioButtonCustomDisabled.radioButton.is()
+                .hidden()
+                .disabled()
+                .core()
+                .attr("type", "radio")
+                .attr("name", "radioDisabled")
+                .hasClass("custom-control-input")
+                .tag(Matchers.is("input"));
+    }
+
+    @Test
+    public void baseInitTest() {
+        checkboxAndRadioButtonCustomDisabled.radioButtonContainer.is()
+                .size(1);
+        checkboxAndRadioButtonCustomDisabled.radioButton.is()
+                .deselected();
+        checkboxAndRadioButtonCustomDisabled.radio1Label.is()
+                .text(is(label1));
+    }
+
+    @Test
+    public void radioButtonTests() {
+        checkboxAndRadioButtonCustomDisabled.radioButtonContainer.select();
+        checkboxAndRadioButtonCustomDisabled.radioButton.is()
+                .deselected();
+        checkboxAndRadioButtonCustomDisabled.radio1Label.click();
+        checkboxAndRadioButtonCustomDisabled.radioButton.is()
+                .deselected();
+    }
+
+    @Test
+    public void checkboxTests() {
         checkboxAndRadioButtonCustomDisabled.checkboxContainer.check();
         checkboxAndRadioButtonCustomDisabled.checkbox.is().deselected();
         checkboxAndRadioButtonCustomDisabled.checkbox.label().click();
