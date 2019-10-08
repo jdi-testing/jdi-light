1fa40f700c51667ea2b3f5509e1b65d240e885a2
Dmitrii_Pavlov <delnote@gmail.com>
2019-10-08 10:31:36 +0300

    Custom Radio and checkbox disabled in process.

---
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/CheckboxAndRadioButtonCustomDisabled.java
@@ -0,0 +1,19 @@
+package io.github.com.sections;
+
+import com.epam.jdi.light.elements.composite.Section;
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
+import com.epam.jdi.light.ui.bootstrap.elements.common.Checkbox;
+import com.epam.jdi.light.ui.bootstrap.elements.common.RadioButton;
+
+public class CheckboxAndRadioButtonCustomDisabled extends Section {
+
+    @UI("#customCheckDisabled1-div")
+    public Checkbox checkboxContainer;
+    @UI("#customCheckDisabled1")
+    public Checkbox checkbox;
+    @UI("#customRadioDisabled2-div")
+    public RadioButton radioButtonContainer;
+    @UI("#customRadioDisabled2")
+    public RadioButton radioButton;
+
+}
---
+++ jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/CheckboxAndRadioButtonCustomDisabled.java
@@ -0,0 +1,61 @@
+package io.github.epam.bootstrap.tests.composite.section;
+
+import io.github.epam.TestsInit;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static io.github.com.StaticSite.bsPage;
+import static io.github.com.pages.BootstrapPage.checkboxAndRadioButtonCustomDisabled;
+import static io.github.com.pages.BootstrapPage.checkboxesCustom;
+import static io.github.epam.states.States.shouldBeLoggedIn;
+import static org.hamcrest.CoreMatchers.is;
+
+/**
+ * Created by Dmitrii Pavlov on 08.10.2019
+ * Email: delnote@gmail.com; Skype: Dmitrii Pavlov
+ */
+
+public class CheckboxAndRadioButtonCustomDisabled extends TestsInit {
+
+    @BeforeMethod
+    public void before() {
+        shouldBeLoggedIn();
+        bsPage.shouldBeOpened();
+        checkboxesCustom.hover();
+    }
+
+    @Test
+    public void checkboxIsValidationTests() {
+        checkboxAndRadioButtonCustomDisabled.checkbox
+                .is()
+                .disappear()
+                .disabled()
+                .core()
+                .attr("type", "checkbox")
+                .hasClass("custom-control-input")
+                .tag(is("input"));
+        checkboxAndRadioButtonCustomDisabled.checkbox.label()
+                .is()
+                .displayed()
+                .enabled()
+                .core()
+                .hasClass("custom-control-label")
+                .text(is("Check this custom checkbox"))
+                .tag(is("label"));
+        checkboxAndRadioButtonCustomDisabled.checkboxContainer
+                .is()
+                .displayed()
+                .enabled()
+                .core()
+                .hasClass("custom-control custom-checkbox")
+                .tag(is("div"));
+    }
+
+    @Test
+    public void clickableTests() {
+        checkboxAndRadioButtonCustomDisabled.checkboxContainer.check();
+        checkboxAndRadioButtonCustomDisabled.checkbox.is().deselected();
+        checkboxAndRadioButtonCustomDisabled.checkbox.label().click();
+        checkboxAndRadioButtonCustomDisabled.checkbox.is().deselected();
+    }
+}
