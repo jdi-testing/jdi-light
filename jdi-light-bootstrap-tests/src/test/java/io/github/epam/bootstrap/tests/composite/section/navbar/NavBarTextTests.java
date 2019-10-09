20c9ffca69ceadc8219b0a11b5724d47c3296bc8
Vsevolod_Adrianov <Vsevolod_Adrianov@epam.com>
2019-10-09 12:12:36 +0300

    add NavBar - Text tests verify

--- jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/navbar/NavBarTextTests.java
+++ jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/navbar/NavBarTextTests.java
@@ -1,6 +1,22 @@
 package io.github.epam.bootstrap.tests.composite.section.navbar;
 
 import io.github.epam.TestsInit;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static io.github.com.StaticSite.bsPage;
+import static io.github.epam.states.States.shouldBeLoggedIn;
 
 public class NavBarTextTests extends TestsInit {
+
+    @BeforeMethod
+    public void before() {
+        shouldBeLoggedIn();
+        bsPage.shouldBeOpened();
+    }
+
+    @Test
+    public void verify() {
+
+    }
 }
