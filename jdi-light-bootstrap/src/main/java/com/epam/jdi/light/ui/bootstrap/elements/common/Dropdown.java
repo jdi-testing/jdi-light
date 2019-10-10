5a673707ae65eeb8ed67fde886e146f26d6d48f1
Daniil Kopylov <Daniil.Kopylov@thomsonreuters.com>
2019-10-10 12:12:40 +0300

    add dropdown class

---
+++ jdi-light-bootstrap/src/main/java/com/epam/jdi/light/ui/bootstrap/elements/common/Dropdown.java
@@ -0,0 +1,14 @@
+package com.epam.jdi.light.ui.bootstrap.elements.common;
+
+import com.epam.jdi.light.asserts.generic.UIAssert;
+import com.epam.jdi.light.elements.base.UIBaseElement;
+import com.epam.jdi.light.elements.complex.WebList;
+import com.epam.jdi.light.elements.interfaces.base.IWebBaseElement;
+import com.epam.jdi.light.elements.interfaces.common.IsButton;
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.Css;
+
+public class Dropdown extends UIBaseElement<UIAssert> implements IsButton{
+    @Css(".dropdown-toggle") public Button dropdownToggle;
+    @Css(".dropdown-item") public WebList items;
+
+}
