3b1c2bb42e22a771a95efea0f3c0ba0f4cc52233
dexguitar <stinzane@gmail.com>
2019-10-10 14:55:04 +0300

    almost finished w/NavbarSupportedContentTests

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/navbar/NavbarSupportedContent.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/navbar/NavbarSupportedContent.java
@@ -2,17 +2,28 @@ package io.github.com.sections.navbar;
 
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.JDropdown;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
+import com.epam.jdi.light.ui.bootstrap.elements.common.Button;
 import com.epam.jdi.light.ui.bootstrap.elements.common.Link;
+import com.epam.jdi.light.ui.bootstrap.elements.common.TextField;
 import com.epam.jdi.light.ui.bootstrap.elements.complex.Collapse;
 import com.epam.jdi.light.ui.bootstrap.elements.complex.DropdownExpand;
+import io.github.com.sections.InputGroupInputWithText;
 
 public class NavbarSupportedContent extends Navbar {
 
     @UI(".navbar-brand")
     public Link brand;
 
+    @UI("button[data-toggle=collapse]")
+    public Button navExpand;
+
+    @UI("input")
+    public TextField searchField;
+
+    @UI(".btn-outline-success")
+    public Button searchButton;
+
     @JDropdown(root = ".navbar-nav",
-            expand = "button[data-toggle=collapse]",
             list = "a")
     public Collapse nav;
 
--- jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/navbar/NavbarSupportedContentTests.java
+++ jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/navbar/NavbarSupportedContentTests.java
@@ -4,7 +4,6 @@ import com.epam.jdi.light.driver.WebDriverFactory;
 import com.epam.jdi.light.elements.common.WindowsManager;
 import io.github.epam.TestsInit;
 import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -15,6 +14,7 @@ import static io.github.com.StaticSite.bsPage;
 import static io.github.com.pages.BootstrapPage.navbarSupportedContent;
 import static io.github.epam.states.States.shouldBeLoggedIn;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
@@ -24,9 +24,10 @@ public class NavbarSupportedContentTests extends TestsInit {
     private static final String bootstrapNavPageUrl = "https://getbootstrap.com/docs/4.3/components/navbar/#nav";
     private static final String jdiPageUrl = "https://github.com/jdi-testing/jdi-light/";
     private static final String jdiBootstrapPageUrl = "https://jdi-testing.github.io/jdi-light/bootstrap.html#";
-    private static final String activeLinkText = "Active link\n(current)";
+    private static final String activeLinkText = "Active link";
     private static final String jdiLinkText = "JDI Light";
     private static final String disabledLinkText = "Disabled link";
+    private static final String dropdownLinkText = "Dropdown";
 
     private static final String dropdownAction = "Action";
     private static final String dropdownActionAlert = "Action Clicked!";
@@ -34,6 +35,10 @@ public class NavbarSupportedContentTests extends TestsInit {
     private static final String dropdownAnotherActionAlert = "Another action Clicked!";
     private static final String dropdownSmthElse = "Something else here";
     private static final String dropdownSmthElseAlert = "Something else happened!";
+
+    private static final String searchButtonText = "Search";
+    private static final String searchButtonAlertText = "Search Button Clicked!";
+
 
     @DataProvider
     public Object[][] navbarLinkData() {
@@ -53,6 +58,19 @@ public class NavbarSupportedContentTests extends TestsInit {
         };
     }
 
+    @DataProvider
+    public Object[][] collapseLinkTextData() {
+        return new Object[][] {
+                {activeLinkText},
+                {jdiLinkText},
+                {dropdownLinkText},
+                {dropdownAction},
+                {dropdownAnotherAction},
+                {dropdownSmthElse},
+                {disabledLinkText}
+        };
+    }
+
     @BeforeMethod
     public void before() {
         shouldBeLoggedIn();
@@ -60,7 +78,7 @@ public class NavbarSupportedContentTests extends TestsInit {
     }
 
     @Test
-    public void navbarBrandTest() {
+    public void navbarLinkTest() {
         navbarSupportedContent.brand.is().text(navbarBrandText);
         navbarSupportedContent.brand.click();
 
@@ -71,15 +89,17 @@ public class NavbarSupportedContentTests extends TestsInit {
     }
 
     @Test(dataProvider = "navbarLinkData")
-    public void navLinkTest(String elementName,
+    public void navLinkContentsTest(String elementName,
                                     String elementText,
-                                    String elementUrl) {
+                                    String elementUrl)
+    {
         navbarSupportedContent.nav.highlight();
         navbarSupportedContent.nav.list().get(elementName)
                 .is()
                 .text(elementText)
                 .and()
                 .attr("href", elementUrl);
+        navbarSupportedContent.nav.unhighlight();
     }
 
     @Test(dataProvider = "dropdownData")
@@ -88,16 +108,65 @@ public class NavbarSupportedContentTests extends TestsInit {
 
         navbarSupportedContent.dropdown.toggle();
         navbarSupportedContent.dropdown.list().select(element);
+
         validateAlert(is(alertText));
+
+        navbarSupportedContent.dropdown.unhighlight();
+    }
+
+    @Test(dataProvider = "collapseLinkTextData", priority = 10)
+    public void resizeTest(String linkText) {
+        Dimension dimension = new Dimension(900, 600);
+        WebDriverFactory.getDriver().manage().window().setSize(dimension);
+
+        navbarSupportedContent.navExpand.highlight();
+        navbarSupportedContent.navExpand.click();
+        navbarSupportedContent.navExpand.unhighlight();
+
+        navbarSupportedContent.nav.is().expanded();
+
+        assertTrue(navbarSupportedContent.nav.list().values().contains(linkText));
+    }
+
+    @Test(priority = 11)
+    public void resizeLinkTest() {
+        Dimension dimension = new Dimension(900, 600);
+        WebDriverFactory.getDriver().manage().window().setSize(dimension);
+
+        navbarSupportedContent.navExpand.highlight();
+        navbarSupportedContent.navExpand.click();
+        navbarSupportedContent.navExpand.unhighlight();
+
+        navbarSupportedContent.nav.is().expanded();
+
+        navbarSupportedContent.nav.list().select("Active link");
+        WindowsManager.switchToWindow(2);
+        assertEquals(getUrl(), bootstrapNavPageUrl);
+        WindowsManager.closeWindow();
+        WindowsManager.switchToWindow(1);
+
+        navbarSupportedContent.nav.list().select("JDI Light");
+        WindowsManager.switchToWindow(2);
+        assertEquals(getUrl(), jdiPageUrl);
+        WindowsManager.closeWindow();
+        WindowsManager.switchToWindow(1);
     }
 
     @Test
-    public void resizeTest() {
-        Dimension dimension = new Dimension(900, 600);
-        WebDriverFactory.getDriver().manage().window().setSize(dimension);
-        navbarSupportedContent.nav.highlight();
-        navbarSupportedContent.nav.toggle();
-        assertTrue(navbarSupportedContent.nav.isExpanded());
+    public void searchFormTest() {
+        navbarSupportedContent.searchField.highlight();
+        navbarSupportedContent.searchField.setValue("JDI Light");
+        assertThat(navbarSupportedContent.searchField.getValue(), is("JDI Light"));
+        navbarSupportedContent.searchField.unhighlight();
+    }
+
+    @Test
+    public void searchButtonTest() {
+        navbarSupportedContent.searchButton.highlight();
+        navbarSupportedContent.searchButton.is().text(searchButtonText);
+        navbarSupportedContent.searchButton.click();
+        validateAlert(is(searchButtonAlertText));
+        navbarSupportedContent.searchButton.unhighlight();
     }
 
 }
