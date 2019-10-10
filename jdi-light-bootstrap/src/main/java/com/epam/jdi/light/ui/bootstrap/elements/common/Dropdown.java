64697743d53e79579e114ee7075a1312b349fdb1
Daniil Kopylov <Daniil.Kopylov@thomsonreuters.com>
2019-10-10 12:59:19 +0300

    Update Dropdown.java

--- jdi-light-bootstrap/src/main/java/com/epam/jdi/light/ui/bootstrap/elements/common/Dropdown.java
+++ jdi-light-bootstrap/src/main/java/com/epam/jdi/light/ui/bootstrap/elements/common/Dropdown.java
@@ -4,7 +4,6 @@ import com.epam.jdi.light.asserts.generic.UIAssert;
 import com.epam.jdi.light.elements.base.UIBaseElement;
 import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.light.elements.complex.WebList;
-import com.epam.jdi.light.elements.interfaces.base.IWebBaseElement;
 import com.epam.jdi.light.elements.interfaces.common.IsButton;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.Css;
 
