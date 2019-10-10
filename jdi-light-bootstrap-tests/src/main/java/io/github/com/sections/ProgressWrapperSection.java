a96f8e470920aa9e2d4b686bcf51cf144a7402e1
Beata Chugunova <Beata_Chugunova@epam.com>
2019-10-10 12:52:17 +0300

    ProgressWrapperSection extends Section

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/ProgressWrapperSection.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/ProgressWrapperSection.java
@@ -1,11 +1,12 @@
 package io.github.com.sections;
 
+import com.epam.jdi.light.elements.composite.Section;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.ui.bootstrap.elements.complex.ProgressBarWrapper;
 
 import java.util.List;
 
-public class ProgressWrapperSection {
+public class ProgressWrapperSection extends Section {
 
     @UI(".progress")
     private List<ProgressBarWrapper> progressBarWrappersList;
