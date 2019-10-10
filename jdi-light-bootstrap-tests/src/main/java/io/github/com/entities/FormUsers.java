cb00bd788c6e4c32e94f7c8b70b0192d77fd3ad8
Dmitrii_Pavlov <delnote@gmail.com>
2019-10-10 17:44:36 +0300

    Forms in process.

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/entities/FormUsers.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/entities/FormUsers.java
@@ -15,7 +15,21 @@ public class FormUsers {
         });
     }
 
+    private static FormContacts defaultCheckContact() {
+        return new FormContacts().set(c -> {
+            c.name = "Mark";
+            c.lastName = "Otto";
+            c.userName = "";
+            c.city = "";
+            c.state = "";
+            c.zip = "";
+            c.accept = "false";
+        });
+    }
+
     public static FormContacts DEFAULT_CONTACT = defaultContact();
+
+    public static FormContacts DEFAULT_CHECK = defaultCheckContact();
 
     public static FormContacts LOWER_CASE_NAME_CONTACT =
             defaultContact().set(c -> c.name = c.name.toLowerCase());
---
+++ jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/form/FormCustomStyleTests.java
@@ -0,0 +1,178 @@
+package io.github.epam.bootstrap.tests.composite.section.form;
+
+import com.epam.jdi.light.ui.bootstrap.elements.common.TextField;
+import io.github.epam.TestsInit;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static com.epam.jdi.light.elements.common.Alerts.validateAlert;
+import static io.github.com.StaticSite.bsPage;
+import static io.github.com.entities.FormUsers.*;
+import static io.github.com.pages.BootstrapPage.formCustomStyles;
+import static io.github.epam.states.States.shouldBeLoggedIn;
+import static org.hamcrest.Matchers.is;
+import static org.testng.Assert.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
+
+public class FormCustomStyleTests extends TestsInit {
+
+    @BeforeMethod
+    public void before() {
+        shouldBeLoggedIn();
+        bsPage.shouldBeOpened();
+    }
+
+    @DataProvider
+    public Object[][] listData() {
+        return new Object[][]{
+                {formCustomStyles.name, "First name", "Mark"},
+                {formCustomStyles.lastName, "Last name", "Otto"},
+                {formCustomStyles.userName, "Username", ""},
+                {formCustomStyles.city, "City", ""},
+                {formCustomStyles.state, "State", ""},
+                {formCustomStyles.zip, "Zip", ""}
+        };
+    }
+
+    @Test(dataProvider = "listData")
+    public void isValidationTests(TextField element, String label, String value) {
+        element
+                .is()
+                .displayed()
+                .enabled()
+                .core()
+                .value(value)
+                .hasClass("form-control")
+                .attr("type", "text")
+                .attr("placeholder", label)
+                .tag(is("input"));
+        element.label()
+                .is()
+                .displayed()
+                .enabled()
+                .core()
+                .text(is(label));
+    }
+
+    @Test
+    public void fillTest() {
+        formCustomStyles.fill(DEFAULT_CONTACT);
+        formCustomStyles.check(DEFAULT_CONTACT);
+        checkContactFormSubmitted();
+    }
+
+    @Test
+    public void submitTextToContactFormTest() {
+        formCustomStyles.submit("Roman");
+        formCustomStyles.check(ONLY_NAME_FILLED_DEFAULT_CONTACT);
+    }
+
+    @Test
+    public void submitEntityToContactFormTest() {
+        formCustomStyles.submit(DEFAULT_CONTACT);
+        formCustomStyles.check(DEFAULT_CHECK);
+    }
+
+    @Test
+    public void submitTextToContactFormUsingCustomButtonTest() {
+        formCustomStyles.submit("Roman", "submit");
+        formCustomStyles.check(ONLY_NAME_FILLED_DEFAULT_CONTACT);
+    }
+
+    @Test
+    public void verifyMethodPositiveTest() {
+        formCustomStyles.fill(DEFAULT_CONTACT);
+        List<String> verified = formCustomStyles.verify(DEFAULT_CONTACT);
+        assertEquals(verified.size(), 0);
+    }
+
+    @Test
+    public void verifyMethodNegativeTest() {
+        formCustomStyles.fill(ALL_EXCEPT_NAME_FILLED_DEFAULT_CONTACT);
+        List<String> verified = formCustomStyles.verify(DEFAULT_CONTACT);
+        assertEquals(verified.size(), 1);
+        assertTrue(verified.get(0).contains("Mark"));
+    }
+
+    @Test
+    public void sendMethodTest() {
+        formCustomStyles.fill(DEFAULT_CONTACT);
+        formCustomStyles.send();
+        formCustomStyles.check(DEFAULT_CHECK);
+    }
+
+    @Test
+    public void sendEntityMethodTest() {
+        formCustomStyles.send(DEFAULT_CONTACT);
+        formCustomStyles.check(DEFAULT_CHECK);
+    }
+
+    @Test
+    public void addEntityMethodTest() {
+        formCustomStyles.add(DEFAULT_CONTACT);
+        formCustomStyles.check(DEFAULT_CHECK);
+    }
+
+    @Test
+    public void publishMethodTest() {
+        formCustomStyles.publish(DEFAULT_CONTACT);
+        formCustomStyles.check(DEFAULT_CHECK);
+    }
+
+    @Test
+    public void saveMethodTest() {
+        ;
+        formCustomStyles.save(DEFAULT_CONTACT);
+        formCustomStyles.check(DEFAULT_CHECK);
+    }
+
+    @Test
+    public void updateMethodTest() {
+        formCustomStyles.update(DEFAULT_CONTACT);
+        formCustomStyles.check(DEFAULT_CHECK);
+    }
+
+    @Test
+    public void cancelMethodTest() {
+        formCustomStyles.cancel(DEFAULT_CONTACT);
+        formCustomStyles.check(DEFAULT_CHECK);
+    }
+
+    @Test
+    public void closeMethodTest() {
+        formCustomStyles.close(DEFAULT_CONTACT);
+        formCustomStyles.check(DEFAULT_CHECK);
+    }
+
+    @Test
+    public void backMethodTest() {
+        formCustomStyles.back(DEFAULT_CONTACT);
+        formCustomStyles.check(DEFAULT_CHECK);
+    }
+
+    @Test
+    public void selectMethodTest() {
+        formCustomStyles.select(DEFAULT_CONTACT);
+        formCustomStyles.check(DEFAULT_CHECK);
+    }
+
+    @Test
+    public void nextMethodTest() {
+        formCustomStyles.next(DEFAULT_CONTACT);
+        formCustomStyles.check(DEFAULT_CHECK);
+    }
+
+    @Test
+    public void searchMethodTest() {
+        formCustomStyles.search(DEFAULT_CONTACT);
+        formCustomStyles.check(DEFAULT_CHECK);
+    }
+
+    private void checkContactFormSubmitted() {
+        formCustomStyles.submit();
+        validateAlert(is("Form filled and submitted successfully"));
+    }
+}
