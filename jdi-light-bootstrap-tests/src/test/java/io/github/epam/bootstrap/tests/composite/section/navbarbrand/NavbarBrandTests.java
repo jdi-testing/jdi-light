47a77439be064429d6a1fd00ab6bb88347737cd7
Aleksandr_Shiganov <aleksandr.shiganov@epam.com>
2019-10-10 21:31:20 +0300

    navbar brand tests

--- jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/navbarbrand/NavbarBrandTests.java
+++ jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/navbarbrand/NavbarBrandTests.java
@@ -1,40 +1,30 @@
 package io.github.epam.bootstrap.tests.composite.section.navbarbrand;
 
 import com.epam.jdi.light.driver.WebDriverFactory;
-import com.epam.jdi.light.elements.complex.JList;
 import com.epam.jdi.light.ui.bootstrap.elements.common.NavbarBrand;
 import io.github.epam.TestsInit;
 import io.github.epam.bootstrap.tests.BaseValidations;
+import org.hamcrest.Matcher;
+import org.hamcrest.core.StringContains;
+import org.hamcrest.core.SubstringMatcher;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
-
 import static com.epam.jdi.light.elements.composite.WebPage.getUrl;
 import static io.github.com.StaticSite.bsPage;
 import static io.github.com.pages.BootstrapPage.navbarSection;
-import static io.github.epam.bootstrap.tests.BaseValidations.baseValidation;
 import static io.github.epam.states.States.shouldBeLoggedIn;
+import static org.hamcrest.core.StringContains.containsString;
 import static org.testng.Assert.assertEquals;
-
-
-/**
- * Created by Aleksandr Shiganov on 3.10.2019
- * Email: aleksandr_shiganov@epam.com;
- */
 
 public class NavbarBrandTests extends TestsInit {
 
-    private String navbarUrl = "https://getbootstrap.com/docs/4.3/components/navbar/#nav";
     private List<NavbarBrand> navbarBrandList;
-
-
-
+    private String imgPath = "images/wolverin.jpg";
+    private String navbarUrl = "https://getbootstrap.com/docs/4.3/components/navbar/#nav";
 
     @BeforeMethod
     public void before() {
@@ -49,60 +39,62 @@ public class NavbarBrandTests extends TestsInit {
     @DataProvider
     public static Object[][] navbarData() {
         return new Object[][]{
-                {"striped_ordinary", "10", "rgba(0, 123, 255, 1)", "0", "100", "progress-bar-striped"},
-                {"striped_success", "25", "rgba(40, 167, 69, 1)", "0", "100", "progress-bar-striped"},
-                {"striped_info", "50", "rgba(23, 162, 184, 1)", "0", "100", "progress-bar-striped"},
-                {"striped_warning", "75", "rgba(255, 193, 7, 1)", "0", "100", "progress-bar-striped"},
-                {"striped_danger", "100", "rgba(220, 53, 69, 1)", "0", "100", "progress-bar-striped"}
+                {"navbar-link", "Brand link"},
+                {"navbar-heading", "Brand heading"},
+                {"nav-bar-as-image", null},
+                {"brand-as-image-and-link", "Brand link"},
         };
-    }
-/*
-    @Test(dataProvider = "navbarData")
-    public void checkProgressData(String progressId, String value, String color,
-                                  String min, String max, String classStriped) {
-
-        List<NavbarBrand> ln = navbarSection.navbarBrandLinkJList.stream().filter(navbarBrand ->
-                        navbarBrand.hasAttribute("href")).collect(Collectors.toList());
-
-    }*/
-
-    @Test(dataProvider = "progressData")
-    public void checkProgressData(String progressId, String value, String color,
-                                  String min, String max, String classStriped) {
     }
 
     @Test
-    public void checkLink() {
-        navbarSection.navbarBrandJList.stream().filter(navbarBrand ->
-                navbarBrand.hasAttribute("href")).forEach(nbb -> {
+    public void checkNavbarClickLink() {
+        navbarBrandList.stream().filter(NavbarBrand::isLink).forEach(nbb -> {
             nbb.click();
-            ArrayList<String> tabs = new ArrayList<>(WebDriverFactory.getDriver().getWindowHandles());
             WebDriver driver = WebDriverFactory.getDriver();
+            ArrayList<String> tabs = new ArrayList<>(WebDriverFactory.getDriver().getWindowHandles());
             driver.switchTo().window(tabs.get(tabs.size() - 1));
             assertEquals(getUrl(), navbarUrl);
             driver.close();
             driver.switchTo().window(tabs.get(tabs.size() - 2));
-            bsPage.checkOpened();
+        });
+    }
+
+    @Test(dataProvider = "navbarData")
+    public void checkNavbarText(String navbarId, String navbarText) {
+        navbarBrandList.stream().filter(navbarBrand ->
+                navbarBrand.attr("id").equals(navbarId)).forEach(nbb -> {
+                                            nbb.highlight();
+                                            nbb.core().is().text(navbarText);
+                                            nbb.core().is().attr("href", navbarUrl);
+                                            nbb.unhighlight();
         });
     }
 
     @Test
-    public void baseValidationTest() {
-        for (int i = 0; i < navbarSection.navbarBrandJList.size(); i++)
-            baseValidation(navbarSection.navbarBrandJList.get(i));
-        //This is a BUG. Start take elements from get(1) instead of
-        /*navbarSection.navbarBrandJList.forEach(BaseValidations::baseValidation);*/
+    public void checkNavbarClickImage() {
+        navbarBrandList.stream()
+                       .filter(nbb -> nbb.isLink() && nbb.childs().size() > 0)
+                       .map(nbbWithIm -> nbbWithIm.childs().get(0))
+                       .forEach( imgFromNavbar -> {
+
+                        imgFromNavbar.highlight("blue");
+                        imgFromNavbar.is().attr("src", containsString(imgPath))
+                                          .tag("img");
+                        imgFromNavbar.unhighlight();
+
+                        imgFromNavbar.click();
+                        WebDriver driver = WebDriverFactory.getDriver();
+                        ArrayList<String> tabs = new ArrayList<>(WebDriverFactory.getDriver().getWindowHandles());
+                        driver.switchTo().window(tabs.get(tabs.size() - 1));
+                        assertEquals(getUrl(), navbarUrl);
+                        driver.close();
+                        driver.switchTo().window(tabs.get(tabs.size() - 2));
+        });
     }
 
     @Test
-    public void baseValidationStreamTest1() {
-        //This is a BUG. Start take elements from get(1) instead of
+    public void baseValidationStreamTest() {
         navbarBrandList.forEach(BaseValidations::baseValidation);
-    }
-
-    @Test
-    public void baseValidationStreamTest2() {
-        //This is a BUG. Start take elements from get(1) instead of
-        navbarSection.navbarBrandJList.forEach(BaseValidations::baseValidation);
+        navbarBrandList.forEach(NavbarBrand::unhighlight);
     }
 }
--- jdi-light-bootstrap/src/main/java/com/epam/jdi/light/ui/bootstrap/elements/common/NavbarBrand.java
+++ jdi-light-bootstrap/src/main/java/com/epam/jdi/light/ui/bootstrap/elements/common/NavbarBrand.java
@@ -1,14 +1,27 @@
 package com.epam.jdi.light.ui.bootstrap.elements.common;
 
 import com.epam.jdi.light.asserts.generic.UIAssert;
+import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.elements.base.UIBaseElement;
 import com.epam.jdi.light.elements.interfaces.base.IListBase;
-import com.epam.jdi.light.elements.interfaces.composite.PageObject;
 import com.epam.jdi.light.ui.bootstrap.interfaces.IsNavbarBrand;
 
 import static com.epam.jdi.light.elements.init.PageFactory.initElements;
+import static com.epam.jdi.light.logger.LogLevels.DEBUG;
+
 
 public class NavbarBrand extends UIBaseElement<UIAssert> implements IsNavbarBrand, IListBase {
+
+    @JDIAction(value = "Get '{name}' text", level = DEBUG)
+    public String getRef() { return uiElement.attr("href"); }
+
+    @JDIAction(value = "Get '{name}' image alt", level = DEBUG)
+    public String getAlt() { return uiElement.attr("alt"); }
+
+    public boolean isLink() {
+        return hasAttribute("href");
+    };
+
     public boolean isNavbarBrand() {
         return hasClass(".navbar-brand");
     }
