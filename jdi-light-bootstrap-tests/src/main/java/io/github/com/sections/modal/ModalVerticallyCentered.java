f314954e3879b6131484a7468f15f1616634f4a4
dexguitar <stinzane@gmail.com>
2019-10-13 14:31:15 +0300

    added tests to ModalVerticallyCenteredTests

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/modal/ModalVerticallyCentered.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/modal/ModalVerticallyCentered.java
@@ -11,4 +11,22 @@ public class ModalVerticallyCentered extends Modal {
     @UI("button:nth-of-type(2)")
     public Button modalCenterScrollableTrigger;
 
+    @UI("#exModalCenter")
+    public Modal modalCenterBg;
+
+    @UI("#exampleModalCenteredScrollable")
+    public Modal modalCenterScrollableBg;
+
+    @UI("#modal-vertical-content-1")
+    public Modal modalContent1;
+
+    @UI("#modal-vertical-content-2")
+    public Modal modalContent2;
+
+    @UI("//*[@id='modal-vertical-content-1']//button[contains(., 'Close')]")
+    public Button dismissModal1Trigger;
+
+    @UI("//*[@id='modal-vertical-content-2']//button[contains(., 'Close')]")
+    public Button dismissModal2Trigger;
+
 }
--- jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/modal/ModalVerticallyCenteredTests.java
+++ jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/modal/ModalVerticallyCenteredTests.java
@@ -1,7 +1,14 @@
 package io.github.epam.bootstrap.tests.composite.section.modal;
 
+import com.epam.jdi.light.driver.WebDriverFactory;
+import com.epam.jdi.light.ui.bootstrap.elements.common.Button;
+import io.github.com.sections.modal.Modal;
 import io.github.epam.TestsInit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static io.github.com.StaticSite.bsPage;
@@ -10,21 +17,95 @@ import static io.github.epam.states.States.shouldBeLoggedIn;
 
 public class ModalVerticallyCenteredTests extends TestsInit {
 
+    @DataProvider
+    public Object[][] modalContentData() {
+        return new Object[][]{
+                {
+                        modalVerticallyCentered.modalCenterTrigger,
+                        modalVerticallyCentered.dismissModal1Trigger,
+                        modalVerticallyCentered.modalContent1,
+                        "modal-vertical-content-1"
+                },
+
+                {
+                        modalVerticallyCentered.modalCenterScrollableTrigger,
+                        modalVerticallyCentered.dismissModal2Trigger,
+                        modalVerticallyCentered.modalContent2,
+                        "modal-vertical-content-2"
+                }
+        };
+    }
+
+    @DataProvider
+    public Object[][] modalCssData() {
+        return new Object[][]{
+                {
+                        modalVerticallyCentered.modalCenterTrigger,
+                        modalVerticallyCentered.dismissModal1Trigger,
+                        modalVerticallyCentered.modalContent1,
+                        "modal-vertical-content-1",
+                        "modal-dialog-centered"
+                },
+
+                {
+                        modalVerticallyCentered.modalCenterScrollableTrigger,
+                        modalVerticallyCentered.dismissModal2Trigger,
+                        modalVerticallyCentered.modalCenterScrollableBg,
+                        "modal-vertical-content-2",
+                        "modal-dialog-scrollable"
+                }
+        };
+    }
+
     @BeforeMethod
     public void before() {
         shouldBeLoggedIn();
         bsPage.shouldBeOpened();
     }
 
-    @Test
-    public void simple() throws InterruptedException {
-        modalVerticallyCentered.modalCenterTrigger.highlight();
-        Thread.sleep(1000);
-        modalVerticallyCentered.modalCenterTrigger.unhighlight();
+    @Test(dataProvider = "modalContentData")
+    public void modalDisplayedHiddenTest(Button showButton,
+                                         Button dismissButton,
+                                         Modal modal,
+                                         String modalId)
+    {
+        WebDriverWait wait = new WebDriverWait(WebDriverFactory.getDriver(), 5);
 
-        modalVerticallyCentered.modalCenterScrollableTrigger.highlight();
-        Thread.sleep(1000);
-        modalVerticallyCentered.modalCenterTrigger.unhighlight();
+        showButton.show();
+        showButton.click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(modalId)));
+
+        modal.is().displayed();
+
+        dismissButton.show();
+        dismissButton.click();
+
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id(modalId)));
+
+        modal.is().hidden();
+    }
+
+    @Test(dataProvider = "modalCssData")
+    public void modalCssTest(Button showButton,
+                             Button dismissButton,
+                             Modal modal,
+                             String modalCss,
+                             String modalBgCss)
+    {
+        WebDriverWait wait = new WebDriverWait(WebDriverFactory.getDriver(), 5);
+
+        showButton.show();
+        showButton.click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(modalCss)));
+
+        modal.core().hasClass(modalBgCss);
+
+        dismissButton.show();
+        dismissButton.click();
+
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id(modalCss)));
     }
 
 }
