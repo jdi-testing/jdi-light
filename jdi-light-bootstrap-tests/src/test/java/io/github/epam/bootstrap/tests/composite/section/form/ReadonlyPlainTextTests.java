7a6286e7a09808b99e74fb302cf80f3690817bf6
BeataChu <beataslav@gmail.com>
2019-10-15 10:54:55 +0300

    Interface implementation and labelTest added
    Tests for rpt2 added

--- jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/form/ReadonlyPlainTextTests.java
+++ jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/form/ReadonlyPlainTextTests.java
@@ -10,6 +10,7 @@ import static io.github.epam.bootstrap.tests.BaseValidations.baseValidation;
 import static io.github.epam.states.States.shouldBeLoggedIn;
 import static io.github.com.pages.BootstrapPage.readonlyPlainText1;
 import static io.github.com.pages.BootstrapPage.readonlyPlainText2;
+import static org.hamcrest.core.StringContains.containsString;
 import static org.testng.Assert.assertEquals;
 
 public class ReadonlyPlainTextTests extends TestsInit {
@@ -21,14 +22,25 @@ public class ReadonlyPlainTextTests extends TestsInit {
     }
 
     String plainTextValue = "email@example.com";
+    String labelTextValue = "Email";
+
+    @Test
+    public void baseValidationTest(){
+        baseValidation(readonlyPlainText1);
+        readonlyPlainText1.unhighlight();
+        baseValidation(readonlyPlainText2);
+        readonlyPlainText2.unhighlight();
+    }
 
     @Test
     public void isValidationTest() {
-        baseValidation(readonlyPlainText1);
-        readonlyPlainText1.unhighlight();
         readonlyPlainText1.hasClass("form-control-plaintext");
         readonlyPlainText1.hasAttribute("readonly");
         assertEquals(readonlyPlainText1.attr("type"), "text");
+
+        readonlyPlainText2.hasClass("form-control-plaintext");
+        readonlyPlainText2.hasAttribute("readonly");
+        assertEquals(readonlyPlainText2.attr("type"), "text");
         }
 
         @Test
@@ -42,4 +54,15 @@ public class ReadonlyPlainTextTests extends TestsInit {
         assertEquals(readonlyPlainText1.getValue(), plainTextValue);
         assertEquals(readonlyPlainText2.getValue(), plainTextValue);
         }
+
+        @Test
+    public void labelTest(){
+        assertEquals(readonlyPlainText1.labelText(), labelTextValue);
+            readonlyPlainText1.label().is().text(containsString(labelTextValue));
+            assertEquals(readonlyPlainText2.labelText(), labelTextValue);
+            readonlyPlainText2.label().is().text(containsString(labelTextValue));
+        }
+
+
+
 }
