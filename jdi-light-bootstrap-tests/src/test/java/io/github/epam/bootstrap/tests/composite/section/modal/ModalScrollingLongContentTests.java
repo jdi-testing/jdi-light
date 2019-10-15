0dcfb55ad4475a816892d6484f67721e1305dfeb
Roman_Bordiugov <rome-0@yandex.ru>
2019-10-15 18:51:49 +0300

    Tests template

--- jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/modal/ModalScrollingLongContentTests.java
+++ jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/modal/ModalScrollingLongContentTests.java
@@ -1,6 +1,18 @@
 package io.github.epam.bootstrap.tests.composite.section.modal;
 
 import io.github.epam.TestsInit;
+import org.testng.annotations.BeforeMethod;
+
+import static io.github.com.StaticSite.bsPage;
+import static io.github.epam.states.States.shouldBeLoggedIn;
 
 public class ModalScrollingLongContentTests extends TestsInit {
+
+    @BeforeMethod
+    public void before() {
+        shouldBeLoggedIn();
+        bsPage.shouldBeOpened();
+    }
+
+
 }
