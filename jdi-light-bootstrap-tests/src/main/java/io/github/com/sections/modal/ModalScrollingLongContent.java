17796b2ce52559252597a9f9e7fbcf60159b177d
Roman_Bordiugov <rome-0@yandex.ru>
2019-10-16 14:28:53 +0300

    Tests added

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/modal/ModalScrollingLongContent.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/modal/ModalScrollingLongContent.java
@@ -1,19 +1,21 @@
 package io.github.com.sections.modal;
 
+import com.epam.jdi.light.elements.composite.Section;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.ui.bootstrap.elements.common.Button;
 import com.epam.jdi.light.ui.bootstrap.elements.complex.Modal;
 
-public class ModalScrollingLongContent extends Modal {
+public class ModalScrollingLongContent extends Section {
 
-    @UI("button:nth-of-type(1)")
-    public Button longScroll;
-    @UI("button:nth-of-type(2)")
-    public Button longScrollable;
+    @UI("div:nth-child(2) > button:nth-child(1)")
+    public Button buttonLongScroll;
+    @UI("div:nth-child(4) > button:nth-child(1)")
+    public Button buttonLongScrollable;
 
     @UI("#exampleModalLong")
     public Modal modalLong;
     @UI("#exampleModalScrollable")
     public Modal modalScrollable;
-
+    @UI("#exampleModalLong div.modal-header")
+    public Section header;
 }
--- jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/modal/ModalScrollingLongContentTests.java
+++ jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/modal/ModalScrollingLongContentTests.java
@@ -1,9 +1,15 @@
 package io.github.epam.bootstrap.tests.composite.section.modal;
 
+import com.epam.jdi.light.ui.bootstrap.elements.common.Button;
+import com.epam.jdi.light.ui.bootstrap.elements.complex.Modal;
 import io.github.epam.TestsInit;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import static io.github.com.StaticSite.bsPage;
+import static io.github.com.pages.BootstrapPage.modalScrollingLongContent;
+import static io.github.epam.bootstrap.tests.BaseValidations.baseValidation;
 import static io.github.epam.states.States.shouldBeLoggedIn;
 
 public class ModalScrollingLongContentTests extends TestsInit {
@@ -14,5 +20,37 @@ public class ModalScrollingLongContentTests extends TestsInit {
         bsPage.shouldBeOpened();
     }
 
+    @DataProvider
+    public Object[][] listData() {
+        return new Object[][]{
+                {modalScrollingLongContent.buttonLongScroll, modalScrollingLongContent.modalLong},
+                {modalScrollingLongContent.buttonLongScrollable, modalScrollingLongContent.modalScrollable}
+        };
+    }
 
+    @Test
+    public void baseValidationTest() {
+        baseValidation(modalScrollingLongContent);
+        baseValidation(modalScrollingLongContent.buttonLongScroll);
+        baseValidation(modalScrollingLongContent.buttonLongScrollable);
+    }
+
+    @Test(dataProvider = "listData")
+    public void isValidationTest(Button showModal, Modal modal) {
+        showModal.click();
+        modal.is().displayed();
+        modal.title.is().text("MODAL TITLE");
+        modal.is()
+                .displayed()
+                .enabled()
+                .core()
+                .hasClass("modal")
+                .hasClass("show")
+                .attr("role", "dialog")
+                .attr("aria-modal", "true")
+                .tag("div");
+        modal.button.click();
+        modal.core().waitSec(2);
+        modal.is().disappear();
+    }
 }
