82a3e98ec3eb56fa8abb91ffbe08a79448153c11
Kseniia Semenova1 <kseniia_semenova1@epam.com>
2019-10-16 17:33:09 +0300

    created class NavbarPlacementTests and created first test

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/pages/BootstrapPage.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/pages/BootstrapPage.java
@@ -106,11 +106,7 @@ import io.github.com.sections.card.CardImageOverlays;
 import io.github.com.sections.card.CardUtilities;
 import io.github.com.sections.card.CardWithGridMarkup;
 import io.github.com.sections.card.CardWithHeaderAndFooter;
-import io.github.com.sections.navbar.NavbarColorScheme;
-import io.github.com.sections.navbar.NavbarForm;
-import io.github.com.sections.navbar.NavbarSupportedContent;
-import io.github.com.sections.navbar.NavbarText;
-import io.github.com.sections.navbar.NavbarWithInputGroupForm;
+import io.github.com.sections.navbar.*;
 import io.github.com.sections.spinner.GrowingSpinners;
 import io.github.com.sections.spinner.SpinnerAlignment;
 import io.github.com.sections.spinner.SpinnerSize;
@@ -561,5 +557,9 @@ public class BootstrapPage extends WebPage {
     public static NavbarExternalContent navbarExternalContent;
     @UI("#navbar-supported-content")
     public static NavbarSupportedContent navbarSupportedContent;
+    @UI("#navbar-placement-default")
+    public static NavbarPlacement navbarPlacementDefault;
+    @UI("#navbar-placement-sticky-top")
+    public static NavbarPlacement navbarPlacementStickyTop;
     //endregion
 }
--- jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/navbar/NavbarPlacement.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/navbar/NavbarPlacement.java
@@ -3,4 +3,6 @@ package io.github.com.sections.navbar;
 import com.epam.jdi.light.elements.composite.Section;
 
 public class NavbarPlacement extends Section {
+
+
 }
