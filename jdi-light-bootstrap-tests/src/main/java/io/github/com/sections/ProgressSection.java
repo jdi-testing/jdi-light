9baaaf03526a83ad23b0453ad4a81391eef0dc90
BeataChu <beataslav@gmail.com>
2019-10-16 16:31:35 +0300

    Initial commit

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/ProgressSection.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/ProgressSection.java
@@ -1,7 +1,9 @@
 package io.github.com.sections;
 
+import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.elements.composite.Section;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
+import com.epam.jdi.light.logger.LogLevels;
 import com.epam.jdi.light.ui.bootstrap.elements.common.Progress;
 
 import java.util.regex.Matcher;
@@ -14,6 +16,7 @@ public class ProgressSection extends Section {
     @UI(".progress-bar")
     public Progress progress;
 
+    @JDIAction(value = "Get '{name}' declared height in pixels", level = LogLevels.DEBUG)
     public int getProgressSectionHeightValueInPx() {
         String textHeight = uiElement.getAttribute("style");
         Pattern pattern = Pattern.compile("\\d+");
