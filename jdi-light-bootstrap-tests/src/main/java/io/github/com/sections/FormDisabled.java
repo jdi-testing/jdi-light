92dc9b7a98fc8ead55327a33bed75295239bf6ab
Dmitrii_Pavlov <delnote@gmail.com>
2019-10-18 10:34:52 +0300

    Form horizontal in progress.

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/FormDisabled.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/FormDisabled.java
@@ -1,6 +1,21 @@
 package io.github.com.sections;
 
+import com.epam.jdi.light.elements.complex.Selector;
 import com.epam.jdi.light.elements.composite.Section;
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
+import com.epam.jdi.light.ui.bootstrap.elements.common.Button;
+import com.epam.jdi.light.ui.bootstrap.elements.common.Checkbox;
+import com.epam.jdi.light.ui.bootstrap.elements.common.TextField;
 
 public class FormDisabled extends Section {
+
+    @UI("#disabledTextInput")
+    public TextField textField;
+    @UI("#disabledSelect")
+    public Selector selector;
+    @UI("#disabledFieldsetCheck")
+    public Checkbox checkbox;
+    @UI("button")
+    public Button submit;
+
 }
--- jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/FormHelpText.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/FormHelpText.java
@@ -1,6 +1,19 @@
 package io.github.com.sections;
 
 import com.epam.jdi.light.elements.composite.Section;
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
+import com.epam.jdi.light.ui.bootstrap.elements.common.Text;
+import com.epam.jdi.light.ui.bootstrap.elements.common.TextField;
 
 public class FormHelpText extends Section {
+
+    @UI("#inputPassword5")
+    public TextField passwordOne;
+    @UI("#inputPassword6")
+    public TextField passwordTwo;
+    @UI("//input[@id='inputPassword5']/../small")
+    public Text passwordOneHelpText;
+    @UI("//input[@id='inputPassword6']/../small")
+    public Text passwordTwoHelpText;
+
 }
