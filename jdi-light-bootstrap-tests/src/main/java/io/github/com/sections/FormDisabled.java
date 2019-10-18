e5b87a020e961f29ea00ea651efab9866302a274
Dmitrii_Pavlov <delnote@gmail.com>
2019-10-18 10:54:56 +0300

    Form horizontal done.

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/FormDisabled.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/FormDisabled.java
@@ -6,9 +6,12 @@ import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.ui.bootstrap.elements.common.Button;
 import com.epam.jdi.light.ui.bootstrap.elements.common.Checkbox;
 import com.epam.jdi.light.ui.bootstrap.elements.common.TextField;
+import com.epam.jdi.light.ui.bootstrap.elements.complex.MediaObject;
 
 public class FormDisabled extends Section {
 
+    @UI("fieldset")
+    public MediaObject formContainer;
     @UI("#disabledTextInput")
     public TextField textField;
     @UI("#disabledSelect")
