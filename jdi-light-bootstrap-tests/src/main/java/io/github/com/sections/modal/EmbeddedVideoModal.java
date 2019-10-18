5e04592e5694c524617382b3e98b470e514a2be2
Andrei_Ulasen <Andrei_Ulasen@epam.com>
2019-10-18 18:00:56 +0300

    Formatting fixed

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/modal/EmbeddedVideoModal.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/modal/EmbeddedVideoModal.java
@@ -1,15 +1,7 @@
 package io.github.com.sections.modal;
 
-import com.epam.jdi.light.elements.common.UIElement;
-import com.epam.jdi.light.elements.composite.Section;
-import com.epam.jdi.light.elements.pageobjects.annotations.Frame;
-import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
-import com.epam.jdi.light.ui.bootstrap.elements.common.Button;
-import com.epam.jdi.light.ui.bootstrap.elements.common.Link;
 import com.epam.jdi.light.ui.bootstrap.elements.composite.Modal;
 
 public class EmbeddedVideoModal extends Modal {
-
-
 
 }
