9608c45ba7f0affb8971a1c04e49216cadbf798c
Vsevolod_Adrianov <Vsevolod_Adrianov@epam.com>
2019-10-21 13:44:43 +0300

    clear modal body

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/modal/ModalTooltipsAndPopoversDialog.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/modal/ModalTooltipsAndPopoversDialog.java
@@ -2,14 +2,11 @@ package io.github.com.sections.modal;
 
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.ui.bootstrap.elements.common.Button;
-import com.epam.jdi.light.ui.bootstrap.elements.common.Text;
 import com.epam.jdi.light.ui.bootstrap.elements.composite.Modal;
 
 public class ModalTooltipsAndPopoversDialog extends Modal {
-    @UI(".modal-body") public ModalTooltipsAndPopoversBody body;
-//    @UI(".modal-body > p:nth-child(2) > a") public Button buttonTriggers;
-//    @UI(".modal-body > p:nth-child(5) > a:nth-child(1)") public Link thisLink;
-//    @UI(".modal-body > p:nth-child(5) > a:nth-child(2)") public Link thatLink;
+    @UI(".modal-body")
+    public ModalTooltipsAndPopoversBody body;
     @UI("//div[@class='modal-footer']//button[1]")
     public Button closeButton;
     @UI("//div[@class='modal-footer']//button[2]")
