f24c083d7483446d66e495bdfe02cd498b9dc65c
Aleksandr_Shiganov <aleksandr.shiganov@epam.com>
2019-10-22 19:50:28 +0300

    navbar section elements

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/navbar/NavbarSection.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/navbar/NavbarSection.java
@@ -1,17 +1,24 @@
 package io.github.com.sections.navbar;
 
-import com.epam.jdi.light.elements.common.UIElement;
-import com.epam.jdi.light.elements.complex.JList;
+import com.epam.jdi.light.elements.base.UIBaseElement;
 import com.epam.jdi.light.elements.composite.Section;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 
 public class NavbarSection extends Section {
 
-    //FindBy(css = ".navbar-brand")
-    @UI(".navbar-brand")
-    public JList<UIElement> allNavbarBrands;
+    //FindBy(css = "#brand-heading")
+    @UI("#brand-heading")
+    public UIBaseElement navbarBrandHeading;
 
-    //FindBy(css = "#brand-as-image,#brand-as-image-and-link")
-    @UI("#brand-as-image,#brand-as-image-and-link")
-    public JList<UIElement> navbarBrandWithImage;
+    //FindBy(css = "#brand-link")
+    @UI("#brand-link")
+    public UIBaseElement navbarBrandLink;
+
+    //FindBy(css = "#brand-as-image")
+    @UI("#brand-as-image")
+    public UIBaseElement navbarBrandAsImage;
+
+    //FindBy(css = "#brand-as-image-and-link")
+    @UI("#brand-as-image-and-link")
+    public UIBaseElement navbarBrandAsImageAndLink;
 }
