7c7825bdf2461bc1f00ab1cc3cc91811ccd722cf
Aleksandr_Shiganov <aleksandr.shiganov@epam.com>
2019-10-22 09:38:37 +0300

    import optimization

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/form/FormGrid.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/form/FormGrid.java
@@ -2,8 +2,6 @@ package io.github.com.sections.form;
 
 import com.epam.jdi.light.elements.composite.Form;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
-import com.epam.jdi.light.ui.bootstrap.elements.common.Button;
-import com.epam.jdi.light.ui.bootstrap.elements.common.Checkbox;
 import com.epam.jdi.light.ui.bootstrap.elements.common.TextField;
 import io.github.com.entities.FormContacts;
 import org.openqa.selenium.Keys;
--- jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/form/FormGridTests.java
+++ jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/form/FormGridTests.java
@@ -1,28 +1,16 @@
 package io.github.epam.bootstrap.tests.composite.section.form;
 
 import com.epam.jdi.light.ui.bootstrap.elements.common.TextField;
-import io.github.com.entities.FormContacts;
 import io.github.epam.TestsInit;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.util.List;
-
-import static com.epam.jdi.light.asserts.core.SoftAssert.jdiAssert;
 import static com.epam.jdi.light.elements.common.Alerts.validateAlert;
 import static io.github.com.StaticSite.bsPage;
-import static io.github.com.entities.FormUsers.ALL_EXCEPT_NAME_FILLED_DEFAULT_CONTACT;
 import static io.github.com.entities.FormUsers.BLANK_CONTACT;
-import static io.github.com.entities.FormUsers.DEFAULT_CHECK;
-import static io.github.com.entities.FormUsers.DEFAULT_CONTACT;
-import static io.github.com.entities.FormUsers.ONLY_NAME_FILLED_DEFAULT_CONTACT;
-import static io.github.com.pages.BootstrapPage.formCustomStyles;
 import static io.github.com.pages.BootstrapPage.formGrid;
-import static io.github.com.pages.BootstrapPage.formServerSide;
 import static io.github.epam.states.States.shouldBeLoggedIn;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 
 public class FormGridTests extends TestsInit {
@@ -67,9 +55,12 @@ public class FormGridTests extends TestsInit {
         };
     }
 
-    @Test (dataProvider = "personalData")
+    @Test(dataProvider = "personalData")
     public void fillTest(String firstName, String lastName) {
-        formGrid.fill(BLANK_CONTACT.set(c -> {c.name = firstName; c.lastName = lastName; }));
+        formGrid.fill(BLANK_CONTACT.set(c -> {
+            c.name = firstName;
+            c.lastName = lastName;
+        }));
         formGrid.check(BLANK_CONTACT);
         checkContactFormSubmitted();
     }
@@ -77,6 +68,9 @@ public class FormGridTests extends TestsInit {
     private void checkContactFormSubmitted() {
         formGrid.submit();
         validateAlert(is("Form filled and submitted successfully"));
-        formGrid.check(BLANK_CONTACT.set(c -> {c.name = ""; c.lastName = ""; }));
+        formGrid.check(BLANK_CONTACT.set(c -> {
+            c.name = "";
+            c.lastName = "";
+        }));
     }
 }
