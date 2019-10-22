db733c9ccae902a3061316ec15f407f825c083ec
Veronika Revyakina <Veronika_Revyakina@epam.com>
2019-10-22 14:45:40 +0300

    Improved tests

--- jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/form/FormAutoSizingAllTests.java
+++ jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/form/FormAutoSizingAllTests.java
@@ -2,24 +2,46 @@ package io.github.epam.bootstrap.tests.composite.section.form;
 
 import io.github.epam.TestsInit;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static com.epam.jdi.light.elements.common.Alerts.validateAlert;
 import static io.github.com.StaticSite.bsPage;
 import static io.github.com.pages.BootstrapPage.formAutoSizingAll;
+import static io.github.epam.bootstrap.tests.BaseValidations.baseValidation;
 import static io.github.epam.states.States.shouldBeLoggedIn;
 import static org.hamcrest.Matchers.is;
 
 public class FormAutoSizingAllTests extends TestsInit {
-    private String email = "delnot@gmail.com";
-    private String placeholderText = "Jane Doe";
-    private String radio = "option2";
-    private String accept = "true";
+
+    private String placeholderNameText = "Jane Doe";
+    private String placeholderEmailText = "Username";
+    private String labelNameText = "Name";
+    private String labelEmailText = "Username";
+    private String labelCheckboxText = "Remember me";
+
+    @DataProvider
+    public Object[][] optionFormAutoSizingTest() {
+        return new Object[][]{
+                {"Veronika", "gmail.com", "true"},
+                {"Ivan", "mail.ru", "false"},
+                {"Fedor", "epam.com", "true"}
+        };
+    }
 
     @BeforeMethod
     public void before() {
         shouldBeLoggedIn();
         bsPage.shouldBeOpened();
+        bsPage.refresh();
+    }
+
+    @Test
+    public void baseValidationTest() {
+        baseValidation(formAutoSizingAll.name);
+        baseValidation(formAutoSizingAll.email);
+        baseValidation(formAutoSizingAll.rememberMeCheckbox);
+        baseValidation(formAutoSizingAll.submit);
     }
 
     @Test
@@ -31,14 +53,15 @@ public class FormAutoSizingAllTests extends TestsInit {
                 .core()
                 .hasClass("form-control mb-2")
                 .attr("type", "text")
-                .attr("placeholder", placeholderText)
-                .tag(is("input"));
+                .attr("placeholder", placeholderNameText)
+                .tag(is("input"))
+                .text("");
         formAutoSizingAll.name.label()
                 .is()
                 .displayed()
                 .enabled()
                 .core()
-                .value("Name");
+                .value(labelNameText);
         formAutoSizingAll.email
                 .is()
                 .displayed()
@@ -46,14 +69,15 @@ public class FormAutoSizingAllTests extends TestsInit {
                 .core()
                 .hasClass("form-control")
                 .attr("type", "text")
-                .attr("placeholder", "Username")
-                .tag(is("input"));
+                .attr("placeholder", placeholderEmailText)
+                .tag(is("input"))
+                .text("");
         formAutoSizingAll.email.label()
                 .is()
                 .displayed()
                 .enabled()
                 .core()
-                .value("Username");
+                .value(labelEmailText);
         formAutoSizingAll.rememberMeCheckbox
                 .is()
                 .displayed()
@@ -61,40 +85,69 @@ public class FormAutoSizingAllTests extends TestsInit {
                 .core()
                 .hasClass("form-check-input")
                 .attr("type", "checkbox")
-                .tag(is("input"));
+                .tag(is("input"))
+                .deselected();
         formAutoSizingAll.rememberMeCheckbox.label()
                 .is()
                 .displayed()
                 .enabled()
                 .core()
-                .value("Remember me");
+                .value(labelCheckboxText);
+    }
+
+    @Test(dataProvider = "optionFormAutoSizingTest")
+    public void fillingNameAndEmailWithDataProviderTest(String name, String email, String checkboxState) {
+        formAutoSizingAll.name
+                .sendKeys(name);
+        formAutoSizingAll.email
+                .sendKeys(email);
+        formAutoSizingAll.rememberMeCheckbox
+                .setValue(checkboxState);
+        formAutoSizingAll.submit.click();
+        validateAlerts();
+        formAutoSizingAll.name
+                .is()
+                .text(name);
+        formAutoSizingAll.email
+                .is()
+                .text(email);
+        formAutoSizingAll.rememberMeCheckbox
+                .check(checkboxState);
     }
 
     @Test
-    public void fillingFormTest() {
+    public void separateElementFillingNameTest() {
         formAutoSizingAll.name
                 .sendKeys("Veronika");
-        formAutoSizingAll.email
-                .sendKeys("ver.revjakina");
-        formAutoSizingAll.rememberMeCheckbox
-                .check();
-
+        formAutoSizingAll.submit.click();
+        validateAlerts();
         formAutoSizingAll.name
                 .is()
                 .text("Veronika");
-        formAutoSizingAll.email
-                .is()
-                .text("ver.revjakina");
-        formAutoSizingAll.rememberMeCheckbox
-                .isSelected();
-        formAutoSizingAll.submit.click();
-        //validateAlerts();
-
     }
 
+    @Test
+    public void separateElementFillingEmailTest() {
+        formAutoSizingAll.email
+                .sendKeys("gmail.com");
+        formAutoSizingAll.submit.click();
+        validateAlerts();
+        formAutoSizingAll.email
+                .is()
+                .text("gmail.com");
+    }
+
+    @Test
+    public void separateElementCheckboxTest() {
+        formAutoSizingAll.rememberMeCheckbox
+                .check();
+        formAutoSizingAll.submit.click();
+        validateAlerts();
+        formAutoSizingAll.rememberMeCheckbox
+                .isSelected();
+    }
 
     private void validateAlerts() {
         validateAlert(is("Submit button clicked."));
-        validateAlert(is("Form submitted successfully"));
     }
 }
---
+++ jdi-light-bootstrap-tests/src/test/java/io/github/epam/bootstrap/tests/composite/section/form/FormAutoSizingSizeSpecificTests.java
@@ -0,0 +1,6 @@
+package io.github.epam.bootstrap.tests.composite.section.form;
+
+import io.github.epam.TestsInit;
+
+public class FormAutoSizingSizeSpecificTests extends TestsInit {
+}
