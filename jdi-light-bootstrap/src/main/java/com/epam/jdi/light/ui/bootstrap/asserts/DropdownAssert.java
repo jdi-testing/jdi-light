ef9a8182a7693f27d213551fd74b8b7229baf86f
ArKhmeln <aakhmelinin@gmail.com>
2019-10-29 17:45:08 +0300

    import fix

--- jdi-light-bootstrap/src/main/java/com/epam/jdi/light/ui/bootstrap/asserts/DropdownAssert.java
+++ jdi-light-bootstrap/src/main/java/com/epam/jdi/light/ui/bootstrap/asserts/DropdownAssert.java
@@ -9,7 +9,6 @@ import org.hamcrest.Matchers;
 import java.util.Arrays;
 
 import static com.epam.jdi.light.asserts.core.SoftAssert.jdiAssert;
-import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.is;
 
 public class DropdownAssert<A extends DropdownAssert, E extends Dropdown> extends UIAssert<A, E> {
