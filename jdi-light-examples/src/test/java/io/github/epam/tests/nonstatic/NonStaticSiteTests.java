b935725f97c696c204e113e95648f23c0bff4add
vladislav_murashov <vladislav_murashov@epam.com>
2019-11-01 15:23:11 +0300

    non static site initialization ready to review finally

--- jdi-light-examples/src/test/java/io/github/epam/tests/nonstatic/NonStaticSiteTests.java
+++ jdi-light-examples/src/test/java/io/github/epam/tests/nonstatic/NonStaticSiteTests.java
@@ -3,7 +3,6 @@ package io.github.epam.tests.nonstatic;
 import io.github.epam.NonStaticTestsInit;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import static io.github.epam.tests.recommended.steps.Preconditions.shouldBeLoggedIn;
 
 public class NonStaticSiteTests extends NonStaticTestsInit {
 
