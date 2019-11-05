46eed84ef44bdb06f8a44d701073559aa5123457
Roman_Bordiugov <rome-0@yandex.ru>
2019-11-05 12:42:17 +0300

    Filling in the data class of Support Message

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/entities/SupportMessage.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/entities/SupportMessage.java
@@ -3,6 +3,6 @@ package io.github.com.entities;
 import com.epam.jdi.tools.DataClass;
 
 public class SupportMessage extends DataClass<SupportMessage> {
-    public String supportInputEmail = "help-me.please@mail.org";
-    public String supportTextArea = "Nothing works! Nothing! Answer me, god damn it!";
+    public String supportInputEmail;
+    public String supportTextArea;
 }
--- jdi-light-bootstrap-tests/src/test/java/io/github/epam/test/data/SupportMessages.java
+++ jdi-light-bootstrap-tests/src/test/java/io/github/epam/test/data/SupportMessages.java
@@ -3,5 +3,12 @@ package io.github.epam.test.data;
 import io.github.com.entities.SupportMessage;
 
 public class SupportMessages {
-    public static SupportMessage EXAMPLE_MESSAGE = new SupportMessage();
+    public static SupportMessage EXAMPLE_MESSAGE = getExampleMessage();
+
+    private static SupportMessage getExampleMessage() {
+        return new SupportMessage().set(field -> {
+            field.supportInputEmail = "help-me.please@mail.org";
+            field.supportTextArea = "Nothing works! Nothing! Answer me, PLEASE!";
+        });
+    }
 }
