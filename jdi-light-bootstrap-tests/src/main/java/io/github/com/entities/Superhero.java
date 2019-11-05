9fbc428182548823dbbbca1111cc10ed88d26995
Roman_Bordiugov <rome-0@yandex.ru>
2019-11-05 12:38:11 +0300

    Filling in the data class of Superhero

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/entities/Superhero.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/entities/Superhero.java
@@ -3,21 +3,10 @@ package io.github.com.entities;
 import com.epam.jdi.tools.DataClass;
 
 public class Superhero extends DataClass<Superhero> {
-
-    public String currentAlias = "Venom";
-    public String alterEgo = "Peter Parker";
-    public String species = "Symbiote";
-    public String selectUniverse = "Marvel";
-    public String superheroRange = "10.0";
-    public String superheroSwitch = "true";
-
-
-//    public String superheroCommonAbilities = "Strength; Agility; Endurance";
-//    public String superheroSpecies = "Symbiote"; // Radios
-
-    /*public String currentAlias = "Punisher";
-    public String alterEgo = "Frank Castle";
-    public String species = "Human"; // Radios
-    public String commonAbilities = "Marksmanship"; // Checkboxes
-    public String universe = "Marvel Earth-616"; // Select*/
+    public String currentAlias;
+    public String alterEgo;
+    public String species;
+    public String selectUniverse;
+    public String superheroRange;
+    public String superheroSwitch;
 }
--- jdi-light-bootstrap-tests/src/test/java/io/github/epam/test/data/Superheroes.java
+++ jdi-light-bootstrap-tests/src/test/java/io/github/epam/test/data/Superheroes.java
@@ -3,5 +3,34 @@ package io.github.epam.test.data;
 import io.github.com.entities.Superhero;
 
 public class Superheroes {
-    public static Superhero EXAMPLE_HERO = new Superhero();
+    public static Superhero EXAMPLE_HERO = getExampleHero();
+    public static Superhero TEMPLATE_HERO = getHeroTemplate();
+
+    private static Superhero getExampleHero() {
+        return new Superhero().set(c -> {
+            c.currentAlias = "Venom";
+            c.alterEgo = "Peter Parker";
+            c.species = "Symbiote";
+            c.selectUniverse = "Marvel";
+            c.superheroRange = "10.0";
+            c.superheroSwitch = "true";
+        });
+    }
+
+    private static Superhero getHeroTemplate() {
+        return new Superhero().set(c -> {
+            c.currentAlias = "";
+            c.alterEgo = "";
+            c.species = "Human";
+            c.selectUniverse = "Select character's universe";
+            c.superheroRange = "50.0";
+            c.superheroSwitch = "false";
+        });
+    }
 }
+/*public String currentAlias = "Venom";
+    public String alterEgo = "Peter Parker";
+    public String species = "Symbiote";
+    public String selectUniverse = "Marvel";
+    public String superheroRange = "10.0";
+    public String superheroSwitch = "true";*/
