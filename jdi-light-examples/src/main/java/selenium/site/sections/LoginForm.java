7cb6caccd9d43daead50630d874bedcaf03718a4
Yulia_Litvin <iulia.litwin@yandex.ru>
2019-11-05 12:51:50 +0300

    fix issues in pr

--- jdi-light-examples/src/main/java/selenium/site/sections/LoginForm.java
+++ jdi-light-examples/src/main/java/selenium/site/sections/LoginForm.java
@@ -4,6 +4,7 @@ import com.epam.jdi.light.elements.composite.Form;
 import org.openqa.selenium.WebElement;
 import selenium.entities.User;
 
+@SuppressWarnings("unused")
 public class LoginForm extends Form<User> {
     private WebElement name;
     private WebElement password;
