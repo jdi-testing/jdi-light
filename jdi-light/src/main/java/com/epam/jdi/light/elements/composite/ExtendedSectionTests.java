8c91887d7d61c4a2438ba90c202920ff92217598
tsvetaev.alex <56589282+tsvetaevAlex@users.noreply.github.com>
2019-11-07 15:33:45 +0300

    Add files via upload
    task:Task_98 Avoid-unused-local-variables
    unused variable has been removed
    line 46: ExtendedSection e = extendedSection;
    file:  /epam/html/tests/elements/composite/section/ExtendedSectionTests.java

---
+++ jdi-light/src/main/java/com/epam/jdi/light/elements/composite/ExtendedSectionTests.java
@@ -0,0 +1,63 @@
+package io.github.epam.html.tests.elements.composite.section;
+
+import com.epam.jdi.light.elements.interfaces.base.ICoreElement;
+import io.github.epam.TestsInit;
+import org.testng.annotations.Test;
+import pseudo.site.dataproviders.section.ExtendedSectionDataProvider;
+import pseudo.site.section.ExtendedSection;
+
+import static io.github.epam.html.tests.elements.composite.CompositeUtils.checkInitializedElement;
+import static pseudo.site.PseudoSite.extendedSection;
+
+public class ExtendedSectionTests extends TestsInit {
+
+    @Test(dataProvider = "extendedSectionWebElementDataProvider", dataProviderClass = ExtendedSectionDataProvider.class)
+    public void extendedSectionWebElementTest(ICoreElement htmlElementToCheck, String expectedLocator, Object expectedParent, String expectedName) {
+        checkInitializedElement(htmlElementToCheck, expectedLocator, expectedParent, expectedName);
+    }
+
+    @Test(dataProvider = "extendedSectionListWebElementDataProvider", dataProviderClass = ExtendedSectionDataProvider.class)
+    public void extendedSectionListWebElementTest(ICoreElement htmlElementToCheck, String expectedLocator, Object expectedParent, String expectedName) {
+        checkInitializedElement(htmlElementToCheck, expectedLocator, expectedParent, expectedName);
+    }
+
+    @Test(dataProvider = "extendedSectionUIElementDataProvider", dataProviderClass = ExtendedSectionDataProvider.class)
+    public void extendedSectionUIElementTest(ICoreElement htmlElementToCheck, String expectedLocator, Object expectedParent, String expectedName) {
+        checkInitializedElement(htmlElementToCheck, expectedLocator, expectedParent, expectedName);
+    }
+
+    @Test(dataProvider = "extendedSectionButtonDataProvider", dataProviderClass = ExtendedSectionDataProvider.class)
+    public void extendedSectionButtonTest(ICoreElement htmlElementToCheck, String expectedLocator, Object expectedParent, String expectedName) {
+        checkInitializedElement(htmlElementToCheck, expectedLocator, expectedParent, expectedName);
+    }
+
+    @Test(dataProvider = "extendedSectionWebListDataProvider", dataProviderClass = ExtendedSectionDataProvider.class)
+    public void extendedSectionWebListTest(ICoreElement htmlElementToCheck, String expectedLocator, Object expectedParent, String expectedName) {
+        checkInitializedElement(htmlElementToCheck, expectedLocator, expectedParent, expectedName);
+    }
+
+    @Test(dataProvider = "extendedSectionListUIElementPublicDataProvider", dataProviderClass = ExtendedSectionDataProvider.class)
+    public void extendedSectionListUIElementPublicTest(ICoreElement htmlElementToCheck, String expectedLocator, Object expectedParent, String expectedName) {
+        checkInitializedElement(htmlElementToCheck, expectedLocator, expectedParent, expectedName);
+    }
+
+    @Test(dataProvider = "extendedSectionListButtonPublicDataProvider", dataProviderClass = ExtendedSectionDataProvider.class)
+    public void extendedSectionListButtonPublicTest(ICoreElement htmlElementToCheck, String expectedLocator, Object expectedParent, String expectedName) {
+        checkInitializedElement(htmlElementToCheck, expectedLocator, expectedParent, expectedName);
+    }
+
+    @Test(dataProvider = "extendedSectionUIListQuestionDataProvider", dataProviderClass = ExtendedSectionDataProvider.class)
+    public void extendedSectionUIListQuestionTest(ICoreElement htmlElementToCheck, String expectedLocator, Object expectedParent, String expectedName) {
+        checkInitializedElement(htmlElementToCheck, expectedLocator, expectedParent, expectedName);
+    }
+
+    @Test(dataProvider = "extendedSectionUIListSomedataDataProvider", dataProviderClass = ExtendedSectionDataProvider.class)
+    public void extendedSectionUIListSomedataTest(ICoreElement htmlElementToCheck, String expectedLocator, Object expectedParent, String expectedName) {
+        checkInitializedElement(htmlElementToCheck, expectedLocator, expectedParent, expectedName);
+    }
+
+    @Test(dataProvider = "extendedSectionDropDownDataProvider", dataProviderClass = ExtendedSectionDataProvider.class)
+    public void extendedSectionDropDownTest(ICoreElement htmlElementToCheck, String expectedLocator, Object expectedParent, String expectedName) {
+        checkInitializedElement(htmlElementToCheck, expectedLocator, expectedParent, expectedName);
+    }
+}
