2521258ac82f25ac2f7e9488373790a524ac5cda
Veronika Revyakina <Veronika_Revyakina@epam.com>
2019-11-07 13:45:05 +0300

    fixed negative test asset contains

--- jdi-light/src/test/java/mockitotests/HasAttrMockTests.java
+++ jdi-light/src/test/java/mockitotests/HasAttrMockTests.java
@@ -3,7 +3,6 @@ package mockitotests;
 import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.tools.map.MapArray;
 import org.mockito.Mockito;
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -14,9 +13,8 @@ import static org.testng.Assert.assertTrue;
 public class HasAttrMockTests {
 
     private UIElement uiElement = null;
-    private WebElement webElement = null;
     private String exceptionMessage1 = "Expected: an array containing";
-    private String exceptionMessage2 = "but: mismatches were";
+    private String exceptionMessage2 = "but: mismatches were: [";
 
 
     @DataProvider
@@ -42,7 +40,6 @@ public class HasAttrMockTests {
     @BeforeMethod
     public void init() {
         uiElement = mock(UIElement.class);
-        webElement = mock(WebElement.class);
     }
 
     //WORKING:
@@ -56,7 +53,7 @@ public class HasAttrMockTests {
     }
 
     //WORKING
-    // TODO: Need to improve assertContains to assertEquals, more efficient String building with params
+    // TODO: Leave assertContains to assertEquals
     @Test(dataProvider = "attributesNegative")
     public void negativeTest1(MapArray<String, String> actualMapAttr, String wrongAttr) {
         uiElement = Mockito.spy(new UIElement());
@@ -65,44 +62,22 @@ public class HasAttrMockTests {
         try {
             uiElement.is().hasAttr(wrongAttr);
         } catch (RuntimeException exp) {
-            assertTrue(exp.getMessage().contains(exceptionMessage1));
-            assertTrue(exp.getMessage().contains(wrongAttr));
-            assertTrue(exp.getMessage().contains(exceptionMessage2));
-            //assertTrue(exp.getMessage().contains(actualMapAttr.keys().toString()));
-            //System.out.println(actualMapAttr.keys());
-            assertTrue(exp.getMessage().contains(actualMapAttr.key(0)));
-            assertTrue(exp.getMessage().contains(actualMapAttr.key(1)));
-            assertTrue(exp.getMessage().contains(actualMapAttr.key(2)));
-
-            /*assertEquals(exp.getMessage().trim(),(exceptionMessage1+" "+wrongAttr+exceptionMessage2+actualMapAttr.key(0)
-            +", was "+actualMapAttr.key(1)+", was "+ actualMapAttr.key(2)).trim());*/
+            String expMessage = exp.getMessage();
+            assertTrue(expMessage.contains(exceptionMessage1 + " \"" + wrongAttr + "\""));
+            assertTrue(expMessage.contains(exceptionMessage2));
+            for (String key : actualMapAttr.keys()) {
+                System.out.println("was  \"" + key + "\"");
+                assertTrue(expMessage.contains("was \"" + key + "\""));
+            }
         }
     }
 
     //WORKING
-   //TODO: Improve Chrome closing (through webElement) -First try in positiveTest1(trey are the same)
+    //TODO: Improve Chrome closing
     @Test(dataProvider = "attributesPositive")
     public void positiveTest(MapArray<String, String> actualMapAttr, String expectedAttr) {
         uiElement = Mockito.spy(new UIElement());
         Mockito.doReturn(actualMapAttr).when(uiElement).attrs();
-        uiElement.is().hasAttr(expectedAttr);
-    }
-
-    @DataProvider
-    public Object[][] attributesPositive1() {
-        return new Object[][]{
-                {new MapArray<>("type", ""), "type"}
-        };
-    }
-
-    //Copy of positiveTest, here tried to improve Chrome closing
-    //TODO: Improve Chrome closing (through webElement)
-    @Test(dataProvider = "attributesPositive1")
-    public void positiveTest1(MapArray<String, String> actualAttr, String expectedAttr) {
-        uiElement = Mockito.spy(new UIElement());
-        Mockito.doReturn(webElement).when(uiElement).getWebElement();
-        Mockito.doReturn(actualAttr.key(0)).when(webElement).getAttribute(expectedAttr);
-        Mockito.doReturn(actualAttr).when(uiElement).attrs();
         uiElement.is().hasAttr(expectedAttr);
     }
 
