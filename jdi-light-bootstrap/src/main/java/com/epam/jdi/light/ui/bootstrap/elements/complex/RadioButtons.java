c40c12220af7dbf579f8259f8f3775a89b5942ae
Aleksandr_Shiganov <aleksandr.shiganov@epam.com>
2019-11-10 19:59:27 +0300

    minor changes - import optimization codacy

--- jdi-light-bootstrap/src/main/java/com/epam/jdi/light/ui/bootstrap/elements/complex/RadioButtons.java
+++ jdi-light-bootstrap/src/main/java/com/epam/jdi/light/ui/bootstrap/elements/complex/RadioButtons.java
@@ -2,15 +2,12 @@ package com.epam.jdi.light.ui.bootstrap.elements.complex;
 
 import com.epam.jdi.light.asserts.generic.UISelectAssert;
 import com.epam.jdi.light.common.ElementArea;
-import com.epam.jdi.light.elements.base.JDIBase;
 import com.epam.jdi.light.elements.base.UIListBase;
-import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.light.elements.complex.WebList;
 import com.epam.jdi.tools.func.JFunc1;
 import org.openqa.selenium.WebElement;
 
 import static com.epam.jdi.light.common.ElementArea.ACTION_CLICK;
-import static com.epam.jdi.light.common.ElementArea.JS;
 import static com.epam.jdi.light.common.TextTypes.LABEL;
 import static com.epam.jdi.light.settings.WebSettings.ANY_ELEMENT;
 
@@ -23,14 +20,14 @@ public class RadioButtons extends UIListBase<UISelectAssert> {
 
     @Override
     public WebList list() {
-        WebList radioBtnWebList = new WebList(base()).setup( jdiB -> jdiB.setSearchRule(ANY_ELEMENT))
+        WebList radioBtnWebList = new WebList(base()).setup(jdiB -> jdiB.setSearchRule(ANY_ELEMENT))
                 .setUIElementName(LABEL);
         radioBtnWebList.setClickArea(ACTION_CLICK);
         return radioBtnWebList;
     }
 
     public WebList list(JFunc1<WebElement, Boolean> searchRule, ElementArea elementArea) {
-        WebList radioBtnWebList = new WebList(base()).setup( jdiB -> jdiB.setSearchRule(searchRule))
+        WebList radioBtnWebList = new WebList(base()).setup(jdiB -> jdiB.setSearchRule(searchRule))
                 .setUIElementName(LABEL);
         radioBtnWebList.setClickArea(elementArea);
         return radioBtnWebList;
