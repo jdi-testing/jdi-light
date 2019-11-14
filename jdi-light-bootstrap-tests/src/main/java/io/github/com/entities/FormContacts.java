f3358633dd8a045c0f00690091ae98bdf698371e
Veronika Revyakina <Veronika_Revyakina@epam.com>
2019-11-14 14:29:23 +0300

    added FormContacts back

---
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/entities/FormContacts.java
@@ -0,0 +1,23 @@
+package io.github.com.entities;
+
+import com.epam.jdi.tools.DataClass;
+
+/**
+ * Created by Dmitrii Pavlov on 10.10.2019
+ * Email: delnote@gmail.com; Skype: Dmitrii Pavlov
+ */
+
+public class FormContacts extends DataClass<FormContacts> {
+
+    public String name;
+    public String lastName;
+    public String userName;
+    public String city;
+    public String state;
+    public String zip;
+    public String accept;
+    public String radio;
+    public String email;
+    public String password;
+
+}
