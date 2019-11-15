11e8b7a2373e334d5b3433cf71a8ae136f95d856
Aleksandr Khmelinin <aakhmelinin@gmail.com>
2019-11-15 11:16:07 +0300

    parent changed

--- jdi-light-bootstrap/src/main/java/com/epam/jdi/light/ui/bootstrap/elements/complex/MultiplebarsProgress.java
+++ jdi-light-bootstrap/src/main/java/com/epam/jdi/light/ui/bootstrap/elements/complex/MultiplebarsProgress.java
@@ -1,10 +1,11 @@
 package com.epam.jdi.light.ui.bootstrap.elements.complex;
 
+import com.epam.jdi.light.elements.base.UIBaseElement;
 import com.epam.jdi.light.elements.complex.JList;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.ui.bootstrap.elements.common.Progress;
 
-public class MultiplebarsProgress extends Progress {
+public class MultiplebarsProgress extends UIBaseElement {
 
     @UI(".progress-bar")
     public JList<Progress> barsList;
