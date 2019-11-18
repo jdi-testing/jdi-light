769ec34b06f1c43210cdaf12127a948e2b9c972a
Vsevolod_Adrianov <Vsevolod_Adrianov@epam.com>
2019-11-18 18:47:31 +0300

    remove some empty strings

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/card/CardWithinCardColumns.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/card/CardWithinCardColumns.java
@@ -5,13 +5,10 @@ import com.epam.jdi.light.ui.bootstrap.elements.complex.Card;
 import com.epam.jdi.light.ui.html.elements.common.Image;
 import com.epam.jdi.light.ui.html.elements.common.Text;
 
-
 public class CardWithinCardColumns extends Card {
-
     @UI(".card-title") public Text title;
     @UI("p:nth-of-type(1)") public Text mainText;
     @UI(".card-text small") public Text mutedText;
     @UI("footer small") public Text footerText;
     @UI(".card-img-top") public Image image;
-
 }
