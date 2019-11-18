0adabda7e90f723eb61f24d91dc4c8f51d62384c
Veronika Revyakina <Veronika_Revyakina@epam.com>
2019-11-18 10:16:54 +0300

    added switchers back

---
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/form/Switches.java
@@ -0,0 +1,14 @@
+package io.github.com.sections.form;
+
+import com.epam.jdi.light.elements.composite.Section;
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
+import com.epam.jdi.light.ui.bootstrap.elements.common.Checkbox;
+
+public class Switches extends Section {
+
+    @UI("#customSwitch1-div")
+    public Checkbox checkbox;
+    @UI("#customSwitch2-div")
+    public Checkbox checkboxDisabled;
+
+}
