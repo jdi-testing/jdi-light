16a2c56ace6582d6fe5664d53f1048a09b40a7f8
Roman Bordiugov <rome-0@yandex.ru>
2019-11-19 16:53:39 +0300

    Element Link would be using from jdi-light-html package now

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/card/CardNavigation.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/card/CardNavigation.java
@@ -3,9 +3,9 @@ package io.github.com.sections.card;
 import com.epam.jdi.light.elements.complex.Menu;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.ui.bootstrap.elements.common.Button;
-import com.epam.jdi.light.ui.bootstrap.elements.common.Link;
 import com.epam.jdi.light.ui.bootstrap.elements.common.Text;
 import com.epam.jdi.light.ui.bootstrap.elements.composite.Card;
+import com.epam.jdi.light.ui.html.elements.common.Link;
 
 public class CardNavigation extends Card {
 
--- jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/navs/NestedNav.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/navs/NestedNav.java
@@ -2,8 +2,8 @@ package io.github.com.sections.navs;
 
 import com.epam.jdi.light.elements.composite.Section;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
-import com.epam.jdi.light.ui.bootstrap.elements.common.Link;
 import com.epam.jdi.light.ui.bootstrap.elements.complex.ListGroup;
+import com.epam.jdi.light.ui.html.elements.common.Link;
 
 public class NestedNav extends Section {
     @UI("nav") public ListGroup navGroup;          // @FindBy(css = "nav")
