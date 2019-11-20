769e3b15ae45f9ab4400c9cac32741e49faa4b6a
Sergei_Plekhanov <sergei_plekhanov@epam.com>
2019-11-20 13:58:22 +0300

    Test properties validation mechanism

--- jdi-light/src/main/java/com/epam/jdi/light/settings/PropertyNames.java
+++ jdi-light/src/main/java/com/epam/jdi/light/settings/PropertyNames.java
@@ -1,5 +1,6 @@
 package com.epam.jdi.light.settings;
 
+@SuppressWarnings("PMD.ClassNamingConventions")
 public class PropertyNames {
 
     public static final String TIMEOUT_WAIT_ELEMENT = "timeout.wait.element";
