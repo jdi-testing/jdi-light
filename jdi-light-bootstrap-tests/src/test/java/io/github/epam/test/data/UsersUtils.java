294908e1457f11cdd08d514a6d2207c7d501ce88
evasilevaIDBS <32126366+evasilevaIDBS@users.noreply.github.com>
2019-11-22 10:55:12 +0300

    Small fix

--- jdi-light-bootstrap-tests/src/test/java/io/github/epam/test/data/UsersUtils.java
+++ jdi-light-bootstrap-tests/src/test/java/io/github/epam/test/data/UsersUtils.java
@@ -2,7 +2,6 @@ package io.github.epam.test.data;
 
 import io.github.com.entities.UserUtils;
 
-@SuppressWarnings("PMD.ClassNamingConventions")
 public class UsersUtils {
     public static UserUtils defaultUserUtils = new UserUtils();
 }
