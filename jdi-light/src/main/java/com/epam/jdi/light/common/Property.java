25d92c5fec73e68eb78c1de706ad6b3a400bb82a
ArKhmeln <aakhmelinin@gmail.com>
2019-11-26 17:28:12 +0300

    added tests for parameters

--- jdi-light/src/main/java/com/epam/jdi/light/common/Property.java
+++ jdi-light/src/main/java/com/epam/jdi/light/common/Property.java
@@ -3,7 +3,12 @@ package com.epam.jdi.light.common;
 public enum Property {
 
     TIMEOUT_WAIT_ELEMENT("timeout.wait.element", "^[1-9][0-9]{1,2}$|^\\d$",
-            "Value of 'timeout.wait.element' must be an integer from 0 to 999.");
+            "Value of 'timeout.wait.element' must be an integer from 0 to 999."),
+    //SCREENS_FOLDER("screens.folder", "^[a-zA-Z]:\\\\[\\\\\\S|*\\S]?.*$", "'screens.folder' must contain the correct folder path"),
+    SCREENS_FOLDER("screens.folder", "^(.*/)([^/]*)$", "'screens.folder' must contain the correct folder path"),
+    KILL_BROWSER("browser.kill", "^afterAndBefore|after|before", "Value of 'browser.kill' must be one of the following:'afterAndBefore', 'after', 'before'"),
+    ELEMENT_SEARCH_STRATEGY("element.search.strategy", "^strict|soft|visible, multiple|any, single", "Value of 'element.search.strategy' must be one of the following: 'strict', 'soft', 'visible, multiple', 'any, single'");
+
 
     private final String name;
     private final String check;
---
+++ jdi-light/src/test/java/io/github/epam/properties/ScreensFolderTests.java
@@ -0,0 +1,45 @@
+package io.github.epam.properties;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.security.InvalidParameterException;
+import java.util.Properties;
+
+import static com.epam.jdi.light.common.Property.SCREENS_FOLDER;
+import static com.epam.jdi.light.common.PropertyValidationUtils.validateProperties;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.fail;
+
+public class ScreensFolderTests {
+
+    @DataProvider
+    public static Object[] negativeData() {
+        return new Object[]{"cd:\\fd", "c:\\f//d", "\\nyet"};
+    }
+
+    @DataProvider
+    public static Object[] positiveData() {
+        return new Object[]{"C:\\ProgramFiles\\tests", "D:\\t1ests"};
+    }
+
+    @Test(dataProvider = "negativeData")
+    public void negativeTest(String value) {
+        Properties properties = new Properties();
+        properties.setProperty(SCREENS_FOLDER.getName(), value);
+        try {
+            validateProperties(properties);
+            fail("Value '" + value + "' should not be valid for this test.");
+        } catch (InvalidParameterException exp) {
+            String expMessage = exp.getMessage();
+            assertEquals(expMessage, SCREENS_FOLDER.getExMsg() + " See example: https://jdi-docs.github.io/jdi-light/#driver-settings");
+        }
+    }
+
+    @Test(dataProvider = "positiveData")
+    public void positiveTest(String value) {
+        Properties properties = new Properties();
+        properties.setProperty(SCREENS_FOLDER.getName(), value);
+        validateProperties(properties);
+    }
+}
