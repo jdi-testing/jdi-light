b0c7466894a8a7ec19ca8520f0ffcb44aab3fb34
Marina Lalazarova <marina_lalazarova@epam.com>
2019-11-26 19:49:28 +0300

    names of methods changed, some method types and assertions input params were changed from int to double

--- jdi-light-html/src/main/java/com/epam/jdi/light/ui/html/asserts/RangeAssert.java
+++ jdi-light-html/src/main/java/com/epam/jdi/light/ui/html/asserts/RangeAssert.java
@@ -6,7 +6,7 @@ import com.epam.jdi.light.ui.html.elements.common.Range;
 import org.hamcrest.Matcher;
 
 import static com.epam.jdi.light.asserts.core.SoftAssert.jdiAssert;
-import static com.epam.jdi.light.ui.html.HtmlUtils.getInt;
+import static com.epam.jdi.light.ui.html.HtmlUtils.getDouble;
 import static org.hamcrest.Matchers.is;
 
 /**
@@ -17,29 +17,43 @@ import static org.hamcrest.Matchers.is;
 public class RangeAssert extends UIAssert<RangeAssert, Range> {
 
     @JDIAction("Assert that '{name}' volume {0}")
-    public RangeAssert volume(Matcher<Integer> volume) {
-        jdiAssert(getInt("value", element.core()), volume);
+    public RangeAssert value(Matcher<Double> value) {
+        jdiAssert(getDouble("value", element.core()), value);
         return this;
     }
-    public RangeAssert volume(int volume) { return volume(is(volume)); }
 
-    @JDIAction("Assert that '{name}' min volume {0}")
-    public RangeAssert minVolume(Matcher<Integer> min) {
-        jdiAssert(getInt("min", element.core()), min);
+    public RangeAssert value(double value) {
+        return value(is(value));
+    }
+
+    @JDIAction("Assert that '{name}' min volume is {0}")
+    public RangeAssert minValue(Matcher<Double> min) {
+        jdiAssert(element.min(), min);
         return this;
     }
-    public RangeAssert minVolume(int minVolume) { return minVolume(is(minVolume)); }
+
+    public RangeAssert minValue(double minValue) {
+        return minValue(is(minValue));
+    }
+
+
     @JDIAction("Assert that '{name}' max volume {0}")
-    public RangeAssert maxVolume(Matcher<Integer> max) {
-        jdiAssert(getInt("max", element.core()), max);
+    public RangeAssert maxValue(Matcher<Double> max) {
+        jdiAssert(element.max(), max);
         return this;
     }
-    public RangeAssert maxVolume(int maxVolume) { return maxVolume(is(maxVolume)); }
 
-    @JDIAction("Assert that '{name}' step {0}")
-    public RangeAssert step(Matcher<Integer> step) {
-        jdiAssert(getInt("step", element.core()), step);
+    public RangeAssert maxValue(double maxValue) {
+        return maxValue(is(maxValue));
+    }
+
+    @JDIAction("Assert that '{name}' step is {0}")
+    public RangeAssert step(Matcher<Double> step) {
+        jdiAssert(element.step(), step);
         return this;
     }
-    public RangeAssert step(int step) { return step(is(step)); }
+
+    public RangeAssert step(double step) {
+        return step(is(step));
+    }
 }
--- jdi-light-html/src/main/java/com/epam/jdi/light/ui/html/elements/common/Range.java
+++ jdi-light-html/src/main/java/com/epam/jdi/light/ui/html/elements/common/Range.java
@@ -7,45 +7,50 @@ import com.epam.jdi.light.elements.interfaces.base.SetValue;
 import com.epam.jdi.light.ui.html.asserts.RangeAssert;
 
 import static com.epam.jdi.light.logger.LogLevels.DEBUG;
-import static com.epam.jdi.light.ui.html.HtmlUtils.asInt;
-import static com.epam.jdi.light.ui.html.HtmlUtils.getInt;
+import static com.epam.jdi.light.ui.html.HtmlUtils.getDouble;
 
 public class Range extends UIBaseElement<RangeAssert> implements HasLabel, SetValue {
 
-    // region Actions
-    @JDIAction(value = "Get '{name}' volume", level = DEBUG)
-    public int volume() {
-        return getInt("value", uiElement);
-    }
+	// region Actions
+	@JDIAction(value = "Get '{name}' volume", level = DEBUG)
+	public double value() {
+		return getDouble("value", uiElement);
+	}
 
-    @JDIAction(value = "Get '{name}' min limit", level = DEBUG)
-    public String min() { return uiElement.attr("min"); }
+	@JDIAction(value = "Get '{name}' min limit", level = DEBUG)
+	public double min() {
+		return getDouble("min", uiElement);
+	}
 
-    @JDIAction(value = "Get '{name}' max limit", level = DEBUG)
-    public String max() { return uiElement.attr("max"); }
+	@JDIAction(value = "Get '{name}' max limit", level = DEBUG)
+	public double max() {
+		return getDouble("max", uiElement);
+	}
 
-    @JDIAction(value = "Get '{name}' step size", level = DEBUG)
-    public String step() { return uiElement.attr("step"); }
+	@JDIAction(value = "Get '{name}' step size", level = DEBUG)
+	public double step() {
+		return getDouble("step", uiElement);
+	}
 
-    @JDIAction(value = "Set volume '{0}' for '{name}'", level = DEBUG)
-    public void setVolume(int volume) {
-        uiElement.setAttribute("value", volume+"");
-    }
-    // endregion
+	public String getValue() {
+		return value() + "";
+	}
 
-    // region Set and get value for Forms
-    public void setValue(String value) {
-        setVolume(asInt(value));
-    }
-    public String getValue() {
-        return volume()+"";
-    }
-    // endregion
+	@JDIAction(value = "Set value '{0}' for '{name}'", level = DEBUG)
+	public void setValue(String value) {
+		uiElement.setAttribute("value", value + "");
+	}
 
-    // region Extend assertions
-    @Override
-    public RangeAssert is() {
-        return new RangeAssert().set(this);
-    }
-    // endregion
+	@JDIAction(value = "Set value '{0}' for '{name}'", level = DEBUG)
+	public void setValue(double value) {
+		uiElement.setAttribute("value", value + "");
+	}
+	// endregion
+
+	// region Extend assertions
+	@Override
+	public RangeAssert is() {
+		return new RangeAssert().set(this);
+	}
+	// endregion
 }
