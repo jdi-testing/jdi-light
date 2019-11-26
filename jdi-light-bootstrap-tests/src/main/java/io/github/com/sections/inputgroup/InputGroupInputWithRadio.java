a03c375f159915896c84c9c8454bc85a00aae21c
IgorGaidukEpam <Igor_Gaiduk@epam.com>
2019-11-26 14:56:17 +0300

    Reverted to the previous version of RadioButtons.java in html module. Extended bootstrap RadioButtons from html RadioButtons.
    Reverted to the previous version 3 files:
    FormValidationSection.java
    InputGroupInputWithRadio.java
    SuperheroForm.java

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/inputgroup/InputGroupInputWithRadio.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/inputgroup/InputGroupInputWithRadio.java
@@ -2,7 +2,7 @@ package io.github.com.sections.inputgroup;
 
 import com.epam.jdi.light.elements.composite.Section;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.Css;
-import com.epam.jdi.light.ui.html.elements.complex.RadioButtons;
+import com.epam.jdi.light.ui.bootstrap.elements.complex.RadioButtons;
 import com.epam.jdi.light.ui.html.elements.common.TextField;
 
 public class InputGroupInputWithRadio extends Section{
