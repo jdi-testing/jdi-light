ce235ed947daf7ebed13a66cc2c20e65efa24831
Elena_Poduzova <Elena_Poduzova@epam.com>
2019-11-27 09:51:44 +0300

    #1343 - Add strict check of parameters in test.properties

--- jdi-light/src/main/java/com/epam/jdi/light/common/Property.java
+++ jdi-light/src/main/java/com/epam/jdi/light/common/Property.java
@@ -11,6 +11,15 @@ public enum Property {
             "Value of 'page.load.strategy' must be 'normal', 'eager' or 'none'."),
     PAGE_CHECK_AFTER_OPEN("page.check.after.open", "NONE|NEW_PAGE|EVERY_PAGE",
             "Value of 'page.check.after.open' must be 'NONE', 'NEW_PAGE', 'EVERY_PAGE'.");
+            "Value of 'timeout.wait.element' must be an integer from 0 to 999."),
+    SMART_LOCATORS("smart.locators", "((#%s|\\[[a-z]+=%s\\])[;, ]{0,1})+",
+        "Value of 'smart.locators' must be a list of regular expressions."),
+    CHROME_CAPABILITIES_PATH("chrome.capabilities.path", "^([A-z]:\\/|[A-z0-9-_+.]+\\/)*([A-z0-9.]+\\/)*(chrome\\.properties)$",
+            "Value of 'chrome.capabilities.path' must be a path to a chrome.options file."),
+    FF_CAPABILITIES_PATH("ff.capabilities.path", "^([A-z]:\\/|[A-z0-9-_+.]+\\/)*([A-z0-9.]+\\/)*(ff\\.properties)$",
+            "Value of 'chrome.capabilities.path' must be a path to a ff.options file."),
+    IE_CAPABILITIES_PATH("ie.capabilities.path", "^([A-z]:\\/|[A-z0-9-_+.]+\\/)*([A-z0-9.]+\\/)*(ie\\.properties)$",
+            "Value of 'chrome.capabilities.path' must be a path to a ie.options file.");
 
     private final String name;
     private final String check;
---
+++ jdi-light/src/test/java/io/github/epam/properties/FFCapabilitiesPathTests.java
@@ -0,0 +1,51 @@
+package io.github.epam.properties;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.security.InvalidParameterException;
+import java.util.Properties;
+
+import static com.epam.jdi.light.common.Property.FF_CAPABILITIES_PATH;
+import static com.epam.jdi.light.common.PropertyValidationUtils.validateProperties;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.fail;
+
+public class FFCapabilitiesPathTests {
+
+    @DataProvider
+    public static Object[] negativeData() {
+        return new Object[]{"../../../chrome.properties",
+                            "../../../ff.txt",
+                            "C:ff.properties"};
+    }
+
+    @DataProvider
+    public static Object[] positiveData() {
+        return new Object[]{"../../../ff.properties",
+                "C:/ff.properties",
+                "../ff.properties",
+                "C:/directory/ff.properties"};
+    }
+
+    @Test(dataProvider = "negativeData")
+    public void negativeTest(String value) {
+        Properties properties = new Properties();
+        properties.setProperty(FF_CAPABILITIES_PATH.getName(), value);
+        try {
+            validateProperties(properties);
+            fail("Value '" + value + "' should not be valid for this test.");
+        } catch (InvalidParameterException exp) {
+            String expMessage = exp.getMessage();
+            assertEquals(expMessage, FF_CAPABILITIES_PATH.getExMsg() + " See example: https://jdi-docs.github.io/jdi-light/#driver-settings");
+        }
+    }
+
+    @Test(dataProvider = "positiveData")
+    public void positiveTest(String value) {
+        Properties properties = new Properties();
+        properties.setProperty(FF_CAPABILITIES_PATH.getName(), value);
+        validateProperties(properties);
+    }
+
+}
