a807357841bf76c8384149d6d5c03ee7138245e4
Marina Lalazarova <marina_lalazarova@epam.com>
2019-12-04 12:45:25 +0300

    Spinner class has now method getColor()

--- jdi-light-bootstrap/src/main/java/com/epam/jdi/light/ui/bootstrap/elements/common/Spinner.java
+++ jdi-light-bootstrap/src/main/java/com/epam/jdi/light/ui/bootstrap/elements/common/Spinner.java
@@ -1,17 +1,33 @@
 package com.epam.jdi.light.ui.bootstrap.elements.common;
 
-import com.epam.jdi.light.elements.common.UIElement;
+import com.epam.jdi.light.elements.base.UIBaseElement;
+import com.epam.jdi.light.ui.bootstrap.asserts.SpinnerAssert;
 import com.epam.jdi.tools.Timer;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * To see an example of Spinner web element please visit https://getbootstrap.com/docs/4.3/components/spinners/
  */
 
-public class Spinner extends UIElement {
+public class Spinner extends UIBaseElement<SpinnerAssert> {
+
     public Spinner disappearAfter(int timeoutSec) {
         assertThat().displayed();
         new Timer(timeoutSec * 1000).wait(() -> core().isHidden());
         assertThat().hidden();
         return this;
+    }
+    public String getColor(){
+        String str = core().getAttribute("class");
+        Pattern pattern = Pattern.compile("text-.+");
+        Matcher matcher = pattern.matcher(str);
+        return matcher.find() ? str.substring(matcher.start(), matcher.end()) : "";
+    }
+
+    @Override
+    public SpinnerAssert is() {
+        return new SpinnerAssert().set(this);
     }
 }
