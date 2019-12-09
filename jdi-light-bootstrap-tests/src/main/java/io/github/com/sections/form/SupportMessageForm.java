1f87f76d96a0a73f39acd934a9b5503ab3e0534f
Ivan Protasov <ivan_protasov1@epam.com>
2019-12-09 12:57:21 +0300

    1307 added searching by Value strategy

--- jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/form/SupportMessageForm.java
+++ jdi-light-bootstrap-tests/src/main/java/io/github/com/sections/form/SupportMessageForm.java
@@ -1,9 +1,7 @@
 package io.github.com.sections.form;
 
-import com.epam.jdi.light.elements.pageobjects.annotations.Name;
-import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
-import com.epam.jdi.light.ui.html.elements.common.Button;
 import com.epam.jdi.light.ui.bootstrap.elements.composite.Form;
+import com.epam.jdi.light.ui.html.elements.common.Button;
 import com.epam.jdi.light.ui.html.elements.common.TextArea;
 import com.epam.jdi.light.ui.html.elements.common.TextField;
 import io.github.com.entities.SupportMessage;
@@ -13,11 +11,7 @@ public class SupportMessageForm extends Form<SupportMessage> {
     public TextField supportEmail;
     public TextArea supportMessage;
 
-    @UI("#support-button-submit")
-    @Name(value = "submit")
     public Button supportButtonSubmit;
 
-    @UI("#support-button-clear")
-    @Name(value = "clear")
     public Button supportButtonClear;
 }
