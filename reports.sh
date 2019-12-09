8442b582ddf90fac0174af77bb28b98465563e9b
Maria Fedorova <maria_fedorova1@epam.com>
2019-12-09 17:58:14 +0300

    fix codacy issue

--- reports.sh
+++ reports.sh
@@ -17,7 +17,7 @@ function collectRelevantComments(){
     curl -H "Authorization: token ${GIT_COMMENT_USER}" \
          -X GET  "${url}"\
          > ${fileName}
-    cat ${fileName} | jq ".[].body" | grep "${matchPattern}"| awk '{print $3}' |sed "s/\"//g" #return list
+    jq ".[].body" ${fileName} | grep "${matchPattern}"| awk '{print $3}' | sed "s/\"//g" #return list
 }
 
 function sendComment() {
