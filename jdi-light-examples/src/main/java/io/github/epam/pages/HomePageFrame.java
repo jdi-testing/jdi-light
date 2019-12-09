5e0f62830e5f553322b1b6adf0e910e575d3c09c
Sergei_Plekhanov <sergei_plekhanov@epam.com>
2019-12-09 12:21:32 +0300

    1104 Frames examples fix

--- jdi-light-examples/src/main/java/io/github/epam/pages/HomePageFrame.java
+++ jdi-light-examples/src/main/java/io/github/epam/pages/HomePageFrame.java
@@ -1,9 +1,13 @@
 package io.github.epam.pages;
 
+import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.light.elements.composite.Section;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.Css;
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import org.openqa.selenium.WebElement;
 
 public class HomePageFrame extends Section {
 	@Css("img#user-icon") public WebElement userIcon;
+    @UI("#user-name")
+    public UIElement userName;
 }
--- jdi-light-html-tests/src/main/java/io/github/com/pages/HomePageFrame.java
+++ jdi-light-html-tests/src/main/java/io/github/com/pages/HomePageFrame.java
@@ -1,8 +1,13 @@
 package io.github.com.pages;
 
+import com.epam.jdi.light.elements.common.UIElement;
+import com.epam.jdi.light.elements.composite.Section;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.Css;
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.ui.html.elements.common.Icon;
 
-public class HomePageFrame {
+public class HomePageFrame extends Section {
 	@Css("img#user-icon") public Icon userIcon;
+	@UI("#user-name")
+	public UIElement userName;
 }
