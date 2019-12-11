2d66f73407c5a711153a0b56c153ece84f008fed
Sergei_Plekhanov <sergei_plekhanov@epam.com>
2019-12-11 17:26:57 +0300

    1552 change data inside the frame in main page

--- jdi-light-examples/src/main/java/io/github/com/StaticSite.java
+++ jdi-light-examples/src/main/java/io/github/com/StaticSite.java
@@ -27,30 +27,41 @@ import static com.epam.jdi.light.common.CheckTypes.MATCH;
 @SuppressWarnings("PMD.ClassNamingConventions")
 @JSite("https://jdi-testing.github.io/jdi-light/")
 public class StaticSite {
-	@Url("/index.html")  @Title(value = "Home", validate = CONTAINS)
-	public static HomePage homePage;
-	@Url("/metals-colors.html") @Title("Metal and Colors")
-	public static MetalAndColorsPage metalAndColorsPage;
-	@Url(value = "/metals-colors.html{0}", template = "metals-colors.html") @Title("Metal and Colors")
-	public static MetalAndColorsPage metalAndColorsPageParams;
-	@Url("/contacts.html") @Title(value = ".*tact \\w{4}", validate = MATCH)
-	public static ContactFormPage contactFormPage;
-	@Url("/performance.html") @Title("Performance page")
-	public static PerformancePage tablePage;
-	@Url("/search.html")
-	public static SearchPage searchPage;
+    @Url("/index.html")
+    @Title(value = "Home", validate = CONTAINS)
+    public static HomePage homePage;
+    @Url("/metals-colors.html")
+    @Title("Metal and Colors")
+    public static MetalAndColorsPage metalAndColorsPage;
+    @Url(value = "/metals-colors.html{0}", template = "metals-colors.html")
+    @Title("Metal and Colors")
+    public static MetalAndColorsPage metalAndColorsPageParams;
+    @Url("/contacts.html")
+    @Title(value = ".*tact \\w{4}", validate = MATCH)
+    public static ContactFormPage contactFormPage;
+    @Url("/performance.html")
+    @Title("Performance page")
+    public static PerformancePage tablePage;
+    @Url("/search.html")
+    public static SearchPage searchPage;
 
-	@Css("[ui=label]") public static List<WebElement> navigation;
-	@Css("[ui=label]") public static WebList navigationL;
-	@UI("//*[@ui='label']//*[contains(text(),'%s')]") public static WebList navigationS;
-	// TODO fix slow @UI("[ui=label][*'%s']") public static WebList navigationS;
-	@UI("//*[@ui='label']//*[contains(text(),'%s')]") public static UIElement menu;
-	// TODO fix slow @UI("[ui=label][*'%s']") public static UIElement menu;
+    @Css("[ui=label]")
+    public static List<WebElement> navigation;
+    @Css("[ui=label]")
+    public static WebList navigationL;
+    @UI("//*[@ui='label']//*[contains(text(),'%s')]")
+    public static WebList navigationS;
+    // TODO fix slow @UI("[ui=label][*'%s']") public static WebList navigationS;
+    @UI("//*[@ui='label']//*[contains(text(),'%s')]")
+    public static UIElement menu;
+    // TODO fix slow @UI("[ui=label][*'%s']") public static UIElement menu;
 
-	@Css("header") public static Header header;
-	@Css("footer") public static Footer footer;
+    @Css("header")
+    public static Header header;
+    @Css("footer")
+    public static Footer footer;
 
     @Frame("frame")
     public static HomePageFrame iframe;
-	public static GithubPage githubPage;
+    public static GithubPage githubPage;
 }
