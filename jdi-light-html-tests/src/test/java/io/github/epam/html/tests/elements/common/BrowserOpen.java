a2cd7b788200bf18248b1525942b5a13f6bcdd46
Dmitry_Lebedev <Dmitry_Lebedev1@epam.com>
2019-12-16 13:01:03 +0300

    make simle test without PO

--- jdi-light-html-tests/src/test/java/io/github/epam/html/tests/elements/common/BrowserOpen.java
+++ jdi-light-html-tests/src/test/java/io/github/epam/html/tests/elements/common/BrowserOpen.java
@@ -1,16 +1,22 @@
 package io.github.epam.html.tests.elements.common;
 
+import com.epam.jdi.light.driver.WebDriverFactory;
+import com.epam.jdi.light.driver.WebDriverUtils;
+import com.epam.jdi.light.settings.WebSettings;
+import com.sun.deploy.security.WSecureRandom;
 import io.github.epam.TestsInit;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static io.github.com.StaticSite.html5Page;
 
-public class BrowserOpen implements TestsInit {
+public class BrowserOpen  {
 
     @Test
     public void before() {
-        html5Page.open();
+        WebSettings.init();
+        WebDriverFactory.getDriver();
+        WebDriverUtils.killAllSeleniumDrivers();
     }
 
 }
