c4b7e9ca1accda7cbd605d980fbf77945cf0e4a0
Sergei_Plekhanov <sergei_plekhanov@epam.com>
2019-12-17 18:45:29 +0300

    1569 clean browser open page

--- jdi-light-html-tests/src/test/java/io/github/epam/html/tests/elements/common/BrowserOpen.java
+++ jdi-light-html-tests/src/test/java/io/github/epam/html/tests/elements/common/BrowserOpen.java
@@ -1,39 +1,20 @@
 package io.github.epam.html.tests.elements.common;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.epam.jdi.light.driver.WebDriverFactory;
+import com.epam.jdi.light.driver.WebDriverUtils;
+import com.epam.jdi.light.settings.WebSettings;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
-import static com.epam.jdi.light.settings.WebSettings.logger;
 
 public class BrowserOpen {
 
     private WebDriver driver;
 
-    @BeforeClass
-    public static void setupClass() {
-        logger.info("Setting up driver");
-        WebDriverManager.chromedriver().setup();
-        logger.info("Driver is ready");
-    }
-
     @Test
     public void before() {
-        logger.info("Creating new Chromedriver");
-        driver = new ChromeDriver();
-        logger.info("Chromedriver instance has been created");
-        logger.info("Opening page...");
-        driver.get("https://jdi-testing.github.io/jdi-light/");
-        logger.info("Page opened");
-        logger.info("Getting page title");
-        logger.info(driver.getTitle());
-        driver.quit();
-
-//        WebSettings.init();
-//        WebDriverFactory.getDriver();
-//        WebDriverUtils.killAllSeleniumDrivers();
+        WebSettings.init();
+        WebDriverFactory.getDriver();
+        WebDriverUtils.killAllSeleniumDrivers();
 
     }
 
