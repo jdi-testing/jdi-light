2d528038141bdccd15e7a8d6b9fa9f51c357c084
Sergei_Plekhanov <sergei_plekhanov@epam.com>
2019-12-18 13:30:22 +0300

    1569 travis fix

--- jdi-light/src/main/java/com/epam/jdi/light/settings/WebSettings.java
+++ jdi-light/src/main/java/com/epam/jdi/light/settings/WebSettings.java
@@ -61,6 +61,7 @@ import static com.epam.jdi.light.settings.TimeoutSettings.TIMEOUT;
 import static com.epam.jdi.tools.EnumUtils.getAllEnumValues;
 import static com.epam.jdi.tools.LinqUtils.filter;
 import static com.epam.jdi.tools.LinqUtils.first;
+import static com.epam.jdi.tools.PathUtils.mergePath;
 import static com.epam.jdi.tools.PropertyReader.fillAction;
 import static com.epam.jdi.tools.PropertyReader.getProperty;
 import static java.lang.Integer.parseInt;
@@ -294,7 +295,7 @@ public class WebSettings {
             }
         } else {
             // TODO use mergePath macos and windows
-            String propertyFilePath = "/../../target/classes/" + path;
+            String propertyFilePath = mergePath("/../../target/classes/" + path);
             properties = PropertyReader.getProperties(propertyFilePath);
         }
         return properties;
