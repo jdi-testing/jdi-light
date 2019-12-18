b3fb072b7e1f7f5d4dbf7edf13a98392feab1e8a
Sergei_Plekhanov <sergei_plekhanov@epam.com>
2019-12-18 16:12:49 +0300

    Revert "1569 clean browser open page"

--- jdi-light-html-tests/src/test/java/io/github/epam/html/tests/elements/common/BrowserOpen.java
+++ jdi-light-html-tests/src/test/java/io/github/epam/html/tests/elements/common/BrowserOpen.java
@@ -6,10 +6,8 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.io.File;
 import java.util.HashMap;
 
-import static com.epam.jdi.light.driver.get.DriverData.DOWNLOADS_DIR;
 import static com.epam.jdi.light.settings.WebSettings.logger;
 
 public class BrowserOpen {
@@ -27,10 +25,6 @@ public class BrowserOpen {
 
             HashMap<String, Object> chromePrefs = new HashMap<>();
             chromePrefs.put("credentials_enable_service", false);
-            new File(DOWNLOADS_DIR).mkdirs();
-            chromePrefs.put("download.default_directory", DOWNLOADS_DIR);
-            chromePrefs.put("profile.default_content_setting_values.notifications", 0);
-            chromePrefs.put("profile.default_content_settings.popups", 0);
             chromePrefs.put("profile.password_manager_enabled", false);
 
             options.addArguments("--headless");
