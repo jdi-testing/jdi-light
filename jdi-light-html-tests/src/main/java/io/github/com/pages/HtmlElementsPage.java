f5776c1869eff2fa28a4d0c59981451cd9e9ea0e
Yulia_Litvin <iulia.litwin@yandex.ru>
2019-12-19 17:38:06 +0300

    remove added element

--- jdi-light-html-tests/src/main/java/io/github/com/pages/HtmlElementsPage.java
+++ jdi-light-html-tests/src/main/java/io/github/com/pages/HtmlElementsPage.java
@@ -60,7 +60,6 @@ public class HtmlElementsPage extends WebPage {
     public static ColorPicker colorPicker;
     @UI("#disabled-picker") public static ColorPicker disabledPicker;
 
-    public static TextField volumeValue;
     public static Range volume;
     // @UI("#volume") public static Range volume;
     @UI("[type=range][disabled]") public static Range disabledRange;
