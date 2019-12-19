84fa335bfedc5c485c3b880e99e1f14172cc29a8
Sergei_Plekhanov <sergei_plekhanov@epam.com>
2019-12-19 10:46:37 +0300

    1569 clean browser open page

--- jdi-light/src/main/java/com/epam/jdi/light/driver/WebDriverUtils.java
+++ jdi-light/src/main/java/com/epam/jdi/light/driver/WebDriverUtils.java
@@ -54,7 +54,7 @@ public final class WebDriverUtils {
     }
 
     private static void killMacOSDriverProcesses(String browserName) {
-        String name = null;
+        String name;
         switch (browserName.toLowerCase()) {
             case "firefox":
                 name = "geckodriver";
@@ -62,7 +62,9 @@ public final class WebDriverUtils {
             case "chrome":
                 name = "chromedriver";
                 break;
-
+            default:
+                name = null;
+                break;
         }
         if (name != null) {
             killAllMacOSDriverProcessesByName(name);
