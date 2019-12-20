41c485eab854b4a70429e2b13f9460dae90a0b94
ArKhmeln <aakhmelinin@gmail.com>
2019-12-20 16:02:46 +0300

    title test examples add

---
+++ jdi-light-html-tests/src/test/java/io/github/epam/example/common/TitleExampleTests.java
@@ -0,0 +1,45 @@
+package io.github.epam.example.common;
+
+import io.github.epam.TestsInit;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static com.epam.jdi.light.elements.common.Alerts.acceptAlert;
+import static com.epam.jdi.light.elements.common.Alerts.getAlertText;
+import static io.github.com.StaticSite.html5Page;
+import static io.github.com.pages.HtmlElementsPage.jdiTitle;
+import static io.github.epam.html.tests.elements.BaseValidations.baseValidation;
+import static io.github.epam.html.tests.site.steps.States.shouldBeLoggedIn;
+import static org.testng.Assert.assertEquals;
+
+public class TitleExampleTests implements TestsInit {
+
+    @BeforeMethod
+    public void before() {
+        shouldBeLoggedIn();
+        html5Page.shouldBeOpened();
+    }
+    String titleText = "JDI TESTING PLATFORM";
+
+    @Test
+    public void textTest() {
+        jdiTitle.is().text(titleText);
+    }
+
+    @Test
+    public void valueTest() {
+        jdiTitle.is().value(titleText);
+    }
+
+    @Test
+    public void clickTest() {
+        jdiTitle.click();
+        assertEquals(getAlertText(), "JDI Title");
+        acceptAlert();
+    }
+
+    @Test
+    public void baseValidationTest() {
+        baseValidation(jdiTitle);
+    }
+}
