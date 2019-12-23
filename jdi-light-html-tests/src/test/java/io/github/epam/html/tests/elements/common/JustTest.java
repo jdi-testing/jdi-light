aff02f16c2aa86e93f4bcec4981fc62075f41e9e
Sergei_Plekhanov <sergei_plekhanov@epam.com>
2019-12-23 17:43:13 +0300

    1589 kill driver on linux

---
+++ jdi-light-html-tests/src/test/java/io/github/epam/html/tests/elements/common/JustTest.java
@@ -0,0 +1,36 @@
+package io.github.epam.html.tests.elements.common;
+
+import org.testng.annotations.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static com.epam.jdi.light.settings.WebSettings.logger;
+
+public class JustTest {
+
+    @Test
+    public void justTest(){
+        try {
+            ProcessBuilder pb = new ProcessBuilder("sh", "ps", "-aux", "grep java");
+            Process process = pb.start();
+            List<String> results = readOutput(process.getInputStream());
+            logger.info("!!! " + results);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private List<String> readOutput(InputStream inputStream) throws IOException {
+        try (BufferedReader output = new BufferedReader(new InputStreamReader(inputStream))) {
+            return output.lines()
+                    .collect(Collectors.toList());
+        }
+    }
+}
