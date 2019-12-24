9ab66b12e0bdc57e75b7afd0290c97c4427161bf
Sergei_Plekhanov <sergei_plekhanov@epam.com>
2019-12-24 18:29:07 +0300

    1589 kill driver on linux

--- jdi-light/src/main/java/com/epam/jdi/light/common/LinuxProcessUtils.java
+++ jdi-light/src/main/java/com/epam/jdi/light/common/LinuxProcessUtils.java
@@ -1,12 +1,8 @@
 package com.epam.jdi.light.common;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
-import static com.epam.jdi.light.settings.WebSettings.logger;
 import static com.epam.jdi.tools.StringUtils.inputStreamToList;
 
 /**
@@ -21,12 +17,9 @@ public class LinuxProcessUtils {
         List<String> chrome;
         try {
             chrome = getPIDsByNamePart(rootNamePart);
-            logger.info("!!! rootNamePart " + rootNamePart);
-            logger.info("!!! GET CHROMEDRIVER PIDS " + chrome);
             for (String pid : chrome) {
                 killProcessByPid(pid);
             }
-            killProcessesByNamePart(rootNamePart);
         } catch (InterruptedException | IOException e) {
             throw new RuntimeException("Can't kill drivers");
         }
@@ -34,28 +27,12 @@ public class LinuxProcessUtils {
 
     private static void killProcessByPid(String pid) throws IOException, InterruptedException {
         Process process = new ProcessBuilder(
-                "pgrep", "chromedriver")
-                .start();
-        process.waitFor();
-
-        logger.info("!!! READ CHROMEDRIVER PROCESSES " + inputStreamToList(process.getInputStream()));
-        Process process2 = new ProcessBuilder(
                 "kill", "-9", pid)
                 .start();
-        process2.waitFor();
-
-        logger.info("!!! CHROMEDRIVER PROCESSES WAS KILLED");
-
-        Process process3 = new ProcessBuilder(
-                "pgrep", "chromedriver")
-                .start();
-        process3.waitFor();
-
-        logger.info("!!! READ CHROME PROCESSES " + inputStreamToList(process3.getInputStream()));
+        process.waitFor();
     }
 
     /**
-     *
      * @param value
      * @return
      * @throws IOException
@@ -63,43 +40,9 @@ public class LinuxProcessUtils {
      */
     private static List<String> getPIDsByNamePart(String value) throws IOException, InterruptedException {
         Process process = new ProcessBuilder(
-            "pgrep", value)
-            .start();
+                "pgrep", value)
+                .start();
         process.waitFor();
         return inputStreamToList(process.getInputStream());
     }
-
-    /**
-     *
-     * @param pid
-     */
-    private static void killChildProcesses(int pid) {
-        String ppid = String.valueOf("-P "+pid);
-        killProcessWithArgs(Collections.singletonList(ppid));
-    }
-
-    /**
-     *
-     * @param name
-     */
-    public static void killProcessesByNamePart(String name) {
-        killProcessWithArgs(Arrays.asList("-aif", name));
-    }
-
-    /**
-     *
-     * @param args
-     */
-    private static void killProcessWithArgs(List<String> args) {
-        List<String> list = new ArrayList<>();
-        list.add("/usr/bin/pkill");
-        list.addAll(args);
-        try {
-            Process child = new ProcessBuilder(list.toArray(new String[list.size()])).start();
-            child.waitFor();
-        } catch (InterruptedException | IOException e) {
-            throw new RuntimeException("Can't kill process: " + e.getMessage());
-        }
-    }
-
 }
