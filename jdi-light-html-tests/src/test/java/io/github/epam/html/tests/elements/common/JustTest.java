b0a478ce74e8d063864c7dbf735793e9aa3aa0ee
Sergei_Plekhanov <sergei_plekhanov@epam.com>
2019-12-24 14:19:23 +0300

    1589 kill driver on linux

--- jdi-light-html-tests/src/test/java/io/github/epam/html/tests/elements/common/JustTest.java
+++ jdi-light-html-tests/src/test/java/io/github/epam/html/tests/elements/common/JustTest.java
@@ -28,7 +28,7 @@ public class JustTest {
     @Test
     public void pgrepTest() throws InterruptedException, IOException {
         Process process = new ProcessBuilder(
-                "pgrep", "-afi", "java")
+                "ps", "aux")
                 .start();
         process.waitFor();
         logger.info("!!! " + readOutput(process.getInputStream()));
