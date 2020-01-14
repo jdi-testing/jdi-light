e8f3cadda988fb51f2ed77fe0f84388d5f81e3fe
Yulia_Litvin <iulia.litwin@yandex.ru>
2020-01-14 12:07:19 +0300

    form example

--- jdi-light-html-tests/src/test/java/io/github/epam/example/composite/FormExampleTests.java
+++ jdi-light-html-tests/src/test/java/io/github/epam/example/composite/FormExampleTests.java
@@ -4,8 +4,6 @@ import io.github.epam.TestsInit;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.List;
-
 import static io.github.com.entities.Users.ALL_EXCEPT_NAME_FILLED_DEFAULT_CONTACT;
 import static io.github.com.entities.Users.DEFAULT_CONTACT;
 import static io.github.com.entities.Users.ONLY_NAME_FILLED_DEFAULT_CONTACT;
@@ -13,8 +11,6 @@ import static io.github.com.pages.ContactFormPage.main;
 import static io.github.com.pages.LogSidebar.lastLogEntry;
 import static io.github.epam.html.tests.site.steps.States.shouldContactPageBeOpenedAndRefreshed;
 import static org.hamcrest.Matchers.containsString;
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
 
 public class FormExampleTests implements TestsInit {
 
@@ -30,23 +26,10 @@ public class FormExampleTests implements TestsInit {
     }
 
     @Test
-    public void submitTextToContactFormCustomTest() {
-        main.contactFormCustom.submit("Roman");
-        main.contactFormCustom.check(ONLY_NAME_FILLED_DEFAULT_CONTACT);
-        checkContactFormCustomSubmitted();
-    }
-
-    @Test
     public void submitEntityToContactFormCustomTest() {
         main.contactFormCustom.submit(DEFAULT_CONTACT);
         main.contactFormCustom.check(DEFAULT_CONTACT);
         checkContactFormCustomSubmitted();
-    }
-
-    @Test
-    public void submitTextToContactFormCustomUsingCustomButtonTest() {
-        main.contactFormCustom.submit("Roman", "custom");
-        main.contactFormCustom.check(ONLY_NAME_FILLED_DEFAULT_CONTACT);
     }
 
     @Test
@@ -71,18 +54,10 @@ public class FormExampleTests implements TestsInit {
     }
 
     @Test
-    public void verifyMethodPositiveTest() {
-        main.contactFormCustom.fill(DEFAULT_CONTACT);
-        List<String> verified = main.contactFormCustom.verify(DEFAULT_CONTACT);
-        assertEquals(verified.size(), 0);
-    }
-
-    @Test
-    public void verifyMethodNegativeTest() {
-        main.contactFormCustom.fill(ALL_EXCEPT_NAME_FILLED_DEFAULT_CONTACT);
-        List<String> verified = main.contactFormCustom.verify(DEFAULT_CONTACT);
-        assertEquals(verified.size(), 1);
-        assertTrue(verified.get(0).contains("Roman"));
+    public void submitTextToContactFormCustomTest() {
+        main.contactFormCustom.submit("Roman");
+        main.contactFormCustom.check(ONLY_NAME_FILLED_DEFAULT_CONTACT);
+        checkContactFormCustomSubmitted();
     }
 
     private void checkContactFormCustomSubmitted() {
