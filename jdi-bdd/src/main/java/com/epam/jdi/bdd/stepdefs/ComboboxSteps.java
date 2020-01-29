5f2dcf435b621805341c94a6d524909896ebb28e
Eugene Philippov <eugene.v.philippov@gmail.com>
2020-01-29 14:26:25 +0300

    Fixed combobox\datalist tests. Tried to fix allure report.

--- jdi-bdd/src/main/java/com/epam/jdi/bdd/stepdefs/ComboboxSteps.java
+++ jdi-bdd/src/main/java/com/epam/jdi/bdd/stepdefs/ComboboxSteps.java
@@ -1,6 +1,7 @@
 package com.epam.jdi.bdd.stepdefs;
 
 import com.epam.jdi.light.elements.interfaces.complex.IsCombobox;
+import com.epam.jdi.light.ui.html.elements.complex.DataListOptions;
 import cucumber.api.java.en.When;
 
 import static com.epam.jdi.light.elements.init.entities.collection.EntitiesCollection.getUI;
@@ -14,8 +15,17 @@ public class ComboboxSteps {
         return getUI(name, IsCombobox.class);
     }
 
+    public static DataListOptions options(String name) {
+        return getUI(name, DataListOptions.class);
+    }
+
     @When("^select index (\\d+) in \"([^\"]*)\"$")
     public void comboboxSelect(int index, String name) {
+        combobox(name).select(index);
+    }
+
+    @When("^select value \"([^\"]*)\" in \"([^\"]*)\"$")
+    public void comboboxSelect(String index, String name) {
         combobox(name).select(index);
     }
 }
