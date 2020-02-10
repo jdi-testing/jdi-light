2b9e4b85f8c974845cc8b5175ad22564d593e372
Roman Iovlev <Roman_Iovlev@epam.com>
2020-02-11 01:07:13 +0300

    optimize imports

--- jdi-light/src/main/java/com/epam/jdi/light/elements/complex/table/BaseTable.java
+++ jdi-light/src/main/java/com/epam/jdi/light/elements/complex/table/BaseTable.java
@@ -3,7 +3,6 @@ package com.epam.jdi.light.elements.complex.table;
 import com.epam.jdi.light.asserts.generic.HasAssert;
 import com.epam.jdi.light.asserts.generic.table.BaseTableAssert;
 import com.epam.jdi.light.common.JDIAction;
-import com.epam.jdi.light.driver.WebDriverByUtils;
 import com.epam.jdi.light.elements.base.UIBaseElement;
 import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.light.elements.complex.IHasSize;
