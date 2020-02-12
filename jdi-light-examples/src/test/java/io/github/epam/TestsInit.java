bc2d9291414699b593d295a5cd2efce2a11196b7
Sergei Solarev <sergei_solarev@epam.com>
2020-02-12 16:56:30 +0300

    Update

--- jdi-light-examples/src/test/java/io/github/epam/TestsInit.java
+++ jdi-light-examples/src/test/java/io/github/epam/TestsInit.java
@@ -19,6 +19,6 @@ public class TestsInit extends TestNGListener {
     }
     @AfterSuite(alwaysRun = true)
     public static void tearDown() {
-        killAllSeleniumDrivers();;
+        killAllSeleniumDrivers();
     }
 }
