167fa4e45e5294134b345fbb5bcee75da2404499
Roman Iovlev <roman_iovlev@epam.com>
2020-02-18 23:39:38 +0300

    on bdd tests all

--- jdi-bdd-tests/src/test/java/cucmberTests/Runner.java
+++ jdi-bdd-tests/src/test/java/cucmberTests/Runner.java
@@ -23,7 +23,7 @@ import static io.github.com.pages.Header.userIcon;
 @CucumberOptions(
         features = "classpath:features"
         , glue = {"com.epam.jdi.bdd", "cucmberTests"}
-        , tags = {"@textarea"}
+        //, tags = {"@textarea"}
 )
 public class Runner extends AbstractTestNGCucumberTests {
     @BeforeClass
