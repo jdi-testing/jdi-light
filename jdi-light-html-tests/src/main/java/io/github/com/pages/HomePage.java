bdc417b9da5c01502fee63a2b81b6aeca93606b9
Ivan Protasov <ivan_protasov1@epam.com>
2020-02-20 15:55:13 +0300

    1713 - Appium configuration
    revert un needed changes

--- jdi-light-html-tests/src/main/java/io/github/com/pages/HomePage.java
+++ jdi-light-html-tests/src/main/java/io/github/com/pages/HomePage.java
@@ -2,7 +2,6 @@ package io.github.com.pages;
 
 import com.epam.jdi.mobile.elements.common.Label;
 import com.epam.jdi.mobile.elements.composite.WebPage;
-import com.epam.jdi.mobile.ui.html.elements.common.Button;
 import com.epam.jdi.mobile.elements.pageobjects.annotations.locators.Css;
 import com.epam.jdi.mobile.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.mobile.ui.html.elements.common.Link;
@@ -12,6 +11,4 @@ public class HomePage extends WebPage {
 	@Css("h3[name='main-title']") public static Label mainTitle;
 	@Css(".main-txt") public static Text jdiText;
 	@UI("['JDI Github']") public static Link githubLink;
-	@UI("#user-icon")
-	public static Button userIcon;
 }
