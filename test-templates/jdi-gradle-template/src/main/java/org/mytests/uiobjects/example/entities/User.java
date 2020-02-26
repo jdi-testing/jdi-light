5ed3aa982fe93ec2b78191e837bbfbd5dd23e21d
Roman Iovlev <Roman_Iovlev@epam.com>
2020-02-26 17:18:01 +0300

    remove unused repos

--- test-templates/jdi-gradle-template/src/main/java/org/mytests/uiobjects/example/entities/User.java
+++
@@ -1,8 +0,0 @@
-package org.mytests.uiobjects.example.entities;
-
-import com.epam.commons.DataClass;
-
-public class User extends DataClass {
-    public String name = "epam";
-    public String password = "1234";
-}
--- test-templates/jdi-gradle-template/src/main/java/org/mytests/uiobjects/example/site/pages/HomePage.java
+++
@@ -1,8 +0,0 @@
-package org.mytests.uiobjects.example.site.pages;
-
-import com.epam.jdi.uitests.web.selenium.elements.composite.WebPage;
-import com.epam.jdi.uitests.web.selenium.elements.pageobjects.annotations.JPage;
-
-@JPage(url = "/index.html", title = "Home Page")
-public class HomePage extends WebPage {
-}
