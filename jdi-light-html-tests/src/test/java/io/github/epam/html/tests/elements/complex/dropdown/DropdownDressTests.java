e5dce26cbe0affee0274620dc22e3b4150d47f51
Eugene Philippov <eugene.v.philippov@gmail.com>
2020-03-03 12:53:32 +0300

    Reverted changes for DropdownDressTests and DropdownExpand

--- jdi-light-html-tests/src/test/java/io/github/epam/html/tests/elements/complex/dropdown/DropdownDressTests.java
+++ jdi-light-html-tests/src/test/java/io/github/epam/html/tests/elements/complex/dropdown/DropdownDressTests.java
@@ -6,12 +6,10 @@ import org.testng.annotations.Test;
 
 import java.util.List;
 
-import static com.epam.jdi.light.asserts.core.SoftAssert.assertResults;
-import static com.epam.jdi.light.asserts.core.SoftAssert.assertSoft;
-import static com.epam.jdi.light.asserts.core.SoftAssert.jdiAssert;
 import static com.epam.jdi.light.common.Exceptions.safeException;
 import static io.github.com.StaticSite.html5Page;
-import static io.github.com.pages.HtmlElementsPage.*;
+import static io.github.com.pages.HtmlElementsPage.disabledDropdown;
+import static io.github.com.pages.HtmlElementsPage.dressCode2;
 import static io.github.epam.html.tests.elements.BaseValidations.baseValidation;
 import static io.github.epam.html.tests.elements.complex.enums.Dress.*;
 import static io.github.epam.html.tests.site.steps.States.shouldBeLoggedIn;
@@ -27,34 +25,31 @@ import static org.testng.Assert.fail;
  */
 
 public class DropdownDressTests implements TestsInit {
-    
+
     @BeforeMethod
     public void before() {
         shouldBeLoggedIn();
         html5Page.shouldBeOpened();
         dressCode2.select(text);
     }
-    
     String text = "Casual";
-    
+
     @Test
     public void selectTest() {
         dressCode2.select("Pirate");
         assertEquals(dressCode2.getValue(), "Pirate");
     }
-    
+
     @Test
     public void selectEnumTest() {
         dressCode2.select(Fancy);
         assertEquals(dressCode2.getValue(), "Fancy");
     }
-    
     @Test
     public void selectNumTest() {
         dressCode2.select(1);
         assertEquals(dressCode2.getValue(), "Fancy");
     }
-    
     @Test
     public void selectedTest() {
         dressCode2.select(Pirate);
@@ -62,7 +57,7 @@ public class DropdownDressTests implements TestsInit {
         assertEquals(dressCode2.getValue(), "Pirate");
         assertEquals(dressCode2.getText(), "Pirate");
     }
-    
+
     @Test
     public void negativeDropdownTest() {
         try {
@@ -73,93 +68,69 @@ public class DropdownDressTests implements TestsInit {
             assertThat(safeException(ex), containsString("Cannot locate option with text: Unknown"));
         }
     }
-    
+
     @Test
     public void sizeTest() {
         assertEquals(dressCode2.size(), 4);
     }
-    
     @Test
     public void disabledTest() {
         if (isFireFox()) return;
         disabledDropdown.select("Pirate");
         assertEquals(disabledDropdown.getValue(), "Disabled");
     }
-    
+
     @Test
     public void labelTest() {
         assertEquals(dressCode2.label().getText(), "Dress code:");
         dressCode2.label().is().text(containsString("Dress"));
     }
-    
+
     @Test
     public void isValidationTest() {
         dressCode2.is().selected("Casual");
         dressCode2.is().selected(Casual);
     }
-    
+
     @Test
     public void valuesTest() {
         List<String> values = dressCode2.values();
         assertEquals(values, asList("Fancy", "Casual", "Disabled", "Pirate"));
     }
-    
+
     @Test
     public void assertValuesTest() {
         dressCode2.assertThat().values(hasItem("Pirate"));
         dressCode2.assertThat().values(hasItems("Fancy", "Pirate", "Casual", "Disabled"));
         dressCode2.assertThat()
-                .disabled(hasItem("Disabled"))
-                .enabled(not(hasItem("Disabled")))
-                .enabled(hasItems("Pirate", "Fancy"));
+            .disabled(hasItem("Disabled"))
+            .enabled(not(hasItem("Disabled")))
+            .enabled(hasItems("Pirate", "Fancy"));
     }
-    
+
     @Test
     public void listEnabledTest() {
         assertEquals(dressCode2.listEnabled(), asList("Fancy", "Casual", "Pirate"));
         dressCode2.is()
-                .enabled(hasItems("Fancy", "Pirate", "Casual"))
-                .enabled(not(hasItem("Disabled")));
+            .enabled(hasItems("Fancy", "Pirate", "Casual"))
+            .enabled(not(hasItem("Disabled")));
     }
-    
+
     @Test
     public void listDisabledTest() {
         assertEquals(dressCode2.listDisabled(), asList("Disabled"));
         dressCode2.is()
-                .disabled(hasItems("Disabled"))
-                .disabled(not(hasItems("Fancy", "Pirate", "Casual")));
+            .disabled(hasItems("Disabled"))
+            .disabled(not(hasItems("Fancy", "Pirate", "Casual")));
     }
-    
     @Test
     public void assertValidationTest() {
         dressCode2.assertThat().values(contains("Fancy", "Casual", "Disabled", "Pirate"));
         disabledDropdown.assertThat().selected("Disabled");
     }
-    
+
     @Test
     public void baseValidationTest() {
         baseValidation(dressCode2);
     }
-    
-    
-//    @Issue(1747)
-//    https://github.com/jdi-testing/jdi-light/issues/1747
-//    @Test
-//    public void expandTest() {
-//        assertSoft();
-//        dressCode2.show();
-//
-//        jdiAssert(dressCode2.isExpanded(), is(false));
-//        dressCode2.expand();
-//        jdiAssert(dressCode2.isExpanded(), is(true));
-//        dressCode2.close();
-//        jdiAssert(dressCode2.isExpanded(), is(false));
-//
-//        // Verify that list is ok after expand/close
-//        jdiAssert(dressCode2.listEnabled(), hasItems("Fancy", "Casual", "Pirate"));
-//        jdiAssert(dressCode2.listDisabled(), hasItems("Disabled"));
-//        jdiAssert(dressCode2.values(), hasItems("Fancy", "Casual", "Disabled", "Pirate"));
-//
-//        assertResults();
-//    }
 }
