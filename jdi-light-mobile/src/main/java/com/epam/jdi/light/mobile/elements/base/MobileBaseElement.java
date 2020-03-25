4df369e655a16ad127ddaedde2b5549cc2ace7e8
Konstantin Mazurin <konstantin_mazurin@epam.com>
2020-03-25 19:30:38 +0300

    Finish merging branch 'jdi-light-mobile' to 'jdi-light-mobile-new-module'

--- jdi-light-mobile/src/main/java/com/epam/jdi/light/mobile/elements/base/MobileBaseElement.java
+++ jdi-light-mobile/src/main/java/com/epam/jdi/light/mobile/elements/base/MobileBaseElement.java
@@ -4,7 +4,7 @@ import com.epam.jdi.light.asserts.generic.UIAssert;
 import com.epam.jdi.light.elements.base.UIBaseElement;
 import com.epam.jdi.light.mobile.interfaces.IMobileCoreElement;
 
-public abstract class MobileBaseElement<A extends UIAssert> extends UIBaseElement<A>
+public abstract class MobileBaseElement<A extends UIAssert<?,?>> extends UIBaseElement<A>
         implements IMobileCoreElement {
 
     protected MobileUIElement mobileElement;
