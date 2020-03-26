d52d424d070c618b4209a3193289b4627cef404c
Aleksandr Sudiarov <aleksandr_sudiarov@epam.com>
2020-03-26 22:39:58 +0300

    test classes added

---
+++ jdi-light-angular-tests/src/test/java/io/github/epam/angular/tests/BasicTests.java
@@ -0,0 +1,23 @@
+package io.github.epam.angular.tests;
+
+import io.github.epam.TestsInit;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static io.github.com.StaticSite.angPage;
+import static io.github.com.StaticSite.userName;
+import static io.github.epam.site.steps.States.shouldBeLoggedIn;
+
+public class BasicTests extends TestsInit {
+
+    @BeforeMethod
+    public void before() {
+        shouldBeLoggedIn();
+        angPage.shouldBeOpened();
+    }
+
+    @Test
+    public void checkUsernameOnAngularPage() {
+        userName.is().displayed();
+    }
+}
---
+++ jdi-light-angular-tests/src/test/java/io/github/epam/site/steps/States.java
@@ -0,0 +1,46 @@
+package io.github.epam.site.steps;
+
+import com.epam.jdi.light.elements.composite.WebPage;
+import io.qameta.allure.Step;
+
+import static com.epam.jdi.light.elements.composite.WebPage.openUrl;
+import static com.epam.jdi.light.settings.WebSettings.DOMAIN;
+import static io.github.com.StaticSite.*;
+import static io.github.com.entities.Users.DEFAULT_USER;
+import static io.github.com.pages.Header.epamLogo;
+
+
+@SuppressWarnings("PMD.ClassNamingConventions")
+public class States {
+    @Step
+    public static void shouldBeLoggedIn() {
+        String url = WebPage.getUrl();
+        if (!url.contains("https://jdi-testing.github.io/jdi-light/") || epamLogo.isNotExist())
+            angPage.open();
+        if (userName.isHidden())
+            login();
+    }
+
+    @Step
+    public static void login() {
+        if (loginForm.isHidden())
+            userIcon.click();
+        loginForm.submit(DEFAULT_USER);
+    }
+
+    @Step
+    public static void shouldBeLoggedOut() {
+        if (!WebPage.getUrl().contains(DOMAIN))
+            openUrl(DOMAIN);
+        if (userName.isDisplayed())
+            logout();
+        if (loginForm.isDisplayed())
+            userIcon.click();
+    }
+    @Step
+    public static void logout() {
+        if (!logout.isDisplayed())
+            userIcon.click();
+        logout.click();
+    }
+}
