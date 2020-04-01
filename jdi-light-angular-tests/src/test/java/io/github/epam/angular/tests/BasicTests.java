eeda40480549fd0f599642128e48201f3933973f
Aleksandr Sudiarov <aleksandr_sudiarov@epam.com>
2020-04-01 21:24:56 +0300

    Finished first test for Angular

--- jdi-light-angular-tests/src/test/java/io/github/epam/angular/tests/BasicTests.java
+++ jdi-light-angular-tests/src/test/java/io/github/epam/angular/tests/BasicTests.java
@@ -4,10 +4,11 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static io.github.com.StaticSite.angPage;
+import static io.github.com.StaticSite.userName;
 import static io.github.com.pages.AngularPage.firstBasicRadioButton;
 import static states.States.shouldBeLoggedIn;
 
-public class BasicRadiosTests {
+public class BasicTests {
 
     @BeforeMethod
     public void before() {
@@ -16,8 +17,7 @@ public class BasicRadiosTests {
     }
 
     @Test
-    public void checkFirstBasicRadioButtonVisibility() {
-        firstBasicRadioButton.is().displayed();
+    public void checkUsernameOnAngularPage() {
+        userName.is().displayed();
     }
-
 }
