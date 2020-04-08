efbdd7232982db4a6501fd10d7fabac09b9591fa
MF <maria_fedorova1@epam.com>
2020-04-08 13:48:55 +0300

    fix run error, change angular pom files and so on

--- jdi-light-html-tests/src/test/java/io/github/epam/TestsInit.java
+++ jdi-light-html-tests/src/test/java/io/github/epam/TestsInit.java
@@ -4,8 +4,10 @@ import com.epam.jdi.light.elements.interfaces.complex.IsCombobox;
 import com.epam.jdi.light.settings.WebSettings;
 import com.epam.jdi.light.ui.html.elements.complex.DataListOptions;
 import io.github.com.StaticSite;
+import io.github.epam.testng.TestNGListener;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Listeners;
 import pseudo.site.PseudoSite;
 
 import static com.epam.jdi.light.driver.WebDriverUtils.killAllSeleniumDrivers;
@@ -15,6 +17,7 @@ import static com.epam.jdi.light.elements.init.PageFactory.initSite;
 import static com.epam.jdi.light.settings.WebSettings.logger;
 import static io.github.com.StaticSite.homePage;
 
+@Listeners(TestNGListener.class)
 public interface TestsInit {
     @BeforeSuite(alwaysRun = true)
     default void setUp() {
