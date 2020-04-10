d1199f66abd7cada4b19ef5275126bb5077b51e4
pavel.tomarovskii <Pavel_Tomarovskii@epam.com>
2020-04-10 13:58:23 +0300

    unused variables were deleted. class was renamed StaticApp --> StaticAppUtil.

--- jdi-light-mobile-tests/src/main/java/nativeApp/contacts/ContactsMainPage.java
+++ jdi-light-mobile-tests/src/main/java/nativeApp/contacts/ContactsMainPage.java
@@ -3,20 +3,13 @@ package nativeapp.contacts;
 import com.epam.jdi.light.elements.pageobjects.annotations.FindBy;
 import com.epam.jdi.light.mobile.elements.common.Button;
 import com.epam.jdi.light.mobile.elements.common.Text;
-import com.epam.jdi.light.ui.html.elements.common.TextField;
 
 public class ContactsMainPage {
 
     private static final String LOCATOR_PATH = "com.google.android.contacts:id/";
 
-    @FindBy(id = "com.google.android.contacts:id/floating_action_button")
+    @FindBy(id = LOCATOR_PATH + "floating_action_button")
     public static Button addNewContactBtn;
-
-    @FindBy(id = LOCATOR_PATH + "og_apd_internal_image_view")
-    public static Button contactsSourceBtn;
-
-    @FindBy(id = LOCATOR_PATH + "open_search_bar_text_view")
-    public static TextField findContactTextField;
 
     @FindBy(id = "android:id/text1")
     public static Text noContactsInThisAccountText;
