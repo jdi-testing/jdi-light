44e2e97e1022a9c075331c401a5660ab930e6f05
Alla_Blinkova <Alla_Blinkova@epam.com>
2020-04-23 14:05:22 +0300

    add simple tests for radio buttons

--- jdi-light-angular-tests/src/main/java/io/github/com/pages/AngularPage.java
+++ jdi-light-angular-tests/src/main/java/io/github/com/pages/AngularPage.java
@@ -1,12 +1,19 @@
 package io.github.com.pages;
 
 import com.epam.jdi.light.elements.composite.WebPage;
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.Css;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
+import com.epam.jdi.light.ui.html.elements.common.Button;
 import com.epam.jdi.light.ui.html.elements.complex.RadioButtons;
 
 public class AngularPage extends WebPage {
 
-    @UI("#mat-radio-2")
-    public static RadioButtons firstBasicRadioButton;
+    @Css("radio-overview-example .mat-radio-group")
+    public static RadioButtons basicRadioButtons;
 
+    @UI("#mat-radio-2")
+    public static Button firstBasicRadioButton;
+
+    @UI("#mat-radio-3")
+    public static Button secondBasicRadioButton;
 }
---
+++ jdi-light-angular-tests/src/test/java/io/github/epam/angular/tests/RadioButtonTests.java
@@ -0,0 +1,34 @@
+package io.github.epam.angular.tests;
+
+import static io.github.com.StaticSite.angPage;
+import static io.github.com.pages.AngularPage.basicRadioButtons;
+import static io.github.com.pages.AngularPage.firstBasicRadioButton;
+import static io.github.com.pages.AngularPage.secondBasicRadioButton;
+import static io.github.epam.site.steps.States.shouldBeLoggedIn;
+import static org.testng.Assert.assertEquals;
+
+import io.github.epam.TestsInit;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class RadioButtonTests extends TestsInit {
+
+    @BeforeMethod
+    public void before() {
+        shouldBeLoggedIn();
+        angPage.shouldBeOpened();
+    }
+
+    @Test
+    public void basicTest() {
+        basicRadioButtons.is().displayed();
+        firstBasicRadioButton.is().displayed();
+        secondBasicRadioButton.is().displayed();
+    }
+
+    @Test
+    public void getTextTest() {
+        assertEquals(firstBasicRadioButton.getText(), "Option 1");
+        assertEquals(secondBasicRadioButton.getText(), "Option 2");
+    }
+}
