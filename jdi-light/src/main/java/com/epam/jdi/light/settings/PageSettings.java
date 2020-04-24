9f0b703839aed5e352d73e84add8f002aade558b
Alla_Blinkova <Alla_Blinkova@epam.com>
2020-04-24 16:47:26 +0300

    remove new line

--- jdi-light/src/main/java/com/epam/jdi/light/settings/PageSettings.java
+++ jdi-light/src/main/java/com/epam/jdi/light/settings/PageSettings.java
@@ -5,7 +5,6 @@ import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.tools.func.JAction1;
 
 public class PageSettings {
-
     public PageChecks checkPageOpen = PageChecks.NONE;
     public JAction1<WebPage> beforeNewPage = WebPage::beforeNewPage;
     public JAction1<WebPage> beforeEachStep = WebPage::beforeThisPage;
