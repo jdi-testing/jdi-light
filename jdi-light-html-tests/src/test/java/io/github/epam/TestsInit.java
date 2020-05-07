cfb591898ed52d344207493b8aec6fb4fe1b4ed7
Akio_Nkodia <Akio_Nkodia@epam.com>
2020-05-07 17:21:22 +0300

    Adf tmp prints to console with chrome properties for debug Replace slashes

--- jdi-light-html-tests/src/test/java/io/github/epam/TestsInit.java
+++ jdi-light-html-tests/src/test/java/io/github/epam/TestsInit.java
@@ -11,10 +11,14 @@ import com.epam.jdi.light.elements.interfaces.complex.IsCombobox;
 import com.epam.jdi.light.ui.html.elements.complex.DataListOptions;
 import io.github.com.StaticSite;
 import io.github.epam.testng.TestNGListener;
+import org.apache.commons.io.FileUtils;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Listeners;
 import pseudo.site.PseudoSite;
+
+import java.io.File;
+import java.io.IOException;
 
 
 @Listeners(TestNGListener.class)
@@ -26,6 +30,14 @@ public interface TestsInit {
         killAllSeleniumDrivers();
         initSite(StaticSite.class);
         initSite(PseudoSite.class);
+        // todo delete
+        System.out.println("!!! TMP FOR DEBUG capabilities.chrome: " + DRIVER.capabilities.chrome.get("arguments"));
+        try {
+            System.out.println("!!! TMP FOR DEBUG chrome.properties: " +
+                    FileUtils.readFileToString(new File("src\\test\\resources\\chrome.properties"), "UTF-8"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         homePage.open();
         logger.toLog("Run Tests");
     }
