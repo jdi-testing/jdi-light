5f36438456676731093529e5d6fda9a2f0f090f0
Alla_Blinkova <Alla_Blinkova@epam.com>
2020-05-19 11:07:03 +0300

    Fix path for performance and examples modules

--- reports.sh
+++ reports.sh
@@ -88,7 +88,7 @@ function grubAllureResults() {
     checkBranchIsOk #there is an exit inside
 
     if [[ "x${TRAVIS_BUILD_STAGE_NAME}" == "xtest" ]] ; then #don't remove x, it's useful
-        for result in $(find jdi*/target/allure-results -maxdepth 1 -type d)
+        for result in $(find -type d -regex ".*/jdi.*/target/allure-results")
         do
             echo RESULT: ${result}
             archiveFile="$(archive "${result}")"
