951bbf10cab628e28a407bd49d41a1dae67f2624
Daria Kashinskaia <daria_kashinskaia@epam.com>
2020-05-19 21:03:31 +0300

    refactor_singltone

--- jdi-light/src/main/java/com/epam/jdi/light/settings/WebSettings.java
+++ jdi-light/src/main/java/com/epam/jdi/light/settings/WebSettings.java
@@ -112,7 +112,7 @@ public class WebSettings {
         logger.debug("init()");
         try {
             getProperties(COMMON.testPropertiesPath);
-			TestProperties.getTestsProperties().forEach((k, v) ->
+			TestProperties.getInstance().getTestsProperties().forEach((k, v) ->
 			{
 				String propertyName = k;
 				JAction1<String> setPropertyAction = v.key;
