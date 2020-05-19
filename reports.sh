274b07500c6d7c46dfa022f179188f0acd415159
Alla_Blinkova <Alla_Blinkova@epam.com>
2020-05-19 12:04:44 +0300

    Fix path in generateAllureReports()

--- reports.sh
+++ reports.sh
@@ -158,7 +158,7 @@ function extractAllureResults() {
 function generateAllureReports() {
     reportDirList="";
     allureDirExistence=false
-    for report in $(ls -d1 jdi*/target/)
+    for report in $(ls -d1 jdi*/target/ */jdi*/target/)
     do
         allureDirExistence=true
         allureDir="${report}allure-results"
