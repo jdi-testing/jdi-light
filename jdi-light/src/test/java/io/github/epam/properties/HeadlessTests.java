82e6e34ca4e4d824a8d7579c02579bdea42c5331
Daria Kashinskaia <daria_kashinskaia@epam.com>
2020-05-21 18:02:27 +0300

    refactor existing property tests; add tests for "strategy", "list.start.index", "log.info.details", "click.type", "text.type", "set.text.type", "remote.type", "smart.locators.toName", "smart.search", "headless" and edge, opera, safari and common capabilities

---
+++ jdi-light/src/test/java/io/github/epam/properties/HeadlessTests.java
@@ -0,0 +1,46 @@
+package io.github.epam.properties;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.security.InvalidParameterException;
+import java.util.Properties;
+
+import static com.epam.jdi.light.common.Property.HEADLESS_PROPERTY;
+import static com.epam.jdi.light.common.PropertyValidationUtils.LINK_TO_EXAMPLES;
+import static com.epam.jdi.light.common.PropertyValidationUtils.validateProperties;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.fail;
+
+public class HeadlessTests {
+
+    @DataProvider
+    public static Object[] negativeData() {
+        return new Object[]{"flse", "", "0"};
+    }
+
+    @DataProvider
+    public static Object[] positiveData() {
+        return new Object[]{"true", "false"};
+    }
+
+    @Test(dataProvider = "negativeData")
+    public void negativeTest(String value) {
+        Properties properties = new Properties();
+        properties.setProperty(HEADLESS_PROPERTY.getName(), value);
+        try {
+            validateProperties(properties);
+            fail("Value '" + value + "' should not be valid for this test.");
+        } catch (InvalidParameterException exp) {
+            String expMessage = exp.getMessage();
+            assertEquals(expMessage, HEADLESS_PROPERTY.getExMsg() + LINK_TO_EXAMPLES);
+        }
+    }
+
+    @Test(dataProvider = "positiveData")
+    public void positiveTest(String value) {
+        Properties properties = new Properties();
+        properties.setProperty(HEADLESS_PROPERTY.getName(), value);
+        validateProperties(properties);
+    }
+}
