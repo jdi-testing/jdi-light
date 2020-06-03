14b470612f2c4e0c4485cac1a18103a1c08b55ac
Richardas Ralis <rralis@idbs.com>
2020-06-03 16:17:59 +0300

    Update according to the PR review.

--- test-examples/jdi-light-examples/src/test/java/io/github/epam/StaticTestsInit.java
+++ test-examples/jdi-light-examples/src/test/java/io/github/epam/StaticTestsInit.java
@@ -1,13 +1,13 @@
 package io.github.epam;
 
 import io.github.com.StaticSite;
+import static io.github.com.StaticSite.*;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 import static com.epam.jdi.light.driver.WebDriverUtils.*;
 import static com.epam.jdi.light.elements.init.PageFactory.*;
 import static com.epam.jdi.light.settings.WebSettings.*;
-import static io.github.com.StaticSite.*;
 
 public class StaticTestsInit {
 
