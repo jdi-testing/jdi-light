08c74e6def5173406762fa75dc48389fcea9736a
Dmitrii Shadrin <Dmitrii_Shadrin@epam.com>
2020-06-05 19:10:39 +0300

    2038::Debugging allure results retrieval for 'examples' and 'performance' tests 5

--- reports.sh
+++ reports.sh
@@ -89,7 +89,7 @@ function grubAllureResults() {
     checkBranchIsOk #there is an exit inside
 
     if [[ "x${TRAVIS_BUILD_STAGE_NAME}" == "xtest" ]] ; then #don't remove x, it's useful
-        for result in $(find jdi*/target/allure-results-${TRAVIS_JDK_VERSION} -maxdepth 1 -type d)
+        for result in $(find -type d -regex ".*/jdi.*/target/allure-results-${TRAVIS_JDK_VERSION}")
         do
             echo RESULT: ${result}
             archiveFile="$(archive "${result}")"
