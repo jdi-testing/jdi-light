2df41806feb5ea0923fbdb77611ea14458e6de7d
Viktar Ramanau <Viktar_Ramanau@epam.com>
2020-06-23 17:21:21 +0300

    2066 filterBy() and searchBy() tests

--- jdi-light-html-tests/src/main/java/io/github/com/pages/TableWithPagesPage.java
+++ jdi-light-html-tests/src/main/java/io/github/com/pages/TableWithPagesPage.java
@@ -1,14 +1,34 @@
 package io.github.com.pages;
 
+import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.light.elements.complex.table.Table;
 import com.epam.jdi.light.elements.composite.WebPage;
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.Css;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.JTable;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
+import org.openqa.selenium.WebElement;
 
 public class TableWithPagesPage extends WebPage {
 
-    @UI("#table-with-pages")
-    @JTable
-    public static Table withPagesTable;
+    @Css("#table-with-pages")
+    @JTable(
+            filter = "th[class^='sorting']",
+            cell = "//tbody/tr[{1}]/td[{1}]",
+            firstColumnIndex = 0,
+            rowHeader = "Type", size = 3,
+            header = {"Type", "Now", "Plans"}
+
+    )
+    public Table withPagesTable;
+
+    @UI("input[type='search']")
+    public static WebElement searchBy;
+
+    public UIElement filterByColumn(String filterName) {
+        UIElement elem = withPagesTable.filterBy(filterName);
+        System.out.println("elem::" + elem);
+        elem.click();
+        return elem;
+    }
 
 }
--- jdi-light-html-tests/src/test/java/io/github/epam/html/tests/elements/complex/table/TableWithPagesTests.java
+++ jdi-light-html-tests/src/test/java/io/github/epam/html/tests/elements/complex/table/TableWithPagesTests.java
@@ -1,11 +1,11 @@
 package io.github.epam.html.tests.elements.complex.table;
 
+import io.github.com.pages.TableWithPagesPage;
 import io.github.epam.TestsInit;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static io.github.com.StaticSite.tableWithPagesPage;
-import static io.github.com.pages.TableWithPagesPage.withPagesTable;
 import static io.github.epam.html.tests.site.steps.States.shouldBeLoggedIn;
 
 public class TableWithPagesTests implements TestsInit {
@@ -18,10 +18,11 @@ public class TableWithPagesTests implements TestsInit {
 
     @Test
     public void searchInTable() {
-        withPagesTable.is().visible().and().size(5);
-        withPagesTable.filterBy("Type");
-        withPagesTable.getCell(1,1).has().text("Test Runner");
-        withPagesTable.searchBy("Aj");
-        withPagesTable.getCell(1,1).has().text("ASP.NET");
+        TableWithPagesPage tableWithPagesPage = new TableWithPagesPage();
+        tableWithPagesPage.withPagesTable.is().visible().and().size(5);
+        tableWithPagesPage.filterByColumn("Type");
+        tableWithPagesPage.withPagesTable.getCell(1, 1).has().text("Test Runner");
+        tableWithPagesPage.searchBy.sendKeys("Aj");
+        tableWithPagesPage.withPagesTable.getCell(1, 1).has().text("ASP.NET");
     }
 }
