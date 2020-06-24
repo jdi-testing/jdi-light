0e4f983a7dc22a2d27c6ceff9a38d577bc69e571
Viktar Ramanau <Viktar_Ramanau@epam.com>
2020-06-24 11:17:51 +0300

    change lacators to the correct one

--- jdi-bdd-tests/src/main/java/io/github/com/pages/GithubPage.java
+++ jdi-bdd-tests/src/main/java/io/github/com/pages/GithubPage.java
@@ -4,7 +4,7 @@ import com.epam.jdi.light.ui.html.elements.common.Text;
 import org.openqa.selenium.support.FindBy;
 
 public class GithubPage {
-    @FindBy(css = "[itemprop=about]")
+    @FindBy(css = "div[class='BorderGrid-cell']>p[class*='f4']")
     public static Text repoDescription;
     // @UI(".repository-meta-content")
     // public static UIElement repoDescription;
--- test-examples/jdi-light-examples/src/main/java/io/github/epam/pages/GithubPage.java
+++ test-examples/jdi-light-examples/src/main/java/io/github/epam/pages/GithubPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class GithubPage {
-    @FindBy(css = "[itemprop=about]")
+    @FindBy(css = "div[class='BorderGrid-cell']>p[class*='f4']")
     public WebElement repoDescription;
     // @Css(".repository-meta-content")
     // public UIElement repoDescription;
