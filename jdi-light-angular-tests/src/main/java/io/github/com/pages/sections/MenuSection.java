5af05ef2a873a34e146c31889f66c0d40d85be6f
Akio_Nkodia <Akio_Nkodia@epam.com>
2020-06-25 20:23:38 +0300

    Start NestedDropdownMenu implementation

--- jdi-light-angular-tests/src/main/java/io/github/com/pages/sections/MenuSection.java
+++ jdi-light-angular-tests/src/main/java/io/github/com/pages/sections/MenuSection.java
@@ -1,28 +1,20 @@
 package io.github.com.pages.sections;
 
-import com.epam.jdi.light.elements.complex.Menu;
+import com.epam.jdi.angular.elements.common.NestedDropdownMenu;
 import com.epam.jdi.light.elements.composite.Section;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.ui.html.elements.common.Text;
-import com.epam.jdi.light.ui.html.elements.complex.Menu2D;
 
 public class MenuSection extends Section {
 
-    //@UI("//button//*[text()='Menu']")
-    @UI("#basic_menu")
-    public Menu basicMenu;
+    @UI("#basic_menu_button")
+    public NestedDropdownMenu basicMenu;
 
-    @UI("#mat-menu-panel-0")
-    public Menu2D matMenuPanel0;
+    @UI("#menu_with_icons_button")
+    public NestedDropdownMenu menuWithIcons;
 
-    @UI("#menu_with_icons")
-    public Menu menuWithIcons;
-
-    @UI("#mat-menu-panel-1")
-    public Menu matMenuPanel1;
-
-    @UI("#nested_menu")
-    public Menu nestedMenu;
+    @UI("#nested_menu_button")
+    public NestedDropdownMenu nestedMenu;
 
     @UI("#basic-menu-selected-option")
     public Text basicMenuSelectedOption;
