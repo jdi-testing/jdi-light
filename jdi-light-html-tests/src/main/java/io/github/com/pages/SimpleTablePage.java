fd1e51b21d5a344f3f13f68d38cededb773590f6
RomanIovlev <roman.iovlev.jdi@gmail.com>
2020-06-30 00:40:26 +0300

    add grid

--- jdi-light-html-tests/src/main/java/io/github/com/pages/SimpleTablePage.java
+++ jdi-light-html-tests/src/main/java/io/github/com/pages/SimpleTablePage.java
@@ -1,14 +1,16 @@
 package io.github.com.pages;
 
 import com.epam.jdi.light.elements.complex.table.DataTable;
+import com.epam.jdi.light.elements.complex.table.Grid;
 import com.epam.jdi.light.elements.complex.table.Table;
 import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.JTable;
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import io.github.com.entities.Furniture;
 
 public class SimpleTablePage extends WebPage {
 
-    public static Table simpleTable;
+    public static Grid simpleTable;
     public static DataTable<?, Furniture> products;
 
     @JTable(
