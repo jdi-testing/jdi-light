f04261e640e666bcb61dcd2447bdb961cec7bee6
Akio Nkodia <Akio_Nkodia@epam.com>
2020-07-05 23:32:47 +0300

    2131 fix failed applitools tests (#2142)
    * Add path to testResource for jdi-eye-demo project
    Add driver, domain, properties to jdi-eye-demo
    Add to jdi-light-applitools-tests other tests package
    
    * Remove submitContactFormFullDataTest() test duplicate
    
    * Fix check() and checkboxes locator in MultiSelect to fix applitools tests
    
    * Update log4j, guava versions

--- jdi-eyes-demo/src/main/java/jdisite/custom/MultiSelect.java
+++ jdi-eyes-demo/src/main/java/jdisite/custom/MultiSelect.java
@@ -4,9 +4,11 @@ import com.epam.jdi.light.asserts.generic.UISelectAssert;
 import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.elements.base.UIListBase;
 import com.epam.jdi.light.elements.common.UIElement;
+import com.epam.jdi.light.elements.complex.JList;
 import com.epam.jdi.light.elements.complex.WebList;
 import com.epam.jdi.light.elements.interfaces.base.HasLabel;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
+import com.epam.jdi.light.ui.html.elements.common.Checkbox;
 
 import java.util.List;
 
@@ -14,12 +16,17 @@ import static com.epam.jdi.light.logger.LogLevels.DEBUG;
 import static com.epam.jdi.tools.LinqUtils.map;
 import static org.apache.commons.lang3.StringUtils.isBlank;
 
-public class MultiSelect extends UIListBase<UISelectAssert<?,?>> implements HasLabel {
-    @UI(".caret") UIElement expander;
-    @UI("ul") UIElement valuesList;
-    @UI("button") UIElement valueText;
-    @UI("li") WebList allValues;
-    @UI("input") WebList value;
+public class MultiSelect extends UIListBase<UISelectAssert<?, ?>> implements HasLabel {
+    @UI(".caret")
+    UIElement expander;
+    @UI("ul")
+    UIElement valuesList;
+    @UI("button")
+    UIElement valueText;
+    @UI("li")
+    WebList allValues;
+    @UI("//a[label[text()='%s']]//input")
+    JList<Checkbox> checkboxes;
 
     @JDIAction("Check '{0}' for '{name}'")
     public void check(String values) {
@@ -27,19 +34,22 @@ public class MultiSelect extends UIListBase<UISelectAssert<?,?>> implements HasL
         expand();
         List<String> listNames = map(values.split(","), String::trim);
         for (String name : allValues.values()) {
-            UIElement input = value.get(name);
-            boolean valueSelected = input.isSelected();
-            if (valueSelected && !listNames.contains(name)
-                    || !valueSelected && listNames.contains(name))
-                input.label().click();
+            Checkbox checkbox = checkboxes.get(name);
+            if (listNames.contains(name)) {
+                checkbox.check();
+            } else {
+                checkbox.uncheck();
+            }
         }
         label().click();
     }
+
     @JDIAction(level = DEBUG)
     private void expand() {
         if (valuesList.isHidden())
             expander.click();
     }
+
     @Override
     public void setValue(String value) {
         check(value);
@@ -55,5 +65,4 @@ public class MultiSelect extends UIListBase<UISelectAssert<?,?>> implements HasL
     public String getValue() {
         return selected();
     }
-
 }
