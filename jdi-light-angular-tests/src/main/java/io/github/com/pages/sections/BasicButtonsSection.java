d420bfcbaedec865bed7bfe6864f4637c7fe58d4
Viktar Ramanau <Viktar_Ramanau@epam.com>
2020-07-08 10:20:49 +0300

    Split variables declaration

--- jdi-light-angular-tests/src/main/java/io/github/com/pages/sections/BasicButtonsSection.java
+++ jdi-light-angular-tests/src/main/java/io/github/com/pages/sections/BasicButtonsSection.java
@@ -7,7 +7,12 @@ import com.epam.jdi.light.ui.html.elements.common.Button;
 
 public class BasicButtonsSection extends Section {
 
-    public static Button basicBasicButton, basicPrimaryButton, basicAccentButton, basicWarnButton, basicLinkButton, basicDisabledButton;
+    public static Button basicBasicButton;
+    public static Button basicPrimaryButton;
+    public static Button basicAccentButton;
+    public static Button basicWarnButton;
+    public static Button basicLinkButton;
+    public static Button basicDisabledButton;
 
     @UI("#basic-buttons-label")
     public Label basicButtonsLabel;
