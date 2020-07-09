ee11b77f4a33920761b6a8c08d28dab3293f5912
Sofi Tur <soturnn@gmail.com>
2020-07-09 17:58:55 +0300

    list tests changed

--- jdi-light-angular-tests/src/main/java/io/github/com/pages/AngularPage.java
+++ jdi-light-angular-tests/src/main/java/io/github/com/pages/AngularPage.java
@@ -9,8 +9,8 @@ import com.epam.jdi.light.ui.html.elements.common.Icon;
 import com.epam.jdi.light.ui.html.elements.complex.RadioButtons;
 import io.github.com.pages.sections.CheckboxSection;
 import io.github.com.pages.sections.InputSection;
-import io.github.com.pages.sections.SlideToggleSection;
 import io.github.com.pages.sections.ListSection;
+import io.github.com.pages.sections.SlideToggleSection;
 
 public class AngularPage extends WebPage {
     @Css("radio-overview-example .mat-radio-group")
