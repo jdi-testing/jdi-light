a5bf3424d8812e98afbcfdc380371d7543d10d11
Elena <ElenaBokova@users.noreply.github.com>
2020-07-09 17:12:33 +0300

    Radio Button Tests Added

--- jdi-light-angular-tests/src/main/java/io/github/com/pages/AngularPage.java
+++ jdi-light-angular-tests/src/main/java/io/github/com/pages/AngularPage.java
@@ -9,17 +9,10 @@ import com.epam.jdi.light.ui.html.elements.common.Icon;
 import com.epam.jdi.light.ui.html.elements.complex.RadioButtons;
 import io.github.com.pages.sections.CheckboxSection;
 import io.github.com.pages.sections.InputSection;
+import io.github.com.pages.sections.RadioButtonSection;
 import io.github.com.pages.sections.SlideToggleSection;
 
 public class AngularPage extends WebPage {
-    @Css("radio-overview-example .mat-radio-group")
-    public static RadioButtons basicRadioButtons;
-
-    @UI("#mat-radio-2")
-    public static Button firstBasicRadioButton;
-
-    @UI("#mat-radio-3")
-    public static Button secondBasicRadioButton;
 
     @UI("#basic_progress_spinner")
     public static ProgressSpinner basicProgressSpinner;
@@ -30,6 +23,7 @@ public class AngularPage extends WebPage {
     @UI("#svg_icon")
     public static Icon svgIcon;
 
+    public static RadioButtonSection radioButtonSection;
     public static CheckboxSection checkboxSection;
     public static SlideToggleSection slideToggleSection;
     public static InputSection inputSection;
