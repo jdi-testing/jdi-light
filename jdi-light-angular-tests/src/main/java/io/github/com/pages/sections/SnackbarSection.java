bf15471c4625f14ac7d89713dd34a0abba73da50
Nick <korobov.nn@edu.spbstu.ru>
2020-07-15 21:53:06 +0300

    Revert "Remove Snackbar class"
    This reverts commit 8c2e39ae5ad18a29d348c8ca66e545a2b5e5ba22.

--- jdi-light-angular-tests/src/main/java/io/github/com/pages/sections/SnackbarSection.java
+++ jdi-light-angular-tests/src/main/java/io/github/com/pages/sections/SnackbarSection.java
@@ -1,5 +1,6 @@
 package io.github.com.pages.sections;
 
+import com.epam.jdi.light.angular.elements.common.Snackbar;
 import com.epam.jdi.light.elements.composite.Section;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.ui.html.elements.common.Button;
@@ -17,7 +18,7 @@ public class SnackbarSection extends Section {
     public Button openButton;
 
     @UI("//*[contains(@id,'cdk-overlay')]//snack-bar-container//simple-snack-bar")
-    public Section basicSnackbar;
+    public Snackbar basicSnackbar;
 
     @UI("#snack-bar-custom-component-input")
     public TextField durationInput;
@@ -26,5 +27,5 @@ public class SnackbarSection extends Section {
     public Button customSnackbarOpenButton;
 
     @UI("#snack-bar-custom-component")
-    public Section customSnackbar;
+    public Snackbar customSnackbar;
 }
---
+++ jdi-light-angular/src/main/java/com/epam/jdi/light/angular/elements/common/Snackbar.java
@@ -0,0 +1,9 @@
+package com.epam.jdi.light.angular.elements.common;
+
+import com.epam.jdi.light.elements.composite.Section;
+
+/**
+ * Angular snack-bar documentation: https://jdi-docs.github.io/jdi-light/?java#progress-spinner
+ */
+public class Snackbar extends Section {
+}
