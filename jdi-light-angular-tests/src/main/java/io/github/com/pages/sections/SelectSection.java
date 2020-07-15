715390a29eaf6783ec9d487c4694a48396945ddc
Ihar_Lipko <ihar_lipko@epam.com>
2020-07-15 18:24:10 +0300

    Updated SelectSection class.

--- jdi-light-angular-tests/src/main/java/io/github/com/pages/sections/SelectSection.java
+++ jdi-light-angular-tests/src/main/java/io/github/com/pages/sections/SelectSection.java
@@ -8,19 +8,19 @@ import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.ui.html.elements.common.Text;
 
 public class SelectSection {
-    @JDropdown(value = "#basic-mat-select")
+    @JDropdown("#basic-mat-select")
     public static MaterialSelector basicMatSelect;
 
     @UI("#basic-native-select")
     public static NativeSelector basicNativeSelect;
 
-    @JDropdown(value = "#two-binding-select")
+    @JDropdown("#two-binding-select")
     public static MaterialSelector twoBindingSelect;
 
     @UI("#select-binding-confirm")
     public static Text selectBindingConfirmation;
 
-    @JDropdown(value = "#form-mat-select")
+    @JDropdown("#form-mat-select")
     public static MaterialSelector formMatSelect;
 
     @UI("#form-mat-select-confirm")
@@ -32,13 +32,13 @@ public class SelectSection {
     @UI("#form-native-select-confirm")
     public static Text formNativeSelectConfirmation;
 
-    @JDropdown(value = "#form-mat-feature-select")
+    @JDropdown("#form-mat-feature-select")
     public static MaterialSelector formMatFeatureSelect;
 
     @UI("#form-native-feature")
     public static NativeSelector formNativeFeatureSelect;
 
-    @JDropdown(value = "#disable-mat-select")
+    @JDropdown("#disable-mat-select")
     public static MaterialSelector disableMatSelect;
 
     @UI("#disable-native-select")
@@ -47,33 +47,33 @@ public class SelectSection {
     @UI("#disable-checkbox-select")
     public static Checkbox checkboxDisableSelect;
 
-    @JDropdown(value = "#reset-mat-select")
+    @JDropdown("#reset-mat-select")
     public static MaterialSelector resetMatSelect;
 
     @UI("#reset-native-select")
     public static NativeSelector resetNativeSelect;
 
-    @JDropdown(value = "#option-groups-mat-select")
+    @JDropdown("#option-groups-mat-select")
     public static MaterialSelector optionGroupsMatSelect;
 
     @UI("#option-groups-native-select")
     public static NativeSelector optionGroupsNativeSelect;
 
-    @JDropdown(value = "#multiple-select")
+    @JDropdown("#multiple-select")
     public static MaterialSelector multipleSelect;
 
-    @JDropdown(value = "#custom-trigger-text-select")
+    @JDropdown("#custom-trigger-text-select")
     public static MaterialSelector customTriggerTextSelect;
 
-    @JDropdown(value = "#no-option-ripple-select")
+    @JDropdown("#no-option-ripple-select")
     public static MaterialSelector noOptionRippleSelect;
 
-    @JDropdown(value = "#custom-panel-styling-select")
+    @JDropdown("#custom-panel-styling-select")
     public static MaterialSelector customPanelStylingSelect;
 
-    @JDropdown(value = "#mat-error-state-matcher-select")
+    @JDropdown("#mat-error-state-matcher-select")
     public static MaterialSelector matErrorStateMatcherSelect;
 
-    @UI(value = "#native-error-state-matcher")
+    @UI("#native-error-state-matcher")
     public static NativeSelector nativeErrorStateMatcherSelect;
 }
