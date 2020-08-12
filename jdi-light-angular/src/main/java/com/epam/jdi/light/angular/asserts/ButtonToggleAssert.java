f293b1abb9ec14b2974988185d0c70ed028576f0
Aliaksandr_Navitski1 <aliaksandr_navitski1@epam.com>
2020-08-12 15:20:31 +0300

    1957_tabs_tests 12.08.2020 3:20 PM

--- jdi-light-angular/src/main/java/com/epam/jdi/light/angular/asserts/ButtonToggleAssert.java
+++ jdi-light-angular/src/main/java/com/epam/jdi/light/angular/asserts/ButtonToggleAssert.java
@@ -2,7 +2,17 @@ package com.epam.jdi.light.angular.asserts;
 
 import com.epam.jdi.light.angular.elements.complex.ButtonToggle;
 import com.epam.jdi.light.asserts.generic.UIAssert;
+import com.epam.jdi.light.common.JDIAction;
+import org.hamcrest.Matchers;
+
+import static com.epam.jdi.light.asserts.core.SoftAssert.jdiAssert;
 
 public class ButtonToggleAssert extends UIAssert<ButtonToggleAssert, ButtonToggle> {
+
+    @JDIAction("Assert that '{name}' has '{0}' class")
+    public ButtonToggleAssert assertButtonToggleIsSelected(String value) {
+        jdiAssert(element.isButtonToggleSelected(value), Matchers.is(true));
+        return this;
+    }
 
 }
