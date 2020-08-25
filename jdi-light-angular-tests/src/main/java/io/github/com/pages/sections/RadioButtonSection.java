ef4ec45ad4a2d8de4cdffdd6571820258b278f85
Aliaksandr_Navitski1 <aliaksandr_navitski1@epam.com>
2020-08-25 12:34:22 +0300

    Merge branch '2212_button_toggle' of https://github.com/jdi-testing/jdi-light into 2212_button_toggle
    # Conflicts:
    #	jdi-light-angular-tests/src/test/java/io/github/epam/angular/tests/elements/complex/ButtonToggleTests.java

--- jdi-light-angular-tests/src/main/java/io/github/com/pages/sections/RadioButtonSection.java
+++ jdi-light-angular-tests/src/main/java/io/github/com/pages/sections/RadioButtonSection.java
@@ -1,16 +1,36 @@
 package io.github.com.pages.sections;
 
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.Css;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
+import com.epam.jdi.light.ui.html.elements.common.Button;
 import com.epam.jdi.light.ui.html.elements.common.Text;
 import com.epam.jdi.light.ui.html.elements.complex.RadioButtons;
 
 public class RadioButtonSection {
 
-    @UI("#basic-radio-group")
+    @Css("#basic-radio-group")
     public static RadioButtons basicRadioButtons;
+
+    @UI("#radio-option-one")
+    public static Button firstBasicRadioButton;
+
+    @UI("#radio-option-two")
+    public static Button secondBasicRadioButton;
 
     @UI("#season-radio-group")
     public static RadioButtons seasonRadioButtons;
+
+    @UI("#favorite-season-winter")
+    public static Button winterNGModelRadioButton;
+
+    @UI("#favorite-season-spring")
+    public static Button springNGModelRadioButton;
+
+    @UI("#favorite-season-summer")
+    public static Button summerNGModelRadioButton;
+
+    @UI("#favorite-season-autumn")
+    public static Button autumnNGModelRadioButton;
 
     @UI("#your-favorite-season-text")
     public static Text favoriteSeason;
--- jdi-light-html/src/main/java/com/epam/jdi/light/ui/html/elements/complex/RadioButtons.java
+++ jdi-light-html/src/main/java/com/epam/jdi/light/ui/html/elements/complex/RadioButtons.java
@@ -12,35 +12,13 @@ import static com.epam.jdi.light.common.TextTypes.LABEL;
  * Created by Roman Iovlev on 26.09.2019
  * Email: roman.iovlev.jdi@gmail.com; Skype: roman.iovlev
  */
-public class RadioButtons extends UIListBase<UISelectAssert<UISelectAssert<?,?>, RadioButtons>> {
+public class RadioButtons extends UIListBase<UISelectAssert<UISelectAssert<?, ?>, RadioButtons>> {
     public RadioButtons() {
         base().setLocator("input[type=radio]");
     }
+
     @Override
     public WebList list() {
         return super.list().setUIElementName(LABEL);
     }
-
-    @JDIAction("Click '{name}' button by tag value '{0}'")
-    public void clickRadioButtonByValue(String value) {
-        getRadioButtonByTagValue(value).click();
-    }
-
-    public UIElement getRadioButtonByTagValue(String value) {
-        System.out.println(list());
-        UIElement element = null;
-
-        for (UIElement e : getRadioButtonsItems()) {
-            if (e.find("input").attr("value").equals(value)) {
-                element = e;
-            }
-        }
-        return element;
-    }
-
-    @JDIAction("Get '{name}' tabs")
-    private WebList getRadioButtonsItems() {
-        return this.finds(".mat-radio-button");
-    }
-
 }
