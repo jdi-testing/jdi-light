80e52471b0a4a80e787e392a9ace6fb7cb267762
Aliaksandr_Navitski1 <aliaksandr_navitski1@epam.com>
2020-08-25 15:37:54 +0300

    2212_button_toggle 21.08.2020 4:19 PM
     Conflicts:
    	jdi-light-angular-tests/src/test/java/io/github/epam/angular/tests/elements/complex/ButtonToggleTests.java

--- jdi-light-html/src/main/java/com/epam/jdi/light/ui/html/elements/complex/RadioButtons.java
+++ jdi-light-html/src/main/java/com/epam/jdi/light/ui/html/elements/complex/RadioButtons.java
@@ -1,9 +1,7 @@
 package com.epam.jdi.light.ui.html.elements.complex;
 
 import com.epam.jdi.light.asserts.generic.UISelectAssert;
-import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.elements.base.UIListBase;
-import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.light.elements.complex.WebList;
 
 import static com.epam.jdi.light.common.TextTypes.LABEL;
