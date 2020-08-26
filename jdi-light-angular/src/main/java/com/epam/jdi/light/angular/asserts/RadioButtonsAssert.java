a757ac85967e11f39c4e93f25846be9d6fa1f4a7
Aliaksandr_Navitski1 <aliaksandr_navitski1@epam.com>
2020-08-26 14:50:08 +0300

    2268_refractor_radio_buttons 26.08.20 2:49 (draft)

--- jdi-light-angular/src/main/java/com/epam/jdi/light/angular/asserts/RadioButtonsAssert.java
+++ jdi-light-angular/src/main/java/com/epam/jdi/light/angular/asserts/RadioButtonsAssert.java
@@ -2,23 +2,6 @@ package com.epam.jdi.light.angular.asserts;
 
 import com.epam.jdi.light.angular.elements.complex.RadioButtons;
 import com.epam.jdi.light.asserts.generic.UIAssert;
-import com.epam.jdi.light.common.JDIAction;
-import org.hamcrest.Matchers;
-
-import static com.epam.jdi.light.asserts.core.SoftAssert.jdiAssert;
 
 public class RadioButtonsAssert extends UIAssert<RadioButtonsAssert, RadioButtons> {
-
-    @JDIAction("Assert that '{name}' is checked when '{0}' radio button value is provided")
-    public RadioButtonsAssert assertRadioButtonIsChecked(String value) {
-        jdiAssert(element.isRadioButtonChecked(value), Matchers.is(true));
-        return this;
-    }
-
-    @JDIAction("Assert that '{name}' is not checked when '{0}' radio button value is provided")
-    public RadioButtonsAssert assertRadioButtonIsNotChecked(String value) {
-        jdiAssert(element.isRadioButtonChecked(value), Matchers.is(false));
-        return this;
-    }
 }
-
