1159d4a56850fceab4cce12a53028e0b68311711
Aliaksandr_Navitski1 <aliaksandr_navitski1@epam.com>
2020-08-26 14:50:08 +0300

    2268_refractor_radio_buttons 26.08.20 2:49

---
+++ jdi-light-angular/src/main/java/com/epam/jdi/light/angular/asserts/RadioButtonsAssert.java
@@ -0,0 +1,7 @@
+package com.epam.jdi.light.angular.asserts;
+
+import com.epam.jdi.light.angular.elements.complex.RadioButtons;
+import com.epam.jdi.light.asserts.generic.UIAssert;
+
+public class RadioButtonsAssert extends UIAssert<RadioButtonsAssert, RadioButtons> {
+}
