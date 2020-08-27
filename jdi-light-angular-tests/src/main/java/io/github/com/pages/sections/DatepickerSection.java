97ed98193bd0be48e87dac3ffbfb0254e5743e12
Ihar_Lipko <ihar_lipko@epam.com>
2020-08-27 16:53:33 +0300

    Updated Datepicker element and test cases.

--- jdi-light-angular-tests/src/main/java/io/github/com/pages/sections/DatepickerSection.java
+++ jdi-light-angular-tests/src/main/java/io/github/com/pages/sections/DatepickerSection.java
@@ -7,12 +7,12 @@ public class DatepickerSection {
     public static Datepicker startDateDatepicker;
     public static Datepicker minMaxDatepicker;
     public static Datepicker filterDatepicker;
+    public static Datepicker inputChangeEventsDatepicker;
     public static Datepicker touchUiDatepicker;
     public static Datepicker openMethodDatepicker;
     public static Datepicker differentLocaleDatepicker;
     public static Datepicker momentJsDatepicker;
     public static Datepicker customFormatsDatepicker;
-    public static Datepicker inputChangeEventsDatepicker;
 
     public static Datepicker selectedDatepicker;
     public static Datepicker deserializeDatepicker;
