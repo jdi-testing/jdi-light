70fc7a0fb6a4b08cef9c23078a0380bba8a33d08
Aliaksandr_Navitski1 <aliaksandr_navitski1@epam.com>
2020-08-27 15:56:58 +0300

    1966 tooltip 27.08.20 rename CdkOverlayContainer to MaterialSelectorContainer

--- jdi-light-angular-tests/src/test/java/io/github/epam/angular/tests/elements/common/TooltipTests.java
+++ jdi-light-angular-tests/src/test/java/io/github/epam/angular/tests/elements/common/TooltipTests.java
@@ -1,7 +1,7 @@
 package io.github.epam.angular.tests.elements.common;
 
 import com.epam.jdi.light.angular.elements.common.Tooltip;
-import com.epam.jdi.light.angular.elements.composite.CdkOverlayContainer;
+import com.epam.jdi.light.angular.elements.composite.MaterialSelectorContainer;
 import com.epam.jdi.tools.func.JAction;
 import io.github.com.pages.AngularPage;
 import io.github.epam.TestsInit;
@@ -45,7 +45,7 @@ public class TooltipTests extends TestsInit {
         position.forEach(
                 (k, v) -> {
                     positionTooltipSelector.click();
-                    (new CdkOverlayContainer()).select(k);
+                    (new MaterialSelectorContainer()).select(k);
                     positionTooltipButton.hover();
                     tooltip.has().assertTooltipPosition(v, positionTooltipButton);
                 }
