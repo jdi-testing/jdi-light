8c79face1c3b19561e3eda52e5dbdb240d505dc2
Aliaksandr_Navitski1 <aliaksandr_navitski1@epam.com>
2020-08-27 22:06:49 +0300

    2268 working version

--- jdi-light-angular-tests/src/main/java/io/github/com/pages/sections/RadioButtonSection.java
+++ jdi-light-angular-tests/src/main/java/io/github/com/pages/sections/RadioButtonSection.java
@@ -2,8 +2,9 @@ package io.github.com.pages.sections;
 
 import com.epam.jdi.light.angular.elements.complex.RadioButtons;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
+import com.epam.jdi.light.ui.html.elements.common.Text;
 
-public class RadioButtonSection  {
+public class RadioButtonSection {
 
     @UI("#basic-radio-group")
     public static RadioButtons basicRadioButtons;
@@ -11,27 +12,6 @@ public class RadioButtonSection  {
     @UI("#season-radio-group")
     public static RadioButtons seasonRadioButtons;
 
-//    @UI("#radio-option-one")
-//    public static Button firstBasicRadioButton;
-//
-//    @UI("#radio-option-two")
-//    public static Button secondBasicRadioButton;
-//
-//    @Css("#season-radio-group")
-//    public static RadioButtons seasonRadioButtons;
-//
-//    @UI("#favorite-season-winter")
-//    public static Button winterNGModelRadioButton;
-//
-//    @UI("#favorite-season-spring")
-//    public static Button springNGModelRadioButton;
-//
-//    @UI("#favorite-season-summer")
-//    public static Button summerNGModelRadioButton;
-//
-//    @UI("#favorite-season-autumn")
-//    public static Button autumnNGModelRadioButton;
-//
-//    @UI("#your-favorite-season-text")
-//    public static Text favoriteSeason;
+    @UI("#your-favorite-season-text")
+    public static Text favoriteSeasonField;
 }
