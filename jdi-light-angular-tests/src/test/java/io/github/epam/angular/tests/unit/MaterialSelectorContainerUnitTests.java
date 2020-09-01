bbc1fcd92153354904a04255d383bcc7972ca3fe
Ihar_Lipko <ihar_lipko@epam.com>
2020-09-01 17:42:12 +0300

    Adjusted MaterialSelectorUnitTests and MaterialSelectorContainerUnitTests classes.

--- jdi-light-angular-tests/src/test/java/io/github/epam/angular/tests/unit/MaterialSelectorContainerUnitTests.java
+++ jdi-light-angular-tests/src/test/java/io/github/epam/angular/tests/unit/MaterialSelectorContainerUnitTests.java
@@ -4,20 +4,51 @@ import com.epam.jdi.light.angular.elements.composite.MaterialSelectorContainer;
 import com.epam.jdi.light.elements.complex.WebList;
 import io.github.epam.TestsInit;
 import org.openqa.selenium.By;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
 
 import static io.github.com.StaticSite.angularPage;
-import static io.github.com.pages.sections.SelectSection.*;
-import static io.github.epam.angular.tests.elements.complex.select.TestsSelectBase.*;
+import static io.github.com.pages.sections.SelectSection.basicMatSelect;
+import static io.github.com.pages.sections.SelectSection.customPanelStylingSelect;
+import static io.github.com.pages.sections.SelectSection.multipleSelect;
+import static io.github.com.pages.sections.SelectSection.optionGroupsMatSelect;
+import static io.github.epam.angular.tests.elements.complex.select.TestsSelectBase.BELLSPROUT;
+import static io.github.epam.angular.tests.elements.complex.select.TestsSelectBase.BULBASAUR;
+import static io.github.epam.angular.tests.elements.complex.select.TestsSelectBase.CHARMANDER;
+import static io.github.epam.angular.tests.elements.complex.select.TestsSelectBase.EXTRA_CHEESE;
+import static io.github.epam.angular.tests.elements.complex.select.TestsSelectBase.FIRE;
+import static io.github.epam.angular.tests.elements.complex.select.TestsSelectBase.FLAREON;
+import static io.github.epam.angular.tests.elements.complex.select.TestsSelectBase.GRASS;
+import static io.github.epam.angular.tests.elements.complex.select.TestsSelectBase.HORSEA;
+import static io.github.epam.angular.tests.elements.complex.select.TestsSelectBase.MEW;
+import static io.github.epam.angular.tests.elements.complex.select.TestsSelectBase.MEWTWO;
+import static io.github.epam.angular.tests.elements.complex.select.TestsSelectBase.MUSHROOM;
+import static io.github.epam.angular.tests.elements.complex.select.TestsSelectBase.ODDISH;
+import static io.github.epam.angular.tests.elements.complex.select.TestsSelectBase.ONION;
+import static io.github.epam.angular.tests.elements.complex.select.TestsSelectBase.PEPPERONI;
+import static io.github.epam.angular.tests.elements.complex.select.TestsSelectBase.PIZZA;
+import static io.github.epam.angular.tests.elements.complex.select.TestsSelectBase.PSYCHIC;
+import static io.github.epam.angular.tests.elements.complex.select.TestsSelectBase.PSYDUCK;
+import static io.github.epam.angular.tests.elements.complex.select.TestsSelectBase.RED;
+import static io.github.epam.angular.tests.elements.complex.select.TestsSelectBase.SAUSAGE;
+import static io.github.epam.angular.tests.elements.complex.select.TestsSelectBase.SQUIRTLE;
+import static io.github.epam.angular.tests.elements.complex.select.TestsSelectBase.STEAK;
+import static io.github.epam.angular.tests.elements.complex.select.TestsSelectBase.TACOS;
+import static io.github.epam.angular.tests.elements.complex.select.TestsSelectBase.TOMATO;
+import static io.github.epam.angular.tests.elements.complex.select.TestsSelectBase.VULPIX;
+import static io.github.epam.angular.tests.elements.complex.select.TestsSelectBase.WATER;
+import static io.github.epam.angular.tests.elements.complex.select.TestsSelectBase.getPokemonsMap;
 import static io.github.epam.site.steps.States.shouldBeLoggedIn;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class MaterialSelectorContainerUnitTests extends TestsInit {
     private MaterialSelectorContainer container;
+    private String[] multiOptions = new String[3];
+    private int[] multiSelect = new int[3];
 
     @BeforeMethod(alwaysRun = true)
     public void before() {
@@ -31,7 +62,6 @@ public class MaterialSelectorContainerUnitTests extends TestsInit {
         basicMatSelect.show();
         basicMatSelect.expand();
         assertTrue(container.isDisplayed());
-        container.collapsePanel();
     }
 
     @Test
@@ -75,20 +105,20 @@ public class MaterialSelectorContainerUnitTests extends TestsInit {
     public void multipleSelectByValuesTest() {
         multipleSelect.show();
         multipleSelect.expand();
-        container.multipleSelect(MUSHROOM, PEPPERONI, TOMATO);
-        assertTrue(multipleSelect.selected(MUSHROOM + ", " + PEPPERONI + ", " + TOMATO));
-        multipleSelect.expand();
-        container.multipleSelect(MUSHROOM, PEPPERONI, TOMATO);
+        multiOptions[0] = MUSHROOM;
+        multiOptions[1] = PEPPERONI;
+        multiOptions[2] = TOMATO;
+        container.multipleSelect(multiOptions);
+        assertTrue(multipleSelect.selected(multiOptions[0] + ", " + multiOptions[1] + ", " + multiOptions[2]));
     }
 
     @Test
     public void multipleSelectByIndexesTest() {
         multipleSelect.show();
         multipleSelect.expand();
-        container.multipleSelect(1, 3, 5);
+        multiSelect = new int[] {1, 3, 5};
+        container.multipleSelect(multiSelect);
         assertTrue(multipleSelect.selected(EXTRA_CHEESE + ", " + ONION + ", " + SAUSAGE));
-        multipleSelect.expand();
-        container.multipleSelect(1, 3, 5);
     }
 
     @Test
@@ -134,5 +164,24 @@ public class MaterialSelectorContainerUnitTests extends TestsInit {
         customPanelStylingSelect.select(RED);
         customPanelStylingSelect.expand();
         assertTrue(container.color(255, 0, 0, 0.5));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void after() {
+        if (container.isDisplayed()) {
+            container.collapsePanel();
+        }
+        if (multiSelect[0] != 0) {
+            multipleSelect.expand();
+            container.multipleSelect(multiSelect);
+            multiSelect = new int[3];
+        }
+        if (multiOptions[0] != null) {
+            multipleSelect.expand();
+            container.multipleSelect(multiOptions);
+            multiOptions[0] = null;
+            multiOptions[1] = null;
+            multiOptions[2] = null;
+        }
     }
 }
