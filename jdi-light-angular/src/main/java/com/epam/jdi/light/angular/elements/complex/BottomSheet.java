e14223ca6df991bf7f32a654c6194ce2aadf1b14
Ammar Barry <ammar_barry@epam.com>
2020-09-02 15:54:04 +0300

    Draft

---
+++ jdi-light-angular/src/main/java/com/epam/jdi/light/angular/elements/complex/BottomSheet.java
@@ -0,0 +1,19 @@
+package com.epam.jdi.light.angular.elements.complex;
+
+import com.epam.jdi.light.angular.asserts.BottomSheetAssert;
+import com.epam.jdi.light.elements.base.UIBaseElement;
+
+public class BottomSheet extends UIBaseElement<BottomSheetAssert> {
+
+//    public void open() {
+//    }
+//
+//    @Override
+//    public BottomSheetAssert is() {
+//        return new BottomSheetAssert().set(this);
+//    }
+//
+//    public void open() {
+//        openBottomSheetButton.click();
+//    }
+}
