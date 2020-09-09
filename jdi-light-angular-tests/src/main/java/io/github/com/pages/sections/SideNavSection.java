d8274c8a8959abc91aa21cf534c3fc192c4de844
Aliaksandr_Navitski1 <aliaksandr_navitski1@epam.com>
2020-09-09 17:03:04 +0300

    draft

--- jdi-light-angular-tests/src/main/java/io/github/com/pages/sections/SideNavSection.java
+++ jdi-light-angular-tests/src/main/java/io/github/com/pages/sections/SideNavSection.java
@@ -1,6 +1,10 @@
 package io.github.com.pages.sections;
 
+import com.epam.jdi.light.angular.elements.common.Checkbox;
+import com.epam.jdi.light.angular.elements.complex.RadioButtons;
 import com.epam.jdi.light.angular.elements.complex.SideNav;
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
+import com.epam.jdi.light.ui.html.elements.common.Button;
 
 public class SideNavSection {
 
@@ -9,10 +13,27 @@ public class SideNavSection {
     public static SideNav implicitMainContent;
 
     public static SideNav openCloseBehavior;
+    @UI("#open-close-behavior mat-checkbox")
+    public static Checkbox sideNavOpened;
+    @UI("#open-close-behavior button")
+    public static Button sideNavToggle;
 
     public static SideNav configurableMode;
+    @UI("#configurable-mode .mat-sidenav button")
+    public static Button sideToggle;
+    @UI("#configurable-mode .mat-sidenav-content button")
+    public static Button contentToggle;
+    @UI("#configurable-mode .mat-sidenav .mat-radio-group")
+    public static RadioButtons sideNavRadioButtons;
+    @UI("#configurable-mode .mat-sidenav-content .mat-radio-group")
+    public static RadioButtons contentRadioButtons;
 
     public static SideNav customEscapeBackdrop;
+    @UI("#custom-escape-backdrop .mat-sidenav button")
+    public static Button toggleSideNav;
+    @UI("#custom-escape-backdrop .mat-sidenav-content button")
+    public static Button openSideNav;
+
 
     public static SideNav fixedPosition;
 
--- jdi-light-angular/src/main/java/com/epam/jdi/light/angular/elements/complex/SideNav.java
+++ jdi-light-angular/src/main/java/com/epam/jdi/light/angular/elements/complex/SideNav.java
@@ -10,7 +10,6 @@ import com.epam.jdi.light.elements.complex.WebList;
  * To see an example of Checkbox web element please visit https://material.angular.io/components/sidenav/overview.
  */
 
-
 public class SideNav extends UIBaseElement<SideNavAssert> {
 
     public static final String STYLE_VISIBLE = "transform: none; visibility: visible;";
@@ -43,66 +42,12 @@ public class SideNav extends UIBaseElement<SideNavAssert> {
         return element;
     }
 
-    /*open close events*/
-    @JDIAction("Get '{name}' checkbox")
-    public UIElement checkbox() {
-        return this.find("mat-checkbox");
-    }
-
-    /*configurable mode*/
-    @JDIAction("Get '{name}' radio buttons")
-    public WebList getNavRadioButtons() {
-        return this.finds("mat-radio-group mat-radio-button");
-    }
-
-    @JDIAction("Get '{name}' radio buttons'")
-    public WebList getContentRadioButtons() {
-        return this.finds("mat-sidenav-content mat-radio-group mat-radio-button");
-    }
-
-    @JDIAction("Get '{name}' radio buttons")
-    public UIElement getNavToggleButton() {
-        return this.getSideNav().find("button");
-    }
-
-    @JDIAction("Get '{name}' radio buttons")
-    public UIElement getContentToggleButton() {
-        return this.getContent().find("button");
-    }
-
-    @JDIAction("Click '{name}' side nav radio button by value '{0}'")
-    public void clickNavRadioButton(String value) {
-        getContentRadioButtons().get(value).click();
-    }
-
-    @JDIAction("Click '{name}' content radio button by value '{0}'")
-    public void clickContentRadioButton(String value) {
-        getContentRadioButtons().get(value).click();
-    }
-
-    @JDIAction("Get '{name}' radio buttons'")
-    public void clickNavToggleButton() {
-        this.getSideNav().find("button").click();
-    }
-
-    @JDIAction("Get '{name}' radio buttons'")
-    public void clickContentToggleButton() {
-        this.getContent().find("button").click();
-    }
-
     @Override
     public SideNavAssert is() {
         return new SideNavAssert().set(this);
     }
 
     /*boolean*/
-    @JDIAction("'{name}' is toggled ")
-    public boolean isNavToggled() {
-        clickContentToggleButton();
-        this.base().timer().wait(() -> this.visualValidation(".mat-sidenav"));
-        return getSideNav().attr("style").equals(STYLE_VISIBLE);
-    }
-
     @JDIAction("'{name}' has '{0}' text")
     public boolean navHasText(String text) {
         return this.getSideNav().text().equals(text);
@@ -110,11 +55,73 @@ public class SideNav extends UIBaseElement<SideNavAssert> {
 
     @JDIAction("'{name}' has '{0}' text")
     public boolean contentHasText(String text) {
-        return this.getContent().text().equals(text);
+        return getContent().text().equals(text);
     }
 
     @JDIAction("'{name}' is visible")
     public boolean navIsVisible() {
         return this.getSideNav().attr(STYLE).equals(STYLE_VISIBLE);
     }
+
+    @JDIAction("Get '{name}' side nav content")
+    public UIElement getEvents() {
+        return getContent().find(".example-events");
+    }
+
 }
+
+/*open close events*/
+  /*  @JDIAction("Get '{name}' checkbox")
+    public UIElement checkbox() {
+        return this.find("mat-checkbox");
+    }*/
+
+
+//    @JDIAction("Get '{name}' radio buttons'")
+//    public void clickContentToggleButton() {
+//        this.getContent().find("button").click();
+//    }
+
+//    @JDIAction("'{name}' is toggled ")
+//    public boolean isNavToggled() {
+//        clickContentToggleButton();
+//        this.base().timer().wait(() -> this.visualValidation(".mat-sidenav"));
+//        return getSideNav().attr("style").equals(STYLE_VISIBLE);
+//    }
+
+//
+//    @JDIAction("Get '{name}' radio buttons")
+//    public WebList getNavRadioButtons() {
+//        return this.finds("mat-radio-group mat-radio-button");
+//    }
+//
+//    @JDIAction("Get '{name}' radio buttons'")
+//    public WebList getContentRadioButtons() {
+//        return this.finds("mat-sidenav-content mat-radio-group mat-radio-button");
+//    }
+//
+//    @JDIAction("Get '{name}' radio buttons")
+//    public UIElement getNavToggleButton() {
+//        return this.getSideNav().find("button");
+//    }
+
+//
+//    @JDIAction("Get '{name}' radio buttons")
+//    public UIElement getContentToggleButton() {
+//        return this.getContent().find("button");
+//    }
+//
+//    @JDIAction("Click '{name}' side nav radio button by value '{0}'")
+//    public void clickNavRadioButton(String value) {
+//        getContentRadioButtons().get(value).click();
+//    }
+//
+//    @JDIAction("Click '{name}' content radio button by value '{0}'")
+//    public void clickContentRadioButton(String value) {
+//        getContentRadioButtons().get(value).click();
+//    }
+//
+//    @JDIAction("Get '{name}' radio buttons'")
+//    public void clickNavToggleButton() {
+//        this.getSideNav().find("button").click();
+//    }
