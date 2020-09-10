cfa0a915d278be3f5af1d5e494b0c4b558ab8eed
Ekaterina_Fursova <ekaterina_fursova@epam.com>
2020-09-10 14:11:19 +0300

    #2248_android_toggle_button: improved test for toggle button element

--- jdi-light-mobile-tests/src/test/java/nativeapp_android/tests/ToggleButtonTest.java
+++ jdi-light-mobile-tests/src/test/java/nativeapp_android/tests/ToggleButtonTest.java
@@ -1,24 +1,25 @@
 package nativeapp_android.tests;
 
+import com.epam.jdi.light.mobile.elements.common.app.android.ToggleButton;
 import nativeapp.android.apiDemos.ButtonsPage;
 import nativeapp.android.apiDemos.IndexPage;
 import nativeapp.android.apiDemos.ViewsPage;
-import nativeapp_android.ToggleButtonTestInit;
+import nativeapp_android.ApiDemosTestInit;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
-public class ToggleButtonTest extends ToggleButtonTestInit {
+public class ToggleButtonTest extends ApiDemosTestInit {
 
     @Test
     public void toggleButtonTest() {
         IndexPage.viewsPage.click();
         ViewsPage.buttonsPage.click();
-        assertEquals(ButtonsPage.toggleButton.isToggleButtonDisplayed(), true);
-        assertEquals("OFF", ButtonsPage.toggleButton.getToggleButtonText());
-        ButtonsPage.toggleButton.toggleButtonClick();
-        assertEquals("ON",ButtonsPage.toggleButton.getToggleButtonText());
-        ButtonsPage.toggleButton.toggleButtonClick();
-        assertEquals("OFF", ButtonsPage.toggleButton.getToggleButtonText());
+        ButtonsPage.toggleButton.is().displayed();
+        ButtonsPage.toggleButton.is().off();
+        ButtonsPage.toggleButton.setToOn();
+        ButtonsPage.toggleButton.is().on();
+        ButtonsPage.toggleButton.setToOff();
+        ButtonsPage.toggleButton.is().off();
     }
 }
--- jdi-light-mobile/src/main/java/com/epam/jdi/light/mobile/elements/common/app/android/ToggleButton.java
+++ jdi-light-mobile/src/main/java/com/epam/jdi/light/mobile/elements/common/app/android/ToggleButton.java
@@ -1,18 +1,29 @@
 package com.epam.jdi.light.mobile.elements.common.app.android;
 
-import com.epam.jdi.light.mobile.elements.common.app.Button;
+import com.epam.jdi.light.common.JDIAction;
+import com.epam.jdi.light.logger.LogLevels;
+import com.epam.jdi.light.mobile.asserts.SwitchAssert;
 
-public class ToggleButton extends Button {
-
-    public void toggleButtonClick() {
-        core().tap();
+public class ToggleButton extends Switch {
+    @JDIAction(value = "Check that '{name}' is on", level = LogLevels.DEBUG)
+    public boolean isOn() {
+        return core().text().equals("ON");
     }
 
-    public String getToggleButtonText() {
-        return core().getText();
+    @JDIAction(value = "Set '{name}' to off", level = LogLevels.DEBUG)
+    public void setToOff() {
+        if (isOn())
+            core().tap();
     }
 
-    public boolean isToggleButtonDisplayed() {
-        return core().isDisplayed();
+    @JDIAction(value = "Set '{name}' to on", level = LogLevels.DEBUG)
+    public void setToOn() {
+        if (!isOn())
+            core().tap();
+    }
+
+    @Override
+    public SwitchAssert is() {
+        return new SwitchAssert().set(this);
     }
 }
