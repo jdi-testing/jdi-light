f2e95bd7cc3d4cd47db2aba7955857cfd8453c97
Aliaksandr_Navitski1 <aliaksandr_navitski1@epam.com>
2020-09-16 20:16:37 +0300

    spinner tests added

--- jdi-light-angular-tests/src/test/java/io/github/epam/angular/tests/elements/common/SpinnerTests.java
+++ jdi-light-angular-tests/src/test/java/io/github/epam/angular/tests/elements/common/SpinnerTests.java
@@ -1,11 +1,16 @@
 package io.github.epam.angular.tests.elements.common;
 
+import com.epam.jdi.tools.func.JAction;
 import io.github.epam.TestsInit;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import static com.epam.jdi.light.elements.composite.WebPage.refresh;
 import static io.github.com.StaticSite.angularPage;
-import static io.github.com.pages.AngularPage.basicProgressSpinner;
+import static io.github.com.pages.sections.SpinnerSection.basicProgressSpinner;
+import static io.github.com.pages.sections.SpinnerSection.showSpinner;
+import static io.github.epam.angular.tests.BaseValidationsUtils.baseValidation;
+import static io.github.epam.angular.tests.elements.BaseValidationsUtils.duration;
 import static io.github.epam.site.steps.States.shouldBeLoggedIn;
 
 public class SpinnerTests extends TestsInit {
@@ -17,8 +22,40 @@ public class SpinnerTests extends TestsInit {
     }
 
     @Test
-    public void checkBasicSpinnerIsDisplayed() {
-        basicProgressSpinner.show();
-        basicProgressSpinner.isDisplayed();
+    public void disappear() {
+        showSpinner.click();
+        basicProgressSpinner.waitFor().disappear();
+    }
+
+    @Test
+    public void baseValidationTest() {
+        showSpinner.click();
+        baseValidation(basicProgressSpinner);
+    }
+
+    @Test
+    public void checkSpinnerHidden() {
+        showSpinner.click();
+        basicProgressSpinner.assertThat().hidden();
+    }
+
+    @Test
+    public void checkSpinnerAppearAndThenDisappear() {
+        showSpinner.click();
+        basicProgressSpinner.is().displayed();
+        basicProgressSpinner.waitFor().hidden();
+    }
+
+    @Test
+    public void checkSpinnerDurationTest() {
+        refresh();
+        showSpinner.show();
+        final int DURATION = 5;
+        JAction action = () -> {
+            basicProgressSpinner.base().timer().wait(() -> basicProgressSpinner.isDisplayed());
+            basicProgressSpinner.base().timer().wait(() -> basicProgressSpinner.isHidden());
+        };
+        showSpinner.click();
+        duration(DURATION, 900, action);
     }
 }
