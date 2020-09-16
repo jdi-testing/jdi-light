46fd23738985972fcd74c5a84cff509bf28dbea5
Aliaksandr_Navitski1 <aliaksandr_navitski1@epam.com>
2020-09-16 20:16:37 +0300

    spinner tests added

--- jdi-light-angular/src/main/java/com/epam/jdi/light/angular/elements/common/Spinner.java
+++ jdi-light-angular/src/main/java/com/epam/jdi/light/angular/elements/common/Spinner.java
@@ -1,5 +1,6 @@
 package com.epam.jdi.light.angular.elements.common;
 
+import com.epam.jdi.light.asserts.generic.UIAssert;
 import com.epam.jdi.light.elements.base.UIBaseElement;
 import com.epam.jdi.light.ui.html.asserts.ProgressAssert;
 
@@ -7,5 +8,5 @@ import com.epam.jdi.light.ui.html.asserts.ProgressAssert;
  * To see an example of ProgressSpinner web element please visit https://material.angular.io/components/progress-spinner/overview.
  */
 
-public class ProgressSpinner extends UIBaseElement<ProgressAssert> {
+public class Spinner extends UIBaseElement<UIAssert<? ,Spinner>> {
 }
