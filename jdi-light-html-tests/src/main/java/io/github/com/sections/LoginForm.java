aa581cef08e77a1c28c63a57ad8281326a335ae4
Ihar_Lipko <ihar_lipko@epam.com>
2020-09-16 11:27:35 +0300

    Adjusted LoginForm and FormsTests classes.

--- jdi-light-html-tests/src/main/java/io/github/com/sections/LoginForm.java
+++ jdi-light-html-tests/src/main/java/io/github/com/sections/LoginForm.java
@@ -1,24 +1,32 @@
 package io.github.com.sections;
 
+import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.light.elements.composite.Form;
-import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.ui.html.elements.common.Button;
 import com.epam.jdi.light.ui.html.elements.common.TextField;
 import io.github.com.entities.User;
+import org.openqa.selenium.By;
 
 import static io.github.com.pages.Header.userIcon;
 
 public class LoginForm extends Form<User> {
-	@UI("#name") TextField name;
-	TextField password;
-	Button loginButton;
+    protected TextField name;
+    protected TextField password;
+    protected Button loginButton;
 
-	public void shouldBeOpened() {
-		if (isHidden())
-			userIcon.click();
-	}
-	@Override
-	public boolean isDisplayed() {
-		return name.isDisplayed();
-	}
+    @Override
+    public boolean isHidden() {
+        return new UIElement(By.cssSelector(".dropdown.uui-profile-menu.open")).isHidden();
+    }
+
+    public void shouldBeOpened() {
+        if (isHidden()) {
+            userIcon.click();
+        }
+    }
+
+    @Override
+    public boolean isDisplayed() {
+        return name.isDisplayed();
+    }
 }
