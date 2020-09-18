25724aef2c63627d25b001e9c84567bd9e0890ea
Aliaksandr_Navitski1 <aliaksandr_navitski1@epam.com>
2020-09-18 14:53:58 +0300

    spinner completed

--- jdi-light-angular-tests/src/main/java/io/github/com/pages/sections/SpinnerSection.java
+++ jdi-light-angular-tests/src/main/java/io/github/com/pages/sections/SpinnerSection.java
@@ -4,7 +4,6 @@ import com.epam.jdi.light.angular.elements.common.Spinner;
 import com.epam.jdi.light.ui.html.elements.common.Button;
 
 public class SpinnerSection {
-
     public static Spinner basicProgressSpinner;
 
     public static Button showSpinner;
