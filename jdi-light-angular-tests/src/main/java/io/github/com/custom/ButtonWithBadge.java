bcdda42bada97266ad33f8dda56d0288ff010424
RomanIovlev <romanyister@gmail.com>
2020-09-18 13:42:10 +0200

    fix angular badge issue

--- jdi-light-angular-tests/src/main/java/io/github/com/custom/ButtonWithBadge.java
+++
@@ -1,7 +0,0 @@
-package io.github.com.custom;
-
-import com.epam.jdi.light.angular.elements.common.WithBadge;
-import com.epam.jdi.light.ui.html.elements.common.Button;
-
-public class ButtonWithBadge extends Button implements WithBadge {
-}
--- jdi-light-angular-tests/src/main/java/io/github/com/custom/IconWithBadge.java
+++
@@ -1,7 +0,0 @@
-package io.github.com.custom;
-
-import com.epam.jdi.light.angular.elements.common.WithBadge;
-import com.epam.jdi.light.ui.html.elements.common.Icon;
-
-public class IconWithBadge extends Icon implements WithBadge {
-}
