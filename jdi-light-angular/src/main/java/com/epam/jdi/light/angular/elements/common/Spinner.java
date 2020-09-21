acda8554a1984517e32d87b87508cf3ab3df7042
Aliaksandr_Navitski1 <aliaksandr_navitski1@epam.com>
2020-09-21 14:21:35 +0300

    spinner completed

--- jdi-light-angular/src/main/java/com/epam/jdi/light/angular/elements/common/Spinner.java
+++ jdi-light-angular/src/main/java/com/epam/jdi/light/angular/elements/common/Spinner.java
@@ -2,7 +2,6 @@ package com.epam.jdi.light.angular.elements.common;
 
 import com.epam.jdi.light.asserts.generic.UIAssert;
 import com.epam.jdi.light.elements.base.UIBaseElement;
-import com.epam.jdi.light.ui.html.asserts.ProgressAssert;
 
 /**
  * To see an example of ProgressSpinner web element please visit https://material.angular.io/components/progress-spinner/overview.
