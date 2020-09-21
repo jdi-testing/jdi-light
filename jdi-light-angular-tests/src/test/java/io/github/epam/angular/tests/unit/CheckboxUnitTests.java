06e2360e919ac2da06e8598bdd0f353a6eb52f5b
Ihar_Lipko <ihar_lipko@epam.com>
2020-09-21 19:49:43 +0300

    Refactored Checkbox element.

--- jdi-light-angular-tests/src/test/java/io/github/epam/angular/tests/unit/CheckboxUnitTests.java
+++ jdi-light-angular-tests/src/test/java/io/github/epam/angular/tests/unit/CheckboxUnitTests.java
@@ -1,51 +1,72 @@
 package io.github.epam.angular.tests.unit;
 
 import io.github.epam.TestsInit;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static io.github.com.StaticSite.angularPage;
-import static io.github.com.pages.AngularPage.checkboxSection;
+import static io.github.com.pages.sections.CheckboxSection.basicCheckbox;
+import static io.github.com.pages.sections.CheckboxSection.checkedCheckbox;
+import static io.github.com.pages.sections.CheckboxSection.disabledCheckbox;
+import static io.github.com.pages.sections.CheckboxSection.indeterminateCheckbox;
+import static io.github.com.pages.sections.CheckboxSection.resultCheckbox;
 import static io.github.epam.site.steps.States.shouldBeLoggedIn;
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 public class CheckboxUnitTests extends TestsInit {
-
-    @BeforeMethod
+    @BeforeMethod(alwaysRun = true)
     public void before() {
         shouldBeLoggedIn();
         angularPage.shouldBeOpened();
+        disabledCheckbox.show();
     }
 
     @Test
     public void checkTest() {
-        checkboxSection.basicCheckbox.check();
-        assertTrue(checkboxSection.basicCheckbox.isSelected());
+        basicCheckbox.check();
+        assertTrue(basicCheckbox.isSelected());
     }
 
     @Test
     public void uncheckTest() {
-        checkboxSection.basicCheckbox.uncheck();
-        assertFalse(checkboxSection.basicCheckbox.isSelected());
+        basicCheckbox.uncheck();
+        assertFalse(basicCheckbox.isSelected());
     }
 
     @Test
-    public void clickTest() {
-        assertTrue(checkboxSection.basicCheckbox.isSelected());
-        checkboxSection.basicCheckbox.click();
-        assertFalse(checkboxSection.basicCheckbox.isSelected());
+    public void labelTest() {
+        assertEquals(basicCheckbox.label().getValue(), "Check me!");
     }
 
     @Test
-    public void testEnabledOption() {
-        checkboxSection.configurableDisabledCheckbox.uncheck();
-        assertTrue(checkboxSection.configurableResultCheckbox.isEnabled());
+    public void indeterminateTest() {
+        indeterminateCheckbox.click();
+        assertTrue(resultCheckbox.isIndeterminate());
     }
 
     @Test
-    public void testDisabledOption() {
-        checkboxSection.configurableDisabledCheckbox.check();
-        assertTrue(checkboxSection.configurableResultCheckbox.isDisabled());
+    public void enabledOptionTest() {
+        disabledCheckbox.uncheck();
+        assertTrue(resultCheckbox.isEnabled());
+    }
+
+    @Test
+    public void disabledOptionTest() {
+        disabledCheckbox.check();
+        assertTrue(resultCheckbox.isDisabled());
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void after() {
+        if (disabledCheckbox.isSelected()) {
+            disabledCheckbox.uncheck();
+        }
+        if (resultCheckbox.isSelected() || resultCheckbox.isIndeterminate()) {
+            indeterminateCheckbox.uncheck();
+            checkedCheckbox.uncheck();
+        }
     }
 }
