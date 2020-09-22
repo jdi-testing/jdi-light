59d95fb214e83ed4f7a87f27a7f9542deacdb334
Aliaksandr_Navitski1 <aliaksandr_navitski1@epam.com>
2020-09-22 15:21:08 +0300

    progress bar refactored

--- jdi-light-angular/src/main/java/com/epam/jdi/light/angular/elements/common/ProgressBar.java
+++ jdi-light-angular/src/main/java/com/epam/jdi/light/angular/elements/common/ProgressBar.java
@@ -10,7 +10,6 @@ import java.util.regex.Pattern;
 
 import static com.epam.jdi.light.angular.elements.enums.ProgressBarModes.BUFFER;
 import static com.epam.jdi.light.angular.elements.enums.ProgressBarModes.DETERMINATE;
-import static com.epam.jdi.light.logger.LogLevels.DEBUG;
 import static com.epam.jdi.light.ui.html.HtmlUtils.getDouble;
 import static com.epam.jdi.light.ui.html.HtmlUtils.getInt;
 
