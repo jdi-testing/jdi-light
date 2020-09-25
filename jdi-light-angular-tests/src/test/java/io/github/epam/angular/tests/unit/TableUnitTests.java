cb023d17d89de4be950f85acbcce5354c39f8f95
Vsevolod_Adrianov <Vsevolod_Adrianov@epam.com>
2020-09-25 21:51:42 +0300

    add expanded row Table test

--- jdi-light-angular-tests/src/test/java/io/github/epam/angular/tests/unit/TableUnitTests.java
+++ jdi-light-angular-tests/src/test/java/io/github/epam/angular/tests/unit/TableUnitTests.java
@@ -4,13 +4,19 @@ import io.github.epam.TestsInit;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.Iterator;
+import java.util.List;
+
 import static io.github.com.StaticSite.angularPage;
-import static io.github.com.pages.sections.TableSection.basicTable;
-import static io.github.com.pages.sections.TableSection.flexTable;
+import static io.github.com.pages.sections.TableSection.*;
 import static io.github.epam.site.steps.States.shouldBeLoggedIn;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotEquals;
+import static org.testng.AssertJUnit.assertTrue;
 
 public class TableUnitTests extends TestsInit {
+
+    final static String[] chemElements = {"Hydrogen","Helium","Lithium","Beryllium","Boron","Carbon","Nitrogen"};
 
     @BeforeMethod(alwaysRun = true)
     public void before() {
@@ -31,5 +37,47 @@ public class TableUnitTests extends TestsInit {
         basicTable.show();
         assertEquals(basicTable.header(),flexTable.header());
         assertEquals(basicTable.getRow(row).getValuesFast(),flexTable.getRow(row).getValuesFast());
+    }
+
+    @Test
+    public void dynamicTablesTest() {
+        dynamicTable.show();
+        List<String> headersBefore = dynamicTable.header();
+
+        addButton.click();
+        dynamicTable.is().shown();
+        assertTrue(headersBefore.size()<dynamicTable.header().size());
+
+        removeButton.click();
+        dynamicTable.is().shown();
+        assertEquals(headersBefore,dynamicTable.header());
+
+        shuffleButton.click();
+        dynamicTable.is().shown();
+        assertNotEquals(headersBefore,dynamicTable.header());
+    }
+
+    @Test
+    public void expandableTablesTest() {
+        int row = 1;
+        int idx = 2;
+        expandableRowsTable.show();
+        for (int nn = 0; nn < chemElements.length; nn++) {
+            row = expandableRowsTable.getRowIndexByName(chemElements[nn]);
+            System.out.println(chemElements[nn]);
+            expandableRowsTable.getRow(row).getFast().click();
+            String[] info = expandableRowsTable.getRow(row+1).getValue().split("\n",5);
+            assertEquals(info[idx],chemElements[nn]);
+        }
+    }
+
+    @Test
+    public void filteringTablesTest() {
+        filteringTable.show();
+    }
+
+    @Test
+    public void footerTablesTest() {
+        tableWithFooter.show();
     }
 }
