870aee80c046093fa2796def05f7abd20f28ba09
Vsevolod_Adrianov <Vsevolod_Adrianov@epam.com>
2020-09-27 19:05:20 +0300

    use jdi basic buttons

--- jdi-light-angular-tests/src/test/java/io/github/epam/angular/tests/unit/TableUnitTests.java
+++ jdi-light-angular-tests/src/test/java/io/github/epam/angular/tests/unit/TableUnitTests.java
@@ -4,7 +4,6 @@ import io.github.epam.TestsInit;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.Iterator;
 import java.util.List;
 
 import static io.github.com.StaticSite.angularPage;
