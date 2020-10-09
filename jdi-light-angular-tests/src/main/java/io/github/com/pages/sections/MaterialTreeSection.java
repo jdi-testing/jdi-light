9a63cee079c55f37cf919ba6f4a208739716d854
Vsevolod_Adrianov <Vsevolod_Adrianov@epam.com>
2020-10-09 19:52:54 +0300

    work with tree node by name

--- jdi-light-angular-tests/src/main/java/io/github/com/pages/sections/MaterialTreeSection.java
+++ jdi-light-angular-tests/src/main/java/io/github/com/pages/sections/MaterialTreeSection.java
@@ -3,15 +3,19 @@ package io.github.com.pages.sections;
 
 import com.epam.jdi.light.angular.elements.complex.MaterialTree;
 import com.epam.jdi.light.elements.common.UIElement;
-import com.epam.jdi.light.elements.complex.JList;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
-import com.epam.jdi.light.ui.html.elements.common.Button;
 
 public class MaterialTreeSection {
     @UI("tree-dynamic-example")
     public static UIElement dynamicExample;
     @UI("#tree-dynamic")
     public static MaterialTree dynamicTree;
-    @UI("#tree-dynamic mat-tree-node")
-    public JList<Button> basicTree;
+    @UI("tree-flat-overview-example")
+    public static UIElement flatExample;
+    @UI("#tree-flat")
+    public static MaterialTree flatTree;
+    @UI("tree-checklist-example")
+    public static UIElement checkBoxesExample;
+    @UI("#tree-checklist")
+    public static MaterialTree checkBoxesTree;
 }
--- jdi-light-angular/src/main/java/com/epam/jdi/light/angular/elements/complex/MaterialTree.java
+++ jdi-light-angular/src/main/java/com/epam/jdi/light/angular/elements/complex/MaterialTree.java
@@ -3,48 +3,42 @@ package com.epam.jdi.light.angular.elements.complex;
 import com.epam.jdi.light.asserts.generic.UIAssert;
 import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.elements.base.UIBaseElement;
-import com.epam.jdi.light.elements.common.UIElement;
-import com.epam.jdi.light.elements.complex.WebList;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import java.util.List;
 
 public class MaterialTree extends UIBaseElement<UIAssert<?, MaterialTree>> {
-    public String treeLocator = "//*[@id='%s']";
-    public String nodeLocator = treeLocator.concat("//mat-tree-node");
-    public String contentsLocator = treeLocator.concat("//*[contains(@class,'mat-expansion-panel-content')]");
-    public String iconsLocator = nodeLocator.concat("//mat-icon");
-    public String titlesLocator = nodeLocator.concat("//mat-panel-title");
 
-    public String navigationButton
-            = "[text()=' %s ']//ancestor::mat-expansion-panel-header//following-sibling::*//button";
-    public String nextButtonLocator = titlesLocator.concat(navigationButton.concat("//span[text()='Next']"));
-    public String previousButtonLocator = titlesLocator.concat(navigationButton.concat("//span[text()='Previous']"));
-    public String endButtonLocator = titlesLocator.concat(navigationButton.concat("//span[text()='End']"));
+    public String cssTreeNodeLocator = "mat-tree-node";
+    public String cssButtonLocator = "button > span > mat-icon";
+    public String cssChevronLocator = cssTreeNodeLocator + " > "+ cssButtonLocator;
 
-    public String expansionIndicatorLocator = nodeLocator.concat("/button/span/mat-icon");
-    public String smartSharp = "smart: #";
-    public String cssSharp = "css='#";
+    public final String role = "role";
+    public final String item = "treeitem";
+    public final String itemgroup = "group";
 
     @JDIAction("Expand '{name}' panel by index '{0}'")
     public void expand(final int indexNumber) {
-        List<UIElement> expansionIndicator = getExpansionIndicatorLocator();
-        List<UIElement> icons = getIconsLocator();
+        List<WebElement> expansionIndicator = getNodeLocator();
         if (!expansionIndicator.isEmpty() && isCollapsed(indexNumber)) {
-            expansionIndicator.get(indexNumber).click();
-        } else if (!icons.isEmpty() && isCollapsed(indexNumber)) {
-            icons.get(indexNumber).click();
+            expansionIndicator.get(indexNumber-1).click();
+        }
+    }
+
+    @JDIAction("Expand '{name}' node by name '{node}'")
+    public void expandByName(final String nodeName) {
+        WebElement expansionIndicator = getNodeByName(nodeName);
+        if (expansionIndicator!=null) {
+            expansionIndicator.findElement(By.cssSelector(cssButtonLocator)).click();
         }
     }
 
     @JDIAction("Collapse '{name}' panel by index '{0}'")
     public void collapse(final int indexNumber) {
-        List<UIElement> expansionIndicator = getExpansionIndicatorLocator();
-        List<UIElement> icons = getIconsLocator();
+        List<WebElement> expansionIndicator = getExpansionButton();
         if (!expansionIndicator.isEmpty() && isExpanded(indexNumber)) {
-            expansionIndicator.get(indexNumber).click();
-        } else if (!icons.isEmpty() && isExpanded(indexNumber)) {
-            icons.get(indexNumber).click();
+            expansionIndicator.get(indexNumber-1).click();
         }
     }
 
@@ -55,74 +49,25 @@ public class MaterialTree extends UIBaseElement<UIAssert<?, MaterialTree>> {
 
     @JDIAction("Is '{name}' '{0}' panel collapsed")
     public boolean isCollapsed(final int indexNumber) {
-        UIElement expansionHeader = getNodeLocator().get(indexNumber);
-        expansionHeader.waitFor();
-        boolean isHeaderExpanded = expansionHeader.attr("aria-expanded").contains("false");
-        UIElement expansionContent = getContentsLocator().get(indexNumber);
-        expansionContent.waitFor();
-        boolean isContentHidden = expansionContent.attr("style").contains("visibility: hidden");
+        WebElement expansionHeader = getNodeLocator().get(indexNumber-1);
+        boolean isHeaderExpanded = expansionHeader.getAttribute("aria-expanded").contains("false");
+        boolean isContentHidden = expansionHeader.getAttribute(role).contains(itemgroup);
         return isHeaderExpanded && isContentHidden;
     }
 
-    @JDIAction("Select '{name}' '{0}' next panel")
-    public void next(final String title) {
-        getNextButtonLocator(title).click();
+    protected WebElement getNodeByName(String name) {
+        List<WebElement> nodes = getNodeLocator();
+        for(WebElement node: nodes) {
+            if(node.getText().split("\n")[1].equalsIgnoreCase(name)) return node;
+        }
+        return null;
     }
 
-    @JDIAction("Select '{name}' '{0}' previous panel")
-    public void previous(final String title) {
-        getPreviousButtonLocator(title).click();
+    protected List<WebElement> getNodeLocator() {
+        return this.uiElement.findElements(By.cssSelector(cssTreeNodeLocator));
     }
 
-    @JDIAction("Select '{name}' '{0}' end selection")
-    public void end(final String title) {
-        getEndButtonLocator(title).click();
-    }
-
-    protected List<UIElement> getNodeLocator() {
-        return new WebList(By.xpath(String.format(nodeLocator,
-                this.uiElement.locator.printLocator().replace(smartSharp, "")
-                        .replace(cssSharp, "").replace("'", ""))));
-    }
-
-    protected List<UIElement> getContentsLocator() {
-        return new WebList(By.xpath(String.format(contentsLocator,
-                this.uiElement.locator.printLocator().replace(smartSharp, "")
-                        .replace(cssSharp, "").replace("'", ""))));
-    }
-
-    protected UIElement getNextButtonLocator(final String tittle) {
-        return new UIElement(By.xpath(String.format(nextButtonLocator,
-                this.uiElement.locator.printLocator().replace(smartSharp, "")
-                        .replace(cssSharp, "").replace("'", ""), tittle)));
-    }
-
-    protected UIElement getPreviousButtonLocator(final String tittle) {
-        return new UIElement(By.xpath(String.format(previousButtonLocator,
-                this.uiElement.locator.printLocator().replace(smartSharp, "")
-                        .replace(cssSharp, "").replace("'", ""), tittle)));
-    }
-
-    protected UIElement getEndButtonLocator(final String tittle) {
-        return new UIElement(By.xpath(String.format(endButtonLocator,
-                this.uiElement.locator.printLocator().replace(smartSharp, "")
-                        .replace(cssSharp, "").replace("'", ""), tittle)));
-    }
-
-    protected List<UIElement> getIconsLocator() {
-        return new WebList(By.xpath(String.format(iconsLocator,
-                this.uiElement.locator.printLocator().replace(smartSharp, "")
-                        .replace(cssSharp, "").replace("'", ""))));
-    }
-
-    protected List<UIElement> getExpansionIndicatorLocator() {
-        String idName = this.uiElement.locator.printLocator().replace(smartSharp, "")
-                .replace(cssSharp, "").replace("'", "");
-        String sl = String.format(expansionIndicatorLocator, idName);
-        List<UIElement> nodes = new WebList(By.xpath(sl));
-        return nodes;
-//        return new WebList(By.xpath(String.format(expansionIndicatorLocator,
-//                this.uiElement.locator.printLocator().replace(smartSharp, "")
-//                        .replace(cssSharp, "").replace("'", ""))));
+    protected List<WebElement> getExpansionButton() {
+        return this.uiElement.findElements(By.cssSelector(cssChevronLocator));
     }
 }
