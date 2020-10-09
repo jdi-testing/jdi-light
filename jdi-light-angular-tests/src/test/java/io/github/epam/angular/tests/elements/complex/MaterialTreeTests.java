869e9040d39f2dced8f571c13b391ca3ccb3d2a3
Vsevolod_Adrianov <Vsevolod_Adrianov@epam.com>
2020-10-09 19:52:54 +0300

    work with tree node by name

--- jdi-light-angular-tests/src/test/java/io/github/epam/angular/tests/elements/complex/MaterialTreeTests.java
+++ jdi-light-angular-tests/src/test/java/io/github/epam/angular/tests/elements/complex/MaterialTreeTests.java
@@ -5,9 +5,12 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static io.github.com.StaticSite.angularPage;
-import static io.github.com.pages.AngularPage.materialTreeSection;
+import static io.github.com.pages.sections.MaterialTreeSection.checkBoxesExample;
+import static io.github.com.pages.sections.MaterialTreeSection.checkBoxesTree;
 import static io.github.com.pages.sections.MaterialTreeSection.dynamicExample;
 import static io.github.com.pages.sections.MaterialTreeSection.dynamicTree;
+import static io.github.com.pages.sections.MaterialTreeSection.flatExample;
+import static io.github.com.pages.sections.MaterialTreeSection.flatTree;
 import static io.github.epam.site.steps.States.shouldBeLoggedIn;
 
 public class MaterialTreeTests extends TestsInit {
@@ -19,11 +22,29 @@ public class MaterialTreeTests extends TestsInit {
 
     @Test
     public void basicTreeTest() {
-//        nestedMenuButton.is().displayed();
         dynamicExample.show();
         dynamicTree.expand(1);
-        dynamicTree.isExpanded(1);
-        materialTreeSection.basicTree.is().displayed();
+        dynamicTree.is().displayed();
     }
 
+    @Test
+    public void expandByNameTest() {
+        dynamicExample.show();
+        dynamicTree.expandByName("Fruits"); // "Vegetables"
+        dynamicTree.is().displayed();
+    }
+
+    @Test
+    public void expandWithCheckBoxsTest() {
+        checkBoxesExample.show();
+        checkBoxesTree.expandByName("Groceries");  // "Reminders"
+        checkBoxesTree.is().displayed();
+    }
+
+    @Test
+    public void flatByNameTest() {
+        flatExample.show();
+        flatTree.expandByName("Vegetables");
+        flatTree.is().displayed();
+    }
 }
