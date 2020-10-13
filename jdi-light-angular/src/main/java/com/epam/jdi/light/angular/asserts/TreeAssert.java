d88f87ed4050296352bd93a72957d8d6447f0568
Vsevolod_Adrianov <Vsevolod_Adrianov@epam.com>
2020-10-13 22:39:35 +0300

    add checkboxes tests

--- jdi-light-angular/src/main/java/com/epam/jdi/light/angular/asserts/TreeAssert.java
+++ jdi-light-angular/src/main/java/com/epam/jdi/light/angular/asserts/TreeAssert.java
@@ -30,40 +30,4 @@ public class TreeAssert extends UIAssert<TreeAssert, MaterialTree> {
     public TreeAssert value(final String value, final String field) {
         return value(Matchers.is(field), value);
     }
-
-//    @JDIAction("Assert that '{name}' has title '{0}' with value '{1}'")
-//    public TreeAssert attribute(final Matcher<String> attrib, final String value) {
-//        jdiAssert(element().attribute(attrib,value), attrib);
-//        return this;
-//    }
-
-//    @JDIAction("Assert that '{name}' has title '{0}' with value '{1}'")
-//    public TreeAssert title(final Matcher<String> title, final int titleNumber) {
-//        jdiAssert(element().title(titleNumber), title);
-//        return this;
-//    }
-//
-//    public TreeAssert title(final String title, final int titleNumber) {
-//        return title(Matchers.is(title), titleNumber);
-//    }
-//
-//    @JDIAction("Assert that '{name}' has description '{0}' with value '{1}'")
-//    public TreeAssert description(final Matcher<String> description, final int titleNumber) {
-//        jdiAssert(element().description(titleNumber), description);
-//        return this;
-//    }
-//
-//    public TreeAssert description(final String description, final int titleNumber) {
-//        return description(Matchers.is(description), titleNumber);
-//    }
-//
-//    @JDIAction("Assert that '{name}' has content '{0}' with value '{1}'")
-//    public TreeAssert content(final Matcher<String> content, final int titleNumber) {
-//        jdiAssert(element().content(titleNumber), content);
-//        return this;
-//    }
-//
-//    public TreeAssert content(final String content, final int titleNumber) {
-//        return content(Matchers.is(content), titleNumber);
-//    }
 }
--- jdi-light-angular/src/main/java/com/epam/jdi/light/angular/elements/complex/MaterialTree.java
+++ jdi-light-angular/src/main/java/com/epam/jdi/light/angular/elements/complex/MaterialTree.java
@@ -1,11 +1,13 @@
 package com.epam.jdi.light.angular.elements.complex;
 
 import com.epam.jdi.light.angular.asserts.TreeAssert;
+import com.epam.jdi.light.angular.elements.common.Checkbox;
 import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.elements.base.UIBaseElement;
 import com.epam.jdi.light.elements.common.UIElement;
 import org.openqa.selenium.By;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MaterialTree extends UIBaseElement<TreeAssert> {
@@ -24,7 +26,7 @@ public class MaterialTree extends UIBaseElement<TreeAssert> {
     @JDIAction("Expand '{name}' node by name '{node}'")
     public void expand(final String nodeName) {
         UIElement expansionIndicator = getNodeByName(nodeName);
-        if (expansionIndicator!=null) {
+        if (!expansionIndicator.isEmpty() && isCollapsed(nodeName)) {
             expansionIndicator.finds(By.cssSelector(cssButtonLocator)).get(1).click();
         }
     }
@@ -33,7 +35,7 @@ public class MaterialTree extends UIBaseElement<TreeAssert> {
     public void collapse(final String nodeName) {
         UIElement expansionIndicator = getNodeByName(nodeName);
         if (!expansionIndicator.isEmpty() && isExpanded(nodeName)) {
-            expansionIndicator.click();
+            expansionIndicator.finds(By.cssSelector(cssButtonLocator)).get(1).click();
         }
     }
 
@@ -53,7 +55,7 @@ public class MaterialTree extends UIBaseElement<TreeAssert> {
 
     @JDIAction("Is '{name}' '{0}' panel collapsed")
     public boolean isCollapsed(final int indexNumber) {
-        UIElement expansionNode = getNodeLocator().get(indexNumber);
+        UIElement expansionNode = getNodes().get(indexNumber);
         expansionNode.waitFor();
         boolean isNodeExpanded = expansionNode.getAttribute(attributeExpanded).contains("false");
         boolean isGroup = expansionNode.getAttribute(attributeRole).contains(itemgroup);
@@ -66,14 +68,14 @@ public class MaterialTree extends UIBaseElement<TreeAssert> {
     }
 
     protected UIElement getNodeByName(String name) {
-        List<UIElement> nodes = getNodeLocator();
+        List<UIElement> nodes = getNodes();
         for(UIElement node: nodes) {
             if(node.getText().split("\n")[1].equalsIgnoreCase(name)) return node;
         }
         return null;
     }
 
-    protected List<UIElement> getNodeLocator() {
+    protected List<UIElement> getNodes() {
         return this.uiElement.finds(By.cssSelector(cssTreeNodeLocator));
     }
 
@@ -85,10 +87,28 @@ public class MaterialTree extends UIBaseElement<TreeAssert> {
     public String value(final String field) {
         return "";
     }
-//
-//    @JDIAction("Get '{name}' '{0}' field")
-//    public TreeAssert attribute(final String attrib, final String field) {
-//        return new TreeAssert().set(this);
-//    }
 
+    public List<UIElement> getNodeItems(int level) {
+        List<UIElement> opened = this.uiElement.finds(By.cssSelector(cssTreeNodeLocator));
+        List<UIElement> branch = new ArrayList();
+        String shift = Integer.toString(level*40)+"px";
+        for(UIElement item : opened) {
+            if(item.getAttribute("style").contains(shift)) {
+                branch.add(item);
+            }
+        }
+        return branch;
+    }
+
+    public Checkbox getCheckbox(int level, String name) {
+        Checkbox checkbox = new Checkbox();
+        List<UIElement> branch = getNodeItems(level);
+        for(UIElement item: branch) {
+            if(item.getText().contains(name)) {
+                checkbox.setCore(checkbox.getClass(), item.find(By.cssSelector("mat-checkbox")));
+                break;
+            }
+        }
+        return checkbox;
+    }
 }
