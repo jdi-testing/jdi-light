9283e382bf126096facf8c30f8f68ff38febd1be
Vsevolod_Adrianov <Vsevolod_Adrianov@epam.com>
2020-10-14 16:42:37 +0300

    restore refresh

--- jdi-light-angular-tests/src/test/java/io/github/epam/angular/tests/elements/complex/MaterialTreeTests.java
+++ jdi-light-angular-tests/src/test/java/io/github/epam/angular/tests/elements/complex/MaterialTreeTests.java
@@ -5,6 +5,7 @@ import io.github.epam.TestsInit;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import static com.epam.jdi.light.elements.composite.WebPage.refresh;
 import static io.github.com.StaticSite.angularPage;
 import static io.github.com.pages.sections.MaterialTreeSection.checkBoxesExample;
 import static io.github.com.pages.sections.MaterialTreeSection.checkBoxesTree;
@@ -36,6 +37,7 @@ public class MaterialTreeTests extends TestsInit {
 
     @Test
     public void expandByNameTest() {
+        refresh();
         dynamicExample.show();
         dynamicTree.expand(FRUITS);
         dynamicTree.is().collapsed(VEGETABLES);
