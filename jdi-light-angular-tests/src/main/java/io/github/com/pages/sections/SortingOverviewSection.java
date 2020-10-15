d49055619e5fb1e550fac2204255f2064e7d2034
Aliaksandr_Navitski1 <aliaksandr_navitski1@epam.com>
2020-10-15 08:59:17 +0300

    corrections

--- jdi-light-angular-tests/src/main/java/io/github/com/pages/sections/SortingOverviewSection.java
+++ jdi-light-angular-tests/src/main/java/io/github/com/pages/sections/SortingOverviewSection.java
@@ -1,10 +1,9 @@
 package io.github.com.pages.sections;
 
 import com.epam.jdi.light.angular.elements.complex.SortingOverview;
-import com.epam.jdi.light.elements.composite.Section;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 
-public class SortingOverviewSection extends Section {
+public class SortingOverviewSection {
     @UI("#sort-headers tr:nth-child(1)")
     public static SortingOverview sortingOverview;
 }
