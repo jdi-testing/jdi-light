e9137c0dc4432d0ef1bb3a950c55ff446206a7d8
Evgeniya Kazakevich <astrejja@gmail.com>
2020-10-22 18:45:15 +0300

    Action Bar tests added

--- jdi-light-mobile-tests/src/main/java/nativeapp/android/apiDemos/ActionBarDisplayOptionsPage.java
+++ jdi-light-mobile-tests/src/main/java/nativeapp/android/apiDemos/ActionBarDisplayOptionsPage.java
@@ -1,4 +1,47 @@
 package nativeapp.android.apiDemos;
 
+import com.epam.jdi.light.mobile.elements.common.app.android.ActionBar;
+import com.epam.jdi.light.mobile.elements.pageobjects.annotations.MobileFindBy;
+
 public class ActionBarDisplayOptionsPage {
+
+    @MobileFindBy(accessibilityId = "More options")
+    public static ActionBar moreOptions;
+
+    @MobileFindBy(accessibilityId = "DISPLAY_HOME_AS_UP")
+    public static ActionBar displayHomeAsUp;
+
+    @MobileFindBy(accessibilityId = "Navigate up")
+    public static ActionBar navigateUp;
+
+    @MobileFindBy(accessibilityId = "DISPLAY_SHOW_HOME")
+    public static ActionBar displayShowHome;
+
+    @MobileFindBy(className = "android.widget.ImageView")
+    public static ActionBar imageView;
+
+    @MobileFindBy(accessibilityId = "DISPLAY_USE_LOGO")
+    public static ActionBar displayUseLogo;
+
+    @MobileFindBy(accessibilityId = "DISPLAY_SHOW_TITLE")
+    public static ActionBar displayShowTitle;
+
+    @MobileFindBy(className = "android.widget.TextView")
+    public static ActionBar textView;
+
+    @MobileFindBy(accessibilityId = "DISPLAY_SHOW_CUSTOM")
+    public static ActionBar displayShowCustom;
+
+    @MobileFindBy(accessibilityId = "Custom View!")
+    public static ActionBar customView;
+
+    @MobileFindBy(accessibilityId = "Navigation")
+    public static ActionBar navigation;
+
+    @MobileFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.view.ViewGroup/" +
+            "android.widget.FrameLayout[1]/android.widget.HorizontalScrollView")
+    public static ActionBar horizontalScrollView;
+
+    @MobileFindBy(accessibilityId = "Cycle Custom View Gravity")
+    public static ActionBar cycleCustomViewGravity;
 }
--- jdi-light-mobile-tests/src/test/java/nativeapp_android/tests/ActionBarTests.java
+++ jdi-light-mobile-tests/src/test/java/nativeapp_android/tests/ActionBarTests.java
@@ -1,23 +1,110 @@
 package nativeapp_android.tests;
 
+import nativeapp.android.apiDemos.ActionBarDisplayOptionsPage;
 import nativeapp.android.apiDemos.ActionBarPage;
 import nativeapp.android.apiDemos.ActionBarUsagePage;
 import nativeapp.android.apiDemos.AppPage;
 import nativeapp.android.apiDemos.IndexPage;
+import nativeapp_android.ApiDemosTestInit;
 import org.testng.annotations.Test;
 
-public class ActionBarTests {
+public class ActionBarTests extends ApiDemosTestInit {
 
     @Test
-    public void actionBarUsageTest() {
+    public void actionBarUsagePageSearchTest() {
         IndexPage.appPage.click();
         AppPage.actionBarPage.click();
         ActionBarPage.actionBarUsagePage.click();
+        ActionBarUsagePage.searchButton.is().iconifiedByDefault();
+        ActionBarUsagePage.searchButton.setExpanded();
+        ActionBarUsagePage.searchVield.is().enabled();
+        ActionBarUsagePage.searchVield.input("Internet");
+        ActionBarUsagePage.text.has().text("Query so far: Internet");
+        ActionBarUsagePage.clearQuery.click();
+        ActionBarUsagePage.searchButton.isIconified();
+    }
+
+    @Test
+    public void actionBarUsagePageEditTest() {
+        IndexPage.appPage.click();
+        AppPage.actionBarPage.click();
+        ActionBarPage.actionBarUsagePage.click();
+        ActionBarUsagePage.edit.is().enabled();
         ActionBarUsagePage.edit.click();
+        ActionBarUsagePage.text.is().displayed();
+    }
+
+    @Test
+    public void actionBarUsagePageMoreOptionsTest() {
+        IndexPage.appPage.click();
+        AppPage.actionBarPage.click();
+        ActionBarPage.actionBarUsagePage.click();
+        ActionBarUsagePage.moreOptions.is().enabled();
         ActionBarUsagePage.moreOptions.click();
-        ActionBarUsagePage.options.select("Add");
+        ActionBarUsagePage.moreOptions.selectOption("Add");
+        ActionBarUsagePage.text.is().displayed();
         ActionBarUsagePage.moreOptions.click();
-        ActionBarUsagePage.options.select("Share");
+        ActionBarUsagePage.moreOptions.selectOption("Sort");
+        ActionBarUsagePage.moreOptions.selectOption("Alphabetically");
+    }
+
+    @Test
+    public void displayOptionsPageMoreOptionsTest() {
+        IndexPage.appPage.click();
+        AppPage.actionBarPage.click();
+        ActionBarPage.displayOptionsPage.click();
+        ActionBarDisplayOptionsPage.moreOptions.is().enabled();
+        ActionBarDisplayOptionsPage.moreOptions.click();
+        ActionBarDisplayOptionsPage.moreOptions.selectOption("Menu Item");
+    }
+
+    @Test
+    public void displayOptionsPageDisplayHomeAsUpTest() {
+        IndexPage.appPage.click();
+        AppPage.actionBarPage.click();
+        ActionBarPage.displayOptionsPage.click();
+        ActionBarDisplayOptionsPage.displayHomeAsUp.is().enabled();
+        ActionBarDisplayOptionsPage.displayHomeAsUp.click();
+        ActionBarDisplayOptionsPage.navigateUp.is().displayed();
+    }
+
+    @Test
+    public void displayOptionsPageDisplayShowHomeAndDisplayUseLogoTest() {
+        IndexPage.appPage.click();
+        AppPage.actionBarPage.click();
+        ActionBarPage.displayOptionsPage.click();
+        ActionBarDisplayOptionsPage.displayShowHome.is().enabled();
+        ActionBarDisplayOptionsPage.displayShowHome.click();
+        ActionBarDisplayOptionsPage.imageView.is().displayed();
+        ActionBarDisplayOptionsPage.displayUseLogo.is().enabled();
+        ActionBarDisplayOptionsPage.displayUseLogo.click();
+        ActionBarDisplayOptionsPage.imageView.is().displayed();
+    }
+
+    @Test
+    public void displayOptionsPageDisplayShowTitleAndDisplayShowCustomAndCycleCustomGravityTest() {
+        IndexPage.appPage.click();
+        AppPage.actionBarPage.click();
+        ActionBarPage.displayOptionsPage.click();
+        ActionBarDisplayOptionsPage.displayShowTitle.is().enabled();
+        ActionBarDisplayOptionsPage.displayShowTitle.click();
+        ActionBarDisplayOptionsPage.textView.is().hidden();
+        ActionBarDisplayOptionsPage.displayShowCustom.is().enabled();
+        ActionBarDisplayOptionsPage.displayShowCustom.click();
+        ActionBarDisplayOptionsPage.customView.is().displayed();
+        ActionBarDisplayOptionsPage.cycleCustomViewGravity.is().enabled();
+        ActionBarDisplayOptionsPage.cycleCustomViewGravity.click();
+    }
+
+    @Test
+    public void displayOptionsPageNavigationTest() {
+        IndexPage.appPage.click();
+        AppPage.actionBarPage.click();
+        ActionBarPage.displayOptionsPage.click();
+        ActionBarDisplayOptionsPage.navigation.is().enabled();
+        ActionBarDisplayOptionsPage.navigation.click();
+        ActionBarDisplayOptionsPage.horizontalScrollView.is().displayed();
+        ActionBarDisplayOptionsPage.horizontalScrollView.selectOption("TAB 1");
     }
 }
 
