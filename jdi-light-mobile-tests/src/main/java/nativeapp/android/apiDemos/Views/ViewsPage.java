07aa39ee4dbb0b8fc02f8199ce9bb59b74ea0c28
Aliaksandr_Navitski1 <aliaksandr_navitski1@epam.com>
2020-11-20 11:55:45 +0300

    initial commit

--- jdi-light-mobile-tests/src/main/java/nativeapp/android/apiDemos/Views/ViewsPage.java
+++ jdi-light-mobile-tests/src/main/java/nativeapp/android/apiDemos/Views/ViewsPage.java
@@ -1,4 +1,4 @@
-package nativeapp.android.apiDemos;
+package nativeapp.android.apiDemos.Views;
 
 import com.epam.jdi.light.mobile.elements.common.app.Button;
 import com.epam.jdi.light.mobile.elements.pageobjects.annotations.MobileFindBy;
@@ -25,5 +25,7 @@ public class ViewsPage {
 
     @MobileFindBy(accessibilityId = "TextFields")
     public static Button textFieldsPage;
+
+
 
 }
