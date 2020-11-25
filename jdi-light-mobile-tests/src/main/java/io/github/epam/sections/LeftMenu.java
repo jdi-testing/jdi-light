f237cea806502dc30fd6a073ad9cb1ddbe2bf8c1
Innokentiy945 <forjimmy945@Gmail.com>
2020-11-25 11:54:40 +0300

    Change click() method to tap()

--- jdi-light-mobile-tests/src/main/java/io/github/epam/sections/LeftMenu.java
+++ jdi-light-mobile-tests/src/main/java/io/github/epam/sections/LeftMenu.java
@@ -1,11 +1,11 @@
 package io.github.epam.sections;
 
-import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.light.elements.composite.Section;
 import com.epam.jdi.light.elements.pageobjects.annotations.FindBy;
+import com.epam.jdi.light.mobile.elements.common.app.Button;
 
 public class LeftMenu extends Section {
 
     @FindBy(css = "#mCSB_1_container > ul > li:nth-child(5)")
-    public static UIElement leftMenu;
+    public static Button leftMenu;
 }
--- jdi-light-mobile-tests/src/test/java/io/github/epam/mobile/tests/MobileWebTextFieldTests.java
+++ jdi-light-mobile-tests/src/test/java/io/github/epam/mobile/tests/MobileWebTextFieldTests.java
@@ -11,15 +11,14 @@ public class MobileWebTextFieldTests  extends StaticTestsInit {
 
     private static String CONDITION1 = "111";
     private static String CONDITION2 = "!}{?)";
-    private static String CONDITION3 = "17";
+    private static String CONDITION3 = "17.0";
     private static String CONDITION4 = "MyName";
-    private static String CONDITION5 = "Мое имя";
 
     @Test
     public void textFieldTests() {
         shouldBeLoggedIn();
-        leftMenu.click();
-        htmlMenu.click();
+        leftMenu.tap();
+        htmlMenu.tap();
 
         HtmlMobilePage.nameArea.focus();
         HtmlMobilePage.nameArea.sendKeys(CONDITION1);
@@ -40,18 +39,13 @@ public class MobileWebTextFieldTests  extends StaticTestsInit {
         HtmlMobilePage.nameArea.sendKeys(CONDITION4);
         HtmlMobilePage.nameArea.has().text(CONDITION4);
         HtmlMobilePage.nameArea.clear();
-
-        HtmlMobilePage.nameArea.focus();
-        HtmlMobilePage.nameArea.sendKeys(CONDITION5);
-        HtmlMobilePage.nameArea.has().text(CONDITION5);
-        HtmlMobilePage.nameArea.clear();
     }
 
     @Test
     public void setTextTests() {
         shouldBeLoggedIn();
-        leftMenu.click();
-        htmlMenu.click();
+        leftMenu.tap();
+        htmlMenu.tap();
         HtmlMobilePage.nameArea.setValue(CONDITION1);
         HtmlMobilePage.nameArea.placeholder().equals(CONDITION1);
     }
@@ -61,8 +55,8 @@ public class MobileWebTextFieldTests  extends StaticTestsInit {
     @Test
     public void isNameAreaDisplayed() {
         shouldBeLoggedIn();
-        leftMenu.click();
-        htmlMenu.click();
+        leftMenu.tap();
+        htmlMenu.tap();
         HtmlMobilePage.nameArea.is().displayed();
     }
 }
