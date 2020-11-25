2f9688b326dfb7393101b349f395b5d3d3942b9c
Vsevolod_Adrianov <Vsevolod_Adrianov@epam.com>
2020-11-25 15:14:18 +0300

    fix ONLY android mobile web check box tests. Initial commit. #2509

--- jdi-light-mobile-tests/src/main/java/io/github/epam/pages/HtmlMobilePage.java
+++ jdi-light-mobile-tests/src/main/java/io/github/epam/pages/HtmlMobilePage.java
@@ -3,13 +3,20 @@ package io.github.epam.pages;
 import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.FindBy;
+import com.epam.jdi.light.mobile.elements.common.app.android.TextField;
 
 
 public class HtmlMobilePage extends WebPage {
 
+    @FindBy(xpath = "//select[@id='multi-dropdown']/..")
+    public static UIElement multiDropdown;
+
     @FindBy(css = "#accept-conditions")
     public UIElement singleCheckBox;
 
-    @FindBy(xpath = "//input[@name='checks-group']/..")
-    public UIElement checkList;
+    @FindBy(xpath = "//input[@type='radio']/..")
+    public static UIElement radioButtons;
+
+    @FindBy(css = "#name")
+    public static TextField nameArea;
 }
