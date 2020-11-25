a013391870c14bcd34b70d05c31e83859276e952
Vsevolod_Adrianov <Vsevolod_Adrianov@epam.com>
2020-11-25 15:39:56 +0300

    add TextArea

--- jdi-light-mobile-tests/src/main/java/io/github/epam/pages/HtmlMobilePage.java
+++ jdi-light-mobile-tests/src/main/java/io/github/epam/pages/HtmlMobilePage.java
@@ -5,6 +5,7 @@ import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.FindBy;
 import com.epam.jdi.light.mobile.elements.common.app.android.TextField;
 import com.epam.jdi.light.ui.html.elements.common.ProgressBar;
+import com.epam.jdi.light.ui.html.elements.common.TextArea;
 
 
 public class HtmlMobilePage extends WebPage {
@@ -25,4 +26,7 @@ public class HtmlMobilePage extends WebPage {
 
     @FindBy(xpath = "//input[@name='checks-group']/..")
     public UIElement checkList;
+
+    @FindBy(css = "#text-area")
+    public static TextArea textArea;
 }
