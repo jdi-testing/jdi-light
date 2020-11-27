4129141d1b31083b8adb0827e1ce5115933c0e64
Vsevolod_Adrianov <Vsevolod_Adrianov@epam.com>
2020-11-27 15:45:13 +0300

    restore correct #	jdi-light-mobile-tests/src/main/java/nativeapp/android/apiDemos/ApiDemosApp.java

--- jdi-light-mobile-tests/src/test/java/nativeapp_android/ActivityTestsInit.java
+++ jdi-light-mobile-tests/src/test/java/nativeapp_android/ActivityTestsInit.java
@@ -2,6 +2,7 @@ package nativeapp_android;
 
 import com.epam.jdi.light.driver.WebDriverFactory;
 import com.epam.jdi.light.mobile.elements.common.AppManager;
+import nativeapp.android.apidemos.ApiDemosApp;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
