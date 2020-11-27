e1d75f646e2b857911557f4c38922a3bd2d9415a
Vsevolod_Adrianov <Vsevolod_Adrianov@epam.com>
2020-11-27 15:41:34 +0300

    Merge branch 'jdi-light-mobile' into 2251-tooltips_correct restore correct # Conflicts: #	jdi-light-mobile-tests/src/main/java/nativeapp/android/apiDemos/ApiDemosApp.java

--- jdi-light-mobile-tests/src/main/java/nativeapp/android/apiDemos/ApiDemosApp.java
+++ jdi-light-mobile-tests/src/main/java/nativeapp/android/apiDemos/ApiDemosApp.java
@@ -1,6 +1,21 @@
-package nativeapp.android.apiDemos;
+package nativeapp.android.apidemos;
 
 import com.epam.jdi.light.mobile.elements.pageobjects.annotations.JApp;
+import nativeapp.android.apidemos.animation.AnimationPage;
+import nativeapp.android.apidemos.animation.CheckboxesPage;
+import nativeapp.android.apidemos.app.AppPage;
+import nativeapp.android.apidemos.app.NotificationPage;
+import nativeapp.android.apidemos.app.StatusBarPage;
+import nativeapp.android.apidemos.views.ButtonsPage;
+import nativeapp.android.apidemos.views.PhotosPage;
+import nativeapp.android.apidemos.views.RatingBarPage;
+import nativeapp.android.apidemos.views.SearchViewActionBarPage;
+import nativeapp.android.apidemos.views.SearchViewAlwaysExpandedPage;
+import nativeapp.android.apidemos.views.SearchViewFilterPage;
+import nativeapp.android.apidemos.views.SearchViewPage;
+import nativeapp.android.apidemos.views.SeekBarPage;
+import nativeapp.android.apidemos.views.TextFieldsPage;
+import nativeapp.android.apidemos.views.ViewsPage;
 
 @JApp(appPackage = "io.appium.android.apis", appActivity = "io.appium.android.apis.ApiDemos")
 public class ApiDemosApp {
@@ -36,5 +51,4 @@ public class ApiDemosApp {
     public static StatusBarPage statusBarPage;
 
     public static PhotosPage photosPage;
-
 }
