0f06fa8bd684f0328d8a52733e8f9dc5f95b946f
Vsevolod_Adrianov <Vsevolod_Adrianov@epam.com>
2020-12-04 19:41:59 +0300

    fix codacy

--- jdi-light-mobile-tests/src/test/java/nativeapp_android/tests/TooltipsTest.java
+++ jdi-light-mobile-tests/src/test/java/nativeapp_android/tests/TooltipsTest.java
@@ -1,17 +1,16 @@
 package nativeapp_android.tests;
 
 import com.epam.jdi.light.driver.WebDriverFactory;
-import com.epam.jdi.light.mobile.elements.common.app.Button;
 import com.epam.jdi.light.mobile.elements.composite.MobileScreen;
 import io.appium.java_client.MobileBy;
 import nativeapp_android.ApiDemosTestInit;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static nativeapp.android.apidemos.IndexPage.viewsPage;
 import static nativeapp.android.apidemos.views.PhotosPage.photo;
 import static nativeapp.android.apidemos.views.PhotosPage.photoButton;
 import static nativeapp.android.apidemos.views.ViewsPage.galleryPage;
+import static org.testng.Assert.assertTrue;
 
 public class TooltipsTest extends ApiDemosTestInit {
 
@@ -22,18 +21,6 @@ public class TooltipsTest extends ApiDemosTestInit {
         galleryPage.click();
         photoButton.tap();
         photo.tap();
-        Assert.assertTrue(WebDriverFactory.getDriver().findElement(MobileBy.xpath("//*[@class='android.widget.Toast']")).getText().equals("0"));
+        assertTrue(WebDriverFactory.getDriver().findElement(MobileBy.xpath("//*[@class='android.widget.Toast']")).getText().equals("0"));
     }
-
-    public void clickOnElementInList(Button element) {
-        while (true) {
-            try {
-                element.click();
-                return;
-            } catch (RuntimeException e) {
-                MobileScreen.scrollDown(1000);
-            }
-        }
-    }
-
 }
