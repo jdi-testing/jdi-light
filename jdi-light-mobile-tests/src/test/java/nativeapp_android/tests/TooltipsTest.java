9a941f7bdc01a4cdb15dd1cd9d3f227fb335f702
Innokentiy945 <forjimmy945@Gmail.com>
2020-12-08 17:15:17 +0300

    Updating ToolTip tests

--- jdi-light-mobile-tests/src/test/java/nativeapp_android/tests/TooltipsTest.java
+++ jdi-light-mobile-tests/src/test/java/nativeapp_android/tests/TooltipsTest.java
@@ -1,26 +1,37 @@
 package nativeapp_android.tests;
 
-import com.epam.jdi.light.driver.WebDriverFactory;
 import com.epam.jdi.light.mobile.elements.composite.MobileScreen;
-import io.appium.java_client.MobileBy;
+import nativeapp.android.apidemos.views.GalleryPage;
 import nativeapp_android.ApiDemosTestInit;
+import org.openqa.selenium.By;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static com.epam.jdi.light.mobile.elements.complex.Images.select;
 import static nativeapp.android.apidemos.IndexPage.viewsPage;
 import static nativeapp.android.apidemos.views.GalleryPage.photos;
 import static nativeapp.android.apidemos.views.ViewsPage.galleryPage;
-import static org.testng.Assert.assertTrue;
 
 public class TooltipsTest extends ApiDemosTestInit {
 
-    @Test
-    public void tooltipTests() {
+    @BeforeMethod
+    public void init() {
         viewsPage.tap();
         MobileScreen.scrollToElementInList(galleryPage);
         galleryPage.tap();
         photos.tap();
-        select(0).click();
-        assertTrue(WebDriverFactory.getDriver().findElement(MobileBy.xpath("//*[@class='android.widget.Toast']")).getText().equals("0"));
     }
+
+    @Test
+    public void getAllToolTipsFromGallery() {
+        GalleryPage.photoOne.longPress();
+        GalleryPage.galleryLayout.find(By.linkText("0")).isExist();
+
+        GalleryPage.photoTwo.longPress();
+        GalleryPage.galleryLayout.find(By.linkText("1")).isExist();
+
+        GalleryPage.photoThree.longPress();
+        GalleryPage.galleryLayout.find(By.linkText("2")).isExist();
+    }
+
+
 }
