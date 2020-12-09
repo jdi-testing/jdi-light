d8fde4bbe6d063919a11a9bb745ef24feacbb2ac
alexarm <alex.armenshin@gmail.com>
2020-12-09 13:10:02 +0300

    implementation travis ci

--- jdi-light-mobile-tests/src/test/java/nativeapp_android/ApiDemosTestInit.java
+++ jdi-light-mobile-tests/src/test/java/nativeapp_android/ApiDemosTestInit.java
@@ -3,11 +3,11 @@ package nativeapp_android;
 import com.epam.jdi.light.driver.WebDriverFactory;
 import com.epam.jdi.light.mobile.elements.common.AppManager;
 import nativeapp.android.apidemos.ApiDemosApp;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 
-
-import java.io.File;
-
+import static com.epam.jdi.light.mobile.driver.MobileDriverData.CAPABILITIES_FOR_ANDROID;
 import static com.epam.jdi.light.mobile.elements.init.PageFactory.initMobile;
 import static com.epam.jdi.light.settings.WebSettings.logger;
 
@@ -15,6 +15,8 @@ public class ApiDemosTestInit {
     @BeforeClass(alwaysRun = true)
     public void setUp() {
 //        final String FILE_NAME = "src/main/resources/ApiDemos-debug.apk";
+        CAPABILITIES_FOR_ANDROID.put("build", "Android Native App tests build 3");
+        CAPABILITIES_FOR_ANDROID.put("name", this.getClass().getSimpleName());
         initMobile(ApiDemosApp.class);
         logger.toLog("Run ApiDemos App Tests");
 //        File file = new File(FILE_NAME);
@@ -22,10 +24,10 @@ public class ApiDemosTestInit {
 //        AppManager.installApp(path);
         AppManager.launchApp();
     }
-//    @AfterMethod(alwaysRun = true)
-//    public void resetApp() {
-//        AppManager.resetApp();
-//    }
+    @AfterMethod(alwaysRun = true)
+    public void resetApp() {
+        AppManager.resetApp();
+    }
 
     @AfterClass(alwaysRun = true)
     public void tearDown() {
