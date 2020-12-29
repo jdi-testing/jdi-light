647dad31370bc12c4108dd7dc11e7ec5cbd896e4
Dinara Shabanova <dinara_shabanova@epam.com>
2020-12-29 12:31:17 +0300

    fix codacy issue

--- jdi-light-mobile-tests/src/test/java/nativeapp_ios/tests/ContactsAppTests.java
+++ jdi-light-mobile-tests/src/test/java/nativeapp_ios/tests/ContactsAppTests.java
@@ -1,11 +1,12 @@
 package nativeapp_ios.tests;
 
-
 import nativeapp_ios.ContactsAppTestsInit;
 import org.testng.annotations.Test;
 
-import static nativeapp.ios.contacts.ContactsApp.*;
-
+import static nativeapp.ios.contacts.ContactsApp.addNewContactPage;
+import static nativeapp.ios.contacts.ContactsApp.contactContextMenu;
+import static nativeapp.ios.contacts.ContactsApp.contactsListPage;
+import static nativeapp.ios.contacts.ContactsApp.groupsPage;
 
 public class ContactsAppTests extends ContactsAppTestsInit {
 
