a7b661087d64c29386b5052ed226d189bf2b8332
tsvetaev.alex <tsvetaev.alex@gmail.com>
2020-12-30 15:58:21 +0300

    Update ContactForm.java

--- jdi-light-html-tests/src/main/java/io/github/com/sections/ContactForm.java
+++ jdi-light-html-tests/src/main/java/io/github/com/sections/ContactForm.java
@@ -11,7 +11,11 @@ import com.epam.jdi.light.ui.html.elements.common.TextField;
 import io.github.com.entities.Contacts;
 
 public class ContactForm extends Form<Contacts> {
-	TextField firstName, lastName, position, passportNumber, passportSeria;
+	TextField firstName;
+	TextField lastName;
+	TextField position;
+	TextField passportNumber;
+	TextField passportSeria;
 	Dropdown gender;
 	IsCombobox religion;
 	Checkbox passport, acceptConditions;
