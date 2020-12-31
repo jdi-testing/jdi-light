57c4d4a542beecbe474d859ed9df20300f2014fc
groonyn <Nikolai_Grunin@epam.com>
2020-12-31 12:27:35 +0300

    n2304 almost implemented

---
+++ jdi-light-mobile-tests/src/main/java/nativeapp/ios/epamiostestapp/LabelViewPage.java
@@ -0,0 +1,11 @@
+package nativeapp.ios.epamiostestapp;
+
+import com.epam.jdi.light.mobile.elements.common.app.ios.Label;
+import com.epam.jdi.light.mobile.elements.composite.ios.app.TabBar;
+import com.epam.jdi.light.mobile.elements.pageobjects.annotations.MobileFindBy;
+
+public class LabelViewPage {
+    //TODO
+    @MobileFindBy(accessibilityId = "here need to have value")
+    public static Label label;
+}
--- jdi-light-mobile-tests/src/test/java/epamiostestapp/tests/LabelsTest.java
+++ jdi-light-mobile-tests/src/test/java/epamiostestapp/tests/LabelsTest.java
@@ -1,14 +1,23 @@
 package epamiostestapp.tests;
 
 import epamiostestapp.IosApplicationInit;
-import nativeapp.ios.epamiostestapp.AlertDialogPage;
 import org.testng.annotations.Test;
 
+import static com.epam.jdi.light.mobile.elements.init.MobileAppFactory.$;
 import static nativeapp.ios.epamiostestapp.MainPage.menuLabelView;
+import static nativeapp.ios.epamiostestapp.LabelViewPage.label;
 
 public class LabelsTest extends IosApplicationInit {
     @Test
     public void iosLabelsTest() {
+        menuLabelView.tap();
+        $(label, "Label1").is().displayed();
+        $(label, "Label2").is().displayed();
+        $(label, "Label1").isDisplayed();//or that one
+    }
+
+    @Test
+    public void iosTextUnderTheLabelsTest() {
         menuLabelView.tap();
 
     }
