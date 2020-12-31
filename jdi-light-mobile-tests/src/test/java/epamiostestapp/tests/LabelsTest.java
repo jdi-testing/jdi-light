2a976fb70cc2fb29c8ac4d319978ea24a87f29f2
Dinara Shabanova <dinara_shabanova@epam.com>
2020-12-31 14:00:07 +0300

    change assertions

--- jdi-light-mobile-tests/src/test/java/epamiostestapp/tests/LabelsTest.java
+++ jdi-light-mobile-tests/src/test/java/epamiostestapp/tests/LabelsTest.java
@@ -2,6 +2,7 @@ package epamiostestapp.tests;
 
 import epamiostestapp.IosApplicationInit;
 import nativeapp.ios.epamiostestapp.LabelViewPage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static nativeapp.ios.epamiostestapp.MainPage.menuLabelView;
@@ -10,20 +11,19 @@ public class LabelsTest extends IosApplicationInit {
     @Test
     public void iosLabelsTest() {
         menuLabelView.tap();
-        String labelAssert = LabelViewPage.label1.getValue();
-        LabelViewPage.label1.is().text(labelAssert);
 
-        LabelViewPage.label2.is().text("Label2");
+        Assert.assertEquals(LabelViewPage.label1.getValue(), "Label1");
+        Assert.assertEquals(LabelViewPage.label2.getValue(), "Label2");
 
     }
 
     @Test
     public void iosTextUnderTheLabelsTest() {
         menuLabelView.tap();
-        String labelAssert = LabelViewPage.text_under_label1.getValue();
-        LabelViewPage.text_under_label1.is().text(labelAssert);
-
-        LabelViewPage.text_under_label2.is().text("Text2 under the label");
+        Assert.assertEquals(LabelViewPage.text_under_label1.getValue(),
+                "Text1 under the label");
+        Assert.assertEquals(LabelViewPage.text_under_label2.getValue(),
+                "Text2 under the label");
 
     }
 }
