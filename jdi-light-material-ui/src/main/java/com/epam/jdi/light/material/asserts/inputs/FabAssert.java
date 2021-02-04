b64b76981b00092da42b6d6b3cbb2ac02d7e3f8d
Valentin_Bodrov <befearless4428876@gmail.com>
2021-02-04 18:26:43 +0300

    rebase

---
+++ jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/asserts/inputs/FabAssert.java
@@ -0,0 +1,26 @@
+package com.epam.jdi.light.material.asserts.inputs;
+
+import com.epam.jdi.light.asserts.generic.UIAssert;
+import com.epam.jdi.light.common.JDIAction;
+import com.epam.jdi.light.material.elements.inputs.Fab;
+import org.hamcrest.Matchers;
+
+import static com.epam.jdi.light.asserts.core.SoftAssert.jdiAssert;
+
+public class FabAssert extends UIAssert<FabAssert, Fab> {
+
+    @JDIAction("Assert that '{name}' is enabled")
+    @Override
+    public FabAssert enabled() {
+        jdiAssert(element().isEnabled(), Matchers.is(true));
+        return this;
+    }
+
+    @JDIAction("Assert that '{name}' is disabled")
+    @Override
+    public FabAssert disabled() {
+        jdiAssert(element().hasClass("Mui-disabled"), Matchers.is(true));
+        return this;
+    }
+
+}
