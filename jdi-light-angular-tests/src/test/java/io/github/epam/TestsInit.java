6bf00c2f91abad057a7e3fe935c919d449e350b4
Natalia Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2021-02-05 22:11:55 +0300

    + reload page after test

--- jdi-light-angular-tests/src/test/java/io/github/epam/TestsInit.java
+++ jdi-light-angular-tests/src/test/java/io/github/epam/TestsInit.java
@@ -3,12 +3,14 @@ package io.github.epam;
 import io.github.com.StaticSite;
 import io.github.epam.testng.TestNGListener;
 import org.testng.SkipException;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Listeners;
 
 import static com.epam.jdi.light.driver.WebDriverUtils.killAllSeleniumDrivers;
 import static com.epam.jdi.light.elements.composite.WebPage.openSite;
+import static com.epam.jdi.light.elements.composite.WebPage.reload;
 import static com.epam.jdi.light.settings.JDISettings.DRIVER;
 import static com.epam.jdi.light.settings.WebSettings.logger;
 
@@ -25,6 +27,12 @@ public class TestsInit {
     @AfterSuite(alwaysRun = true)
     public static void tearDown() {
         killAllSeleniumDrivers();
+    }
+
+    @AfterMethod
+    public static void refreshPage() {
+        // definitely needed after fail
+        reload();
     }
 
     public boolean isFireFox() {
