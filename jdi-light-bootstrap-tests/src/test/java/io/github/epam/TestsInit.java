e8fcd254f49896240890ded245c6bd73ec17ab02
RomanIovlev <romanyister@gmail.com>
2021-02-07 13:15:30 +0100

    revert bdd changes

--- jdi-light-bootstrap-tests/src/test/java/io/github/epam/TestsInit.java
+++ jdi-light-bootstrap-tests/src/test/java/io/github/epam/TestsInit.java
@@ -1,17 +1,12 @@
 package io.github.epam;
 
-import com.epam.jdi.light.elements.common.UIElement;
 import io.github.com.StaticSite;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
-import static com.epam.jdi.light.common.Exceptions.exception;
 import static com.epam.jdi.light.driver.WebDriverUtils.killAllSeleniumDrivers;
 import static com.epam.jdi.light.elements.composite.WebPage.openSite;
-import static com.epam.jdi.light.elements.init.UIFactory.$;
-import static com.epam.jdi.light.settings.WebSettings.SMART_SEARCH;
 import static com.epam.jdi.light.settings.WebSettings.logger;
-import static com.epam.jdi.tools.StringUtils.splitHyphen;
 
 @SuppressWarnings("PMD.ClassNamingConventions")
 public interface TestsInit {
