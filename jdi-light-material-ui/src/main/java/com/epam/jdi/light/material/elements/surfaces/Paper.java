3887350d2a4bdb31f61a7fc4a5d1b2c99b28a920
Stanislav Dmitriev <stanislav_dmitriev@epam.com>
2021-02-08 18:25:44 +0300

    Added method to get inner element of paper container

--- jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/elements/surfaces/Paper.java
+++ jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/elements/surfaces/Paper.java
@@ -2,13 +2,20 @@ package com.epam.jdi.light.material.elements.surfaces;
 
 import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.elements.base.UIBaseElement;
+import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.light.material.asserts.surfaces.PaperAssert;
+import org.openqa.selenium.By;
 
 public class Paper extends UIBaseElement<PaperAssert> {
 
     @JDIAction("Get text for '{name}'")
     public String getText() {
         return core().getText();
+    }
+
+    @JDIAction("Get inner element of '{name}'")
+    public UIElement getInnerElement() {
+        return this.find(By.cssSelector("*"));
     }
 
     @Override
