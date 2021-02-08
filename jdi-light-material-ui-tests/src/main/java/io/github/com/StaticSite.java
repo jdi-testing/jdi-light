8360e3cc5df7733438d24cbf48bdfd290f1bb179
Valentin_Bodrov <befearless4428876@gmail.com>
2021-02-08 16:24:24 +0300

    Text field component and tests added

--- jdi-light-material-ui-tests/src/main/java/io/github/com/StaticSite.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/StaticSite.java
@@ -41,6 +41,18 @@ public class StaticSite {
     @Url("/material-ui-button--disabled")
     public static WebPage inputButtonDisabledPage;
 
+    @Url("/material-ui-textfield--field-with-default")
+    public static WebPage inputTextFieldFilledWithDefaultPage;
+
+    @Url("/material-ui-textfield--filled")
+    public static WebPage inputTextFieldFilledPage;
+
+    @Url("/material-ui-textfield--disabled")
+    public static WebPage inputTextFieldDisabledPage;
+
+    @Url("/material-ui-textfield--time")
+    public static WebPage inputTextFieldTimePage;
+
     // surfaces
     // accordion
     @Frame("storybook-preview-iframe")
