ba8dff3e3119f2e4bd230db680cc72c04c7d44a2
Natalia Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2021-02-11 02:48:24 +0300

    debug table

--- jdi-light/src/main/java/com/epam/jdi/light/elements/complex/table/BaseTable.java
+++ jdi-light/src/main/java/com/epam/jdi/light/elements/complex/table/BaseTable.java
@@ -93,7 +93,7 @@ public abstract class BaseTable<T extends BaseTable<?,?>, A extends BaseTableAss
     @Override
     public UIElement core() {
         UIElement core = super.core();
-        if (hasRunDrivers() && !locatorsValidated && core.isDisplayed()) {
+        if (hasRunDrivers() && !locatorsValidated && core.isExist()) {
             try {
                 locatorsValidated = true;
                 validateLocators(core);
