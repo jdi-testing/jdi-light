165a6ae2f96543928ce5737900ce7bf9b2741713
AlinaSelyutina <AlinaSelyutina>
2021-02-11 13:35:48 +0300

    small fixes

--- jdi-light-material-ui-tests/src/main/java/io/github/com/pages/displaydata/VerticalDividerFrame.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/pages/displaydata/VerticalDividerFrame.java
@@ -1,12 +1,12 @@
 package io.github.com.pages.displaydata;
 
 import com.epam.jdi.light.elements.composite.Section;
-import com.epam.jdi.light.material.annotations.JDIDivider;
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.material.elements.displaydata.Divider;
 
 public class VerticalDividerFrame extends Section {
 
-    @JDIDivider(root = "#root", divider = ".MuiDivider-vertical")
+    @UI(".MuiDivider-root")
     public Divider divider;
 
 }
