c101c895abdc3f7a1f6134cdf2045c1b1100a53d
AlinaSelyutina <AlinaSelyutina>
2021-02-16 15:56:49 +0300

    small fix

--- jdi-light-material-ui-tests/src/main/java/io/github/com/StaticSite.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/StaticSite.java
@@ -18,7 +18,7 @@ import io.github.com.pages.inputs.SwitchFrame;
 import io.github.com.pages.surfaces.AccordionFrame;
 import io.github.com.pages.surfaces.PaperFrame;
 
-@JSite("http://localhost:6006/?path=/story")
+@JSite("https://jdi-testing.github.io/jdi-light/")
 public class StaticSite {
 
     // inputs
