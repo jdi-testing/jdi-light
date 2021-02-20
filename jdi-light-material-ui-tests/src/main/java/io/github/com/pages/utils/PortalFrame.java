427dcf1536e26302cb50fe4bdea9d35ce0d9dfbe
PETERSBURG\Ekaterina_Vasileva <ekaterina.vasileva@operative.com>
2021-02-20 11:01:15 +0300

    Updates according to comments

--- jdi-light-material-ui-tests/src/main/java/io/github/com/pages/utils/PortalFrame.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/pages/utils/PortalFrame.java
@@ -2,12 +2,14 @@ package io.github.com.pages.utils;
 
 import com.epam.jdi.light.elements.composite.Section;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
+import com.epam.jdi.light.material.elements.inputs.Button;
 import com.epam.jdi.light.material.elements.utils.Portal;
 
 public class PortalFrame extends Section {
     @UI("//button")
-    public Portal button;
+    public Button button;
 
+    // TODO update with public Label or etc.
     @UI("//div[@class='makeStyles-alert-1'][1]")
     public Portal field1;
 
--- jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/elements/utils/Portal.java
+++ jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/elements/utils/Portal.java
@@ -5,11 +5,6 @@ import com.epam.jdi.light.elements.base.UIBaseElement;
 import com.epam.jdi.light.material.asserts.utils.PortalAssert;
 
 public class Portal  extends UIBaseElement<PortalAssert> {
-    @JDIAction("Click '{name}'")
-    public void click() {
-        core().click();
-    }
-
     @Override
     public PortalAssert is() {
         return new PortalAssert().set(this);
