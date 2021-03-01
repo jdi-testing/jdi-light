820d329b9e0c5b36ca99da987bb903caa0c02842
AlinaSelyutina <AlinaSelyutina>
2021-03-01 17:31:03 +0300

    small fix

--- jdi-light-material-ui-tests/src/main/java/io/github/com/pages/utils/PopoverHoverFrame.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/pages/utils/PopoverHoverFrame.java
@@ -1,15 +1,15 @@
 package io.github.com.pages.utils;
 
-import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.light.elements.composite.Section;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
+import com.epam.jdi.light.material.elements.displaydata.Typography;
 import com.epam.jdi.light.material.elements.utils.Popover;
 
 
 public class PopoverHoverFrame extends Section {
 
     @UI(".MuiTypography-root")
-    public UIElement labelToHover;
+    public Typography labelToHover;
 
     @UI(".MuiPopover-paper")
     public Popover popover;
