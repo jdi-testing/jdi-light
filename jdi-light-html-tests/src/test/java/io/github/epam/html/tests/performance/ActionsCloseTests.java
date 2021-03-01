f68fc34a0e72eeedd93fbcb3dd3623e1c04fd4bf
RomanIovlev <roman.iovlev.jdi@gmail.com>
2021-03-01 17:38:26 +0100

    fix actions issue while close driver

---
+++ jdi-light-html-tests/src/test/java/io/github/epam/html/tests/performance/ActionsCloseTests.java
@@ -0,0 +1,57 @@
+package io.github.epam.html.tests.performance;
+
+import com.epam.jdi.light.driver.WebDriverFactory;
+import com.epam.jdi.tools.Timer;
+import io.github.epam.TestsInit;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static io.github.com.StaticSite.html5Page;
+import static io.github.com.pages.HtmlElementsPage.yourName;
+import static io.github.epam.html.tests.site.steps.States.shouldBeLoggedIn;
+import static org.testng.Assert.assertEquals;
+
+/**
+ * Created by Roman Iovlev on 01.03.2019
+ * Email: roman.iovlev.jdi@gmail.com; Skype: roman.iovlev
+ */
+
+public class ActionsCloseTests implements TestsInit {
+
+    @BeforeMethod
+    public void before() {
+        shouldBeLoggedIn();
+        html5Page.shouldBeOpened();
+        yourName.setText(text);
+    }
+    String text = "TextField";
+
+    @Test
+    public void getValueTest() {
+        yourName.core().actionsWithElement(a -> a.
+            click().
+            keyDown(Keys.CONTROL).
+            sendKeys("a").
+            keyUp(Keys.CONTROL));
+        yourName.sendKeys("Test");
+        assertEquals(yourName.getValue(), text+"Test");
+    }
+
+    @Test
+    public void sendKeysTest() {
+        yourName.core().actionsWithElement(a -> a.
+            click().
+            keyDown(Keys.CONTROL).
+            sendKeys("a").
+            keyUp(Keys.CONTROL));
+        yourName.sendKeys("Test");
+        assertEquals(yourName.getValue(), text+"Test");
+    }
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        WebDriverFactory.close();
+    }
+
+}
