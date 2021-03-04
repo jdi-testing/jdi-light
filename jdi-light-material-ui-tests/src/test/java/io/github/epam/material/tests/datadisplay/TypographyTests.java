608d849dbec3ee38508fb45ea43aec8f209ece4d
AlinaSelyutina <AlinaSelyutina>
2021-03-04 13:42:51 +0300

    small fix for Typography

--- jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/datadisplay/TypographyTests.java
+++ jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/datadisplay/TypographyTests.java
@@ -13,13 +13,13 @@ public class TypographyTests extends TestsInit {
     public void typographyViewTest() {
         dataDisplayTypographyPage.open();
 
-        typographyFrame.h1Typography.has().text(containsString("Head 1"));
+        typographyFrame.h1Typography.has().text("Head 1");
         typographyFrame.h1Typography.has().styleClass("h1");
 
         typographyFrame.subtitle2Typography.has().styleClass("subtitle2");
-        typographyFrame.subtitle2Typography.has().text(containsString("Subtitle 2"));
+        typographyFrame.subtitle2Typography.has().text("Subtitle 2");
 
         typographyFrame.buttonTypography.has().styleClass("button");
-        typographyFrame.buttonTypography.has().text(containsString("BUTTON TEXT"));
+        typographyFrame.buttonTypography.has().text("BUTTON TEXT");
     }
 }
