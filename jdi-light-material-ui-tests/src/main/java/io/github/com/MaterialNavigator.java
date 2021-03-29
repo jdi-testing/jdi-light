042f8f1d03e202c1d4003013e961dccc1d146e20
Andrei_Lun <Andrei_Lun@epam.com>
2021-03-29 12:27:29 +0300

    added explicit wait modify driver init

--- jdi-light-material-ui-tests/src/main/java/io/github/com/MaterialNavigator.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/MaterialNavigator.java
@@ -1,24 +1,29 @@
 package io.github.com;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static com.epam.jdi.light.driver.WebDriverFactory.getDriver;
 
 public class MaterialNavigator {
     private static String url = "https://jdi-testing.github.io/jdi-light/material/";
+    private static WebDriver driver = getDriver();
+
     public static void openMainMaterialPage() {
-        getDriver().navigate().to(url);
+        driver.navigate().to(url);
     }
 
 
     public static void openSection(String firstSection) {
         openMainMaterialPage();
-        getDriver().findElement(By.xpath("//div[@class='MuiList-root']//span[text()='"+firstSection+"']")).click();
+        driver.findElement(By.xpath("//div[@class='MuiList-root']//span[text()='"+firstSection+"']")).click();
     }
 
     public static void openSection(String firstSection, String secondSection) {
         openMainMaterialPage();
-        getDriver().findElement(By.xpath("//div[@class='MuiListItemText-root']//span[text()='"+firstSection+"']")).click();
-        getDriver().findElement(By.xpath("//div[@class='MuiListItemText-root']//span[text()='"+secondSection+"']")).click();
+        driver.findElement(By.xpath("//div[@class='MuiListItemText-root']//span[text()='"+firstSection+"']")).click();
+        new WebDriverWait(driver, 2).until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='MuiListItemText-root']//span[text()='"+secondSection+"']"))).click();
     }
 }
