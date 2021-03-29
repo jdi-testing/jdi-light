30a0632bd5f632eaa688ff571634889fc60f87ff
Andrei_Lun <Andrei_Lun@epam.com>
2021-03-29 14:44:13 +0300

    move locators to variable

--- jdi-light-material-ui-tests/src/main/java/io/github/com/MaterialNavigator.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/MaterialNavigator.java
@@ -10,6 +10,8 @@ import static com.epam.jdi.light.driver.WebDriverFactory.getDriver;
 public class MaterialNavigator {
     private static String url = "https://jdi-testing.github.io/jdi-light/material/";
     private static WebDriver driver = getDriver();
+    private static String oneSectionLocator = "//div[@class='MuiList-root']//span[text()='%s']";
+    private static String twoSectionLocator = "//div[@class='MuiListItemText-root']//span[text()='%s']";
 
     public static void openMainMaterialPage() {
         driver.navigate().to(url);
@@ -17,13 +19,13 @@ public class MaterialNavigator {
 
     public static void openSection(String firstSection) {
         openMainMaterialPage();
-        driver.findElement(By.xpath(String.format("//div[@class='MuiList-root']//span[text()='%s']", firstSection))).click();
+        driver.findElement(By.xpath(String.format(oneSectionLocator, firstSection))).click();
     }
 
     public static void openSection(String firstSection, String secondSection) {
         openMainMaterialPage();
-        driver.findElement(By.xpath(String.format("//div[@class='MuiListItemText-root']//span[text()='%s']", firstSection))).click();
+        driver.findElement(By.xpath(String.format(twoSectionLocator, firstSection))).click();
         new Timer(2000L)
-                .wait(() -> driver.findElement(By.xpath(String.format("//div[@class='MuiListItemText-root']//span[text()='%s']", secondSection))).click());
+                .wait(() -> driver.findElement(By.xpath(String.format(twoSectionLocator, secondSection))).click());
     }
 }
