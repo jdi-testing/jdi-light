bd2dd4de93f0de7571a89d3e298bf8bb3aa12f2d
Fwerfad <30774377+Fwerfad@users.noreply.github.com>
2021-04-06 18:38:44 +0300

    replaced Text with TextArea

--- jdi-light-material-ui-tests/src/main/java/io/github/com/pages/utils/ClickAwayListenerFrame.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/pages/utils/ClickAwayListenerFrame.java
@@ -3,14 +3,14 @@ package io.github.com.pages.utils;
 import com.epam.jdi.light.elements.composite.Section;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.ui.html.elements.common.Button;
-import com.epam.jdi.light.ui.html.elements.common.Text;
+import com.epam.jdi.light.ui.html.elements.common.TextArea;
 
 public class ClickAwayListenerFrame extends Section {
     @UI("#root > div > button")
     public Button openMenuButton;
 
     @UI("(//div[contains(@class, 'makeStyles-dropdown')])")
-    public Text text;
+    public TextArea text;
 
     public void clickAroundTextPopup(int x, int y) {
         text.core().click(x, y);
