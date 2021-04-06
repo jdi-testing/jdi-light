176516e5d2c3b458eafb2ee4997621f044b55061
Gertcii <polishkagerc@gmail.com>
2021-04-06 19:07:13 +0300

    enabled and disabled field tests (#2710)
    without changing data types

--- jdi-light-material-ui-tests/src/main/java/io/github/com/pages/surfaces/AccordionFrame.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/pages/surfaces/AccordionFrame.java
@@ -1,5 +1,6 @@
 package io.github.com.pages.surfaces;
 
+import com.epam.jdi.light.elements.complex.dropdown.Dropdown;
 import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.material.elements.inputs.Button;
@@ -14,12 +15,16 @@ import java.util.List;
 
 public class AccordionFrame extends WebPage {
 
-    @UI(".MuiAccordion-root")
-    public List<Accordion> accordion;
+    @UI(".MuiAccordion-root[1]")
+    public Accordion enableAccordion;
 
-    @UI(".MuiButton-root")
-    public List<Button> button;
+    @UI(".MuiAccordion-root[5]")
+    public Accordion disableAccordion;
 
+    @UI("span.MuiIconButton-label[1]")
+    public Button enableButton;
 
+    @UI("span.MuiIconButton-label[5]")
+    public Button disableButton;
 
 }
--- jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/surfaces/AccordionTests.java
+++ jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/surfaces/AccordionTests.java
@@ -1,43 +1,43 @@
 package io.github.epam.material.tests.surfaces;
 
-import com.epam.jdi.light.material.elements.surfaces.Accordion;
+
 import io.github.epam.TestsInit;
 import org.testng.annotations.Test;
-
-import java.util.List;
-import java.util.Locale;
-
 import static io.github.com.MaterialNavigator.openSection;
 import static io.github.com.StaticSite.accordionFrame;
-import static io.github.com.StaticSite.surfaceAccordionDisabledPage;
-import static io.github.com.StaticSite.surfaceAccordionPage;
 
 public class AccordionTests extends TestsInit {
 
     @Test
-    public void defaultAccordionTest() {
-
+    public void defaultAccordionTest(){
         openSection("Accordion");
-
-        List<Accordion> accordionList = accordionFrame.accordion;
-
-        for (Accordion accordionItem : accordionList
-        ) {
-
-            if (accordionItem.summary().text().contains("Disabled Accordion")) {
-                accordionItem.click();
-                accordionItem.is().collapsed();
-
-            } else {
-                accordionItem.is().collapsed();
-                accordionItem.click();
-                accordionItem.is().contentVisible();
-                accordionItem.click();
-                accordionItem.is().collapsed();
-                accordionItem.is().contentInvisible();
-            }
-        }
+        accordionFrame.enableAccordion.isEnabled();
+        accordionFrame.enableAccordion.is().collapsed();
+        accordionFrame.enableAccordion.click();
+        accordionFrame.enableAccordion.is().contentVisible();
+        accordionFrame.enableAccordion.click();
+        accordionFrame.enableAccordion.is().collapsed();
+        accordionFrame.enableAccordion.is().contentInvisible();
     }
 
+    @Test
+    public void buttonAccordionTest(){
+        openSection("Accordion");
+        accordionFrame.enableAccordion.is().collapsed();
+        accordionFrame.enableButton.click();
+        accordionFrame.enableAccordion.is().contentVisible();
+        accordionFrame.enableButton.click();
+        accordionFrame.enableAccordion.is().contentInvisible();
+    }
 
+    @Test
+    public void disableAccordionTest(){
+        openSection("Accordion");
+        accordionFrame.disableAccordion.isDisabled();
+    }
+
+    @Test
+    public void disableButtonAccordionTest(){
+        accordionFrame.disableButton.isDisabled();
+    }
 }
