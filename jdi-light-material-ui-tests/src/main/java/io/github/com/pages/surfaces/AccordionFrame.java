8ab4f814d7485b5827b659341f1f9cb14219f7b1
Gertcii <polishkagerc@gmail.com>
2021-04-06 15:59:45 +0300

    defaultAccordionTest has been changed

--- jdi-light-material-ui-tests/src/main/java/io/github/com/pages/surfaces/AccordionFrame.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/pages/surfaces/AccordionFrame.java
@@ -1,15 +1,25 @@
 package io.github.com.pages.surfaces;
 
-import com.epam.jdi.light.elements.composite.Section;
+import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.material.elements.inputs.Button;
 import com.epam.jdi.light.material.elements.surfaces.Accordion;
 
-public class AccordionFrame extends Section {
+import java.util.List;
+
+/**
+ * To see an example of Accordion web element please visit
+ * https://jdi-testing.github.io/jdi-light/material/accordion_surface
+ */
+
+public class AccordionFrame extends WebPage {
 
     @UI(".MuiAccordion-root")
-    public Accordion accordion;
+    public List<Accordion> accordion;
 
     @UI(".MuiButton-root")
-    public Button button;
+    public List<Button> button;
+
+
+
 }
