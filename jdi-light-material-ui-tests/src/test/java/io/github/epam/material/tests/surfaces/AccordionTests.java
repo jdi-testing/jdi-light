ce9dfb44af1fab2b0153d53e81d2355359bb97cd
Gertcii <polishkagerc@gmail.com>
2021-04-08 09:57:17 +0300

    Accordion 2710
    AccordionAssert has been removed;
    Accordion and AccordionTests have been modified;

--- jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/surfaces/AccordionTests.java
+++ jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/surfaces/AccordionTests.java
@@ -1,43 +1,30 @@
 package io.github.epam.material.tests.surfaces;
 
-
 import io.github.epam.TestsInit;
 import org.testng.annotations.Test;
+
 import static io.github.com.MaterialNavigator.openSection;
-import static io.github.com.StaticSite.accordionFrame;
+import static io.github.com.pages.surfaces.AccordionPage.*;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
 
 public class AccordionTests extends TestsInit {
 
     @Test
-    public void defaultAccordionTest(){
-        openSection("Accordion");
-        accordionFrame.enableAccordion.isEnabled();
-        accordionFrame.enableAccordion.is().collapsed();
-        accordionFrame.enableAccordion.click();
-        accordionFrame.enableAccordion.is().contentVisible();
-        accordionFrame.enableAccordion.click();
-        accordionFrame.enableAccordion.is().collapsed();
-        accordionFrame.enableAccordion.is().contentInvisible();
-    }
-
-    @Test
-    public void buttonAccordionTest(){
-        openSection("Accordion");
-        accordionFrame.enableAccordion.is().collapsed();
-        accordionFrame.enableButton.click();
-        accordionFrame.enableAccordion.is().contentVisible();
-        accordionFrame.enableButton.click();
-        accordionFrame.enableAccordion.is().contentInvisible();
-    }
-
-    @Test
     public void disableAccordionTest(){
         openSection("Accordion");
-        accordionFrame.disableAccordion.isDisabled();
+        assertTrue(disabledAccordion.isDisabled());
     }
 
     @Test
-    public void disableButtonAccordionTest(){
-        accordionFrame.disableButton.isDisabled();
+    public void defaultAccordionTest() {
+        openSection("Accordion");
+        assertTrue(enabledAccordion.isEnabled());
+        enabledAccordion.expand();
+        assertTrue(enabledAccordion.list().isDisplayed());
+        enabledAccordion.close();
+        enabledAccordion.is().collapsed();
+        assertFalse(enabledAccordion.list().isDisplayed());
     }
+
 }
--- jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/elements/surfaces/Accordion.java
+++ jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/elements/surfaces/Accordion.java
@@ -1,60 +1,20 @@
 package com.epam.jdi.light.material.elements.surfaces;
 
 import com.epam.jdi.light.common.JDIAction;
-import com.epam.jdi.light.elements.base.UIBaseElement;
-import com.epam.jdi.light.elements.common.UIElement;
-import com.epam.jdi.light.material.asserts.surfaces.AccordionAssert;
-import org.openqa.selenium.By;
+import com.epam.jdi.light.elements.complex.dropdown.Dropdown;
+
 
 /**
  * To see an example of Accordion web element please visit
  * https://jdi-testing.github.io/jdi-light/material/accordion_surface
  */
-public class Accordion extends UIBaseElement<AccordionAssert> {
-
-    @JDIAction("Get summary text for '{name}'")
-    public String summaryValue() {
-        return summary().getText();
-    }
-
-    @JDIAction("Get content text for '{name}'")
-    public String contentValue() {
-        return content().getText();
-    }
-
-    @JDIAction("Click '{name}'")
-    public void click() {
-        core().click();
-    }
-
-    public void expand() {
-        if (isCollapsed()) {
-            summary().click();
-            this.is().expanded();
-        }
-    }
-
-    public void collapse() {
-        if (isExpanded()) {
-            summary().click();
-            this.is().collapsed();
-        }
-    }
-
-    @JDIAction("Is '{name}' is expanded")
-    public Boolean isExpanded() {
-        return hasClass("Mui-expanded");
-    }
-
-    @JDIAction("Is '{name}' is collapsed")
-    public Boolean isCollapsed() {
-        return !isExpanded();
-    }
+public class Accordion extends Dropdown {
 
     @JDIAction("Is '{name} disabled")
     @Override
     public boolean isDisabled() {
-        return hasClass("Mui-disabled");
+
+        return value().hasClass("Mui-disabled");
     }
 
     @JDIAction("Is '{name} enabled")
@@ -63,16 +23,4 @@ public class Accordion extends UIBaseElement<AccordionAssert> {
         return !isDisabled();
     }
 
-    public UIElement content() {
-        return this.find(By.cssSelector("div[class*='MuiAccordionDetails-root']"));
-    }
-
-    public UIElement summary() {
-        return this.find(By.cssSelector("div[class*='MuiAccordionSummary-root']"));
-    }
-
-    @Override
-    public AccordionAssert is() {
-        return new AccordionAssert().set(this);
-    }
 }
