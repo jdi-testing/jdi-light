658a58d3f9e9d4a5c7d5bbb1e80af299063d82e0
Nikita_Iashin <nikita_iashin@epam.com>
2021-04-12 10:42:45 +0300

    groped test according picker labels added comment to an example page changed coordinates for date picker click

--- jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/inputs/DateTimePickersTests.java
+++ jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/inputs/DateTimePickersTests.java
@@ -8,6 +8,11 @@ import static io.github.com.MaterialNavigator.openSection;
 import static io.github.com.pages.inputs.DateTimePickersPage.*;
 import static org.hamcrest.Matchers.containsString;
 
+/**
+ * To see an example of Material Date and Time pickers
+ * please visit https://material.io/components/date-pickers#usage.
+ */
+
 public class DateTimePickersTests extends TestsInit {
 
     @BeforeMethod
@@ -16,85 +21,76 @@ public class DateTimePickersTests extends TestsInit {
     }
 
     @Test
-    public void pickDateViaInlineDropdownTest() {
+    public void datePickerInlineTest() {
         inlineDatePicker.iCore().label().has().text("Date picker inline");
+
         inlineDatePicker.select("12");
         inlineDatePicker.has().text(containsString("/12/"));
+
+        inlineDatePicker.value().setText("10/10/2022");
+        inlineDatePicker.has().text("10/10/2022");
     }
 
     @Test
-    public void pickDateViaInlineTextFieldTest() {
-        inlineDatePicker.value().setText("10/10/2021");
-        inlineDatePicker.has().text("10/10/2021");
-    }
+    public void datePickerDialogTest() {
+        dialogPicker.iCore().label().has().text("Date picker dialog");
 
-    @Test
-    public void pickDateViaDialogPopUpTest(){
         dialogPicker.expand();
         dialogPicker.select("12");
         dialogPicker.command("ENTER");
         dialogPicker.has().text(containsString("/12/"));
-    }
 
-    @Test
-    public void escapeDatePickingViaDialogPopUpTest(){
         dialogPicker.expand();
-        dialogPicker.select("12");
+        dialogPicker.select("11");
         dialogPicker.command("ESCAPE");
-        dialogPicker.has().text("08/18/2014");
-    }
+        dialogPicker.has().text("08/12/2014");
 
-    @Test
-    public void pickDateViaDialogTextFieldTest(){
-        dialogPicker.iCore().label().has().text("Date picker dialog");
         dialogPicker.value().setText("10/10/2021");
         dialogPicker.has().text("10/10/2021");
     }
 
     @Test
-    public void pickTimeViaTimePopUpTest(){
-        timePicker.expand();
-        timePicker.list().get("5").click(1, 1);
-        timePicker.list().get("15").click(3, 3);
-        timePicker.command("ENTER");
-        timePicker.has().text("05:14 PM");
-    }
-
-    @Test
-    public void pickTimeViaTimeTextFieldTest(){
+    public void timePickerTest() {
         timePicker.iCore().label().has().text("Time picker");
-        timePicker.value().setText("05:14 AM");
-        timePicker.has().text("05:14 AM");
-    }
 
-    @Test
-    public void escapeTimePickingUsingTimePopUpTest(){
         timePicker.expand();
-        timePicker.list().get("5").click(1, 1);
-        timePicker.list().get("15").click(3, 3);
+        timePicker.list().get("5").click(0, 0);
+        timePicker.list().get("30").click(15, 0);
+        timePicker.command("ENTER");
+        timePicker.has().text("05:30 PM");
+
+        timePicker.expand();
+        timePicker.list().get("11").click(0, 0);
+        timePicker.list().get("15").click(15, 0);
         timePicker.command("ESCAPE");
-        timePicker.has().text("09:11 PM");
+        timePicker.has().text("05:30 PM");
+
+        timePicker.value().setText("05:35 AM");
+        timePicker.has().text("05:35 AM");
     }
 
     @Test
-    public void pickDateViaBirthdayPickerTextField(){
+    public void birthdayPickerTest() {
         birthdayPicker.iCore().label().has().text("Birthday");
+
         birthdayPicker.has().text("2017-05-24");
         birthdayPicker.value().setText("2021-04-05");
         birthdayPicker.has().text("2021-04-05");
     }
 
     @Test
-    public void pickAppointmentViaAppointmentPickerTextField(){
+    public void appointmentPickerTest() {
         appointmentPicker.iCore().label().has().text("Next appointment");
+
         appointmentPicker.has().text("2017-05-24T10:30");
         appointmentPicker.value().setText("2021-11-12T11:12");
         appointmentPicker.has().text("2021-11-12T11:12");
     }
 
     @Test
-    public void pickTimeViaAlarmPickerTextField(){
+    public void alarmPickerTest() {
         alarmPicker.iCore().label().has().text("Alarm clock");
+
         alarmPicker.has().text("07:30");
         alarmPicker.value().setText("08:00");
         alarmPicker.has().text("08:00");
