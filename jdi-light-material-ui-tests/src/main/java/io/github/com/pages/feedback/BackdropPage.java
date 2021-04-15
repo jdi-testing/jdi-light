e25484a24ffe96eebba328e363afb37d86db4441
Gertcii <polishkagerc@gmail.com>
2021-04-15 12:30:15 +0300

    Backdrop 2714

--- jdi-light-material-ui-tests/src/main/java/io/github/com/pages/feedback/BackdropPage.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/pages/feedback/BackdropPage.java
@@ -1,16 +1,16 @@
 package io.github.com.pages.feedback;
 
-import com.epam.jdi.light.elements.composite.Section;
+import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.material.elements.feedback.Backdrop;
 import com.epam.jdi.light.material.elements.inputs.Button;
 
-public class BackdropFrame extends Section {
+public class BackdropPage extends WebPage {
 
     @UI(".MuiButton-root")
-    public Button showBackdropButton;
+    public static Button showBackdropButton;
 
     @UI(".MuiBackdrop-root")
-    public Backdrop backdrop;
+    public static Backdrop backdrop;
 
 }
--- jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/feedback/BackdropTests.java
+++ jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/feedback/BackdropTests.java
@@ -1,22 +1,23 @@
 package io.github.epam.material.tests.feedback;
 
+import com.epam.jdi.tools.Timer;
 import io.github.epam.TestsInit;
 import org.testng.annotations.Test;
-
-import static io.github.com.StaticSite.backdropFrame;
-import static io.github.com.StaticSite.feedbackBackdropDefaultPage;
+import static io.github.com.MaterialNavigator.openSection;
+import static io.github.com.pages.feedback.BackdropPage.*;
 
 public class BackdropTests extends TestsInit {
 
+    private static Timer timer = new Timer(1000L);
+
     @Test
     public void defaultBackdropTest() {
-        feedbackBackdropDefaultPage.open();
+        openSection("Backdrop");
 
-        backdropFrame.backdrop.is().hidden();
-        backdropFrame.showBackdropButton.click();
-        backdropFrame.backdrop.is().visible();
-        backdropFrame.backdrop.hide();
-        backdropFrame.backdrop.is().hidden();
+        timer.wait(() -> backdrop.is().hidden());
+        showBackdropButton.click();
+        backdrop.is().visible();
+        backdrop.hide();
+        timer.wait(() -> backdrop.is().hidden());
     }
-
 }
