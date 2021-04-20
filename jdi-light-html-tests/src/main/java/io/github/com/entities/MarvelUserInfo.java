40daa53c42553f84d5a332fdecd456fdae6ca99f
tsvetaev.alex <tsvetaev.alex@gmail.com>
2021-04-20 13:00:36 +0300

    Update MarvelUserInfo.java

--- jdi-light-html-tests/src/main/java/io/github/com/entities/MarvelUserInfo.java
+++ jdi-light-html-tests/src/main/java/io/github/com/entities/MarvelUserInfo.java
@@ -3,7 +3,6 @@ package io.github.com.entities;
 import com.epam.jdi.tools.DataClass;
 
 public class MarvelUserInfo extends DataClass<MarvelUserInfo> {
-    public String;
     public String number;
     public String type;
     public String user;
