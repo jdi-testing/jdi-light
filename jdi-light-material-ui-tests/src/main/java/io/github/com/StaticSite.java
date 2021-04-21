7a9dead22c44dff07c5aa0ffcea8d7e8cb960b78
LunAndrey <80455818+LunAndrey@users.noreply.github.com>
2021-04-21 10:17:01 +0300

    2727 - MaterialUI - Popover (#2902)
    * update popover tests
    
    * commit according to comments

--- jdi-light-material-ui-tests/src/main/java/io/github/com/StaticSite.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/StaticSite.java
@@ -441,16 +441,7 @@ public class StaticSite {
 
     // popover
     @Frame("storybook-preview-iframe")
-    public static PopoverClickFrame popoverClickFrame;
-
-    @Url("/material-ui-utils-popover--click")
-    public static WebPage utilsPopoverClickPage;
-
-    @Frame("storybook-preview-iframe")
-    public static PopoverHoverFrame popoverHoverFrame;
-
-    @Url("/material-ui-utils-popover--hover")
-    public static WebPage utilsPopoverHoverPage;
+    public static PopoverPage popoverPage;
 
     // utils
     // Click away listener
--- jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/asserts/utils/PopoverAssert.java
+++ jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/asserts/utils/PopoverAssert.java
@@ -10,37 +10,12 @@ import static com.epam.jdi.light.asserts.core.SoftAssert.jdiAssert;
 
 public class PopoverAssert extends UIAssert<PopoverAssert, Popover> {
 
+    // Used by other tests. Can be deleted in future
     @JDIAction("Assert that '{name}' is visible")
     public PopoverAssert visible() {
         boolean isVisible = new Timer(base().getTimeout() * 1000L)
                 .wait(() -> element().isDisplayed());
         jdiAssert(isVisible, Matchers.is(true));
-        return this;
-    }
-
-    @JDIAction("Assert that '{name}' is invisible")
-    public PopoverAssert invisible() {
-        boolean isVisible = new Timer(base().getTimeout() * 1000L)
-                .wait(() -> !element().isDisplayed());
-        jdiAssert(isVisible, Matchers.is(true));
-        return this;
-    }
-
-    @JDIAction("Assert that '{name}' text equals '{0}'")
-    public PopoverAssert textEquals(final String expectedText) {
-        jdiAssert(element().getText().equals(expectedText), Matchers.is(true));
-        return this;
-    }
-
-    @JDIAction("Assert that '{name}' text equals '{0}' (ignore case)")
-    public PopoverAssert textEqualsIgnoreCase(final String expectedText) {
-        jdiAssert(element().getText().equalsIgnoreCase(expectedText), Matchers.is(true));
-        return this;
-    }
-
-    @JDIAction("Assert that '{name}' text contains '{0}'")
-    public PopoverAssert textContains(final String expectedSubstring) {
-        jdiAssert(element().getText().contains(expectedSubstring), Matchers.is(true));
         return this;
     }
 }
