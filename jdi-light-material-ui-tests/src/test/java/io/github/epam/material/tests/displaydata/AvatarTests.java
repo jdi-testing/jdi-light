8532ccc0e7631adf3c14961e3d617c642e1bf46c
Fwerfad <30774377+Fwerfad@users.noreply.github.com>
2021-04-22 15:16:12 +0300

    fixes on comments

--- jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/displaydata/AvatarTests.java
+++ jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/displaydata/AvatarTests.java
@@ -1,7 +1,6 @@
 package io.github.epam.material.tests.displaydata;
 
 import com.epam.jdi.light.elements.common.UIElement;
-import io.github.com.StaticSite;
 import io.github.epam.TestsInit;
 import org.hamcrest.Matchers;
 import org.testng.annotations.BeforeTest;
@@ -9,6 +8,7 @@ import org.testng.annotations.Test;
 
 import java.util.List;
 
+import static io.github.com.MaterialNavigator.openSection;
 import static io.github.com.pages.displaydata.AvatarPage.onlineStatus;
 import static io.github.com.pages.displaydata.AvatarPage.avatarWithPhoto;
 import static io.github.com.pages.displaydata.AvatarPage.avatarWithoutPhoto;
@@ -18,21 +18,19 @@ import static io.github.com.pages.displaydata.AvatarPage.avatarWithoutPhoto;
 public class AvatarTests extends TestsInit {
     @BeforeTest
     public void beforeTest() {
-        //openSection("Avatar");
-        StaticSite.dataDisplayAvatarPage.open();
-        StaticSite.dataDisplayAvatarPage.isOpened();
+        openSection("Avatar");
     }
 
     @Test
     public void avatarTests() {
-        testLogic(avatarWithoutPhoto, true);
-        testLogic(avatarWithPhoto, false);
+        basicAvatarChecks(avatarWithoutPhoto, true);
+        basicAvatarChecks(avatarWithPhoto, false);
         onlineStatus.get(1).has().classValue(Matchers.containsString("MuiBadge-dot"));
         onlineStatus.get(2).has().text("R");
         onlineStatus.get(2).has().classValue(Matchers.containsString("MuiBadge-anchorOriginBottomRightCircle"));
     }
 
-    private void testLogic(List<UIElement> elements, boolean noPhoto) {
+    private void basicAvatarChecks(List<UIElement> elements, boolean noPhoto) {
         for (int i = 1; i <= elements.size(); i++) {
             elements.get(i).isDisplayed();
             elements.get(i).hasImage();
