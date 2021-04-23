6d3c2e4c62195aefd76c639a09b5374898ed0a9a
Olga_Poddubnaia <poddubnaiaolga@gmail.com>
2021-04-23 11:34:56 +0300

    added Lists, asserts

--- jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/displaydata/TypographyTests.java
+++ jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/displaydata/TypographyTests.java
@@ -1,13 +1,17 @@
 package io.github.epam.material.tests.displaydata;
 
+import com.epam.jdi.light.ui.html.elements.common.Text;
 import io.github.epam.TestsInit;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static io.github.com.MaterialNavigator.openSection;
 import static io.github.com.pages.displaydata.TypographyPage.typographyTexts;
+import static org.hamcrest.Matchers.containsString;
 
 /**
  * To see an example of Typography web element please visit https://material-ui.com/components/typography/
@@ -24,27 +28,22 @@ public class TypographyTests extends TestsInit {
 
 
     @Test
-    public void typographyViewTest() {
+    public void typographyTextTest() {
 
-        ArrayList<String> textField = new ArrayList<>();
-        for (int i = 0; i < 6; i++) {
-            textField.add("Head " + (i + 1));
+        List<String> textFields = Arrays.asList("Head 1", "Head 2", "Head 3", "Head 4", "Head 5", "Head 6",
+                "Subtitle 1", "Subtitle 2", "Body 1", "Body 2", "BUTTON TEXT", "Caption text", "OVERLINE TEXT");
+
+        List<String> typographyClasses = Arrays.asList("MuiTypography-h1", "MuiTypography-h2", "MuiTypography-h3",
+                "MuiTypography-h4", "MuiTypography-h5", "MuiTypography-h6", "MuiTypography-subtitle1",
+                "MuiTypography-subtitle2", "MuiTypography-body1", "MuiTypography-body2", "MuiTypography-button",
+                "MuiTypography-caption", "MuiTypography-overline");
+
+        for (Text typographyText : typographyTexts) {
+            Assert.assertTrue(textFields.contains(typographyText.getText()));
         }
-        textField.add("Subtitle 1");
-        textField.add("Subtitle 2");
-        textField.add("Body 1");
-        textField.add("Body 2");
-        textField.add("BUTTON TEXT");
-        textField.add("Caption text");
-        textField.add("OVERLINE TEXT");
 
         for (int i = 0; i < typographyTexts.size(); i++) {
-            typographyTexts.get(i+1).has().text(textField.get(i));
+            typographyTexts.get(i + 1).has().classValue(containsString(typographyClasses.get(i)));
         }
-
-      /* typographyTexts.has().text("Head 1");
-        typographyTexts.has().styleClass("h1");*/
-
-
     }
 }
