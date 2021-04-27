b01383aa829c08ffa89c6ee9dc8f1b838e981fc5
Olga_Poddubnaia <poddubnaiaolga@gmail.com>
2021-04-27 14:03:33 +0300

    added @Tests, changed locators

--- jdi-light-material-ui-tests/src/main/java/io/github/com/pages/utils/TransitionPage.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/pages/utils/TransitionPage.java
@@ -3,20 +3,19 @@ package io.github.com.pages.utils;
 import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
-import com.epam.jdi.light.elements.pageobjects.annotations.locators.XPath;
 import com.epam.jdi.light.ui.html.elements.common.Checkbox;
 
 import java.util.List;
 
 public class TransitionPage extends WebPage {
 
-    @UI("//*[contains(@class,'MuiPaper-elevation4 MuiPaper-rounded')]")
+    @UI("//*[contains(@class,'MuiPaper-root')]")
     public static List<UIElement> transitions;
 
-    @XPath("//input[contains(@type,'checkbox')]")
+    @UI("//*[contains(@class,'MuiCollapse-entered')]")
+    public static List<UIElement> displayedCollapseFadeTransitions;
+
+
+    @UI("//*[contains(@class,'MuiSwitch-switchBase MuiSwitch-colorSecondary')]")
     public static List<Checkbox> checkboxes;
-
-  /*  @UI("//*[contains(@type,'checkbox')]")
-    public static List<Checkbox> checkboxes;*/
-
 }
--- jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/utils/TransitionTests.java
+++ jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/utils/TransitionTests.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 
 import static io.github.com.MaterialNavigator.openSection;
 import static io.github.com.pages.utils.TransitionPage.checkboxes;
+import static io.github.com.pages.utils.TransitionPage.displayedCollapseFadeTransitions;
 import static io.github.com.pages.utils.TransitionPage.transitions;
 
 /**
@@ -19,33 +20,41 @@ public class TransitionTests extends TestsInit {
         openSection("Transitions");
     }
 
-/*
-//both of this test were previously used
-  @Test
-    public void fadeTransitionTest() {
-
-        transition.is().hidden();
-        transition.clickSwitch();
-        transition.is().visible();
-        transition.has().content("Content");
+    @Test
+    public void collapseDisplayTest() {
+        checkboxes.get(1).click();
+        displayedCollapseFadeTransitions.get(1).is().displayed();
+        displayedCollapseFadeTransitions.get(2).is().displayed();
     }
 
     @Test
-    public void collapseTransitionTest() {
+    public void fadeDisplayTest() {
+        checkboxes.get(2).click();
+        displayedCollapseFadeTransitions.get(3).is().hidden();
+        displayedCollapseFadeTransitions.get(4).is().hidden();
+    }
 
-        transition.is().hidden();
-        transition.clickSwitch();
-        transition.is().visible();
-        transition.has().content("Content");
-    }*/
-
-    //tried to check functionality of elements but checkboxes does not work
     @Test
-    public void transitionsTest() {
-        transitions.get(1).isEnabled();
-        transitions.get(2).isEnabled();
-        checkboxes.get(3).check();
-        transitions.get(1).is().enabled();
-        transitions.get(2).is().enabled();
+    public void growDisplayTest() {
+        checkboxes.get(3).click();
+        transitions.get(6).is().displayed();
+        transitions.get(5).is().displayed();
+    }
+
+    @Test
+    public void slideDisplayTest() {
+        checkboxes.get(4).click();
+        transitions.get(7).is().displayed();
+    }
+
+    @Test
+    public void zoomDisplayTest() {
+        transitions.get(8).is().hidden();
+        transitions.get(9).is().hidden();
+
+
+        checkboxes.get(5).click();
+        transitions.get(8).is().displayed();
+        transitions.get(9).is().displayed();
     }
 }
