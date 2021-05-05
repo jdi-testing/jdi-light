99467b23f22f2199d30afb73589e930d3d376863
Fwerf <afs1424ad@gmail.com>
2021-05-05 11:13:09 +0300

    Implemented tests on Radio Removed unused dependencies on RadioButtonPage

--- jdi-light-material-ui-tests/src/main/java/io/github/com/pages/inputs/RadioButtonPage.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/pages/inputs/RadioButtonPage.java
@@ -5,25 +5,27 @@ import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.ui.html.elements.common.Button;
 import com.epam.jdi.light.ui.html.elements.common.Text;
-import com.epam.jdi.light.ui.html.elements.complex.RadioButtons;
 
 import java.util.List;
 
 public class RadioButtonPage extends WebPage {
-    @UI("//h2[text()='Simple radio']/following-sibling::fieldset[1]/div/label/span/span/input")
+    @UI("//h2[text()='Simple radio']/following-sibling::fieldset[1]/div/label")
     public static List<UIElement> simpleRadioButtons;
 
-    @UI("//h2[text()='Simple radio']/following-sibling::fieldset[1]/div/label/span[2]")
-    public static List<UIElement> simpleRadioTexts;
+    @UI("//h2[text()='Simple radio']/following-sibling::fieldset[1]/div/label/span[1]")
+    public static List<UIElement> simpleRadioButtonsClass;
 
     @UI("//h2[text()='Simple radio']/following-sibling::fieldset[1]/p")
     public static Text lastRadioText;
 
-    @UI("//h2[text()='Label placement']/following-sibling::fieldset[1]/div/label/span/span/input")
+    @UI("//h2[text()='Label placement']/following-sibling::fieldset[1]/div/label")
     public static List<UIElement> labelPlacementButtons;
-    ////h2[text()='Show error']/following-sibling::form/fieldset/div/label/span/span/input
-    @UI("[name=quiz]")
-    public static RadioButtons showErrorButtons;
+
+    @UI("//h2[text()='Label placement']/following-sibling::fieldset[1]/div/label/span[1]")
+    public static List<UIElement> labelPlacementButtonsClass;
+
+    @UI("//h2[text()='Show error']/following-sibling::form/fieldset/div/label")
+    public static List<UIElement> showErrorButtons;
 
     @UI("//h2[text()='Show error']/following-sibling::form/fieldset/p")
     public static Text errorText;
--- jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/inputs/RadioButtonTests.java
+++ jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/inputs/RadioButtonTests.java
@@ -1,9 +1,12 @@
 package io.github.epam.material.tests.inputs;
 
+import com.epam.jdi.tools.Timer;
 import io.github.epam.TestsInit;
 import org.hamcrest.Matchers;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
 
 import static io.github.com.pages.inputs.RadioButtonPage.*;
 import static io.github.com.MaterialNavigator.openSection;
@@ -11,6 +14,9 @@ import static io.github.com.StaticSite.radioButtonPage;
 
 
 public class RadioButtonTests extends TestsInit {
+    static ArrayList<String> labels = new ArrayList() {{add("First"); add("Second"); add("Third"); add("Disabled");}};
+    static ArrayList<String> classes = new ArrayList() {{add("Top"); add("Start"); add("Bottom");}};
+    static ArrayList<String> messages = new ArrayList() {{add("You got it!"); add("Sorry, wrong answer!");}};
 
     @BeforeTest()
     public void beforeTest() {
@@ -25,69 +31,42 @@ public class RadioButtonTests extends TestsInit {
         }
     }
 
-    public void selectRadio(int x, int section) {
-        switch(section) {
-            case 1 :
-                System.out.println(simpleRadioButtons.get(x).base().searchRules.toString());
-                simpleRadioButtons.get(x).select();
-                lastRadioText.has().text(Matchers.containsString(simpleRadioTexts.get(x).text()));
-                break;
-            case 2 :
-                labelPlacementButtons.get(x).select();
-                break;
-            case 3:
-                showErrorButtons.select(x);
-                break;
+    @Test
+    public void labelPlacementTest() {
+        for (int i = 1; i <= 4; i++) {
+            selectRadio(i, 2);
         }
     }
 
     @Test
-    public void labelPlacementTest() {
-
-    }
-
-    @Test
     public void showErrorTest() {
-
+        for (int i = 1; i <= 2; i++) {
+            selectRadio(i, 3);
+        }
     }
 
-    @Test
-    public void defaultRadioButtonTest() {
-//
-//        radioButtonPage.firstRadioButton.is().enabled();
-//        radioButtonPage.secondRadioButton.is().enabled();
-//        radioButtonPage.thirdRadioButton.is().enabled();
-//        radioButtonPage.disabledRadioButton.is().disabled();
-//
-//        radioButtonPage.firstRadioButton.is().unchecked();
-//        radioButtonPage.secondRadioButton.is().unchecked();
-//        radioButtonPage.thirdRadioButton.is().unchecked();
-//        radioButtonPage.disabledRadioButton.is().unchecked();
-//
-//        radioButtonPage.firstRadioButton.toggle();
-//        radioButtonPage.firstRadioButton.is().checked();
-//        radioButtonPage.secondRadioButton.is().unchecked();
-//        radioButtonPage.thirdRadioButton.is().unchecked();
-//        radioButtonPage.disabledRadioButton.is().unchecked();
-//        radioButtonPage.textField.is().text(containsString("First"));
-//
-//        radioButtonPage.secondRadioButton.toggle();
-//        radioButtonPage.firstRadioButton.is().unchecked();
-//        radioButtonPage.secondRadioButton.is().checked();
-//        radioButtonPage.thirdRadioButton.is().unchecked();
-//        radioButtonPage.disabledRadioButton.is().unchecked();
-//        radioButtonPage.textField.is().text(containsString("Second"));
-//
-//        radioButtonPage.firstRadioButton.is().unchecked();
-//        radioButtonPage.secondRadioButton.is().checked();
-//        radioButtonPage.thirdRadioButton.is().unchecked();
-//        radioButtonPage.disabledRadioButton.is().unchecked();
-    }
-
-    @Test
-    public void disabledRadioButtonTest() {
-//        radioDefaultPage.open();
-//
-//        radioButtonPage.disabledRadioButton.is().disabled();
+    public void selectRadio(int x, int section) {
+        switch(section) {
+            case 1 :
+                simpleRadioButtons.get(x).select();
+                if (x != 4)
+                    new Timer(5000L).wait(() -> simpleRadioButtonsClass.get(x).has().classValue(Matchers.containsString("Mui-checked")));
+                simpleRadioButtons.get(x).has().text(labels.get(x - 1));
+                lastRadioText.has().text(Matchers.containsString(simpleRadioButtons.get(x).text()));
+                break;
+            case 2 :
+                if (x != 4)
+                    labelPlacementButtons.get(x).has().classValue(Matchers.containsString(classes.get(x - 1)));
+                labelPlacementButtons.get(x).select();
+                new Timer(5000L).wait(() -> labelPlacementButtonsClass.get(x).has().classValue(Matchers.containsString("Mui-checked")));
+                break;
+            case 3:
+                showErrorButtons.get(x).select();
+                checkAnswer.click();
+                new Timer(5000L).wait(() -> errorText.is().text(messages.get(x - 1)));
+                break;
+            default:
+                break;
+        }
     }
 }
