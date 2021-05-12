f018908bab304a17af504726c90ee7d080e76309
Mariia_Atamas <varteya@gmail.com>
2021-05-12 12:04:19 +0300

    Comments are deleted

--- jdi-light-material-ui-tests/src/main/java/io/github/com/pages/navigation/StepperPage.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/pages/navigation/StepperPage.java
@@ -11,42 +11,36 @@ import com.epam.jdi.light.ui.html.elements.common.TextField;
 import java.util.ArrayList;
 
 public class StepperPage extends WebPage {
-    //Simple Linear Stepper
     @UI("//*[@id='simpleStepper']//following-sibling::div//*[@class='MuiTypography-root MuiTypography-body1']")
     public static Text simpleLinearStepperTitle;
 
     @UI("//*[@id='simpleStepper']//following-sibling::div//button[contains(@class, 'MuiButtonBase-root')]")
     public static WebList simpleLinearStepperButton;
 
-    //Non-linear Stepper
     @UI("//*[@id='nonLinearStepper']//following-sibling::div//*[@class='MuiTypography-root MuiTypography-body1']")
     public static Text nonlinearStepperTitle;
 
     @UI("//*[@id='nonLinearStepper']//following-sibling::div//button[contains(@class, 'MuiButtonBase-root')]")
     public static WebList nonlinearStepperButton;
 
-    //Vertical Stepper
     @UI("(//*[text()='Vertical stepper']/following-sibling::div//p)[1]")
     public static Text verticalStepperTitle;
 
     @UI("//*[text()='Vertical stepper']/following-sibling::div//button[contains(@class, MuiButtonBase-root)]")
     public static WebList verticalStepperButton;
 
-    //Mobile Text Stepper
     @UI("(//*[text()='Mobile text stepper']/following-sibling::div//*[contains(@class, 'MuiMobileStepper-root')])[1]")
     public static Text mobileTextStepperTitle;
 
     @UI("//*[text()='Mobile text stepper']/following-sibling::div//button")
     public static WebList mobileTextStepperButton;
 
-    //Mobile Dots Stepper
     @UI("//*[@id='activeDotStep']")
     public static Text mobileDotsStepperTitle;
 
     @UI("//*[text()='Mobile dots stepper']/following-sibling::div//button")
     public static WebList mobileDotsStepperButton;
 
-    //Mobile Progress Stepper
     @UI("//*[@id='activeProgressStep']")
     public static Text mobileProgressStepperTitle;
 
