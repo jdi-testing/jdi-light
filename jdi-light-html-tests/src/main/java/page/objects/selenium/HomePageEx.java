9c9b4dc0e362050a96e63775f7e37c047157ee61
tsvetaevAlex <56589282+tsvetaevAlex@users.noreply.github.com>
2021-05-13 12:29:11 +0300

    ields should be declared at the top of the class, before any method declarations, constructors, initializers or inner classes.

--- jdi-light-html-tests/src/main/java/page/objects/selenium/HomePageEx.java
+++ jdi-light-html-tests/src/main/java/page/objects/selenium/HomePageEx.java
@@ -9,11 +9,11 @@ import static com.epam.jdi.light.elements.init.UIFactory.$;
 
 
 public class HomePageEx extends PageBase {
+    public WebElement password = $("form #password");
+    public UIElement passwordUI = $("form #password");
     public HomePageEx() { super("/index.html", "Home");}
     @FindBy(css="form #name") public WebElement userName;
-    public WebElement password = $("form #password");
 
     @UI("form #name") public UIElement userNameUI;
-    public UIElement passwordUI = $("form #password");
     @UI("#login-button") public UIElement enterUI;
 }
