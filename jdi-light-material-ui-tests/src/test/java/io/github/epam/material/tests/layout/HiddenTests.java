363182028b1802e07d785e20902e88135f42dc79
Igor_Gaiduk <Igor_Gaiduk@epam.com>
2021-05-13 21:51:40 +0300

    Updated hidden tests

--- jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/layout/HiddenTests.java
+++ jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/layout/HiddenTests.java
@@ -1,36 +1,61 @@
 package io.github.epam.material.tests.layout;
 
-import com.epam.jdi.light.material.elements.layout.Hidden;
+import com.epam.jdi.light.driver.WebDriverFactory;
+import com.epam.jdi.light.ui.html.elements.common.Button;
+import com.epam.jdi.light.ui.html.elements.common.Text;
 import io.github.epam.TestsInit;
+import org.openqa.selenium.Dimension;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import static io.github.com.StaticSite.defaultHiddenPage;
-import static io.github.com.StaticSite.hiddenFrame;
+import static io.github.com.StaticSite.hiddenPage;
+import static io.github.com.pages.layout.HiddenPage.*;
 
 public class HiddenTests extends TestsInit {
 
+    private static final int WIDTH_LABEL_LENGHT = 15;
+
+    @BeforeTest
+    public void before() {
+        hiddenPage.open();
+    }
+
     @Test
     public void defaultHiddenTest() {
-        defaultHiddenPage.open();
+        currentWidth.is().displayed();
+        xsDown.is().displayed();
+        smDown.is().displayed();
+        mdDown.is().displayed();
 
-        String width = hiddenFrame.currentWidth.getWidth();
+        String width = getWidth(currentWidth);
         checkWidth(width);
 
-        hiddenFrame.currentWidth.setHalfScreenWidthSize();
-        width = hiddenFrame.currentWidth.getWidth();
+        setHalfScreenWidthSize();
+        width = getWidth(currentWidth);
         checkWidth(width);
     }
 
     private void checkWidth(String width) {
         String[] widthArr = {"xs", "sm", "md", "lg", "xl"};
-        Hidden[] hiddenArr = {hiddenFrame.xsDown, hiddenFrame.smDown, hiddenFrame.mdDown, hiddenFrame.lgDown, hiddenFrame.xlDown};
+        Button[] hiddenArr = {xsDown, smDown, mdDown, lgDown, xlDown};
         int j = 0;
         for (int i = j; !width.equals(widthArr[i]); i++) {
-            hiddenArr[i].is().visible();
+            hiddenArr[i].is().displayed();
             j++;
         }
-        for (int i = j; i < 5; i++) {
+        for (int i = j; i < widthArr.length; i++) {
             hiddenArr[i].is().notVisible();
         }
+    }
+
+    private void setHalfScreenWidthSize(){
+        Dimension xy = WebDriverFactory.getDriver().manage().window().getSize();
+        Dimension xy2 = new Dimension(xy.width / 2, xy.height );
+        WebDriverFactory.getDriver().manage().window().setSize(xy2);
+    }
+
+    private String getWidth(Text element) {
+        String text = element.getText();
+        return text.substring(WIDTH_LABEL_LENGHT);
     }
 }
