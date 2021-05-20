c207ee0437f4253f35974631632882aa66ddc72d
Fwerf <afs1424ad@gmail.com>
2021-05-20 12:19:53 +0300

    shortened tests to better suit their purpose

--- jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/navigation/TabTests.java
+++ jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/navigation/TabTests.java
@@ -24,45 +24,7 @@ public class TabTests extends TestsInit {
     private int tableIndex;
     private Timer timer = new Timer(5000L);
 
-    public void scrollForward() {
-        timer.wait(() ->scrollButtons.get(2).click());
-    }
-
-    public void scrollBackward() {
-        timer.wait(() -> scrollButtons.get(1).click());
-    }
-
-    public void scrollUpward() {
-        scrollButtons.get(3).click();
-    }
-
-    public void scrollDownward() {
-        scrollButtons.get(4).click();
-    }
-
-    public void clickButton(int indexRow) {
-        tableLocators.get(tableIndex).get(indexRow).click();
-    }
-
-    public void checkDisabled(int indexRow) {
-        tableLocators.get(tableIndex).get(indexRow).has().classValue(containsString("Mui-disabled"));
-    }
-
-    public boolean isNotVisible(int indexRow) {
-        return tableLocators.get(tableIndex).get(indexRow).isNotVisible();
-    }
-
-    public Button getButton(int indexRow) {
-        return tableLocators.get(tableIndex).get(indexRow);
-    }
-
-    public void assertNotVisible(int indexRow) {
-        tableLocators.get(tableIndex).get(indexRow).is().notVisible();
-    }
-
-    public void assertVisible(int indexRow) {
-        timer.wait(() -> tableLocators.get(tableIndex).get(indexRow).is().visible());
-    }
+    public void clickButton(int indexRow) { tableLocators.get(tableIndex).get(indexRow).click(); }
 
     public void checkLastItem(String name) {
         lastItemText.get(tableIndex).has().text(containsString(name));
@@ -78,70 +40,46 @@ public class TabTests extends TestsInit {
     @Test
     public void simpleTabTest() {
         tableIndex = 1;
-        for (int i = 1; i <= 5; i++) {
-            if (i != 4) {
-                clickButton(i);
-                checkLastItem(itemList.get(i));
-            }
-            else
-                checkDisabled(i);
-        }
+        clickButton(1);
+        checkLastItem(itemList.get(1));
+        tableLocators.get(tableIndex).get(4).has().classValue(containsString("Mui-disabled"));
+        clickButton(5);
+        checkLastItem(itemList.get(5));
     }
 
     @Test
     public void scrollableTabTest(){
         tableIndex = 2;
-        for (int i = 11; i >= 1; i--) {
-            if (isNotVisible(i))
-                if (i > 6)
-                    scrollForward();
-                else
-                    scrollBackward();
-            clickButton(i);
-            checkLastItem(itemList.get(i));
-        }
-        for (int i = 1; i <= 11; i++) {
-            if (isNotVisible(i))
-                if (i > 6)
-                    scrollForward();
-                else
-                    scrollBackward();
-            clickButton(i);
-            checkLastItem(itemList.get(i));
-        }
+        clickButton(9);
+        checkLastItem(itemList.get(9));
+        timer.wait(() -> scrollButtons.get(2).click());
+        clickButton(11);
+        checkLastItem(itemList.get(11));
+        timer.wait(() -> scrollButtons.get(1).click());
+        clickButton(1);
+        checkLastItem(itemList.get(1));
     }
 
     @Test
     public void preventScrollTest() {
         tableIndex = 3;
         WindowsManager.resizeWindow(1000, 800);
-        for (int i = 1; i <= 5; i++) {
-            clickButton(i);
-            checkLastItem(itemList.get(i));
-        }
-        assertNotVisible(6);
-        Button button = getButton(5);
-        button.click();
-        button.core().press(Keys.RIGHT);
-        assertVisible(6);
+        clickButton(1);
+        checkLastItem(itemList.get(1));
+        clickButton(5);
+        checkLastItem(itemList.get(5));
     }
 
     @Test
     public void verticalTabTest() {
         tableIndex = 4;
-        for (int i = 7; i >= 1; i--) {
-            if (isNotVisible(i))
-                if (i == 4 || i == 1)
-                    scrollUpward();
-            clickButton(i);
-            checkLastItem(itemList.get(i));
-        }
-        for (int i = 1; i <= 7; i++) {
-            if (isNotVisible(i))
-                if (i == 4 || i == 7)
-                    scrollDownward();
-            clickButton(i);
-            checkLastItem(itemList.get(i));
-        }
+        clickButton(1);
+        checkLastItem(itemList.get(1));
+        scrollButtons.get(4).click();
+        clickButton(4);
+        checkLastItem(itemList.get(4));
+        scrollButtons.get(4).click();
+        clickButton(7);
+        checkLastItem(itemList.get(7));
     }
 }
