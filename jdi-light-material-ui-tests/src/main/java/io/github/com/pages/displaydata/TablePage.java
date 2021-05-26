fbae4269c571e4c17c7fe581317e942e8745eab1
Fwerf <afs1424ad@gmail.com>
2021-05-26 12:06:43 +0300

    removed unused param and imports

--- jdi-light-material-ui-tests/src/main/java/io/github/com/pages/displaydata/TablePage.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/pages/displaydata/TablePage.java
@@ -1,9 +1,7 @@
 package io.github.com.pages.displaydata;
 
 
-import com.epam.jdi.light.elements.complex.table.Table;
 import com.epam.jdi.light.elements.composite.WebPage;
-import com.epam.jdi.light.elements.pageobjects.annotations.locators.JTable;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.ui.html.elements.common.Button;
 import com.epam.jdi.light.ui.html.elements.common.Text;
@@ -11,13 +9,6 @@ import com.epam.jdi.light.ui.html.elements.common.Text;
 import java.util.List;
 
 public class TablePage extends WebPage {
-    @JTable(
-            root = ".MuiDataGrid-main",
-            cell = ".MuiDataGrid-main div[role='row'] div[tabindex]",
-            headers = ".MuiDataGrid-colCellWrapper [tabindex='-1']",
-            row = ".MuiDataGrid-main div[role='row']")
-    public static Table table;
-
     @UI(".MuiDataGrid-main div[role='row'] div[tabindex]")
     public static List<Button> tableCells;
 
