ac53b1e31d8d5d26d857609bcd7574805fbf3f37
Vsevolod_Adrianov <Vsevolod_Adrianov@epam.com>
2021-06-03 12:04:21 +0300

    import Button

--- jdi-light-material-ui-tests/src/main/java/io/github/com/pages/utils/PopperPage.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/pages/utils/PopperPage.java
@@ -3,12 +3,13 @@ package io.github.com.pages.utils;
 import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
+import com.epam.jdi.light.ui.html.elements.common.Button;
 
 import java.util.List;
 
 public class PopperPage extends WebPage {
     @UI("button")
-    public static List<com.epam.jdi.light.ui.html.elements.common.Button> buttons;
+    public static List<Button> buttons;
 
     @UI("[role='tooltip']")
     public static UIElement tooltip;
