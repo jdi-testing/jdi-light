98d04206bbbeb3f8aee27734ca8831ba2fdecb00
EduardLimonov <49101914+EduardLimonov@users.noreply.github.com>
2021-06-28 22:20:53 +0300

    Create ButtonGroupPage.java

---
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/pages/inputs/ButtonGroupPage.java
@@ -0,0 +1,24 @@
+package io.github.com.pages.inputs;
+import com.epam.jdi.light.elements.composite.WebPage;
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
+import com.epam.jdi.light.material.annotations.JDIButtonGroup;
+import com.epam.jdi.light.material.elements.inputs.ButtonGroup;
+
+
+public class ButtonGroupPage extends WebPage {
+
+    @UI("//*[@id=\"__next\"]/div/div/div[2]/div/div/div/div[1]/div")
+    public ButtonGroup basicButtonGroup;
+
+    @UI("//*[@id=\"__next\"]/div/div/div[2]/div/div/div/div[2]/div")
+    public ButtonGroup verticalButtonGroup;
+
+    @UI("//*[@id=\"__next\"]/div/div/div[2]/div/div/div/div[3]/div")
+    @JDIButtonGroup(
+            root = "#root",
+            mainButton = ".MuiButton-root[1]",
+            expand = ".MuiButton-root[2]",
+            list = ".MuiPaper-root #split-button-menu")
+    public ButtonGroup splitButtonGroup;
+
+}
