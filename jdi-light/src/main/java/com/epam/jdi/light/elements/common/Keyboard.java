85d569aaa2e30e832d11ba7ad4f42bc0a95ae749
RomanIovlev <roman.iovlev.jdi@gmail.com>
2021-07-02 23:23:42 +0200

    update Keyboard

--- jdi-light/src/main/java/com/epam/jdi/light/elements/common/Keyboard.java
+++ jdi-light/src/main/java/com/epam/jdi/light/elements/common/Keyboard.java
@@ -14,34 +14,51 @@ import java.lang.reflect.Field;
 import java.util.List;
 
 import static com.epam.jdi.light.common.Exceptions.exception;
+import static com.epam.jdi.light.driver.get.DriverData.getOs;
 import static com.epam.jdi.tools.LinqUtils.first;
 import static java.lang.reflect.Modifier.isStatic;
 
 public class Keyboard {
     static Robot robot;
+
     static Robot getRobot() {
-        if (robot == null)
+        if (robot == null) {
             try {
                 robot = new Robot();
             } catch (Exception ex) {
                 throw exception(ex, "Can't instantiate Robot");
             }
+        }
         return robot;
     }
+
+    public static int waitBeforePasteTextMs = 500;
+
     public static void pasteText(CharSequence text) {
-        pasteText(text, 10);
+        pasteText(text, waitBeforePasteTextMs);
     }
+
     public static void pasteText(CharSequence text, long timeToWaitMSec) {
         try {
             StringSelection stringSelection = new StringSelection(text.toString());
             Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
             clipboard.setContents(stringSelection, (clipboard1, contents) -> { });
             Timer.sleep(timeToWaitMSec);
-            commands("Ctrl+V", "Enter");
+            switch (getOs()) {
+                case MAC:
+                    commands("Meta+Tab", "Meta+Shift+G", "Meta+V", "Enter", "Enter");
+                    break;
+                case WIN:
+                    commands("Ctrl+V", "Enter");
+                    break;
+            }
         } catch (Exception ex) {
             throw exception(ex, "Past text '%s' failed with exception", text);
         }
     }
+
+    public static int waitBetweenCommandsMs = 300;
+
     public static void commands(String... commands) {
         for (String keyGroup : commands) {
             String[] keys = keyGroup.split("\\+");
@@ -52,11 +69,14 @@ public class Keyboard {
             for (String key : keys) {
                 getRobot().keyRelease(getKeyCode(key));
             }
+            Timer.sleep(waitBetweenCommandsMs);
         }
     }
+
     public static void command(String sequence) {
         commands(sequence.replace(" ", "").split(","));
     }
+
     private static int getKeyCode(String key) {
         List<Field> keys = getKeys();
         String keyName = key.replace(" ", "").replace("_", "");
@@ -66,9 +86,10 @@ public class Keyboard {
         }
         throw exception("Unknown key '%s'", key);
     }
+
     private static String getKeyName(Field field) {
         int underscore = field.getName().indexOf('_');
-        String key = field.getName().substring(underscore + 1).toLowerCase().replaceAll(" ", "").replaceAll("_", "");
+        String key = field.getName().substring(underscore + 1).toLowerCase().replace(" ", "").replaceAll("_", "");
         switch (key) {
             case "control":
                 return "ctrl";
@@ -78,20 +99,24 @@ public class Keyboard {
                 return key;
         }
     }
+
     private static List<Field> getKeys() {
         return LinqUtils.filter(KeyEvent.class.getDeclaredFields(),
                 f -> isStatic(f.getModifiers()) && f.getName().startsWith("VK_"));
     }
+
     public static void press(String key) {
         press(getKeyCode(key));
     }
+
     public static void press(int key) {
         getRobot().keyPress(key);
         getRobot().keyRelease(key);
     }
+
     public static void press(Keys key) {
         List<Field> keys = getKeys();
-        String keyName = key.name().replaceAll(" ", "").replaceAll("_", "");
+        String keyName = key.name().replace(" ", "").replace("_", "");
         Field keyField = first(keys, f -> getKeyName(f).equalsIgnoreCase(keyName));
         if (keyField == null) {
             throw exception("Unknown key '%s'", key);
