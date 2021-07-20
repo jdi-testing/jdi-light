495aa327637be96de2262da36bd43d423d17f6e3
RomanIovlev <roman.iovlev.jdi@gmail.com>
2021-07-20 09:46:43 +0200

    headless

--- tests-jdi-nova/src/test/java/com/jdiai/TestInit.java
+++ tests-jdi-nova/src/test/java/com/jdiai/TestInit.java
@@ -7,13 +7,14 @@ import static com.jdiai.JDI.domain;
 import static com.jdiai.jsbuilder.QueryLogger.ALL;
 import static com.jdiai.jsbuilder.QueryLogger.LOG_QUERY;
 import static com.jdiai.jswraper.driver.DriverManager.killDrivers;
+import static com.jdiai.jswraper.driver.JDIDriver.DRIVER_OPTIONS;
 
 public interface TestInit {
 
     @BeforeSuite(alwaysRun = true)
     default void setUp() {
         killDrivers();
-        // DRIVER_OPTIONS.chrome = cap -> cap.addArguments("--headless");
+        DRIVER_OPTIONS.chrome = cap -> cap.addArguments("--headless");
         LOG_QUERY = ALL;
         domain = "https://jdi-testing.github.io/jdi-light";
     }
