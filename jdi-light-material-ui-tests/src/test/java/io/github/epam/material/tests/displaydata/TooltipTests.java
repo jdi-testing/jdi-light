4de684fe05197b321292b1bce0d87453f60e9e3f
daxdue <sergei_kochetkov@epam.com>
2021-07-21 17:28:46 +0300

    Added tests for uncovered elements. Added text assert with Mather t to TooltipAssert

--- jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/displaydata/TooltipTests.java
+++ jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/displaydata/TooltipTests.java
@@ -1,9 +1,12 @@
 package io.github.epam.material.tests.displaydata;
 
 import io.github.epam.TestsInit;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static io.github.com.StaticSite.tooltipPage;
+import static io.github.com.pages.displaydata.TooltipPage.*;
+import static org.hamcrest.Matchers.containsString;
 
 /**
  * To see an example of Tooltip web element please visit
@@ -12,12 +15,81 @@ import static io.github.com.StaticSite.tooltipPage;
 
 public class TooltipTests extends TestsInit {
 
-    @Test
-    public void defaultTooltipTest() {
+    @BeforeMethod
+    public void before() {
         tooltipPage.open();
-        tooltipPage.buttonWithTooltip.hover();
-        tooltipPage.buttonWithTooltip.tooltip().is().visible();
-        tooltipPage.buttonWithTooltip.tooltip().has().text("Delete");
+        tooltipPage.isOpened();
     }
 
+    @Test
+    public void deleteButtonWithTooltipTest() {
+        deleteButtonWithTooltip.is().visible();
+        deleteButtonWithTooltip.hover();
+        deleteButtonWithTooltip.tooltip().is().visible();
+        deleteButtonWithTooltip.tooltip().has().text("Delete");
+    }
+
+    @Test
+    public void addButtonWithTooltipTest() {
+        addButtonWithTooltip.is().visible();
+        addButtonWithTooltip.hover();
+        addButtonWithTooltip.tooltip().is().visible();
+        addButtonWithTooltip.tooltip().has().text("Add");
+    }
+
+    @Test
+    public void buttonWithLightTooltipTest() {
+        buttonWithLightTooltip.is().visible();
+        buttonWithLightTooltip.hover();
+        buttonWithLightTooltip.tooltip().is().visible();
+        buttonWithLightTooltip.tooltip().has().text("Add");
+    }
+
+    @Test
+    public void buttonWithBootstrapTooltipTest() {
+        buttonWithBootstrapTooltip.is().visible();
+        buttonWithBootstrapTooltip.hover();
+        buttonWithBootstrapTooltip.tooltip().is().visible();
+        buttonWithBootstrapTooltip.tooltip().has().text("Add");
+    }
+
+    @Test
+    public void buttonWithHtmlTooltipTest() {
+        buttonWithHtmlTooltip.is().visible();
+        buttonWithHtmlTooltip.hover();
+        buttonWithHtmlTooltip.tooltip().is().visible();
+        buttonWithHtmlTooltip.tooltip().has().text(containsString("Tooltip with HTML"));
+    }
+
+    @Test
+    public void hoverOrTouchTooltip() {
+        hoverOrTouchButtonWithTooltip.is().visible();
+        hoverOrTouchButtonWithTooltip.hover();
+        hoverOrTouchButtonWithTooltip.tooltip().is().visible();
+        hoverOrTouchButtonWithTooltip.tooltip().has().text("Add");
+    }
+
+    @Test
+    public void hoverButtonTooltipTest() {
+        hoverButtonWithTooltip.is().visible();
+        hoverButtonWithTooltip.hover();
+        hoverButtonWithTooltip.tooltip().is().visible();
+        hoverButtonWithTooltip.tooltip().has().text("Add");
+    }
+
+    @Test
+    public void clickButtonTooltipTest() {
+        clickButtonWithTooltip.is().visible();
+        clickButtonWithTooltip.click();
+        clickButtonWithTooltip.tooltip().is().visible();
+        clickButtonWithTooltip.tooltip().has().text("Add");
+    }
+
+    @Test
+    public void disabledButtonWithTooltipTest() {
+        disabledButtonWithTooltip.is().visible();
+        disabledButtonWithTooltip.hover();
+        disabledButtonWithTooltip.tooltip().is().visible();
+        disabledButtonWithTooltip.tooltip().has().text("You don't have permission to do this");
+    }
 }
--- jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/asserts/displaydata/TooltipAssert.java
+++ jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/asserts/displaydata/TooltipAssert.java
@@ -3,6 +3,7 @@ package com.epam.jdi.light.material.asserts.displaydata;
 import com.epam.jdi.light.asserts.generic.UIAssert;
 import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.material.elements.displaydata.Tooltip;
+import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 
 import static com.epam.jdi.light.asserts.core.SoftAssert.jdiAssert;
@@ -12,6 +13,12 @@ public class TooltipAssert extends UIAssert<TooltipAssert, Tooltip> {
     @JDIAction("Assert that '{name}' text is '{0}'")
     public TooltipAssert text(String text) {
         jdiAssert(element().getValue(), Matchers.is(text));
+        return this;
+    }
+
+    @JDIAction("Assert that '{name}' text {0}")
+    public TooltipAssert text(Matcher<String> condition) {
+        jdiAssert(element().getValue(), condition);
         return this;
     }
 
