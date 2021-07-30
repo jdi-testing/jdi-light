c21fe17916d3bbfe1b8f0bbbc5e85070ed77f3b0
Igor.Gaiduk <igor.gaiduk@syngenta.com>
2021-07-30 18:03:39 +0300

    Initial commit for vuetify tests project

---
+++ jdi-light-vuetify-tests/src/main/java/io/github/com/StaticSite.java
@@ -0,0 +1,32 @@
+package io.github.com;
+import com.epam.jdi.light.elements.composite.Form;
+import com.epam.jdi.light.elements.pageobjects.annotations.JSite;
+import com.epam.jdi.light.elements.pageobjects.annotations.Url;
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.Css;
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
+import com.epam.jdi.light.ui.html.elements.common.Button;
+import com.epam.jdi.light.ui.html.elements.common.Text;
+import com.epam.jdi.tools.DataClass;
+import io.github.com.pages.VuetifyPage;
+
+@JSite("https://jdi-testing.github.io/jdi-light/")
+public class StaticSite {
+
+    public static User DEFAULT_USER = new User();
+
+    @Url("/vuetify.html")
+    public static VuetifyPage vuetifyPage;
+
+    @UI("#login-form")
+    public static Form<User> loginForm;
+
+    @Css(".logout")
+    public static Button logout;
+    public static Button userIcon;
+    public static Text userName;
+
+    public static class User extends DataClass<User> {
+        public String name = "Roman";
+        public String password = "Jdi1234";
+    }
+}
