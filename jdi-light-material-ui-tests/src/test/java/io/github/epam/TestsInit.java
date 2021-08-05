9f6eb46837cc0ca60f5ffb724fe5f4821e14cbe7
Natalia_Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2021-08-05 15:29:49 +0300

    change parallel

--- jdi-light-material-ui-tests/src/test/java/io/github/epam/TestsInit.java
+++ jdi-light-material-ui-tests/src/test/java/io/github/epam/TestsInit.java
@@ -7,7 +7,7 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Listeners;
 
 import static com.epam.jdi.light.driver.WebDriverUtils.killAllSeleniumDrivers;
-import static com.epam.jdi.light.elements.composite.WebPage.openSite;
+import static com.epam.jdi.light.elements.init.PageFactory.initSite;
 import static com.epam.jdi.light.settings.WebSettings.logger;
 
 @Listeners(TestNGListener.class)
@@ -15,8 +15,7 @@ public class TestsInit {
 
     @BeforeSuite(alwaysRun = true)
     public static void setUp() {
-        killAllSeleniumDrivers();
-        openSite(StaticSite.class);
+        initSite(StaticSite.class);
         logger.toLog("Run Tests");
     }
 
