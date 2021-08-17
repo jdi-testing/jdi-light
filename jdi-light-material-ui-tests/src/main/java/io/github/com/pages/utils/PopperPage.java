4e81aab469b7e7f702c6dcd76ce3e496e9065248
Rustam Saitov <78560640+strrust@users.noreply.github.com>
2021-08-17 12:54:15 +0300

    Popper element type changed in PopperPage, tests refactored based on new type

--- jdi-light-material-ui-tests/src/main/java/io/github/com/pages/utils/PopperPage.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/pages/utils/PopperPage.java
@@ -1,8 +1,8 @@
 package io.github.com.pages.utils;
 
-import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
+import com.epam.jdi.light.material.elements.utils.Popper;
 import com.epam.jdi.light.ui.html.elements.common.Button;
 
 import java.util.List;
@@ -12,5 +12,5 @@ public class PopperPage extends WebPage {
     public static List<Button> buttons;
 
     @UI("[role='tooltip']")
-    public static UIElement tooltip;
+    public static Popper popper;
 }
