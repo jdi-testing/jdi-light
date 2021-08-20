f217b3e61aa39ae619959a63b94aa130d81185ec
Khiem Cao <>
2021-08-20 09:38:10 +0700

    Update Button, Remove ButtonWithTooltip, update TooltipPage

--- jdi-light-material-ui-tests/src/main/java/io/github/com/pages/displaydata/TooltipPage.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/pages/displaydata/TooltipPage.java
@@ -2,35 +2,35 @@ package io.github.com.pages.displaydata;
 
 import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
-import com.epam.jdi.light.material.elements.inputs.ButtonWithTooltip;
+import com.epam.jdi.light.material.elements.inputs.Button;
 
 public class TooltipPage extends WebPage {
 
     @UI("//*[contains(@class, 'MuiIconButton-root')]")
-    public static ButtonWithTooltip deleteButtonWithTooltip;
+    public static Button deleteButtonWithTooltip;
 
     @UI("(//button[contains(@title, 'Add')])[1]")
-    public static ButtonWithTooltip addButtonWithTooltip;
+    public static Button addButtonWithTooltip;
 
     @UI("//h2[contains(text(), 'Customizable tooltips')]/following::button[1]")
-    public static ButtonWithTooltip buttonWithLightTooltip;
+    public static Button buttonWithLightTooltip;
 
     @UI("//h2[contains(text(), 'Customizable tooltips')]/following::button[2]")
-    public static ButtonWithTooltip buttonWithBootstrapTooltip;
+    public static Button buttonWithBootstrapTooltip;
 
     @UI("//h2[contains(text(), 'Customizable tooltips')]/following::button[3]")
-    public static ButtonWithTooltip buttonWithHtmlTooltip;
+    public static Button buttonWithHtmlTooltip;
 
     @UI("//div[@id='touchItem']/button")
-    public static ButtonWithTooltip hoverOrTouchButtonWithTooltip;
+    public static Button hoverOrTouchButtonWithTooltip;
 
     @UI("//div[@id='hoverItem']/button")
-    public static ButtonWithTooltip hoverButtonWithTooltip;
+    public static Button hoverButtonWithTooltip;
 
     @UI("//div[@id='clickItem']/*/button")
-    public static ButtonWithTooltip clickButtonWithTooltip;
+    public static Button clickButtonWithTooltip;
 
     @UI("#disabledBtn")
-    public static ButtonWithTooltip disabledButtonWithTooltip;
+    public static Button disabledButtonWithTooltip;
 
 }
