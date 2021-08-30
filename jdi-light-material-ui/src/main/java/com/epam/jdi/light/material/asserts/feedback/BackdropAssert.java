0f7ce0a3def61849fc8bc4cca58273083246ff54
Khiem Cao <>
2021-08-30 19:35:27 +0700

    Update Backdrop component

--- jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/asserts/feedback/BackdropAssert.java
+++ jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/asserts/feedback/BackdropAssert.java
@@ -1,23 +1,8 @@
 package com.epam.jdi.light.material.asserts.feedback;
 
 import com.epam.jdi.light.asserts.generic.UIAssert;
-import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.material.elements.feedback.Backdrop;
-import org.hamcrest.Matchers;
-
-import static com.epam.jdi.light.asserts.core.SoftAssert.jdiAssert;
 
 public class BackdropAssert extends UIAssert<BackdropAssert, Backdrop> {
 
-    @JDIAction("Assert that '{name}' text is visible")
-    public BackdropAssert visible() {
-        jdiAssert(element().isVisible(), Matchers.is(true));
-        return this;
-    }
-
-    @JDIAction("Assert that '{name}' text is hidden")
-    public BackdropAssert hidden() {
-        jdiAssert(element().isHidden(), Matchers.is(true));
-        return this;
-    }
 }
--- jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/elements/feedback/Backdrop.java
+++ jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/elements/feedback/Backdrop.java
@@ -1,13 +1,13 @@
 package com.epam.jdi.light.material.elements.feedback;
 
-import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.elements.base.UIBaseElement;
+import com.epam.jdi.light.elements.interfaces.base.HasClick;
 import com.epam.jdi.light.material.asserts.feedback.BackdropAssert;
 
-public class Backdrop extends UIBaseElement<BackdropAssert> {
+public class Backdrop extends UIBaseElement<BackdropAssert> implements HasClick {
 
-    @JDIAction("Click '{name}'")
-    public void click() {
-        core().click();
+    @Override
+    public BackdropAssert is() {
+        return new BackdropAssert().set(this);
     }
 }
