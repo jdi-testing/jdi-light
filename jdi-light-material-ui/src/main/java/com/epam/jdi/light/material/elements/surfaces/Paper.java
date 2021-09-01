9044a388a4c86e79eea444a92f4d2d6d7905541d
Khiem Cao <>
2021-09-01 14:50:31 +0700

    Update Paper component

--- jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/elements/surfaces/Paper.java
+++ jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/elements/surfaces/Paper.java
@@ -1,23 +1,14 @@
 package com.epam.jdi.light.material.elements.surfaces;
 
-import com.epam.jdi.light.common.JDIAction;
+import com.epam.jdi.light.asserts.generic.TextAssert;
 import com.epam.jdi.light.elements.base.UIBaseElement;
-import com.epam.jdi.light.material.asserts.surfaces.PaperAssert;
+import com.epam.jdi.light.elements.interfaces.base.HasClick;
+import com.epam.jdi.light.elements.interfaces.common.IsText;
 
-public class Paper extends UIBaseElement<PaperAssert> {
+public class Paper extends UIBaseElement<TextAssert> implements IsText, HasClick {
 
     @Override
-    public PaperAssert is() {
-        return new PaperAssert().set(this);
-    }
-
-    @JDIAction("Click '{name}'")
-    public void click() {
-        core().click();
-    }
-
-    @JDIAction("Get text of '{name}'")
-    public String getText() {
-        return core().getText();
+    public TextAssert is() {
+        return new TextAssert().set(this);
     }
 }
