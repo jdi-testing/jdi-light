7438aba697949a2b8c360001e704ba3c46fe02f8
Liliia_Iskanderova <iskanderovalilya@mail.ru>
2021-09-08 07:55:32 +0300

    3227 Some more unused imports removed

--- jdi-light-material-ui-tests/src/main/java/io/github/com/pages/feedback/BackdropPage.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/pages/feedback/BackdropPage.java
@@ -1,6 +1,5 @@
 package io.github.com.pages.feedback;
 
-import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.material.elements.feedback.ProgressBar;
