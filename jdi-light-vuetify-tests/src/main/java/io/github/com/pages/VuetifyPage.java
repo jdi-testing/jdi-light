d629ee5ca5c2dcd1ecdd8c0657e5f67ac03d460c
Liliia Iskanderova <iskanderovalilya@mail.ru>
2021-09-15 15:01:28 +0300

    Badge unused imports removed

--- jdi-light-vuetify-tests/src/main/java/io/github/com/pages/VuetifyPage.java
+++ jdi-light-vuetify-tests/src/main/java/io/github/com/pages/VuetifyPage.java
@@ -1,8 +1,6 @@
 package io.github.com.pages;
 
-import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.light.elements.composite.WebPage;
-import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 
 public class VuetifyPage extends WebPage {
 }
