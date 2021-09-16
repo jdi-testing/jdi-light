86ca2700aa98eb7e98f2bd95034183f551dac176
Liliia Iskanderova <iskanderovalilya@mail.ru>
2021-09-16 15:45:50 +0300

    Dividers: unused import removed

--- jdi-light-vuetify-tests/src/main/java/io/github/com/pages/DividersPage.java
+++ jdi-light-vuetify-tests/src/main/java/io/github/com/pages/DividersPage.java
@@ -1,6 +1,5 @@
 package io.github.com.pages;
 
-import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.vuetify.elements.common.Divider;
 
