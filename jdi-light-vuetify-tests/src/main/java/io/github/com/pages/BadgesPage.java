6a43099df518ce5c24c8b9c910a100ec739329e3
Liliia Iskanderova <iskanderovalilya@mail.ru>
2021-09-16 12:33:37 +0300

    Badges: changes from develop branch are merged

--- jdi-light-vuetify-tests/src/main/java/io/github/com/pages/BadgesPage.java
+++ jdi-light-vuetify-tests/src/main/java/io/github/com/pages/BadgesPage.java
@@ -1,6 +1,5 @@
 package io.github.com.pages;
 
-import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.WaitAfterAction;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.ui.html.elements.common.Button;
@@ -9,7 +8,7 @@ import com.epam.jdi.light.vuetify.elements.common.Badge;
 
 import java.util.List;
 
-public class BadgesPage extends WebPage {
+public class BadgesPage extends VuetifyPage {
 
     @UI("#simpleBadges .v-badge__badge")
     public static List<Badge> simpleBadges;
