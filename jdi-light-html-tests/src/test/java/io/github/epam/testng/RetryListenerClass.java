400e0338fb7f92539be8229ce31b18cbc9678773
RomanIovlev <roman.iovlev.jdi@gmail.com>
2021-09-24 12:51:32 +0200

    merge with master

--- jdi-light-html-tests/src/test/java/io/github/epam/testng/RetryListenerClass.java
+++ jdi-light-html-tests/src/test/java/io/github/epam/testng/RetryListenerClass.java
@@ -11,14 +11,9 @@ public class RetryListenerClass implements IAnnotationTransformer {
 
     @Override
     public void transform(ITestAnnotation testAnnotation, Class testClass, Constructor testConstructor, Method testMethod)	{
-<<<<<<< HEAD
         IRetryAnalyzer retry = testAnnotation.getRetryAnalyzer();
-=======
-        Class<? extends IRetryAnalyzer> retry = testAnnotation.getRetryAnalyzerClass();
->>>>>>> 32f04ff0abf700608512f6bbe3b8e21628b1121b
         if (retry == null)	{
             testAnnotation.setRetryAnalyzer(RetryFailedTestCases.class);
         }
-
     }
 }
