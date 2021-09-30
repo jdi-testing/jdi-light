a738303e17b5bdcfca877e6fa2d5423fdb4814b6
Liliia Iskanderova <iskanderovalilya@mail.ru>
2021-09-30 17:45:49 +0300

    Unused imports removed

--- jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/complex/AutocompletesTests.java
+++ jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/complex/AutocompletesTests.java
@@ -2,7 +2,6 @@ package io.github.epam.vuetify.tests.complex;
 
 import io.github.epam.TestsInit;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
