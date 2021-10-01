179b7d3a4da0638f7ecbc2051351d75939374c85
Robert Andreev <andreev.robert.a@gmail.com>
2021-10-01 11:42:23 +0300

    Add isOpened assert for paginationPage in PaginationTests

--- jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/complex/PaginationTests.java
+++ jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/complex/PaginationTests.java
@@ -26,6 +26,7 @@ public class PaginationTests extends TestsInit {
     @BeforeClass
     public void before() {
         paginationPage.open();
+        paginationPage.isOpened();
     }
 
     @Test
