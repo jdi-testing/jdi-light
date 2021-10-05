1e17d2f37575fb1e780963801e5c2633a570de27
Konstantin Bulavin <konstantin_bulavin@epam.com>
2021-10-05 11:00:22 +0300

    Ready for review

--- jdi-light-vuetify-tests/src/main/java/io/github/com/pages/SwitchesPage.java
+++ jdi-light-vuetify-tests/src/main/java/io/github/com/pages/SwitchesPage.java
@@ -1,5 +1,35 @@
 package io.github.com.pages;
 
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
+import com.epam.jdi.light.ui.html.elements.common.Text;
+import com.epam.jdi.light.vuetify.elements.common.Switch;
+
+import java.util.List;
+
 public class SwitchesPage extends VuetifyPage {
+
+    @UI("#ColorsSwitch .v-input--switch")
+    public static List<Switch> colorSwitches;
+
+    @UI("#FlatSwitch .v-input--switch")
+    public static List<Switch> flatSwitches;
+
+    @UI("#InsetSwitch .v-input--switch")
+    public static List<Switch> insetSwitches;
+
+    @UI("#ModelAsArraySwitch .v-input--switch")
+    public static List<Switch> modelAsArraySwitches;
+
+    @UI("#ModelAsArraySwitch > p")
+    public static Text modelArray;
+
+    @UI("#ModelAsBooleanSwitch .v-input--switch")
+    public static List<Switch> modelAsBooleanSwitches;
+
+    @UI("#StatesSwitch .v-input--switch")
+    public static List<Switch> statesSwitches;
+
+    @UI("#LabelSwitch .v-input--switch")
+    public static List<Switch> labelSwitches;
 
 }
--- jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/common/SwitchTests.java
+++ jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/common/SwitchTests.java
@@ -6,14 +6,14 @@ import io.github.epam.TestsInit;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import static io.github.com.StaticSite.switchPage;
-import static io.github.com.pages.SwitchPage.*;
+import static io.github.com.StaticSite.switchesPage;
+import static io.github.com.pages.SwitchesPage.*;
 
 public class SwitchTests extends TestsInit {
 
     @BeforeClass
     public void before() {
-        switchPage.open();
+        switchesPage.open();
     }
 
     @Test
