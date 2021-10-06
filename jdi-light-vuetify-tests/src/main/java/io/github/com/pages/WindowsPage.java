641f40848abf7da8fef306832e175839ba230103
Robert Andreev <andreev.robert.a@gmail.com>
2021-10-06 23:27:48 +0300

    Add first 4 tests for WindowsTests

--- jdi-light-vuetify-tests/src/main/java/io/github/com/pages/WindowsPage.java
+++ jdi-light-vuetify-tests/src/main/java/io/github/com/pages/WindowsPage.java
@@ -1,5 +1,6 @@
 package io.github.com.pages;
 
+import com.epam.jdi.light.elements.complex.WebList;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.ui.html.elements.common.Button;
 import com.epam.jdi.light.vuetify.elements.complex.Windows;
@@ -10,22 +11,46 @@ public class WindowsPage extends VuetifyPage {
     @UI("#ReverseWindow .v-window")
     public static Windows<SlideWindow> reverseWindows;
 
-    @UI("#ReverseWindow button[5]")
-    public static Button next;
+    @UI("#ReverseWindow .v-card__actions > button:first-of-type")
+    public static Button reverseBack;
+
+    @UI("#ReverseWindow .v-item-group button")
+    public static WebList reverseNavigation;
+
+    @UI("#ReverseWindow .v-card__actions > button:last-of-type")
+    public static Button reverseNext;
 
     @UI("#VerticalWindow .v-window")
     public static Windows<SlideWindow> verticalWindows;
 
+    @UI("#VerticalWindow .v-card__actions > button:first-of-type")
+    public static Button verticalBack;
+
+    @UI("#VerticalWindow .v-item-group button")
+    public static WebList verticalNavigation;
+
+    @UI("#VerticalWindow .v-card__actions > button:last-of-type")
+    public static Button verticalNext;
+
     @UI("#CustomizedArrowsWindow.v-window")
     public static Windows<SlideWindow> customizedArrowsWindows;
+
+    @UI("#CustomizedArrowsWindow .v-window__prev button")
+    public static Button previousSlide;
+
+    @UI("#CustomizedArrowsWindow .v-window__next button")
+    public static Button nextSlide;
 
     @UI("#AccountCreationWindow .v-window")
     public static Windows<?> accountCreationWindows;
 
-    @UI("#AccountCreationWindow button[2]")
-    public static Button next2;
+    @UI("#AccountCreationWindow .v-card__actions > button:first-of-type")
+    public static Button accountBack;
 
-    @UI("#OnboardingWindow .v-window")
-    public static Windows<SlideWindow> onboardingWindows;
+    @UI("#AccountCreationWindow .v-card__actions > button:last-of-type")
+    public static Button accountNext;
+
+//    @UI("#OnboardingWindow .v-window")
+//    public static Windows<SlideWindow> onboardingWindows;
 
 }
--- jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/complex/WindowsTests.java
+++ jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/complex/WindowsTests.java
@@ -1,11 +1,14 @@
 package io.github.epam.vuetify.tests.complex;
 
+import com.epam.jdi.light.elements.common.UIElement;
+import io.github.com.custom.windows.AccountCreatedWindow;
+import io.github.com.custom.windows.PasswordCreationWindow;
 import io.github.com.custom.windows.SignUpWindow;
-import io.github.com.custom.windows.SlideWindow;
 import io.github.epam.TestsInit;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import static com.epam.jdi.light.vuetify.elements.enums.Colors.*;
 import static io.github.com.StaticSite.windowsPage;
 import static io.github.com.pages.WindowsPage.*;
 
@@ -18,33 +21,78 @@ public class WindowsTests extends TestsInit {
     }
 
     @Test
-    public void sameSectionTest() {
-        reverseWindows.getActive().header().has().text("Slide 1");
-        next.click();
-        reverseWindows.getActive().header().has().text("Slide 2");
-        next.click();
-        reverseWindows.getActive().has().text("Slide 3");
-        next.click();
+    public void reverseWindowsTest() {
+        int i = 1;
+        for (UIElement nav : reverseNavigation) {
+            nav.click();
+            reverseWindows.getActive().header().has().text("Slide " + i);
+            reverseWindows.getActive().header().has().css("color", WHITE.value());
+            reverseWindows.getActive().sheet().has().css("background-color", GREY.value());
+            i++;
+        }
+        for (i = 3; i >= 1; i--) {
+            reverseWindows.getActive().header().has().text("Slide " + i);
+            reverseWindows.getActive().header().has().css("color", WHITE.value());
+            reverseWindows.getActive().sheet().has().css("background-color", GREY.value());
+            reverseBack.click();
+        }
+    }
 
-        reverseWindows.getActive(SlideWindow.class).has().text("Slide 1");
-        next.click();
-        reverseWindows.getActive(SlideWindow.class).has().text("Slide 2");
-        next.click();
-        reverseWindows.getActive(SlideWindow.class).has().text("Slide 3");
+    @Test
+    public void verticalWindowsTest() {
+        int i = 1;
+        for (UIElement nav : verticalNavigation) {
+            nav.click();
+            verticalWindows.getActive().header().has().text("Slide " + i);
+            verticalWindows.getActive().header().has().css("color", WHITE.value());
+            verticalWindows.getActive().sheet().has().css("background-color", GREY.value());
+            i++;
+        }
+        verticalNext.click();
+        verticalWindows.getActive().header().has().text("Slide 1");
+        verticalBack.click();
+        verticalWindows.getActive().header().has().text("Slide 3");
     }
 
     @Test
     public void customizedArrowsWindowsTest() {
-        customizedArrowsWindows.getActive();
+        customizedArrowsWindows.hover();
+        previousSlide.is().hidden();
+        nextSlide.is().visible();
+        customizedArrowsWindows.getActive().header().has().text("Slide 1");
+        customizedArrowsWindows.getActive().header().has().css("color", WHITE.value());
+        customizedArrowsWindows.getActive().sheet().has().css("background-color", GREY.value());
+
+        int i;
+        for (i = 2; i <= 5; i++) {
+            nextSlide.click();
+            customizedArrowsWindows.getActive().header().has().text("Slide " + i);
+            customizedArrowsWindows.getActive().header().has().css("color", WHITE.value());
+            customizedArrowsWindows.getActive().sheet().has().css("background-color", GREY.value());
+        }
+        nextSlide.is().hidden();
     }
 
     @Test
-    public void baseTest() {
-        accountCreationWindows.getActive(SignUpWindow.class).email().sendKeys("2");
-        System.out.println(accountCreationWindows.getActive(SignUpWindow.class).label().getValue());
-        accountCreationWindows.getActive(SignUpWindow.class).email().has().text("john@vuetifyjs.com2");
-        next2.click();
-        next2.click();
+    public void accountCreationWindowsTest() {
+        SignUpWindow signUpWindow = accountCreationWindows.getActive(SignUpWindow.class);
+        signUpWindow.email().has().value("john@vuetifyjs.com");
+        signUpWindow.email().label().has().text("Email");
+        signUpWindow.caption().has().text("This is the email you will use to login to your Vuetify account");
+        accountNext.click();
+
+        PasswordCreationWindow passwordCreationWindow = accountCreationWindows.getActive(PasswordCreationWindow.class);
+        passwordCreationWindow.password().label().has().text("Password");
+        passwordCreationWindow.confirmPassword().label().has().text("Confirm Password");
+        passwordCreationWindow.caption().has().text("Please enter a password for your account");
+        accountNext.click();
+
+        AccountCreatedWindow accountCreatedWindow = accountCreationWindows.getActive(AccountCreatedWindow.class);
+        System.out.println(accountCreatedWindow.image().height());
+        accountCreatedWindow.welcome().has().text("Welcome to Vuetify");
+        accountCreatedWindow.caption().has().text("Thanks for signing up!");
+
+
     }
 
 
