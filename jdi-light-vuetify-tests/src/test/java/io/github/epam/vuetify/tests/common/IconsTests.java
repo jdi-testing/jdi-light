e75c1707442ead4e5b140d191759e2fe90326bce
Aleksandr_Kashin2 <aleksandr_kashin2@epam.com>
2021-10-07 15:13:37 +0300

    added data provider

--- jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/common/IconsTests.java
+++ jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/common/IconsTests.java
@@ -1,17 +1,10 @@
 package io.github.epam.vuetify.tests.common;
 
 import io.github.epam.TestsInit;
+import io.github.epam.vuetify.tests.data.IconsTestsDataProvider;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import static com.epam.jdi.light.vuetify.elements.enums.Colors.BLUE_DARKEN_2;
-import static com.epam.jdi.light.vuetify.elements.enums.Colors.BLUE_GREY_DARKEN_2;
-import static com.epam.jdi.light.vuetify.elements.enums.Colors.BLUE_LIGHTEN_2;
-import static com.epam.jdi.light.vuetify.elements.enums.Colors.GREEN_DARKEN_2;
-import static com.epam.jdi.light.vuetify.elements.enums.Colors.ORANGE_DARKEN_2;
-import static com.epam.jdi.light.vuetify.elements.enums.Colors.PURPLE_DARKEN_2;
-import static com.epam.jdi.light.vuetify.elements.enums.Colors.RED_LIGHTEN_2;
-import static com.epam.jdi.light.vuetify.elements.enums.Colors.TEAL_DARKEN_2;
 import static com.epam.jdi.light.vuetify.elements.enums.Colors.WHITE;
 import static io.github.com.StaticSite.iconsPage;
 import static io.github.com.pages.IconsPage.buttonsIcons;
@@ -28,43 +21,14 @@ public class IconsTests extends TestsInit {
         iconsPage.open();
     }
 
-    @Test
-    public void colorIconsTests() throws Exception {
-        colorIcons.get(1).is().displayed();
-        colorIcons.get(1).has().type("mdi-domain");
-        colorIcons.get(1).has().color(GREEN_DARKEN_2.value());
-        colorIcons.get(1).has().height("36px");
-        colorIcons.get(1).has().width("36px");
-
-        colorIcons.get(2).is().displayed();
-        colorIcons.get(2).has().type("mdi-message-text");
-        colorIcons.get(2).has().color(BLUE_DARKEN_2.value());
-        colorIcons.get(2).has().height("36px");
-        colorIcons.get(2).has().width("36px");
-
-        colorIcons.get(3).is().displayed();
-        colorIcons.get(3).has().type("mdi-dialpad");
-        colorIcons.get(3).has().color(PURPLE_DARKEN_2.value());
-        colorIcons.get(3).has().height("36px");
-        colorIcons.get(3).has().width("36px");
-
-        colorIcons.get(4).is().displayed();
-        colorIcons.get(4).has().type("mdi-email");
-        colorIcons.get(4).has().color(TEAL_DARKEN_2.value());
-        colorIcons.get(4).has().height("36px");
-        colorIcons.get(4).has().width("36px");
-
-        colorIcons.get(5).is().displayed();
-        colorIcons.get(5).has().type("mdi-call-split");
-        colorIcons.get(5).has().color(BLUE_GREY_DARKEN_2.value());
-        colorIcons.get(5).has().height("36px");
-        colorIcons.get(5).has().width("36px");
-
-        colorIcons.get(6).is().displayed();
-        colorIcons.get(6).has().type("mdi-arrow-up-bold-box-outline");
-        colorIcons.get(6).has().color(ORANGE_DARKEN_2.value());
-        colorIcons.get(6).has().height("36px");
-        colorIcons.get(6).has().width("36px");
+    @Test(dataProvider = "colorIconsTestsDataProvider", dataProviderClass = IconsTestsDataProvider.class)
+    public void colorIconsTests(int number, String iconType, String color,
+                                String height, String width) throws Exception {
+        colorIcons.get(number).is().displayed();
+        colorIcons.get(number).has().type(iconType);
+        colorIcons.get(number).has().color(color);
+        colorIcons.get(number).has().height(height);
+        colorIcons.get(number).has().width(width);
     }
 
     @Test
@@ -79,242 +43,40 @@ public class IconsTests extends TestsInit {
         clickIcon.handleAlert();
     }
 
-    @Test
-    public void buttonsIconsTests() throws Exception {
-        buttonsIcons.get(1).is().displayed();
-        buttonsIcons.get(1).is().clickable();
-        buttonsIcons.get(1).has().type("mdi-checkbox-marked-circle");
-        buttonsIcons.get(1).has().color(WHITE.value());
-        buttonsIcons.get(1).has().height("18px");
-        buttonsIcons.get(1).has().width("18px");
-
-        buttonsIcons.get(2).is().displayed();
-        buttonsIcons.get(2).is().clickable();
-        buttonsIcons.get(2).has().type("mdi-cancel");
-        buttonsIcons.get(2).has().color(WHITE.value());
-        buttonsIcons.get(2).has().height("18px");
-        buttonsIcons.get(2).has().width("18px");
-
-        buttonsIcons.get(3).is().displayed();
-        buttonsIcons.get(3).is().clickable();
-        buttonsIcons.get(3).has().type("mdi-minus-circle");
-        buttonsIcons.get(3).has().color(WHITE.value());
-        buttonsIcons.get(3).has().height("18px");
-        buttonsIcons.get(3).has().width("18px");
-
-        buttonsIcons.get(4).is().displayed();
-        buttonsIcons.get(4).is().clickable();
-        buttonsIcons.get(4).has().type("mdi-arrow-left");
-        buttonsIcons.get(4).has().color(WHITE.value());
-        buttonsIcons.get(4).has().height("18px");
-        buttonsIcons.get(4).has().width("18px");
-
-        buttonsIcons.get(5).is().displayed();
-        buttonsIcons.get(5).is().clickable();
-        buttonsIcons.get(5).has().type("mdi-wrench");
-        buttonsIcons.get(5).has().color(WHITE.value());
-        buttonsIcons.get(5).has().height("24px");
-        buttonsIcons.get(5).has().width("24px");
-
-        buttonsIcons.get(6).is().displayed();
-        buttonsIcons.get(6).is().clickable();
-        buttonsIcons.get(6).has().type("mdi-cloud-upload");
-        buttonsIcons.get(6).has().color(WHITE.value());
-        buttonsIcons.get(6).has().height("24px");
-        buttonsIcons.get(6).has().width("24px");
-
-        buttonsIcons.get(7).is().displayed();
-        buttonsIcons.get(7).is().clickable();
-        buttonsIcons.get(7).has().type("mdi-thumb-up");
-        buttonsIcons.get(7).has().color(BLUE_LIGHTEN_2.value());
-        buttonsIcons.get(7).has().height("24px");
-        buttonsIcons.get(7).has().width("24px");
-
-        buttonsIcons.get(8).is().displayed();
-        buttonsIcons.get(8).is().clickable();
-        buttonsIcons.get(8).has().type("mdi-thumb-down");
-        buttonsIcons.get(8).has().color(RED_LIGHTEN_2.value());
-        buttonsIcons.get(8).has().height("24px");
-        buttonsIcons.get(8).has().width("24px");
+    @Test(dataProvider = "buttonsIconsTestsDataProvider", dataProviderClass = IconsTestsDataProvider.class)
+    public void buttonsIconsTests(int number, String iconType, String color,
+                                  String height, String width) throws Exception {
+        buttonsIcons.get(number).is().displayed();
+        buttonsIcons.get(number).is().clickable();
+        buttonsIcons.get(number).has().type(iconType);
+        buttonsIcons.get(number).has().color(color);
+        buttonsIcons.get(number).has().height(height);
+        buttonsIcons.get(number).has().width(width);
     }
 
-    @Test
-    public void fontAwesomeIconsTests() throws Exception {
-        fontAwesomeIcons.get(1).is().displayed();
-        fontAwesomeIcons.get(1).has().type("fa-lock");
-        fontAwesomeIcons.get(1).has().height("24px");
-        fontAwesomeIcons.get(1).has().width("21px");
-
-        fontAwesomeIcons.get(2).is().displayed();
-        fontAwesomeIcons.get(2).has().type("fa-search");
-        fontAwesomeIcons.get(2).has().height("24px");
-        fontAwesomeIcons.get(2).has().width("24px");
-
-        fontAwesomeIcons.get(3).is().displayed();
-        fontAwesomeIcons.get(3).has().type("fa-list");
-        fontAwesomeIcons.get(3).has().height("24px");
-        fontAwesomeIcons.get(3).has().width("24px");
-
-        fontAwesomeIcons.get(4).is().displayed();
-        fontAwesomeIcons.get(4).has().type("fa-edit");
-        fontAwesomeIcons.get(4).has().height("24px");
-        fontAwesomeIcons.get(4).has().width("27px");
-
-        fontAwesomeIcons.get(5).is().displayed();
-        fontAwesomeIcons.get(5).has().type("fa-tachometer-alt");
-        fontAwesomeIcons.get(5).has().height("24px");
-        fontAwesomeIcons.get(5).has().width("27px");
-
-        fontAwesomeIcons.get(6).is().displayed();
-        fontAwesomeIcons.get(6).has().type("fa-circle-notchfa-spin");
-        fontAwesomeIcons.get(6).has().height("24px");
-        fontAwesomeIcons.get(6).has().width("24px");
+    @Test(dataProvider = "fontAwesomeIconsTestsDataProvider", dataProviderClass = IconsTestsDataProvider.class)
+    public void fontAwesomeIconsTests(int number, String iconType, String height, String width) throws Exception {
+        fontAwesomeIcons.get(number).is().displayed();
+        fontAwesomeIcons.get(number).has().type(iconType);
+        fontAwesomeIcons.get(number).has().height(height);
+        fontAwesomeIcons.get(number).has().width(width);
     }
 
-    @Test
-    public void materialDesignIconsTests() throws Exception {
-        materialDesignIcons.get(1).is().displayed();
-        materialDesignIcons.get(1).has().type("home");
-        materialDesignIcons.get(1).has().height("16px");
-        materialDesignIcons.get(1).has().width("16px");
-
-        materialDesignIcons.get(2).is().displayed();
-        materialDesignIcons.get(2).has().type("event");
-        materialDesignIcons.get(2).has().height("16px");
-        materialDesignIcons.get(2).has().width("16px");
-
-        materialDesignIcons.get(3).is().displayed();
-        materialDesignIcons.get(3).has().type("info");
-        materialDesignIcons.get(3).has().height("16px");
-        materialDesignIcons.get(3).has().width("16px");
-
-        materialDesignIcons.get(4).is().displayed();
-        materialDesignIcons.get(4).has().type("folder_open");
-        materialDesignIcons.get(4).has().height("16px");
-        materialDesignIcons.get(4).has().width("16px");
-
-        materialDesignIcons.get(5).is().displayed();
-        materialDesignIcons.get(5).has().type("widgets");
-        materialDesignIcons.get(5).has().height("16px");
-        materialDesignIcons.get(5).has().width("16px");
-
-        materialDesignIcons.get(6).is().displayed();
-        materialDesignIcons.get(6).has().type("gavel");
-        materialDesignIcons.get(6).has().height("16px");
-        materialDesignIcons.get(6).has().width("16px");
-
-        materialDesignIcons.get(7).is().displayed();
-        materialDesignIcons.get(7).has().type("home");
-        materialDesignIcons.get(7).has().height("24px");
-        materialDesignIcons.get(7).has().width("24px");
-
-        materialDesignIcons.get(8).is().displayed();
-        materialDesignIcons.get(8).has().type("event");
-        materialDesignIcons.get(8).has().height("24px");
-        materialDesignIcons.get(8).has().width("24px");
-
-        materialDesignIcons.get(9).is().displayed();
-        materialDesignIcons.get(9).has().type("info");
-        materialDesignIcons.get(9).has().height("24px");
-        materialDesignIcons.get(9).has().width("24px");
-
-        materialDesignIcons.get(10).is().displayed();
-        materialDesignIcons.get(10).has().type("folder_open");
-        materialDesignIcons.get(10).has().height("24px");
-        materialDesignIcons.get(10).has().width("24px");
-
-        materialDesignIcons.get(11).is().displayed();
-        materialDesignIcons.get(11).has().type("widgets");
-        materialDesignIcons.get(11).has().height("24px");
-        materialDesignIcons.get(11).has().width("24px");
-
-        materialDesignIcons.get(12).is().displayed();
-        materialDesignIcons.get(12).has().type("gavel");
-        materialDesignIcons.get(12).has().height("24px");
-        materialDesignIcons.get(12).has().width("24px");
-
-        materialDesignIcons.get(13).is().displayed();
-        materialDesignIcons.get(13).has().type("home");
-        materialDesignIcons.get(13).has().height("36px");
-        materialDesignIcons.get(13).has().width("36px");
-
-        materialDesignIcons.get(14).is().displayed();
-        materialDesignIcons.get(14).has().type("event");
-        materialDesignIcons.get(14).has().height("36px");
-        materialDesignIcons.get(14).has().width("36px");
-
-        materialDesignIcons.get(15).is().displayed();
-        materialDesignIcons.get(15).has().type("info");
-        materialDesignIcons.get(15).has().height("36px");
-        materialDesignIcons.get(15).has().width("36px");
-
-        materialDesignIcons.get(16).is().displayed();
-        materialDesignIcons.get(16).has().type("folder_open");
-        materialDesignIcons.get(16).has().height("36px");
-        materialDesignIcons.get(16).has().width("36px");
-
-        materialDesignIcons.get(17).is().displayed();
-        materialDesignIcons.get(17).has().type("widgets");
-        materialDesignIcons.get(17).has().height("36px");
-        materialDesignIcons.get(17).has().width("36px");
-
-        materialDesignIcons.get(18).is().displayed();
-        materialDesignIcons.get(18).has().type("gavel");
-        materialDesignIcons.get(18).has().height("36px");
-        materialDesignIcons.get(18).has().width("36px");
-
-        materialDesignIcons.get(19).is().displayed();
-        materialDesignIcons.get(19).has().type("home");
-        materialDesignIcons.get(19).has().height("40px");
-        materialDesignIcons.get(19).has().width("40px");
-
-        materialDesignIcons.get(20).is().displayed();
-        materialDesignIcons.get(20).has().type("event");
-        materialDesignIcons.get(20).has().height("40px");
-        materialDesignIcons.get(20).has().width("40px");
-
-        materialDesignIcons.get(21).is().displayed();
-        materialDesignIcons.get(21).has().type("info");
-        materialDesignIcons.get(21).has().height("40px");
-        materialDesignIcons.get(21).has().width("40px");
-
-        materialDesignIcons.get(22).is().displayed();
-        materialDesignIcons.get(22).has().type("folder_open");
-        materialDesignIcons.get(22).has().height("40px");
-        materialDesignIcons.get(22).has().width("40px");
-
-        materialDesignIcons.get(23).is().displayed();
-        materialDesignIcons.get(23).has().type("widgets");
-        materialDesignIcons.get(23).has().height("40px");
-        materialDesignIcons.get(23).has().width("40px");
-
-        materialDesignIcons.get(24).is().displayed();
-        materialDesignIcons.get(24).has().type("gavel");
-        materialDesignIcons.get(24).has().height("40px");
-        materialDesignIcons.get(24).has().width("40px");
+    @Test(dataProvider = "materialDesignIconsTestsDataProvider", dataProviderClass = IconsTestsDataProvider.class)
+    public void materialDesignIconsTests(int number, String iconType, String height, String width) throws Exception {
+        materialDesignIcons.get(number).is().displayed();
+        materialDesignIcons.get(number).has().type(iconType);
+        materialDesignIcons.get(number).has().height(height);
+        materialDesignIcons.get(number).has().width(width);
     }
 
-    @Test
-    public void mdiSvgIconsTests() throws Exception {
-        mdiSvgIcons.get(1).is().displayed();
-        mdiSvgIcons.get(1).has().type("svg icon");
-        mdiSvgIcons.get(1).has().height("24px");
-        mdiSvgIcons.get(1).has().width("24px");
+    @Test(dataProvider = "mdiSvgIconsTestsDataProvider", dataProviderClass = IconsTestsDataProvider.class)
+    public void mdiSvgIconsTests(int number, String height, String width) throws Exception {
+        mdiSvgIcons.get(number).is().displayed();
+        mdiSvgIcons.get(number).has().type("svg icon");
+        mdiSvgIcons.get(number).has().height(height);
+        mdiSvgIcons.get(number).has().width(width);
 
-        mdiSvgIcons.get(2).is().displayed();
-        mdiSvgIcons.get(2).has().type("svg icon");
-        mdiSvgIcons.get(2).has().height("24px");
-        mdiSvgIcons.get(2).has().width("24px");
-
-        mdiSvgIcons.get(3).is().displayed();
-        mdiSvgIcons.get(3).has().type("svg icon");
-        mdiSvgIcons.get(3).has().height("24px");
-        mdiSvgIcons.get(3).has().width("24px");
-
-        mdiSvgIcons.get(4).is().displayed();
-        mdiSvgIcons.get(4).has().type("svg icon");
         mdiSvgIcons.get(4).has().color(WHITE.value());
-        mdiSvgIcons.get(4).has().height("18px");
-        mdiSvgIcons.get(4).has().width("18px");
     }
 }
