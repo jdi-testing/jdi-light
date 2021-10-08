b427962676c3df0472e895136656e6748a52d2a9
Liliia Iskanderova <iskanderovalilya@mail.ru>
2021-10-08 14:05:47 +0300

    Input: element developed, and tested

--- jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/asserts/InputAssert.java
+++ jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/asserts/InputAssert.java
@@ -15,8 +15,20 @@ public class InputAssert extends UIAssert<InputAssert, Input> {
     }
 
     @JDIAction("Assert that '{name}' is disabled")
-    public InputAssert textFromDisabled(String text) {
-        jdiAssert(element().getTextFromDisabled(), Matchers.equalTo(text));
+    public InputAssert textInSlot() {
+        jdiAssert(element().hasTextInSlot(), Matchers.is(true));
+        return this;
+    }
+
+    @JDIAction("Assert that '{name}' is disabled")
+    public InputAssert textInSlot(String text) {
+        jdiAssert(element().getTextInSlot(), Matchers.equalTo(text));
+        return this;
+    }
+
+    @JDIAction("Assert that '{name}' has text field")
+    public InputAssert textField() {
+        jdiAssert(element().hasTextField(), Matchers.is(true));
         return this;
     }
 
@@ -56,9 +68,81 @@ public class InputAssert extends UIAssert<InputAssert, Input> {
         return this;
     }
 
+    @JDIAction("Assert that '{name}' has error message")
+    public InputAssert errorMessage() {
+        jdiAssert(element().hasErrorMessage(), Matchers.is(true));
+        return this;
+    }
+
+    @JDIAction("Assert that '{name}' has not error message")
+    public InputAssert notErrorMessage() {
+        jdiAssert(element().hasErrorMessage(), Matchers.is(false));
+        return this;
+    }
+
     @JDIAction("Assert that '{name}' is loading")
     public InputAssert loading() {
         jdiAssert(element().isLoading(), Matchers.is(true));
+        return this;
+    }
+
+    @JDIAction("Assert that '{name}' is loading")
+    public InputAssert loaded() {
+        jdiAssert(element().isLoading(), Matchers.is(false));
+        return this;
+    }
+
+    @JDIAction("Assert that '{name}' has prepend outer")
+    public InputAssert prependOuter() {
+        jdiAssert(element().hasPrependOuter(), Matchers.is(true));
+        return this;
+    }
+
+    @JDIAction("Assert that '{name}' has prepend inner")
+    public InputAssert prependInner() {
+        jdiAssert(element().hasPrependInner(), Matchers.is(true));
+        return this;
+    }
+
+    @JDIAction("Assert that '{name}' has append outer")
+    public InputAssert appendOuter() {
+        jdiAssert(element().hasAppendOuter(), Matchers.is(true));
+        return this;
+    }
+
+    @JDIAction("Assert that '{name}' has append inner")
+    public InputAssert appendInner() {
+        jdiAssert(element().hasAppendInner(), Matchers.is(true));
+        return this;
+    }
+
+    @JDIAction("Assert that '{name}' has switch")
+    public InputAssert switchInput() {
+        jdiAssert(element().hasSwitch(), Matchers.is(true));
+        return this;
+    }
+
+    @JDIAction("Assert that {name}'s switch is checked")
+    public InputAssert checkedSwitch() {
+        jdiAssert(element().switchIsChecked(), Matchers.is(true));
+        return this;
+    }
+
+    @JDIAction("Assert that {name}'s switch is unchecked")
+    public InputAssert uncheckedSwitch() {
+        jdiAssert(element().switchIsChecked(), Matchers.is(false));
+        return this;
+    }
+
+    @JDIAction("Assert that '{name}' has attribute '{0}'")
+    public InputAssert className(String className) {
+        jdiAssert(element().hasClass(className), Matchers.is(true));
+        return this;
+    }
+
+    @JDIAction("Assert that '{name}' has not attribute '{0}'")
+    public InputAssert notClassName(String className) {
+        jdiAssert(element().hasAttribute(className), Matchers.is(false));
         return this;
     }
 }
--- jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/elements/common/Input.java
+++ jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/elements/common/Input.java
@@ -5,19 +5,29 @@ import com.epam.jdi.light.driver.get.OsTypes;
 import com.epam.jdi.light.elements.base.UIBaseElement;
 import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.light.vuetify.asserts.InputAssert;
+import com.epam.jdi.tools.Timer;
 import org.openqa.selenium.Keys;
 
 import static com.epam.jdi.light.driver.get.DriverData.getOs;
 
+/**
+ * To see an example of Input web element please visit https://vuetifyjs.com/en/components/inputs/
+ */
+
 public class Input extends UIBaseElement<InputAssert> {
 
-    private final String INPUT_FIELD = "div input[type='text']";
-    private final String TEXT_FROM_DISABLED = "div .v-input__slot";
     private final String LABEL = "div label";
+    private final String INPUT = "div input";
+    private final String SLOT = "div .v-input__slot";
     private final String MESSAGE = "div .v-messages__message";
+    private final String PREPEND_OUTER = ".v-input__prepend-outer";
+    private final String PREPEND_INNER = "div .v-input__prepend-inner";
+    private final String APPEND_OUTER = ".v-input__append-outer";
+    private final String APPEND_INNER = "div .v-input__append-inner";
+    private final String SWITCH_SELECTION_CONTROL = "div .v-input--selection-controls__ripple";
 
-    private UIElement inputField() {
-        return this.find(INPUT_FIELD);
+    private UIElement input() {
+        return this.find(INPUT);
     }
 
     private UIElement label() {
@@ -28,6 +38,26 @@ public class Input extends UIBaseElement<InputAssert> {
         return this.find(MESSAGE);
     }
 
+    private UIElement prependOuter() {
+        return this.find(PREPEND_OUTER);
+    }
+
+    private UIElement prependInner() {
+        return this.find(PREPEND_INNER);
+    }
+
+    private UIElement appendOuter() {
+        return this.find(APPEND_OUTER);
+    }
+
+    private UIElement appendInner() {
+        return this.find(APPEND_INNER);
+    }
+
+    private UIElement switchSelectionControl() {
+        return this.find(SWITCH_SELECTION_CONTROL);
+    }
+
     @Override
     public InputAssert is() {
         return new InputAssert().set(this);
@@ -35,35 +65,51 @@ public class Input extends UIBaseElement<InputAssert> {
 
     @JDIAction("Check that '{name}' is disabled")
     public boolean isDisabled() {
-        return this.hasAttribute("v-input--is-disabled");
+        return this.hasClass("v-input--is-disabled");
+    }
+
+    @JDIAction("Check that '{name}' has text field")
+    public boolean hasTextField() {
+        return this.input().attr("type").equals("text");
     }
 
     @JDIAction("Type text to {name}'s input field")
     public void typeText(String text) {
-        inputField().sendKeys(text);
+        input().sendKeys(text + Keys.ENTER);
+    }
+
+    @JDIAction("Clear {name}'s text field and type text to it")
+    public void clearAndTypeText(String text) {
+        this.clearTextField();
+        input().sendKeys(text + Keys.ENTER);
     }
 
     @JDIAction("Check that '{name}' has typed text in the text field")
     public boolean hasTypedText() {
-        return !inputField().getText().isEmpty();
+        return !input().getText().isEmpty();
     }
 
     @JDIAction("Get {name}'s typed text")
     public String getTypedText() {
-        return inputField().getText();
+        return input().getText();
     }
 
     @JDIAction("Get text from disabled '{name}'")
-    public String getTextFromDisabled() {
-        return this.find(TEXT_FROM_DISABLED).getText();
+    public boolean hasTextInSlot() {
+        return !this.find(SLOT).getText().isEmpty();
+    }
+
+    @JDIAction("Get text from disabled '{name}'")
+    public String getTextInSlot() {
+        return this.find(SLOT).getText();
     }
 
     @JDIAction("Clear {name}'s input field")
     public void clearTextField() {
         if (getOs().equals(OsTypes.MAC)) {
-            inputField().sendKeys(Keys.chord(Keys.COMMAND, "a", Keys.DELETE));
+            input().sendKeys(Keys.chord(Keys.COMMAND, "a", Keys.DELETE));
         } else {
-            inputField().sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+            input().sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
         }
     }
 
@@ -82,6 +128,11 @@ public class Input extends UIBaseElement<InputAssert> {
         return message().isExist();
     }
 
+    @JDIAction("Check that '{name}' has error message")
+    public boolean hasErrorMessage() {
+        return this.hasClass("error--text");
+    }
+
     @JDIAction("Get {name}'s message")
     public String getMessage() {
         return this.find(MESSAGE).getText();
@@ -89,6 +140,79 @@ public class Input extends UIBaseElement<InputAssert> {
 
     @JDIAction("Check that '{name}' is loading")
     public boolean isLoading() {
-        return this.hasAttribute("v-input--is-loading");
+        return this.hasClass("v-input--is-loading");
+    }
+
+    @JDIAction("Check that '{name}' has prepend outer")
+    public boolean hasPrependOuter() {
+        return new Timer(base().getTimeout() * 1000L)
+                .wait(() -> this.prependOuter().isExist());
+    }
+
+    @JDIAction("Click on {name}'s prepend outer")
+    public void clickOnPrependOuter() {
+        this.prependOuter().click();
+    }
+
+    @JDIAction("Check that '{name}' has prepend inner")
+    public boolean hasPrependInner() {
+        return new Timer(base().getTimeout() * 1000L)
+                .wait(() -> this.prependInner().isExist());
+    }
+
+    @JDIAction("Click on {name}'s prepend inner")
+    public void clickOnPrependInner() {
+        this.prependInner().click();
+    }
+
+    @JDIAction("Check that '{name}' has append outer")
+    public boolean hasAppendOuter() {
+        return new Timer(base().getTimeout() * 1000L)
+                .wait(() -> this.appendOuter().isExist());
+    }
+
+    @JDIAction("Click on {name}'s prepend outer")
+    public void clickOnAppendOuter() {
+        this.appendOuter().click();
+    }
+
+    @JDIAction("Check that '{name}' has append inner")
+    public boolean hasAppendInner() {
+        return new Timer(base().getTimeout() * 1000L)
+                .wait(() -> this.appendInner().isExist());
+    }
+
+    @JDIAction("Click on {name}'s prepend inner")
+    public void clickOnAppendInner() {
+        this.appendInner().click();
+    }
+
+    @JDIAction("Check that {name}'s switch is checked")
+    public boolean hasSwitch() {
+        return this.input().attr("role").equals("switch");
+    }
+
+    @JDIAction("Check that {name}'s switch is checked")
+    public boolean switchIsChecked() {
+        return this.input().attr("aria-checked").equals("true");
+    }
+
+    @JDIAction("Check {name}'s switch")
+    public void checkSwitch() {
+        if (!switchIsChecked()) {
+            this.switchSelectionControl().click();
+        }
+    }
+
+    @JDIAction("Check {name}'s switch")
+    public void uncheckSwitch() {
+        if (switchIsChecked()) {
+            this.switchSelectionControl().click();
+        }
+    }
+
+    @JDIAction("Check that '{name}' has class '{0}'")
+    public boolean hasClass(String className) {
+        return this.core().hasClass(className);
     }
 }
