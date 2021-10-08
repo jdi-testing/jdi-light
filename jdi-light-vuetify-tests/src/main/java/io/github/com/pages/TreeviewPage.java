dac172c6de6bdb1120d89bb43c28fb58a3abad88
Robert Andreev <andreev.robert.a@gmail.com>
2021-10-08 14:56:56 +0300

    Simplify activatableTreeViewTest in TreeViewTests

--- jdi-light-vuetify-tests/src/main/java/io/github/com/pages/TreeviewPage.java
+++ jdi-light-vuetify-tests/src/main/java/io/github/com/pages/TreeviewPage.java
@@ -1,14 +1,14 @@
 package io.github.com.pages;
 
+import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.light.vuetify.annotations.JDITreeView;
 import com.epam.jdi.light.vuetify.elements.complex.TreeView;
 import io.github.com.custom.treeview.CheckedNode;
-import io.github.com.custom.treeview.SimpleNode;
 
 public class TreeviewPage extends VuetifyPage {
 
     @JDITreeView(core = "#ActivatableTreeview")
-    public static TreeView<SimpleNode> activatableTreeView;
+    public static TreeView<UIElement> activatableTreeView;
 
     @JDITreeView(core = "#SelectableTreeview")
     public static TreeView<CheckedNode> selectableTreeView;
--- jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/complex/TreeViewTests.java
+++ jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/complex/TreeViewTests.java
@@ -1,14 +1,15 @@
 package io.github.epam.vuetify.tests.complex;
 
+import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.light.vuetify.elements.complex.TreeView;
 import io.github.com.custom.treeview.CheckedNode;
-import io.github.com.custom.treeview.SimpleNode;
 import io.github.epam.TestsInit;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static io.github.com.StaticSite.treeviewPage;
-import static io.github.com.pages.TreeviewPage.*;
+import static io.github.com.pages.TreeviewPage.activatableTreeView;
+import static io.github.com.pages.TreeviewPage.selectableTreeView;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
@@ -26,34 +27,35 @@ public class TreeViewTests extends TestsInit {
         assertThat(activatableTreeView.isPseudoRoot(), is(true));
         activatableTreeView.children().has().size(4);
 
-        TreeView<SimpleNode> app = activatableTreeView.getNode(1);
+        TreeView<UIElement> app = activatableTreeView.getNode(1);
         app.children().has().size(3);
-        app.root().text.has().text("Applications :");
-        app.getRoot(1).text.has().text("Calendar : app");
-        app.getRoot(2).text.has().text("Chrome : app");
-        app.getRoot(3).text.has().text("Webstorm : app");
+        app.root().has().text("Applications :");
+        app.getRoot(1).has().text("Calendar : app");
+        app.getRoot(2).has().text("Chrome : app");
+        app.getRoot(3).has().text("Webstorm : app");
 
-        TreeView<SimpleNode> doc = activatableTreeView.getNode(2);
-        doc.root().text.has().text("Documents :");
+        TreeView<UIElement> doc = activatableTreeView.getNode(2);
+        doc.root().has().text("Documents :");
         doc.children().has().size(2);
-        doc.getRoot(1).text.has().text("vuetify :");
-        doc.getRoot(2).text.has().text("material2 :");
+        doc.getRoot(1).has().text("vuetify :");
+        doc.getRoot(2).has().text("material2 :");
 
-        TreeView<SimpleNode> vue = doc.getNode(1);
+        TreeView<UIElement> vue = doc.getNode(1);
         vue.children().has().size(1);
-        vue.getRoot(1).text.has().text("src :");
+        vue.getRoot(1).has().text("src :");
 
-        TreeView<SimpleNode> mat = doc.getNode(1);
+        TreeView<UIElement> mat = doc.getNode(1);
         mat.children().has().size(1);
-        mat.getRoot(1).text.has().text("src :");
+        mat.getRoot(1).has().text("src :");
 
-        TreeView<SimpleNode> videos = activatableTreeView.getNode(4);
+        TreeView<UIElement> videos = activatableTreeView.getNode(4);
         videos.children().has().size(3);
-        videos.root().text.has().text("Videos :");
-        videos.getRoot(1).text.has().text("Tutorials :");
-        videos.getRoot(2).text.has().text("Intro : mov");
+
+        videos.root().has().text("Videos :");
+        videos.getRoot(1).has().text("Tutorials :");
+        videos.getRoot(2).has().text("Intro : mov");
         assertThat(videos.getNode(2).isLeaf(), is(true));
-        videos.getRoot(3).text.has().text("Conference introduction : avi");
+        videos.getRoot(3).has().text("Conference introduction : avi");
         assertThat(videos.getNode(3).isLeaf(), is(true));
     }
 
