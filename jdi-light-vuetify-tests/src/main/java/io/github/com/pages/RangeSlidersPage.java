43dc8362e08e273f6a8bf6696c107426c4d2d140
Anna_Vasileva2 <annav00@mail.ru>
2021-10-11 10:55:07 +0300

    create locators in range sliders page

--- jdi-light-vuetify-tests/src/main/java/io/github/com/pages/RangeSlidersPage.java
+++ jdi-light-vuetify-tests/src/main/java/io/github/com/pages/RangeSlidersPage.java
@@ -1,5 +1,22 @@
 package io.github.com.pages;
 
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
+import com.epam.jdi.light.vuetify.elements.common.RangeSlider;
+
 public class RangeSlidersPage extends VuetifyPage {
 
+    @UI("#DisabledRangeSlider .v-slider")
+    public static RangeSlider disabledRangeSlider;
+
+    @UI("#MinAndMaxRangeSlider .v-slider")
+    public static RangeSlider minAndMaxRangeSlider;
+
+    @UI("#StepRangeSlider .v-slider")
+    public static RangeSlider stepRangeSlider;
+
+    @UI("#VerticalSlidersRangeSlider .v-slider")
+    public static RangeSlider verticalSlidersRangeSlider;
+
+    @UI("#ThumbLabelRangeSlider .v-slider")
+    public static RangeSlider thumbLabelRangeSlider;
 }
