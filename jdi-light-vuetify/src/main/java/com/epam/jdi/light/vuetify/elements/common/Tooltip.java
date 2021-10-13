3eaeee91902e34343907258097dce4d5d9eddac3
Aleksandr_Kashin2 <aleksandr_kashin2@epam.com>
2021-10-13 15:18:29 +0300

    added tooltip description

--- jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/elements/common/Tooltip.java
+++ jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/elements/common/Tooltip.java
@@ -4,6 +4,10 @@ import com.epam.jdi.light.elements.base.UIBaseElement;
 import com.epam.jdi.light.elements.interfaces.common.IsText;
 import com.epam.jdi.light.vuetify.asserts.TooltipAssert;
 
+/**
+ * To see an example of Tooltip web element please visit https://vuetifyjs.com/en/components/tooltips/
+ */
+
 public class Tooltip extends UIBaseElement<TooltipAssert> implements IsText {
 
     public TooltipAssert is() {
