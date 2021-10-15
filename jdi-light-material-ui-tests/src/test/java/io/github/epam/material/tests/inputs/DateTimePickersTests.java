ab75ce0b06c2e30775c091b2e05e20ca8f1cf38e
Konstantin Nadonenko <konstantin.nadonenko@ukg.com>
2021-10-15 15:33:02 +0300

    imports fix

--- jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/inputs/DateTimePickersTests.java
+++ jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/inputs/DateTimePickersTests.java
@@ -3,7 +3,6 @@ package io.github.epam.material.tests.inputs;
 import io.github.epam.TestsInit;
 import static org.hamcrest.Matchers.containsString;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import static io.github.com.StaticSite.dateTimePickersPage;
