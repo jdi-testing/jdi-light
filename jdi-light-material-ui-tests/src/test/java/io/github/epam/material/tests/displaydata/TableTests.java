c2660d09018014e765834d08853e17fc449fc8ed
Evgenii Kuznetsov <vgenii_Kuznetsov3@epam.com>
2021-10-25 23:50:42 +0300

    Added thread.sleep() in sortingAndSelectingTableTest

--- jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/displaydata/TableTests.java
+++ jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/displaydata/TableTests.java
@@ -108,6 +108,12 @@ public class TableTests extends TestsInit {
         scrollButtons.get(3).hover();
         scrollButtons.get(3).click();
 
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+
         sortingSelectingTable.headerUI().get(2).check();
         rowsPerPageBtn.click();
         rowsPerPageValues.get(2).click();
