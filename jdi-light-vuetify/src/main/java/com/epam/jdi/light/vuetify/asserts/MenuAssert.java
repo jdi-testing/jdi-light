970383105a852c25ede91a137b3c536699861e4d
Aleksandr_Kashin2 <aleksandr_kashin2@epam.com>
2021-10-26 13:55:53 +0300

    refactored menu asserts

--- jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/asserts/MenuAssert.java
+++ jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/asserts/MenuAssert.java
@@ -12,44 +12,44 @@ import static com.epam.jdi.light.asserts.core.SoftAssert.jdiAssert;
 
 public class MenuAssert extends UIAssert<MenuAssert, Menu> {
 
-    @JDIAction("Assert that {name} is displayed")
+    @JDIAction("Assert that '{name}' is displayed")
     public MenuAssert displayed() {
         Timer.waitCondition(element()::isDisplayed);
         jdiAssert(element().isDisplayed(), Matchers.is(true));
         return this;
     }
 
-    @JDIAction("Assert that {name} has expected position")
+    @JDIAction("Assert that '{name}' has expected position")
     public MenuAssert position(int top, int left) {
         jdiAssert(element().hasPosition(), Matchers.equalTo(String.format("top: %spx; left: %dpx", top, left)));
         return this;
     }
 
-    @JDIAction("Assert that {name} has expected number of options")
+    @JDIAction("Assert that '{name}' has expected number of options")
     public MenuAssert numberOfOptions(int numberOfOptions) {
         jdiAssert(element().hasNumberOfOptions(), Matchers.is(numberOfOptions));
         return this;
     }
 
-    @JDIAction("Assert that {name} has expected options titles")
+    @JDIAction("Assert that '{name}' has expected options titles")
     public MenuAssert optionsTitles(List<String> optionsTitles) {
         jdiAssert(element().hasOptionsTitles(), Matchers.is(optionsTitles));
         return this;
     }
 
-    @JDIAction("Assert that {name} has removed radius")
+    @JDIAction("Assert that '{name}' has removed radius")
     public MenuAssert removedRadius() {
         jdiAssert(element().hasRemovedRadius(), Matchers.is(true));
         return this;
     }
 
-    @JDIAction("Assert that {name} has large radius")
+    @JDIAction("Assert that '{name}' has large radius")
     public MenuAssert largeRadius() {
         jdiAssert(element().hasLargeRadius(), Matchers.is(true));
         return this;
     }
 
-    @JDIAction("Assert that {name} has custom radius")
+    @JDIAction("Assert that '{name}' has custom radius")
     public MenuAssert customRadius() {
         jdiAssert(element().hasCustomRadius(), Matchers.is(true));
         return this;
