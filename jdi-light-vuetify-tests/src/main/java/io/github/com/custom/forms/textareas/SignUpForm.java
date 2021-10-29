6f3b8f579788471440b8dcd158bcd31abae4bda5
Robert Andreev <andreev.robert.a@gmail.com>
2021-10-29 05:52:35 +0300

    Remove custom TextFieldArea to TextField in SignUpForm

--- jdi-light-vuetify-tests/src/main/java/io/github/com/custom/forms/textareas/SignUpForm.java
+++ jdi-light-vuetify-tests/src/main/java/io/github/com/custom/forms/textareas/SignUpForm.java
@@ -5,18 +5,18 @@ import com.epam.jdi.light.elements.composite.Form;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.vuetify.elements.common.Button;
 import com.epam.jdi.light.vuetify.elements.complex.TextArea;
-import io.github.com.custom.textareas.TextFieldArea;
+import com.epam.jdi.light.vuetify.elements.complex.TextField;
 import io.github.com.entities.textareas.SignUpData;
 
 public class SignUpForm extends Form<SignUpData> {
     @UI(".v-input[1]")
-    public static TextFieldArea password;
+    public static TextField password;
 
     @UI(".v-input[2]")
-    public static TextFieldArea phone;
+    public static TextField phone;
 
     @UI(".v-input[3]")
-    public static TextFieldArea email;
+    public static TextField email;
 
     @UI(".v-input[4]")
     public static TextArea bio;
