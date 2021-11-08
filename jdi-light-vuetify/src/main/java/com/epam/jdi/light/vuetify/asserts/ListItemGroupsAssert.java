d8f2e7dc3a9d7e08508010bb7e33e45152be9518
foria1405 <fominadd1405@yandex.ru>
2021-11-09 02:51:57 +0300

    Delete unused comment lines

--- jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/asserts/ListItemGroupsAssert.java
+++ jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/asserts/ListItemGroupsAssert.java
@@ -1,14 +1,14 @@
 package com.epam.jdi.light.vuetify.asserts;
 
+import static com.epam.jdi.light.asserts.core.SoftAssert.jdiAssert;
+
 import com.epam.jdi.light.asserts.generic.UIAssert;
 import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.vuetify.elements.complex.ListItemGroups;
 import org.hamcrest.Matchers;
 
-import static com.epam.jdi.light.asserts.core.SoftAssert.jdiAssert;
 
-public class ListItemGroupsAssert extends UIAssert<ListItemGroupsAssert, ListItemGroups>{
-
+public class ListItemGroupsAssert extends UIAssert<ListItemGroupsAssert, ListItemGroups> {
 
     @JDIAction("Assert that '{name}' is active")
     public ListItemGroupsAssert active() {
@@ -52,60 +52,4 @@ public class ListItemGroupsAssert extends UIAssert<ListItemGroupsAssert, ListIte
         jdiAssert(element().getText(), Matchers.containsString(text));
         return this;
     }
-
-
-
-    //    @JDIAction("Assert that '{name}' is displayed'")
-    //    public ChipAssert displayed() {
-    //        jdiAssert(element().isDisplayed(), Matchers.is(true));
-    //        return this;
-    //    }
-    //
-    //    @JDIAction("Assert that '{name}' is not displayed")
-    //    public ChipAssert notDisplayed() {
-    //        jdiAssert(element().isDisplayed(), Matchers.is(false));
-    //        return this;
-    //    }
-
-    //    @JDIAction("Assert that {name}'s label contains '{0}'")
-    //    public ChipAssert containsText(String text) {
-    //        jdiAssert(element().getText(), Matchers.containsString(text));
-    //        return this;
-    //    }
-
-    //    @JDIAction("Assert that '{name}' is selected")
-    //    public ComboboxAssert selected(String value) {
-    //        jdiAssert(element().isSelected(value), Matchers.is(true));
-    //        return this;
-    //    }
-    //
-    //    @JDIAction("Assert that '{name}' is selected")
-    //    public ComboboxAssert selected(List<String> values) {
-    //        jdiAssert(element().isSelected(values), Matchers.is(true));
-    //        return this;
-    //    }
-    //
-    //    @JDIAction("Assert that '{name}' is not selected")
-    //    public ComboboxAssert notSelected(String value) {
-    //        jdiAssert(element().isSelected(value), Matchers.is(false));
-    //        return this;
-    //    }
-    //
-    //    @JDIAction("Assert that '{name}' is not selected")
-    //    public ComboboxAssert notSelected(List<String> values) {
-    //        jdiAssert(element().isSelected(values), Matchers.is(false));
-    //        return this;
-    //    }
-    //
-    //    @JDIAction("Assert that '{name}' is disabled")
-    //    public ComboboxAssert disabled() {
-    //        jdiAssert(element().isDisabled(), Matchers.is(true));
-    //        return this;
-    //    }
-    //
-    //    @JDIAction("Assert that '{name}' is expanded")
-    //    public ComboboxAssert active() {
-    //        jdiAssert(element().isDisabled(), Matchers.is(false));
-    //        return this;
-    //    }
 }
