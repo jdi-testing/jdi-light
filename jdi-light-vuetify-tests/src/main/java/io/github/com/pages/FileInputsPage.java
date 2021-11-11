17a47b42c388f904e4502c561bf903f9c75d206f
Robert Andreev <andreev.robert.a@gmail.com>
2021-11-11 10:22:26 +0300

    Add FileInput component on FileInputsPage

--- jdi-light-vuetify-tests/src/main/java/io/github/com/pages/FileInputsPage.java
+++ jdi-light-vuetify-tests/src/main/java/io/github/com/pages/FileInputsPage.java
@@ -8,7 +8,33 @@ public class FileInputsPage extends VuetifyPage {
     @UI("#AcceptFileInput.v-file-input")
     public static FileInput acceptFileInput;
 
+    @UI("#ChipsFileInput .v-file-input[1]")
+    public static FileInput chipsFileInput;
+
+    @UI("#ChipsFileInput .v-file-input[2]")
+    public static FileInput smallChipsFileInput;
+
     @UI("#CounterFileInput.v-file-input")
     public static FileInput counterFileInput;
 
+    @UI("#DenseFileInput.v-file-input")
+    public static FileInput denseFileInput;
+
+    @UI("#MultipleFileInput.v-file-input")
+    public static FileInput multiplyFileInput;
+
+    @UI("#PrependIconFileInput.v-file-input")
+    public static FileInput prependIconFileInput;
+
+    @UI("#ShowSizeFileInput.v-file-input")
+    public static FileInput showSizeFileInput;
+
+    @UI("#ValidationFileInput.v-file-input")
+    public static FileInput validationFileInput;
+
+    @UI("#SelectionFileInput.v-file-input")
+    public static FileInput selectionFileInput;
+
+    @UI("#ComplexSelectionSlotFileInput.v-file-input")
+    public static FileInput complexFileInput;
 }
