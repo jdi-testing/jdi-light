ef9b0079207de00cab9aec50fb27ac3523dabbb1
Robert Andreev <andreev.robert.a@gmail.com>
2021-11-11 10:20:30 +0300

    Add FileInput component

--- jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/asserts/FileInputAssert.java
+++ jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/asserts/FileInputAssert.java
@@ -1,7 +1,60 @@
 package com.epam.jdi.light.vuetify.asserts;
 
-import com.epam.jdi.light.asserts.generic.UIAssert;
+import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.vuetify.elements.complex.FileInput;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 
-public class FileInputAssert extends UIAssert<FileInputAssert, FileInput> {
+import java.util.List;
+
+import static com.epam.jdi.light.asserts.core.SoftAssert.jdiAssert;
+
+public class FileInputAssert extends TextFieldAssert {
+
+    @Override
+    public FileInput element() {
+        return (FileInput) super.element();
+    }
+
+    @JDIAction("Assert that '{name}' can accept multiply files")
+    public FileInputAssert multiply() {
+        jdiAssert(element().isMultiply() ? "multiply" : "not multiply", Matchers.is("multiply"));
+        return this;
+    }
+
+    @JDIAction("Assert that '{name}' has chips")
+    public FileInputAssert chips() {
+        jdiAssert(element().hasChips() ? "has chips" : "has no chips", Matchers.is("has chips"));
+        return this;
+    }
+
+    @JDIAction("Assert that '{name}' accept {0}")
+    public FileInputAssert accept(Matcher<String> condition) {
+        jdiAssert(element().accept(), condition);
+        return this;
+    }
+
+    @JDIAction("Assert that '{name}' has file {0}")
+    public FileInputAssert file(Matcher<String> condition) {
+        jdiAssert(element().getText(), condition);
+        return this;
+    }
+
+    @JDIAction("Assert that '{name}' has file {0}")
+    public FileInputAssert file(String file) {
+        jdiAssert(element().getText(), Matchers.is(file));
+        return this;
+    }
+
+    @JDIAction("Assert that '{name}' has files {0}")
+    public FileInputAssert files(Matcher<? super List<String>> condition) {
+        jdiAssert(element().getFiles(), condition);
+        return this;
+    }
+
+    @JDIAction("Assert that '{name}' has files {0}")
+    public FileInputAssert files(List<String> files) {
+        jdiAssert(element().getFiles(), Matchers.is(files));
+        return this;
+    }
 }
--- jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/elements/complex/FileInput.java
+++ jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/elements/complex/FileInput.java
@@ -1,25 +1,146 @@
 package com.epam.jdi.light.vuetify.elements.complex;
 
 import com.epam.jdi.light.common.JDIAction;
+import com.epam.jdi.light.elements.common.UIElement;
+import com.epam.jdi.light.elements.complex.WebList;
+import com.epam.jdi.light.vuetify.asserts.FileInputAssert;
 
+import java.util.Collections;
+import java.util.List;
+
+import static com.epam.jdi.light.asserts.core.SoftAssert.assertSoft;
 import static com.epam.jdi.light.common.Exceptions.exception;
 
 public class FileInput extends TextField {
 
+    @JDIAction("Check that '{name}' can accept multiply files")
     public boolean isMultiply() {
         return textInputField().hasAttribute("multiple");
     }
 
-    public boolean isHideInput() {
-        return find("v-input__control").attr("style").contains("display: none;");
+    @Override
+    @JDIAction("Check that '{name}' is displayed")
+    public boolean isDisplayed() {
+        return !find(".v-input__control").attr("style").contains("display: none;");
+    }
+
+    @JDIAction("Check that '{name}' has chips")
+    public boolean hasChips() {
+        return find(".v-file-input__text--chips").isExist();
+    }
+
+    public UIElement files() {
+        return find(".v-file-input__text");
+    }
+
+    @JDIAction("Get '{name}' accepted types")
+    public String accept() {
+        return textInputField().attr("accept");
+    }
+
+    @Override
+    @JDIAction("Get '{name}' text")
+    public String getText() {
+        return files().getText();
+    }
+
+    @JDIAction("Get '{name}' files list")
+    public List<String> getFiles() {
+        WebList chips = files().finds(".v-chip");
+        if (chips.isNotEmpty()) {
+            return chips.map(UIElement::text);
+        }
+        return Collections.singletonList(getText());
+    }
+
+    @Override
+    @JDIAction("Input '{0}' in '{name}'")
+    public void sendKeys(CharSequence... value) {
+        textInputField().getWebElement().sendKeys(value);
+    }
+
+    @Override
+    @JDIAction("Input '{0}' in '{name}'")
+    public void input(String value) {
+        clear();
+        sendKeys(value);
+    }
+
+    @Override
+    @JDIAction("Set '{0}' in '{name}'")
+    public void setText(String value) {
+        clear();
+        sendKeys(value);
+    }
+
+    @Override
+    @JDIAction("Focus on '{name}'")
+    public void focus() {
+        sendKeys("");
+    }
+
+    @Override
+    @JDIAction("Clear '{name}' file input")
+    public void clear() {
+        if (!appendInnerIcons().isEmpty()) {
+            getAppendInnerIcon().click();
+        }
     }
 
     @JDIAction("Upload file '{0}' for '{name}'")
     public void uploadFile(String path) {
         if (isDisabled())
             throw exception("FileInput '%s' is disabled. Can't upload file", getName());
-        textInputField().getWebElement().sendKeys(path);
+        sendKeys(path);
     }
 
+    @JDIAction("Upload file '{0}' for '{name}'")
+    public void uploadFiles(String... paths) {
+        for (String path : paths) {
+            uploadFile(path);
+        }
+    }
 
+    public void uploadFiles(List<String> paths) {
+        uploadFiles(paths.toArray(new String[0]));
+    }
+
+    @Override
+    public FileInputAssert is() {
+        FileInputAssert fileInputAssert = new FileInputAssert();
+        fileInputAssert.set(this);
+        return fileInputAssert;
+    }
+
+    @Override
+    public FileInputAssert assertThat() {
+        return is();
+    }
+
+    @Override
+    public FileInputAssert has() {
+        return is();
+    }
+
+    @Override
+    public FileInputAssert waitFor() {
+        return is();
+    }
+
+    @Override
+    public FileInputAssert waitFor(int sec) {
+        this.waitSec(sec);
+        return is();
+    }
+
+    @Override
+    public FileInputAssert shouldBe() {
+        return is();
+    }
+
+    @Override
+    public FileInputAssert verify() {
+        assertSoft();
+        return is();
+    }
 }
