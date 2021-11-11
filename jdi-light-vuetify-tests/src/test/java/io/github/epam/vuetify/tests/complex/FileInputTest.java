1922db62f8e7feecdf17c0c05d766f7d1d00347f
Robert Andreev <andreev.robert.a@gmail.com>
2021-11-11 10:23:13 +0300

    Add FileInput tests

--- jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/complex/FileInputTest.java
+++ jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/complex/FileInputTest.java
@@ -1,21 +1,38 @@
 package io.github.epam.vuetify.tests.complex;
 
-import com.epam.jdi.light.elements.common.UIElement;
-import io.github.com.pages.FileInputsPage;
+import com.epam.jdi.light.vuetify.elements.enums.Colors;
 import io.github.epam.TestsInit;
-import org.openqa.selenium.By;
+import org.hamcrest.Matchers;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
 
 import static com.epam.jdi.light.settings.JDISettings.COMMON;
 import static com.epam.jdi.tools.PathUtils.mergePath;
 import static io.github.com.StaticSite.fileInputsPage;
 import static io.github.com.pages.FileInputsPage.acceptFileInput;
+import static io.github.com.pages.FileInputsPage.chipsFileInput;
+import static io.github.com.pages.FileInputsPage.counterFileInput;
+import static io.github.com.pages.FileInputsPage.denseFileInput;
+import static io.github.com.pages.FileInputsPage.multiplyFileInput;
+import static io.github.com.pages.FileInputsPage.prependIconFileInput;
+import static io.github.com.pages.FileInputsPage.showSizeFileInput;
+import static io.github.com.pages.FileInputsPage.smallChipsFileInput;
+import static io.github.com.pages.FileInputsPage.validationFileInput;
+import static java.util.Arrays.asList;
+import static org.hamcrest.Matchers.containsString;
 
 public class FileInputTest extends TestsInit {
 
-    String path1 = "C:\\Users\\Robert_Andreev\\Desktop\\Instruction.png";
-    String path2 = "C:\\Users\\Robert_Andreev\\Desktop\\Instruction.pdf";
+    Path pathTXT;
+    Path pathPNG;
 
     @BeforeClass
     public void before() {
@@ -23,20 +40,108 @@ public class FileInputTest extends TestsInit {
         fileInputsPage.checkOpened();
     }
 
+    @BeforeClass
+    public void createFiles() throws IOException {
+        pathTXT = Paths.get(mergePath(COMMON.projectPath, "/src/test/resources/TextTestFile.txt"));
+        pathPNG = Paths.get(mergePath(COMMON.projectPath, "/src/test/resources/ImageTestFile.png"));
+        Files.deleteIfExists(pathTXT);
+        Files.deleteIfExists(pathPNG);
+        Files.write(pathTXT, "LOREM IPSUM".getBytes(), StandardOpenOption.CREATE);
+        Files.write(pathPNG, "Lorem ipsum dolor sit amet, no nam oblique veritus.".getBytes(), StandardOpenOption.CREATE);
+    }
+
     @Test
     public void acceptFileInput() {
-        acceptFileInput.uploadFile(path1);
-        acceptFileInput.has().text("Instruction.png");
+        acceptFileInput.uploadFile(pathTXT.toString());
+        acceptFileInput.has().file(pathTXT.getFileName().toString());
     }
 
     @Test
-    public void baseFileTe2st() {
-//        fileInput.highlight();
-//        UIElement element = fileInput.core().find("input");
-//        element.getWebElement().sendKeys(path1);
-//        element.getWebElement().sendKeys(path2);
-////        element.sendKeys(path);
-//        Thread.sleep(5000);
+    public void chipsFileInputTest() {
+        chipsFileInput.is().multiply();
+        chipsFileInput.has().chips();
+        chipsFileInput.uploadFiles(asList(pathTXT.toString(), pathPNG.toString()));
+        chipsFileInput.has().files(asList(pathTXT.getFileName().toString(), pathPNG.getFileName().toString()));
     }
 
+    @Test
+    public void smallChipsFileInputTest() {
+        smallChipsFileInput.is().multiply();
+        chipsFileInput.has().chips();
+        smallChipsFileInput.uploadFile(pathTXT.toString());
+        smallChipsFileInput.has().file(pathTXT.getFileName().toString());
+        smallChipsFileInput.uploadFile(pathPNG.toString());
+        smallChipsFileInput.has().files(asList(pathTXT.getFileName().toString(), pathPNG.getFileName().toString()));
+    }
+
+    @Test
+    public void counterFileInputTest() {
+        counterFileInput.is().multiply();
+
+        counterFileInput.uploadFile(pathTXT.toString());
+        counterFileInput.has().file("TextTestFile.txt (11 B)");
+        counterFileInput.counter().has().text("1 files (11 B in total)");
+
+        counterFileInput.clear();
+
+        counterFileInput.uploadFile(pathPNG.toString());
+        counterFileInput.has().file("ImageTestFile.png (51 B)");
+        counterFileInput.counter().has().text("1 files (51 B in total)");
+
+        counterFileInput.clear();
+
+        counterFileInput.uploadFiles(asList(pathTXT.toString(), pathPNG.toString()));
+        counterFileInput.has().text("2 files");
+        counterFileInput.counter().has().text("2 files (62 B in total)");
+    }
+
+    @Test
+    public void denseFileInputTest() {
+
+    }
+
+    @Test
+    public void multiplyFileInputTest() {
+        multiplyFileInput.uploadFile(pathTXT.toString());
+        multiplyFileInput.has().text(pathTXT.getFileName().toString());
+        for (int i = 2; i <= 10; i++) {
+            multiplyFileInput.uploadFile(pathTXT.toString());
+            multiplyFileInput.has().text(i + " files");
+        }
+    }
+
+    @Test
+    public void prependIconFileInputTest() {
+        prependIconFileInput.getPrependOuterIcon().has().type("mdi-camera");
+        prependIconFileInput.label().has().text("File input");
+        prependIconFileInput.is().filled();
+    }
+
+    @Test
+    public void showSizeFileInputTest() {
+        showSizeFileInput.uploadFile(pathTXT.toString());
+        showSizeFileInput.has().text("TextTestFile.txt (11 B)");
+        showSizeFileInput.counter().has().text("1 files (11 B in total)");
+    }
+
+    @Test
+    public void validationFileInputTest() {
+        validationFileInput.is().accept(containsString("image/png, image/jpeg, image/bmp"));
+    }
+
+    @Test
+    public void selectionFileInputTest() {
+
+    }
+
+    @Test
+    public void complexFileInputTest() {
+
+    }
+
+    @AfterClass
+    public void deleteFiles() throws IOException {
+        Files.deleteIfExists(pathTXT);
+        Files.deleteIfExists(pathPNG);
+    }
 }
