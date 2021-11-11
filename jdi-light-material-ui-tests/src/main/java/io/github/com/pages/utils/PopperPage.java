825f7c4d16519b0234c89739998d6a86242e488f
Evgenii Kuznetsov <vgenii_Kuznetsov3@epam.com>
2021-11-11 12:54:02 +0300

    Popper was refactored

--- jdi-light-material-ui-tests/src/main/java/io/github/com/pages/utils/PopperPage.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/pages/utils/PopperPage.java
@@ -3,14 +3,10 @@ package io.github.com.pages.utils;
 import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.material.elements.utils.Popper;
-import com.epam.jdi.light.ui.html.elements.common.Button;
-
-import java.util.List;
 
 public class PopperPage extends WebPage {
-    @UI("button")
-    public static List<Button> buttons;
 
     @UI("[role='tooltip']")
     public static Popper popper;
+
 }
