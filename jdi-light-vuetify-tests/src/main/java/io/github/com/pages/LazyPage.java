2adea6e15bb47f864ee28446a6b129dc21a70671
Anastasiia Egorova1 <anastasiia_egorova1@epam.com>
2021-11-16 16:27:42 +0300

    implement lazy: Lazy, JDILazy, LazyAssert, LazyPage, LazyTests

--- jdi-light-vuetify-tests/src/main/java/io/github/com/pages/LazyPage.java
+++ jdi-light-vuetify-tests/src/main/java/io/github/com/pages/LazyPage.java
@@ -1,5 +1,14 @@
 package io.github.com.pages;
 
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
+import com.epam.jdi.light.vuetify.elements.complex.*;
+
+
 public class LazyPage extends VuetifyPage {
 
+    @UI(".v-lazy")
+    public static Lazy lazy;
+
+    public static final String ITEM_TITLE = ".v-card__title";
+    public static final String ITEM_TEXT = ".v-card__text";
 }
---
+++ jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/elements/complex/Lazy.java
@@ -0,0 +1,54 @@
+package com.epam.jdi.light.vuetify.elements.complex;
+
+import com.epam.jdi.light.common.JDIAction;
+import com.epam.jdi.light.elements.base.UIBaseElement;
+import com.epam.jdi.light.elements.common.UIElement;
+import com.epam.jdi.light.elements.complex.ISetup;
+import com.epam.jdi.light.vuetify.annotations.JDILazy;
+import com.epam.jdi.light.vuetify.asserts.LazyAssert;
+import com.epam.jdi.tools.Timer;
+
+import java.lang.reflect.Field;
+
+import static com.epam.jdi.light.elements.init.UIFactory.$;
+
+public class Lazy extends UIBaseElement<LazyAssert> implements ISetup {
+
+    protected String ITEM_LOCATOR = ".mx-auto.v-card";
+
+    public UIElement hiddenItem() {
+        if (ITEM_LOCATOR.isEmpty()) {
+            return core();
+        }
+        return $(ITEM_LOCATOR);
+    }
+
+    @Override
+    public boolean isHidden() {
+        return core().isHidden();
+    }
+
+    @Override
+    @JDIAction("Check that '{name}' is displayed")
+    public boolean isDisplayed() {
+        return core().isDisplayed();
+    }
+
+    @JDIAction("Scroll to '{name}'")
+    public void scrollIntoView() {
+        Timer.waitCondition(this::isDisplayed);
+        core().jsExecute("scrollIntoView()");
+    }
+
+    public LazyAssert is() {
+        return new LazyAssert().set(this);
+    }
+
+    @Override
+    public void setup(Field field) {
+        if (!field.isAnnotationPresent(JDILazy.class)) {
+            return;
+        }
+        ITEM_LOCATOR = field.getAnnotation(JDILazy.class).item();
+    }
+}
