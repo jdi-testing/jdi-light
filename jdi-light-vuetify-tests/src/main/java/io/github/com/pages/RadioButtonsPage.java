2cda5f423d1c5cc2b7f1f3d6dbe3cde8517d5d48
Konstantin Bulavin <konstantin_bulavin@epam.com>
2021-11-18 16:28:41 +0300

    Work in progress

--- jdi-light-vuetify-tests/src/main/java/io/github/com/pages/RadioButtonsPage.java
+++ jdi-light-vuetify-tests/src/main/java/io/github/com/pages/RadioButtonsPage.java
@@ -2,23 +2,23 @@ package io.github.com.pages;
 
 import com.epam.jdi.light.elements.common.Label;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
-import com.epam.jdi.light.vuetify.elements.common.RadioButton;
+import com.epam.jdi.light.vuetify.elements.complex.RadioButtons;
 
 import java.util.List;
 
 public class RadioButtonsPage extends VuetifyPage {
 
     @UI("#ColorsRadioButton div .v-radio")
-    public static List<RadioButton> colorRadioButton;
+    public static RadioButtons colorRadioButtons;
 
     @UI("#DirectionRadioButton div .v-radio")
-    public static List<RadioButton> directionRadioButton;
+    public static List<RadioButtons> directionRadioButtons;
 
     @UI("#MandatoryRadioButton div .v-radio")
-    public static List<RadioButton> mandatoryRadioButton;
+    public static List<RadioButtons> mandatoryRadioButtons;
 
     @UI("#LabelRadioButton div .v-radio")
-    public static List<RadioButton> labelRadioButton;
+    public static List<RadioButtons> labelRadioButtons;
 
     @UI("#MandatoryRadioButton > p ")
     public static Label mandatoryText;
--- jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/common/RadioButtonsTests.java
+++ jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/common/RadioButtonsTests.java
@@ -17,35 +17,44 @@ public class RadioButtonsTests extends TestsInit {
     }
 
     @Test
-    public void colorsTest() {
-        colorRadioButton.get(1).is().checked();
-        colorRadioButton.get(1).is().color(Colors.RED.value());
-        colorRadioButton.get(1).is().labelText("red");
-
-        colorRadioButton.get(6).is().unchecked();
-        colorRadioButton.get(6).check();
-        colorRadioButton.get(6).is().checked();
-        colorRadioButton.get(6).is().color(Colors.ORANGE_DARKEN_3.value());
-        colorRadioButton.get(6).is().labelText("orange darken-3");
+    public void demoTest() throws InterruptedException {
+        colorRadioButtons.select(3);
+        //Thread.sleep(2000);
+        System.out.println(colorRadioButtons.list().size());
+        colorRadioButtons.list().selected(3);
+        //colorRadioButtons.is().selected(3);
     }
 
-    @Test
-    public void directionTest() {
-        directionRadioButton.get(1).is().groupDirection("column");
-        directionRadioButton.get(3).is().groupDirection("row");
-    }
-
-    @Test
-    public void mandatoryTest() {
-        mandatoryRadioButton.get(1).check();
-        mandatoryText.is().text("radio-1");
-        mandatoryRadioButton.get(2).check();
-        mandatoryText.is().text("radio-2");
-    }
-
-    @Test
-    public void labelTest() {
-        labelRadioButton.get(1).is().labelText("Of course it's Google");
-        labelRadioButton.get(2).is().labelText("Definitely Duckduckgo");
-    }
+//    @Test
+//    public void colorsTest() {
+//        colorRadioButtons.get(1).is().checked();
+//        colorRadioButtons.get(1).is().color(Colors.RED.value());
+//        colorRadioButtons.get(1).is().labelText("red");
+//
+//        colorRadioButtons.get(6).is().unchecked();
+//        colorRadioButtons.get(6).check();
+//        colorRadioButtons.get(6).is().checked();
+//        colorRadioButtons.get(6).is().color(Colors.ORANGE_DARKEN_3.value());
+//        colorRadioButtons.get(6).is().labelText("orange darken-3");
+//    }
+//
+//    @Test
+//    public void directionTest() {
+//        directionRadioButtons.get(1).is().groupDirection("column");
+//        directionRadioButtons.get(3).is().groupDirection("row");
+//    }
+//
+//    @Test
+//    public void mandatoryTest() {
+//        mandatoryRadioButtons.get(1).check();
+//        mandatoryText.is().text("radio-1");
+//        mandatoryRadioButtons.get(2).check();
+//        mandatoryText.is().text("radio-2");
+//    }
+//
+//    @Test
+//    public void labelTest() {
+//        labelRadioButtons.get(1).is().labelText("Of course it's Google");
+//        labelRadioButtons.get(2).is().labelText("Definitely Duckduckgo");
+//    }
 }
