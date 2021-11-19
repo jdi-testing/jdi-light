d608d7aebee429243fb80e110b3a2ac3b9ac13e6
Aleksandr_Kashin2 <aleksandr_kashin2@epam.com>
2021-11-19 22:23:42 +0300

    minor edit

--- jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/interfaces/inputs/HasPlaceholder.java
+++ jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/interfaces/inputs/HasPlaceholder.java
@@ -1,8 +1,8 @@
 package com.epam.jdi.light.material.interfaces.inputs;
 
-import com.epam.jdi.light.elements.interfaces.base.HasLabel;
+import com.epam.jdi.light.elements.interfaces.base.ICoreElement;
 
-public interface HasPlaceholder extends HasLabel {
+public interface HasPlaceholder extends ICoreElement {
 
     boolean hasPlaceholder();
 
