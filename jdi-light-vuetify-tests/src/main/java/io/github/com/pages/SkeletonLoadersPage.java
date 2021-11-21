24a45126fbd7b26ccc161a8391b0c470c6b63fef
switzerrrland <switzerland.ptrs@gmail.com>
2021-11-21 18:12:53 +0300

    Implemented SkeletonLoadersPage and tests

--- jdi-light-vuetify-tests/src/main/java/io/github/com/pages/SkeletonLoadersPage.java
+++ jdi-light-vuetify-tests/src/main/java/io/github/com/pages/SkeletonLoadersPage.java
@@ -1,5 +1,25 @@
 package io.github.com.pages;
 
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
+import com.epam.jdi.light.vuetify.elements.complex.SkeletonLoader;
+
 public class SkeletonLoadersPage extends VuetifyPage {
 
+    @UI("#BoilerplateComponentSkeletonLoader > div > div:nth-child(1) > div:nth-child(1)")
+    public static SkeletonLoader imageAvatarArticleActionsSkeletonLoader;
+
+    @UI("#BoilerplateComponentSkeletonLoader > div > div:nth-child(1) > div:nth-child(2)")
+    public static SkeletonLoader datePickerSkeletonLoader;
+
+    @UI("#BoilerplateComponentSkeletonLoader > div > div:nth-child(2) > div:nth-child(1)")
+    public static SkeletonLoader articleActionsSkeletonLoader;
+
+    @UI("#BoilerplateComponentSkeletonLoader > div > div:nth-child(2) > div:nth-child(2)")
+    public static SkeletonLoader tableImageSkeletonLoader;
+
+    @UI("#BoilerplateComponentSkeletonLoader > div > div:nth-child(3) > div:nth-child(1)")
+    public static SkeletonLoader avatarTextImageActionsSkeletonLoader;
+
+    @UI("#BoilerplateComponentSkeletonLoader > div > div:nth-child(3) > div:nth-child(2)")
+    public static SkeletonLoader avatarTextImageArticleSkeletonLoader;
 }
