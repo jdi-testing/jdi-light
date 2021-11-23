c9ac4ef6a219ab0a03ecd31a923de6d8419aa5a4
Robert Andreev <andreev.robert.a@gmail.com>
2021-11-23 14:46:58 +0300

    Rework Typography component

--- jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/elements/displaydata/Typography.java
+++ jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/elements/displaydata/Typography.java
@@ -1,29 +1,23 @@
 package com.epam.jdi.light.material.elements.displaydata;
 
-import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.elements.base.UIBaseElement;
+import com.epam.jdi.light.elements.interfaces.common.IsText;
 import com.epam.jdi.light.material.asserts.displaydata.TypographyAssert;
 
 /**
  * To see an example of Typography web element please visit
  * https://mui.com/components/typography/
  */
-
-public class Typography extends UIBaseElement<TypographyAssert> {
+public class Typography extends UIBaseElement<TypographyAssert> implements IsText {
 
     private static final String STYLE_CLASS_PATTERN = "MuiTypography-%s";
 
-    @Override
-    public TypographyAssert has() {
-        return new TypographyAssert().set(this);
-    }
-
-    @JDIAction("Get text of '{name}'")
-    public String getText() {
-        return core().getText();
-    }
-
     public boolean hasStyleClass(String style) {
         return core().hasClass(String.format(STYLE_CLASS_PATTERN, style));
+    }
+
+    @Override
+    public TypographyAssert is() {
+        return new TypographyAssert().set(this);
     }
 }
