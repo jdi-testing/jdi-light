917b4ebf66fb874df4a2eb3444d9a77dbc728879
Robert Andreev <andreev.robert.a@gmail.com>
2021-11-23 18:08:30 +0300

    Fix TypographyPage

--- jdi-light-material-ui-tests/src/main/java/io/github/com/pages/displaydata/TypographyPage.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/pages/displaydata/TypographyPage.java
@@ -2,12 +2,12 @@ package io.github.com.pages.displaydata;
 
 import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
-import com.epam.jdi.light.ui.html.elements.common.Text;
+import com.epam.jdi.light.material.elements.displaydata.Typography;
 
 import java.util.List;
 
 public class TypographyPage extends WebPage {
 
-    @UI("//*[contains(@class,'MuiTypography-gutterBottom')]")
-    public static List<Text> typographyTexts;
+    @UI(".MuiGrid-root[2] .MuiTypography-root")
+    public static List<Typography> typographyTexts;
 }
