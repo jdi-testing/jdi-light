461cd011f520fc5583808a11a8b69ecf7b45e4e9
Alena Sidorova <alena_sidorova@epam.com>
2021-11-25 10:22:13 +0300

    Added: IconsDataProvider.java
    Updated:
    IconsTests.java (data providers added, tests shortened because of data provider implementation)

--- jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/displaydata/IconsTests.java
+++ jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/displaydata/IconsTests.java
@@ -1,9 +1,13 @@
 package io.github.epam.material.tests.displaydata;
 
+import com.epam.jdi.light.material.elements.displaydata.Icon;
 import io.github.epam.TestsInit;
+import io.github.epam.test.data.IconsDataProvider;
 import org.hamcrest.Matchers;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 import static com.epam.jdi.light.asserts.core.SoftAssert.jdiAssert;
 import static io.github.com.StaticSite.iconsPage;
@@ -21,52 +25,31 @@ public class IconsTests extends TestsInit {
         iconsPage.open();
     }
 
-    @Test
-    public void validationTest() {
-        simpleIcons.forEach(el -> el.is().displayed());
-        simpleIcons.forEach(el -> el.is().enabled());
-        simpleIcons.forEach(el -> el.is().attr("focusable", "false"));
-        jdiAssert(simpleIcons.size(), Matchers.is(13));
-
-        colorIcons.forEach(el -> el.is().displayed());
-        colorIcons.forEach(el -> el.is().enabled());
-        colorIcons.forEach(el -> el.is().attr("focusable", "false"));
-        jdiAssert(colorIcons.size(), Matchers.is(6));
-
-        differentSizesIcons.forEach(el -> el.is().displayed());
-        differentSizesIcons.forEach(el -> el.is().enabled());
-        differentSizesIcons.forEach(el -> el.is().attr("focusable", "false"));
-        jdiAssert(differentSizesIcons.size(), Matchers.is(4));
+    @Test(dataProviderClass = IconsDataProvider.class, dataProvider = "validationTestDataProvider")
+    public void validationTest(List<Icon> iconsList, int iconsListSize) {
+        iconsList.forEach(el -> el.is().displayed());
+        iconsList.forEach(el -> el.is().enabled());
+        jdiAssert(iconsList.size(), Matchers.is(iconsListSize));
     }
 
-    @Test
-    public void simpleIconsTest() {
-        simpleIcons.get(1).hover();
-        simpleLastHover.has().text("Last hover: DeleteIcon");
-        simpleIcons.get(2).click();
-        simpleLastClick.has().text("Last click: DeleteForeverIcon");
-
-        simpleIcons.forEach(icon -> icon.is().notColored());
-        simpleIcons.forEach(icon -> icon.has().color("rgba(0, 0, 0, 0.87)"));
-        simpleIcons.forEach(icon -> icon.has().height(24));
-        simpleIcons.forEach(icon -> icon.has().width(24));
+    @Test(dataProviderClass = IconsDataProvider.class, dataProvider = "simpleIconsTestDataProvider")
+    public void simpleIconsTest(int elementIndexForHover, String resultHoverFieldText,
+                                int elementIndexForClick, String resultClickFieldText) {
+        simpleIcons.get(elementIndexForHover).hover();
+        simpleLastHover.has().text(resultHoverFieldText);
+        simpleIcons.get(elementIndexForClick).click();
+        simpleLastClick.has().text(resultClickFieldText);
     }
 
-    @Test
-    public void colorIconsTest() {
-        colorIcons.get(1).is().notColored();
-        colorIcons.get(2).has().color("rgba(63, 81, 181, 1)");
-        colorIcons.get(3).has().color("rgba(245, 0, 87, 1)");
-        colorIcons.get(4).has().color("rgba(0, 0, 0, 0.54)");
-        colorIcons.get(5).has().color("rgba(0, 0, 0, 0.26)");
-        colorIcons.get(6).has().color("rgba(76, 175, 80, 1)");
+    @Test(dataProviderClass = IconsDataProvider.class, dataProvider = "colorIconsTestDataProvider")
+    public void colorIconsTest(int notColoredElementIndex, int coloredElementIndex, String color) {
+        colorIcons.get(notColoredElementIndex).is().notColored();
+        colorIcons.get(coloredElementIndex).is().colored();
+        colorIcons.get(coloredElementIndex).has().color(color);
     }
 
-    @Test
-    public void differentSizesIconsTest() {
-        differentSizesIcons.get(1).has().height(20).and().width(20);
-        differentSizesIcons.get(2).has().height(24).and().width(24);
-        differentSizesIcons.get(3).has().height(35).and().width(35);
-        differentSizesIcons.get(4).has().height(40).and().width(40);
+    @Test(dataProviderClass = IconsDataProvider.class, dataProvider = "differentSizesIconsTestDataProvider")
+    public void differentSizesIconsTest(int elementIndex, int height, int width) {
+        differentSizesIcons.get(elementIndex).has().height(height).and().width(width);
     }
 }
