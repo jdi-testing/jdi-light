79f965db9d932439c7f6bb2e77ca0dc7a30cf320
Vladislav_Prokopenko <daark_light@mail.ru>
2021-11-26 11:57:16 +0300

    DatePickersMonth: implementation of element and tests

--- jdi-light-vuetify-tests/src/main/java/io/github/com/pages/DatePickersMonthPage.java
+++ jdi-light-vuetify-tests/src/main/java/io/github/com/pages/DatePickersMonthPage.java
@@ -9,49 +9,49 @@ public class DatePickersMonthPage extends VuetifyPage {
     // LOCATORS FOR: Allowed month
     @JDatePickerMonth(
             root = "#AllowedMonthPicker > div")
-    public static DatePickerMonth allowedMP;
+    public static DatePickerMonth allowedMonthPicker;
 
     // LOCATORS FOR: Colors
     @JDatePickerMonth(
             root = "#ColorsMonthPicker > div:nth-child(1)")
-    public static DatePickerMonth firstColorMP;
+    public static DatePickerMonth firstColorMonthPicker;
 
     @JDatePickerMonth(
             root = "#ColorsMonthPicker > div:nth-child(2)")
-    public static DatePickerMonth secondColorMP;
+    public static DatePickerMonth secondColorMonthPicker;
 
     // LOCATORS FOR: Icons
     @JDatePickerMonth(
             root = "#IconsMonthPicker > div")
-    public static DatePickerMonth iconsMP;
+    public static DatePickerMonth iconsMonthPicker;
 
     // LOCATORS FOR: Multiple
     @JDatePickerMonth(
             root = "#MultipleMonthPicker > div")
-    public static DatePickerMonth multipleMP;
+    public static DatePickerMonth multipleMonthPicker;
 
     // LOCATORS FOR: Readonly
     @JDatePickerMonth(
             root = "#ReadonlyMonthPicker > div")
-    public static DatePickerMonth readonlyMP;
+    public static DatePickerMonth readonlyMonthPicker;
 
     // LOCATORS FOR: Show current
     @JDatePickerMonth(
             root = "#ShowCurrentMonthPicker > div:nth-child(1)")
-    public static DatePickerMonth firstShowCurrentMP;
+    public static DatePickerMonth firstShowCurrentMonthPicker;
 
     @JDatePickerMonth(
             root = "#ShowCurrentMonthPicker > div:nth-child(2)")
-    public static DatePickerMonth secondShowCurrentMP;
+    public static DatePickerMonth secondShowCurrentMonthPicker;
 
     // LOCATORS FOR: Width
     @JDatePickerMonth(
             root = "#WidthMonthPicker > div:nth-child(1)")
-    public static DatePickerMonth firstWidthMP;
+    public static DatePickerMonth firstWidthMonthPicker;
 
     @JDatePickerMonth(
             root = "#WidthMonthPicker > div.v-picker.v-card.v-picker--date.mt-4.v-picker--full-width.theme--light")
-    public static DatePickerMonth secondWidthMP;
+    public static DatePickerMonth secondWidthMonthPicker;
 
     @UI("div.v-main__wrap > div.container > div")
     public static Section mainWindow;
@@ -60,24 +60,24 @@ public class DatePickersMonthPage extends VuetifyPage {
     @JDatePickerMonth(
             root = "#DialogMenuMonthPicker > div:nth-child(1)",
             expandedRoot = "#app > div.v-menu__content.theme--light.menuable__content__active")
-    public static DatePickerMonth pickerInMenuMP;
+    public static DatePickerMonth pickerInMenuMonthPicker;
 
     @JDatePickerMonth(
             root = "#DialogMenuMonthPicker > div:nth-child(3)",
             expandedRoot = "#app > div.v-dialog__content.v-dialog__content--active")
-    public static DatePickerMonth pickerInDialogMP;
+    public static DatePickerMonth pickerInDialogMonthPicker;
 
     // LOCATORS FOR: Internationalization
     @JDatePickerMonth(
             root = "#InternationalizationMonthPicker > div:nth-child(1)")
-    public static DatePickerMonth thaiMP;
+    public static DatePickerMonth thaiMonthPicker;
 
     @JDatePickerMonth(
             root = "#InternationalizationMonthPicker > div:nth-child(2)")
-    public static DatePickerMonth swedishMP;
+    public static DatePickerMonth swedishMonthPicker;
 
     // LOCATORS FOR: Orientation
     @JDatePickerMonth(
             root = "#OrientationMonthPicker")
-    public static DatePickerMonth orientationMP;
+    public static DatePickerMonth orientationMonthPicker;
 }
--- jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/complex/DatePickersMonthTests.java
+++ jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/complex/DatePickersMonthTests.java
@@ -18,22 +18,22 @@ import java.util.Set;
 
 import static com.epam.jdi.light.asserts.core.SoftAssert.jdiAssert;
 import static io.github.com.StaticSite.datePickersMonthPage;
-import static io.github.com.pages.DatePickersMonthPage.allowedMP;
-import static io.github.com.pages.DatePickersMonthPage.firstColorMP;
-import static io.github.com.pages.DatePickersMonthPage.firstShowCurrentMP;
-import static io.github.com.pages.DatePickersMonthPage.firstWidthMP;
-import static io.github.com.pages.DatePickersMonthPage.iconsMP;
+import static io.github.com.pages.DatePickersMonthPage.allowedMonthPicker;
+import static io.github.com.pages.DatePickersMonthPage.firstColorMonthPicker;
+import static io.github.com.pages.DatePickersMonthPage.firstShowCurrentMonthPicker;
+import static io.github.com.pages.DatePickersMonthPage.firstWidthMonthPicker;
+import static io.github.com.pages.DatePickersMonthPage.iconsMonthPicker;
 import static io.github.com.pages.DatePickersMonthPage.mainWindow;
-import static io.github.com.pages.DatePickersMonthPage.multipleMP;
-import static io.github.com.pages.DatePickersMonthPage.orientationMP;
-import static io.github.com.pages.DatePickersMonthPage.pickerInDialogMP;
-import static io.github.com.pages.DatePickersMonthPage.pickerInMenuMP;
-import static io.github.com.pages.DatePickersMonthPage.readonlyMP;
-import static io.github.com.pages.DatePickersMonthPage.secondColorMP;
-import static io.github.com.pages.DatePickersMonthPage.secondShowCurrentMP;
-import static io.github.com.pages.DatePickersMonthPage.secondWidthMP;
-import static io.github.com.pages.DatePickersMonthPage.swedishMP;
-import static io.github.com.pages.DatePickersMonthPage.thaiMP;
+import static io.github.com.pages.DatePickersMonthPage.multipleMonthPicker;
+import static io.github.com.pages.DatePickersMonthPage.orientationMonthPicker;
+import static io.github.com.pages.DatePickersMonthPage.pickerInDialogMonthPicker;
+import static io.github.com.pages.DatePickersMonthPage.pickerInMenuMonthPicker;
+import static io.github.com.pages.DatePickersMonthPage.readonlyMonthPicker;
+import static io.github.com.pages.DatePickersMonthPage.secondColorMonthPicker;
+import static io.github.com.pages.DatePickersMonthPage.secondShowCurrentMonthPicker;
+import static io.github.com.pages.DatePickersMonthPage.secondWidthMonthPicker;
+import static io.github.com.pages.DatePickersMonthPage.swedishMonthPicker;
+import static io.github.com.pages.DatePickersMonthPage.thaiMonthPicker;
 import static org.hamcrest.Matchers.is;
 
 public class DatePickersMonthTests extends TestsInit {
@@ -78,146 +78,146 @@ public class DatePickersMonthTests extends TestsInit {
 
     @Test
     public void testAllowedMonthPicker() {
-        Timer.waitCondition(() -> allowedMP.isVisible());
-        allowedMP.has().disabledMonthsNonEmptyList();
-        allowedMP.getEnabledMonths();
-        allowedMP.has().clickableEnabledMonths();
-        allowedMP.has().nonClickableDisabledMonths();
+        Timer.waitCondition(() -> allowedMonthPicker.isVisible());
+        allowedMonthPicker.has().disabledMonthsNonEmptyList();
+        allowedMonthPicker.getEnabledMonths();
+        allowedMonthPicker.has().clickableEnabledMonths();
+        allowedMonthPicker.has().nonClickableDisabledMonths();
     }
 
     @Test
     public void testColorsMonthPicker() {
-        Timer.waitCondition(() -> firstColorMP.isVisible());
-        firstColorMP.has().color(GREEN_COLOR_HEX);
-        secondColorMP.has().color(BLUE_COLOR_HEX);
-        firstColorMP.selectMonth(chosenMonth);
-        firstColorMP.has().month(chosenMonthFull);
-        firstColorMP.selectMonth(chosenMonthTwo);
-        firstColorMP.has().month(chosenMonthTwoFull);
-        firstColorMP.has().year(Integer.toString(currentYear));
-        firstColorMP.nextYear();
-        firstColorMP.has().year(Integer.toString(currentYear + 1));
-        firstColorMP.previousYear();
-        firstColorMP.previousYear();
-        firstColorMP.has().year(Integer.toString(currentYear - 1));
-        firstColorMP.selectMonth(chosenMonth);
-        firstColorMP.changeYear();
-        firstColorMP.selectYear(Integer.toString(currentYear + 99));
-        firstColorMP.has().year(Integer.toString(currentYear + 99));
-        firstColorMP.changeYear();
-        firstColorMP.selectYear(Integer.toString(currentYear));
-        firstColorMP.changeYear();
-        firstColorMP.selectYear(Integer.toString(currentYear - 100));
-        firstColorMP.has().year(Integer.toString(currentYear - 100));
-        firstColorMP.changeYearCornerButton();
-        firstColorMP.selectYear(Integer.toString(currentYear));
-        firstColorMP.has().year(Integer.toString(currentYear));
+        Timer.waitCondition(() -> firstColorMonthPicker.isVisible());
+        firstColorMonthPicker.has().color(GREEN_COLOR_HEX);
+        secondColorMonthPicker.has().color(BLUE_COLOR_HEX);
+        firstColorMonthPicker.selectMonth(chosenMonth);
+        firstColorMonthPicker.has().month(chosenMonthFull);
+        firstColorMonthPicker.selectMonth(chosenMonthTwo);
+        firstColorMonthPicker.has().month(chosenMonthTwoFull);
+        firstColorMonthPicker.has().year(Integer.toString(currentYear));
+        firstColorMonthPicker.nextYear();
+        firstColorMonthPicker.has().year(Integer.toString(currentYear + 1));
+        firstColorMonthPicker.previousYear();
+        firstColorMonthPicker.previousYear();
+        firstColorMonthPicker.has().year(Integer.toString(currentYear - 1));
+        firstColorMonthPicker.selectMonth(chosenMonth);
+        firstColorMonthPicker.changeYear();
+        firstColorMonthPicker.selectYear(Integer.toString(currentYear + 99));
+        firstColorMonthPicker.has().year(Integer.toString(currentYear + 99));
+        firstColorMonthPicker.changeYear();
+        firstColorMonthPicker.selectYear(Integer.toString(currentYear));
+        firstColorMonthPicker.changeYear();
+        firstColorMonthPicker.selectYear(Integer.toString(currentYear - 100));
+        firstColorMonthPicker.has().year(Integer.toString(currentYear - 100));
+        firstColorMonthPicker.changeYearCornerButton();
+        firstColorMonthPicker.selectYear(Integer.toString(currentYear));
+        firstColorMonthPicker.has().year(Integer.toString(currentYear));
     }
 
     @Test
     public void testIconsMonthPicker() {
-        Timer.waitCondition(() -> iconsMP.isVisible());
-        iconsMP.has().nextYearIconClass(NEXT_YEAR_ICON_CLASS);
-        iconsMP.has().previousYearIconClass(PREVIOUS_YEAR_ICON_CLASS);
-        iconsMP.has().additionalYearIcon();
+        Timer.waitCondition(() -> iconsMonthPicker.isVisible());
+        iconsMonthPicker.has().nextYearIconClass(NEXT_YEAR_ICON_CLASS);
+        iconsMonthPicker.has().previousYearIconClass(PREVIOUS_YEAR_ICON_CLASS);
+        iconsMonthPicker.has().additionalYearIcon();
     }
 
     @Test
     public void testMultipleMonthPicker() {
-        Timer.waitCondition(() -> multipleMP.isVisible());
-        List<String> firstlyActiveMonths = multipleMP.getAllActiveMonths();
+        Timer.waitCondition(() -> multipleMonthPicker.isVisible());
+        List<String> firstlyActiveMonths = multipleMonthPicker.getAllActiveMonths();
         CHECKED_MULTIPLE_MONTHS.stream().forEach(elem -> {
             if (!firstlyActiveMonths.contains(elem)) {
-                multipleMP.selectMonth(elem);
+                multipleMonthPicker.selectMonth(elem);
             }
         });
         firstlyActiveMonths.addAll(CHECKED_MULTIPLE_MONTHS);
         Set<String> allExpectedChosenMonths = new HashSet<>(firstlyActiveMonths);
-        multipleMP.has().properSetOfActiveMonths(allExpectedChosenMonths);
-        multipleMP.has().month(allExpectedChosenMonths.size() + SELECTION_TEXT);
-        allExpectedChosenMonths.stream().forEach(elem -> multipleMP.selectMonth(elem));
-        multipleMP.has().month(EMPTY_MONTH_FIELD);
-        multipleMP.selectMonth(chosenMonth);
-        multipleMP.has().month(chosenMonthFull);
+        multipleMonthPicker.has().properSetOfActiveMonths(allExpectedChosenMonths);
+        multipleMonthPicker.has().month(allExpectedChosenMonths.size() + SELECTION_TEXT);
+        allExpectedChosenMonths.stream().forEach(elem -> multipleMonthPicker.selectMonth(elem));
+        multipleMonthPicker.has().month(EMPTY_MONTH_FIELD);
+        multipleMonthPicker.selectMonth(chosenMonth);
+        multipleMonthPicker.has().month(chosenMonthFull);
     }
 
     @Test
     public void testReadOnlyMonthPicker() {
-        Timer.waitCondition(() -> readonlyMP.isVisible());
-        readonlyMP.selectMonth(chosenMonth);
-        readonlyMP.has().month(currentMonthFull);
-        readonlyMP.selectMonth(chosenMonthTwo);
-        readonlyMP.has().month(currentMonthFull);
+        Timer.waitCondition(() -> readonlyMonthPicker.isVisible());
+        readonlyMonthPicker.selectMonth(chosenMonth);
+        readonlyMonthPicker.has().month(currentMonthFull);
+        readonlyMonthPicker.selectMonth(chosenMonthTwo);
+        readonlyMonthPicker.has().month(currentMonthFull);
     }
 
     @Test
     public void testShowCurrentMonthPicker() {
-        Timer.waitCondition(() -> firstShowCurrentMP.isVisible());
-        firstShowCurrentMP.has().month(currentMonthFull);
-        secondShowCurrentMP.has().properOutlinedMonthBorder(MONTH_BORDER);
+        Timer.waitCondition(() -> firstShowCurrentMonthPicker.isVisible());
+        firstShowCurrentMonthPicker.has().month(currentMonthFull);
+        secondShowCurrentMonthPicker.has().properOutlinedMonthBorder(MONTH_BORDER);
     }
 
     @Test
     public void testWidthMonthPicker() {
-        Timer.waitCondition(() -> firstWidthMP.isVisible());
-        firstWidthMP.has().pickerWidth(WIDTH_OF_PREDEFINED_WIDTH_DP);
-        secondWidthMP.has().pickerWidth(mainWindow.getSize().getWidth());
+        Timer.waitCondition(() -> firstWidthMonthPicker.isVisible());
+        firstWidthMonthPicker.has().pickerWidth(WIDTH_OF_PREDEFINED_WIDTH_DP);
+        secondWidthMonthPicker.has().pickerWidth(mainWindow.getSize().getWidth());
     }
 
     @Test
     public void testDialogAndMenuMonthPicker() {
-        Timer.waitCondition(() -> pickerInMenuMP.isVisible());
-        pickerInMenuMP.expand();
-        pickerInMenuMP.has().resultDate(date.format(formatterYearHyphenMonth));
-        pickerInMenuMP.selectMonth(chosenMonth);
-        pickerInMenuMP.has().resultDate(LocalDate.of(currentYear, Month.valueOf(chosenMonthFull.
+        Timer.waitCondition(() -> pickerInMenuMonthPicker.isVisible());
+        pickerInMenuMonthPicker.expand();
+        pickerInMenuMonthPicker.has().resultDate(date.format(formatterYearHyphenMonth));
+        pickerInMenuMonthPicker.selectMonth(chosenMonth);
+        pickerInMenuMonthPicker.has().resultDate(LocalDate.of(currentYear, Month.valueOf(chosenMonthFull.
                 toUpperCase(Locale.ROOT)), Integer.parseInt(CHOSEN_DAY)).format(formatterYearHyphenMonth));
-        pickerInMenuMP.selectMonth(chosenMonthTwo);
-        pickerInMenuMP.has().resultDate(LocalDate.of(currentYear, Month.valueOf(chosenMonthTwoFull.
+        pickerInMenuMonthPicker.selectMonth(chosenMonthTwo);
+        pickerInMenuMonthPicker.has().resultDate(LocalDate.of(currentYear, Month.valueOf(chosenMonthTwoFull.
                 toUpperCase(Locale.ROOT)), Integer.parseInt(CHOSEN_DAY)).format(formatterYearHyphenMonth));
-        pickerInMenuMP.nextYear();
-        pickerInMenuMP.has().year(Integer.toString(currentYear + 1));
-        pickerInMenuMP.previousYear();
-        pickerInMenuMP.previousYear();
-        pickerInMenuMP.has().year(Integer.toString(currentYear - 1));
-        pickerInMenuMP.changeYear();
-        pickerInMenuMP.selectYear(Integer.toString(currentYear + 99));
-        pickerInMenuMP.has().year(Integer.toString(currentYear + 99));
-        pickerInMenuMP.changeYear();
-        pickerInMenuMP.selectYear(Integer.toString(currentYear));
-        pickerInMenuMP.changeYear();
-        pickerInMenuMP.selectYear(Integer.toString(currentYear - 100));
-        pickerInMenuMP.has().year(Integer.toString(currentYear - 100));
-        pickerInMenuMP.clickOk();
-        pickerInMenuMP.expand();
-        pickerInMenuMP.has().monthFieldIsNotExist();
-        pickerInMenuMP.clickCancel();
-        pickerInDialogMP.expand();
-        pickerInDialogMP.has().monthField();
-        pickerInDialogMP.clickOk();
+        pickerInMenuMonthPicker.nextYear();
+        pickerInMenuMonthPicker.has().year(Integer.toString(currentYear + 1));
+        pickerInMenuMonthPicker.previousYear();
+        pickerInMenuMonthPicker.previousYear();
+        pickerInMenuMonthPicker.has().year(Integer.toString(currentYear - 1));
+        pickerInMenuMonthPicker.changeYear();
+        pickerInMenuMonthPicker.selectYear(Integer.toString(currentYear + 99));
+        pickerInMenuMonthPicker.has().year(Integer.toString(currentYear + 99));
+        pickerInMenuMonthPicker.changeYear();
+        pickerInMenuMonthPicker.selectYear(Integer.toString(currentYear));
+        pickerInMenuMonthPicker.changeYear();
+        pickerInMenuMonthPicker.selectYear(Integer.toString(currentYear - 100));
+        pickerInMenuMonthPicker.has().year(Integer.toString(currentYear - 100));
+        pickerInMenuMonthPicker.clickOk();
+        pickerInMenuMonthPicker.expand();
+        pickerInMenuMonthPicker.has().monthFieldIsNotExist();
+        pickerInMenuMonthPicker.clickCancel();
+        pickerInDialogMonthPicker.expand();
+        pickerInDialogMonthPicker.has().monthField();
+        pickerInDialogMonthPicker.clickOk();
     }
 
     @Test
     public void testInternationalizationMonthPicker() {
-        Timer.waitCondition(() -> thaiMP.isVisible());
-        jdiAssert(swedishMP.getAllMonths(), is(SWEDISH_SHORT_MONTHS),
+        Timer.waitCondition(() -> thaiMonthPicker.isVisible());
+        jdiAssert(swedishMonthPicker.getAllMonths(), is(SWEDISH_SHORT_MONTHS),
                 "For Swedish picker: shown and expected short month names are not the same");
         List<String> shownSwedishMonths = new ArrayList<>();
         SWEDISH_SHORT_MONTHS.stream().forEach(elem -> {
-            swedishMP.selectMonth(elem.toLowerCase());
-            swedishMP.hoverMonth(elem.toLowerCase());
-            shownSwedishMonths.add(swedishMP.getMonth());
+            swedishMonthPicker.selectMonth(elem.toLowerCase());
+            swedishMonthPicker.hoverMonth(elem.toLowerCase());
+            shownSwedishMonths.add(swedishMonthPicker.getMonth());
         });
         jdiAssert(shownSwedishMonths, is(SWEDISH_FULL_MONTHS),
                 "For Swedish picker: shown and expected full month names are not the same");
 
-        jdiAssert(thaiMP.getAllMonths(), is(THAI_SHORT_MONTHS),
+        jdiAssert(thaiMonthPicker.getAllMonths(), is(THAI_SHORT_MONTHS),
                 "For Thai picker: shown and expected short month names are not the same");
         List<String> shownThaiMonths = new ArrayList<>();
         THAI_SHORT_MONTHS.stream().forEach(elem -> {
-            thaiMP.selectMonth(elem);
-            thaiMP.hoverMonth(elem);
-            shownThaiMonths.add(thaiMP.getMonth());
+            thaiMonthPicker.selectMonth(elem);
+            thaiMonthPicker.hoverMonth(elem);
+            shownThaiMonths.add(thaiMonthPicker.getMonth());
         });
         jdiAssert(shownThaiMonths, is(THAI_FULL_MONTHS),
                 "For Thai picker: shown and expected full month names are not the same");
@@ -225,9 +225,9 @@ public class DatePickersMonthTests extends TestsInit {
 
     @Test
     public void testOrientationMonthPicker() {
-        Timer.waitCondition(() -> orientationMP.isVisible());
-        orientationMP.has().portraitOrientation();
-        orientationMP.switchOrientation();
-        orientationMP.has().landscapeOrientation();
+        Timer.waitCondition(() -> orientationMonthPicker.isVisible());
+        orientationMonthPicker.has().portraitOrientation();
+        orientationMonthPicker.switchOrientation();
+        orientationMonthPicker.has().landscapeOrientation();
     }
 }
