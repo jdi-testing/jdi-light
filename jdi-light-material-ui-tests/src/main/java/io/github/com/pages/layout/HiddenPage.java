7769fc4baefc420e9498c3501181a5d5b45160ef
Robert Andreev <andreev.robert.a@gmail.com>
2021-11-30 20:54:03 +0300

    Fix HiddenTests

--- jdi-light-material-ui-tests/src/main/java/io/github/com/pages/layout/HiddenPage.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/pages/layout/HiddenPage.java
@@ -2,26 +2,15 @@ package io.github.com.pages.layout;
 
 import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
-import com.epam.jdi.light.ui.html.elements.common.Button;
+import com.epam.jdi.light.material.elements.surfaces.Paper;
 import com.epam.jdi.light.ui.html.elements.common.Text;
+
+import java.util.List;
 
 public class HiddenPage extends WebPage {
     @UI(".MuiTypography-subtitle1")
     public static Text currentWidth;
 
-    @UI("//div[contains(text(),'xsDown')]")
-    public static Button xsDown;
-
-    @UI("//div[text()='smDown']")
-    public static Button smDown;
-
-    @UI("//div[text()='mdDown']")
-    public static Button mdDown;
-
-    @UI("//div[text()='lgDown']")
-    public static Button lgDown;
-
-    @UI("//div[text()='xlDown']")
-    public static Button xlDown;
-
+    @UI(".MuiPaper-root")
+    public static List<Paper> papers;
 }
--- jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/layout/HiddenTests.java
+++ jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/layout/HiddenTests.java
@@ -1,32 +1,24 @@
 package io.github.epam.material.tests.layout;
 
 import com.epam.jdi.light.driver.WebDriverFactory;
-import com.epam.jdi.light.ui.html.elements.common.Button;
-import com.epam.jdi.light.ui.html.elements.common.Text;
 import io.github.epam.TestsInit;
+import org.hamcrest.Matchers;
 import org.openqa.selenium.Dimension;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import static com.epam.jdi.light.common.Exceptions.exception;
+import static com.epam.jdi.light.asserts.core.SoftAssert.jdiAssert;
 import static io.github.com.StaticSite.hiddenPage;
 import static io.github.com.pages.layout.HiddenPage.currentWidth;
-import static io.github.com.pages.layout.HiddenPage.lgDown;
-import static io.github.com.pages.layout.HiddenPage.mdDown;
-import static io.github.com.pages.layout.HiddenPage.smDown;
-import static io.github.com.pages.layout.HiddenPage.xlDown;
-import static io.github.com.pages.layout.HiddenPage.xsDown;
+import static io.github.com.pages.layout.HiddenPage.papers;
 
 /**
  * To see an example of Hidden web element please visit
  * https://material-ui.com/components/hidden/
  */
-
 public class HiddenTests extends TestsInit {
-
-    private static final int WIDTH_LABEL_LENGTH = 15;
 
     @BeforeMethod
     public void before() {
@@ -34,53 +26,36 @@ public class HiddenTests extends TestsInit {
         hiddenPage.isOpened();
     }
 
+    @DataProvider(name = "Screen Width Dividers")
+    public Object[][] screenWidthDividers() {
+        return new Object[][]{
+                { 600 + 13, 0, "xs"},
+                { 600 + 14, 1, "sm"},
+                { 960 + 13, 1, "sm"},
+                { 960 + 14, 2, "md"},
+                {1280 + 13, 2, "md"},
+                {1280 + 14, 3, "lg"},
+                {1920 + 13, 3, "lg"},
+                {1920 + 14, 4, "xl"}};
+    }//600 960 1280 1920
+
+    @Test(dataProvider = "Screen Width Dividers")
+    public void hiddenTestWithScreenWidthDifferentScreenWidth(int width, int size, String expectedWidth) {
+        setWidth(width);
+        jdiAssert(papers.size(), Matchers.equalTo(size));
+        papers.forEach(element -> element.is().displayed());
+        currentWidth.has().text("Current width: " + expectedWidth);
+    }
+
+    private void setWidth(int width) {
+        Dimension currentScreenSize = WebDriverFactory.getDriver().manage().window().getSize();
+        Dimension newScreenSize = new Dimension(width, currentScreenSize.getHeight());
+        WebDriverFactory.getDriver().manage().window().setSize(newScreenSize);
+    }
+
     @AfterMethod
     public void after() {
         WebDriverFactory.getDriver().manage().window().maximize();
-    }
-
-    @Test(dataProvider = "Screen Width Dividers")
-    public void hiddenTestWithScreenWidthDifferentScreenWidth(int divider) {
-        currentWidth.is().displayed();
-        xsDown.is().displayed();
-        smDown.is().displayed();
-        mdDown.is().displayed();
-        divideScreenWidthSize(divider);
-        String width = getWidth(currentWidth);
-        checkWidth(width);
-    }
-
-    @DataProvider(name = "Screen Width Dividers")
-    public Object[][] screenWidthDividers() {
-        return new Object[][]{{1}, {2}, {3}};
-    }
-
-    private void checkWidth(String width) {
-        String[] widthArr = {"xs", "sm", "md", "lg", "xl"};
-        Button[] hiddenArr = {xsDown, smDown, mdDown, lgDown, xlDown};
-        int j = 0;
-        for (int i = j; !width.equals(widthArr[i]); i++) {
-            hiddenArr[i].is().displayed();
-            j++;
-        }
-        for (int i = j; i < widthArr.length; i++) {
-            hiddenArr[i].is().notVisible();
-        }
-    }
-
-    private void divideScreenWidthSize(int divider) {
-        Dimension lastScreenSize = WebDriverFactory.getDriver().manage().window().getSize();
-        try {
-            Dimension currentScreenSize = new Dimension(lastScreenSize.width / divider,
-                    lastScreenSize.height);
-            WebDriverFactory.getDriver().manage().window().setSize(currentScreenSize);
-        } catch (ArithmeticException e) {
-            throw exception("Wrong divider: " + divider);
-        }
-    }
-
-    private String getWidth(Text element) {
-        String text = element.getText();
-        return text.substring(WIDTH_LABEL_LENGTH);
+        WebDriverFactory.getDriver().manage().window().fullscreen();
     }
 }
