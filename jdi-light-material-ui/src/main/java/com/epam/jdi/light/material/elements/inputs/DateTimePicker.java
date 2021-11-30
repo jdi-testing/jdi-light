521b8ab493aba65e9d156b3d08e1a54ffb440c8d
Robert Andreev <andreev.robert.a@gmail.com>
2021-11-30 19:00:27 +0300

    Fix DateTimePicker because command("Enter") not worked when browser is unfocused

--- jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/elements/inputs/DateTimePicker.java
+++ jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/elements/inputs/DateTimePicker.java
@@ -4,6 +4,8 @@ import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.elements.base.UIBaseElement;
 import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.light.elements.complex.WebList;
+
+import static com.epam.jdi.light.elements.init.UIFactory.$;
 import static com.epam.jdi.light.elements.init.UIFactory.$$;
 import com.epam.jdi.light.material.asserts.inputs.DateTimePickerAssert;
 
@@ -55,7 +57,7 @@ public class DateTimePicker extends UIBaseElement<DateTimePickerAssert> {
 
     @JDIAction("Confirm changes in {name} and close")
     public void confirm() {
-        command("Enter");
+        $("//div[contains(@role,'dialog')]//*[contains(text(),'OK')]").click();
     }
 
     @JDIAction("Set text in {name} input field")
