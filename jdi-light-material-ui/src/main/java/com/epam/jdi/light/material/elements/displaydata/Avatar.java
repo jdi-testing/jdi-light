5bca8ecab8d5fd788c452f4390bd3320fb45f0ad
Aleksandr_Kashin2 <aleksandr_kashin2@epam.com>
2021-12-06 16:42:56 +0300

    refactored MUI avatars

--- jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/elements/displaydata/Avatar.java
+++ jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/elements/displaydata/Avatar.java
@@ -3,24 +3,21 @@ package com.epam.jdi.light.material.elements.displaydata;
 import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.elements.base.UIBaseElement;
 import com.epam.jdi.light.elements.interfaces.base.HasClick;
+import com.epam.jdi.light.elements.interfaces.common.IsText;
 import com.epam.jdi.light.material.asserts.displaydata.AvatarAssert;
 import com.epam.jdi.light.material.interfaces.base.HasImage;
+import com.epam.jdi.light.material.interfaces.displaydata.HasBadge;
 
 /**
  * To see an example of Avatar web element please visit
  * https://mui.com/components/avatars/
  */
 
-public class Avatar extends UIBaseElement<AvatarAssert> implements HasImage, HasClick {
+public class Avatar extends UIBaseElement<AvatarAssert> implements HasImage, HasBadge, HasClick, IsText {
 
-    @JDIAction("Get avatar '{name}' badge")
-    public boolean hasBadge() {
-        return core().children().get(3).hasClass("MuiBadge-badge");
-    }
-
-    @JDIAction("Get avatar '{name}' icon")
-    public boolean hasIcon() {
-        return core().firstChild().hasClass("MuiSvgIcon-root");
+    @JDIAction("Get avatar '{name}'s icon")
+    public Icon icon() {
+        return new Icon().setCore(Icon.class, find(".MuiSvgIcon-root"));
     }
 
     @Override
