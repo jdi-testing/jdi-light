a2646ee678f7cc2cc3ddb9ecac864596f4ab8675
Kratlis <kwork@fduch.ru>
2021-12-10 09:33:00 +0300

    refactored Grid

--- jdi-light-material-ui-tests/src/main/java/io/github/com/pages/layout/GridPage.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/pages/layout/GridPage.java
@@ -1,27 +1,17 @@
 package io.github.com.pages.layout;
 
-import com.epam.jdi.light.elements.complex.table.Grid;
 import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
-import com.epam.jdi.light.ui.html.elements.common.Button;
-import com.epam.jdi.light.ui.html.elements.common.TextField;
-
-import java.util.List;
+import com.epam.jdi.light.material.elements.layout.Grid;
 
 public class GridPage extends WebPage {
 
     @UI(".MuiContainer-root")
     public static Grid rootGrid;
 
-    @UI("//h2[text()='Complex grid']/preceding::div[contains(@class,'MuiGrid-spacing')]")
+    @UI("#basicGrid")
     public static Grid basicGrid;
 
-    @UI("//h2[text()='Complex grid']/following::div[contains(@class,'MuiPaper-rounded')]/div[contains(@class,'MuiGrid-spacing')]")
+    @UI("#complexGrid")
     public static Grid complexGrid;
-
-    @UI("//div[contains(@class,'MuiPaper-rounded')]")
-    public static List<Button> listButton;
-
-    @UI("//input[contains(@class,'MuiInput-input')]")
-    public static List<TextField> listInput;
 }
---
+++ jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/asserts/layout/GridAssert.java
@@ -0,0 +1,17 @@
+package com.epam.jdi.light.material.asserts.layout;
+
+import static com.epam.jdi.light.asserts.core.SoftAssert.jdiAssert;
+
+import com.epam.jdi.light.asserts.generic.UIAssert;
+import com.epam.jdi.light.common.JDIAction;
+import com.epam.jdi.light.material.elements.layout.Grid;
+import org.hamcrest.Matchers;
+
+public class GridAssert extends UIAssert<GridAssert, Grid> {
+
+    @JDIAction("Assert that {name} has {0} items")
+    public GridAssert items(int num) {
+        jdiAssert(element().items().size(), Matchers.is(num));
+        return this;
+    }
+}
