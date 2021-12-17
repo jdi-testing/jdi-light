dbfc978b9149f9383a24407c72a965fb2f74004d
Kratlis <kwork@fduch.ru>
2021-12-17 23:05:01 +0300

    Link is refactored

--- jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/elements/navigation/Link.java
+++ jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/elements/navigation/Link.java
@@ -1,71 +1,76 @@
 package com.epam.jdi.light.material.elements.navigation;
 
+import static com.epam.jdi.light.asserts.core.SoftAssert.assertSoft;
+
 import com.epam.jdi.light.common.JDIAction;
-import com.epam.jdi.light.elements.base.UIBaseElement;
+import com.epam.jdi.light.elements.interfaces.base.HasClick;
 import com.epam.jdi.light.material.asserts.navigation.LinkAssert;
+import com.epam.jdi.light.material.elements.displaydata.Typography;
+import com.epam.jdi.light.material.interfaces.base.HasColor;
 
 /**
  * To see an example of Links web element please visit
  * https://mui.com/components/links/
  */
 
-public class Link extends UIBaseElement<LinkAssert> {
+public class Link extends Typography implements HasColor, HasClick {
+
     @JDIAction("Click '{name}'")
     public void click() {
         core().click();
     }
 
-    @JDIAction("Is '{name}' color primary")
-    public boolean isColorPrimary() {
-        return core().hasClass("MuiTypography-colorPrimary");
+    @JDIAction("Is '{name}' underlined")
+    public boolean isUnderlined() {
+        return css("text-decoration-line").equals("underline");
     }
 
-    @JDIAction("Is '{name}' color secondary")
-    public boolean isColorSecondary() {
-        return core().hasClass("MuiTypography-colorSecondary");
+    @JDIAction("Is '{name}' not underlined")
+    public boolean isNotUnderlined() {
+        return !isUnderlined();
     }
 
-    @JDIAction("Is '{name}' color text primary")
-    public boolean isColorTextPrimary() {
-        return core().hasClass("MuiTypography-colorTextPrimary");
+    @JDIAction("Get '{name} text")
+    public String linkText() {
+        return core().getText();
     }
 
-    @JDIAction("Is '{name}' color text secondary")
-    public boolean isColorTextSecondary() {
-        return core().hasClass("MuiTypography-colorTextSecondary");
-    }
-
-    @JDIAction("Is '{name}' color error")
-    public boolean isColorError() {
-        return core().hasClass("MuiTypography-colorError");
-    }
-
-    @JDIAction("Is '{name}' underline always")
-    public boolean isUnderlineAlways() {
-        return core().hasClass("MuiLink-underlineAlways");
-    }
-
-    @JDIAction("Is '{name}' MuiTypography body 2")
-    public boolean isTypographyBody() {
-        return core().hasClass("MuiTypography-body2");
-    }
-
-    @JDIAction("Is '{name}' color inherit")
-    public boolean isColorInherit() {
-        return core().hasClass("MuiTypography-colorInherit");
-    }
-
-    @JDIAction("Is '{name}' underline hover")
-    public boolean isUnderlineHover() {
-        return core().hasClass("MuiLink-underlineHover");
+    @Override
+    public LinkAssert assertThat() {
+        return is();
     }
 
     @Override
     public LinkAssert is() {
-        return new LinkAssert().set(this);
+        LinkAssert linkAssert = new LinkAssert();
+        linkAssert.set(this);
+        return linkAssert;
     }
 
-    public String getLinkText() {
-        return core().getText();
+    @Override
+    public LinkAssert has() {
+        return is();
+    }
+
+    @Override
+    public LinkAssert waitFor() {
+        return is();
+    }
+
+    @Override
+    public LinkAssert waitFor(int sec) {
+        this.waitSec(sec);
+        return is();
+    }
+
+    @Override
+    public LinkAssert shouldBe() {
+        return is();
+    }
+
+    @Override
+    public LinkAssert verify() {
+        assertSoft();
+        return is();
     }
 }
