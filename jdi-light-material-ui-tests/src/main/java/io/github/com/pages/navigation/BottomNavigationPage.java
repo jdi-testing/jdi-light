19e774ce099822e2fa7e8b4b6267426682abfa1d
Elizaveta Rumyantseva <rumyantseva26@gmail.com>
2021-12-18 22:13:48 +0300

    Bottom Navigation tests refactoring

--- jdi-light-material-ui-tests/src/main/java/io/github/com/pages/navigation/BottomNavigationPage.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/pages/navigation/BottomNavigationPage.java
@@ -3,10 +3,18 @@ package io.github.com.pages.navigation;
 import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.material.elements.navigation.BottomNavigation;
+import com.epam.jdi.light.ui.html.elements.common.Text;
+
+import java.util.List;
 
 public class BottomNavigationPage extends WebPage {
 
     @UI(".MuiBottomNavigationAction-root")
     public static BottomNavigation bottomNavigationItems;
 
+    @UI(".MuiBottomNavigationAction-root>span>span")
+    public static List<Text> bottomNavigationItemsText;
+
+    @UI("#currentPosition")
+    public static Text currentPosition;
 }
