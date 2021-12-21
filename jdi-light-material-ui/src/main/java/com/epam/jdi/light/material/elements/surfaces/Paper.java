f25ea0bb971d654764630db23f3569bfc19b1225
Valeriia_Peters <valeriia_peters@epam.com>
2021-12-21 18:33:58 +0300

    Initial commit

--- jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/elements/surfaces/Paper.java
+++ jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/elements/surfaces/Paper.java
@@ -1,19 +1,25 @@
 package com.epam.jdi.light.material.elements.surfaces;
 
 import com.epam.jdi.light.asserts.generic.TextAssert;
+import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.elements.base.UIBaseElement;
+import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.light.elements.interfaces.base.HasClick;
 import com.epam.jdi.light.elements.interfaces.common.IsText;
+import com.epam.jdi.light.material.asserts.surfaces.PaperAssert;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
 
 /**
  * To see an example of Paper web element please visit
  * https://mui.com/components/paper/
  */
 
-public class Paper extends UIBaseElement<TextAssert> implements IsText, HasClick {
+public class Paper extends UIBaseElement<PaperAssert> {
 
     @Override
-    public TextAssert is() {
-        return new TextAssert().set(this);
+    public PaperAssert is() {
+        return new PaperAssert().set(this);
     }
 }
