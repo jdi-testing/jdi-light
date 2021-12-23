4c35422682df311f156fe7d4c40dc13aa1c9fd51
AndreySinelnikov <55104290+AndreySinelnikov@users.noreply.github.com>
2021-12-23 15:50:44 +0300

    Implement enhanced transfer list tests

--- jdi-light-material-ui-tests/src/main/java/io/github/com/pages/inputs/EnhancedTransferListPage.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/pages/inputs/EnhancedTransferListPage.java
@@ -1,12 +1,12 @@
 package io.github.com.pages.inputs;
 import com.epam.jdi.light.elements.composite.WebPage;
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.material.annotations.JDITransferList;
+import com.epam.jdi.light.material.elements.inputs.EnhancedTransferListNew;
 import com.epam.jdi.light.material.elements.inputs.transferlist.EnhancedTransferList;
 
 public class EnhancedTransferListPage extends WebPage {
 
-    @JDITransferList(root = "#root", allItemsLeftCheckbox = "(//span[./input[@aria-label='all items selected']])[1]",
-        allItemsRightCheckbox = "(//span[./input[@aria-label='all items selected']])[2]")
-    public static EnhancedTransferList enhancedTransferList;
-
+    @UI(".MuiGrid-justify-xs-center")
+    public static EnhancedTransferListNew enhancedTransferListNew;
 }
--- jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/inputs/TransferListTestsNew.java
+++ jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/inputs/TransferListTestsNew.java
@@ -3,10 +3,14 @@ package io.github.epam.material.tests.inputs;
 import io.github.epam.TestsInit;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import static io.github.com.StaticSite.enhancedTransferListPage;
 import static io.github.com.StaticSite.simpleTransferListPage;
+import static io.github.com.pages.inputs.EnhancedTransferListPage.enhancedTransferListNew;
 import static io.github.com.pages.inputs.SimpleTransferListPage.simpleTransferListNew;
 
 public class TransferListTestsNew extends TestsInit {
@@ -28,13 +32,14 @@ public class TransferListTestsNew extends TestsInit {
         simpleTransferListNew.leftList().checkItemByText(textOfItemToTransfer);
         simpleTransferListNew.leftList().getItemByText(textOfItemToTransfer).is().checked();
         simpleTransferListNew.moveCheckedItemsRight();
-        simpleTransferListNew.rightList().has().itemsWithTexts(Collections.singleton(textOfItemToTransfer));
+        simpleTransferListNew.rightList().has().itemWithText(textOfItemToTransfer);
 
         // Transfer items by interacting with element components
         simpleTransferListNew.rightList().getItemByText(textOfItemToTransfer).is().unchecked();
         simpleTransferListNew.rightList().getItemByText(textOfItemToTransfer).checkbox().check();
+        simpleTransferListNew.has().allRightListItemsChecked();
         simpleTransferListNew.moveCheckedItemsLeftButton().click();
-        simpleTransferListNew.leftList().has().itemsWithTexts(Collections.singleton(textOfItemToTransfer));
+        simpleTransferListNew.leftList().has().itemWithText(textOfItemToTransfer);
     }
 
     @Test
@@ -42,5 +47,26 @@ public class TransferListTestsNew extends TestsInit {
         enhancedTransferListPage.open();
         enhancedTransferListPage.checkOpened();
 
+        enhancedTransferListNew.leftListHeader().has().text("Choices");
+        enhancedTransferListNew.has().allLeftListItemsUnchecked();
+        enhancedTransferListNew.leftListSubheader().has().text("0/4 selected");
+        enhancedTransferListNew.checkAllLeftItems();
+        enhancedTransferListNew.has().allLeftListItemsChecked();
+        enhancedTransferListNew.leftListSubheader().has().text("4/4 selected");
+
+        enhancedTransferListNew.rightListHeader().has().text("Chosen");
+        enhancedTransferListNew.rightListSubheader().has().text("0/4 selected");
+        enhancedTransferListNew.moveCheckedItemsRight();
+
+        Set<String> allItemTextsSet = new HashSet<>(Arrays.asList("List item 1", "List item 2", "List item 3",
+                "List item 4", "List item 5", "List item 6", "List item 7", "List item 8"));
+
+        enhancedTransferListNew.rightList().has().itemsWithTexts(allItemTextsSet);
+        enhancedTransferListNew.rightListSubheader().has().text("0/8 selected");
+        enhancedTransferListNew.checkAllRightItems();
+        enhancedTransferListNew.uncheckAllRightItems();
+        enhancedTransferListNew.has().allRightListItemsUnchecked();
+        enhancedTransferListNew.rightList().checkItemByText("List item 7");
+        enhancedTransferListNew.moveCheckedItemsLeft();
     }
 }
