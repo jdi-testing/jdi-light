2a03057b548f8e228aecaba9fc4914db71f0fd02
Valeriia_Peters <valeriia_peters@epam.com>
2021-12-27 15:23:59 +0300

    Deleted unused inports

--- jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/elements/surfaces/Paper.java
+++ jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/elements/surfaces/Paper.java
@@ -1,15 +1,7 @@
 package com.epam.jdi.light.material.elements.surfaces;
 
-import com.epam.jdi.light.asserts.generic.TextAssert;
-import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.elements.base.UIBaseElement;
-import com.epam.jdi.light.elements.common.UIElement;
-import com.epam.jdi.light.elements.interfaces.base.HasClick;
-import com.epam.jdi.light.elements.interfaces.common.IsText;
 import com.epam.jdi.light.material.asserts.surfaces.PaperAssert;
-
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.not;
 
 /**
  * To see an example of Paper web element please visit
