8dd7bdc5dc022c2d65196270c881bf7762978d79
AndreySinelnikov <55104290+AndreySinelnikov@users.noreply.github.com>
2022-01-10 13:11:54 +0300

    Rename ChipGroupsTests to ChipGroupTest; refactor ChipGroupsPage, ChipGroupTests

--- jdi-light-vuetify-tests/src/main/java/io/github/com/pages/ChipGroupsPage.java
+++ jdi-light-vuetify-tests/src/main/java/io/github/com/pages/ChipGroupsPage.java
@@ -4,19 +4,18 @@ import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.ui.html.elements.complex.Tabs;
 import com.epam.jdi.light.vuetify.elements.common.Button;
 import com.epam.jdi.light.vuetify.elements.complex.Card;
-import com.epam.jdi.light.vuetify.elements.complex.ChipGroups;
-import java.util.List;
+import com.epam.jdi.light.vuetify.elements.complex.ChipGroup;
 
 public class ChipGroupsPage extends VuetifyPage {
 
-    @UI("#ColumnChipGroup .v-chip")
-    public static List<ChipGroups> columnChipGroup;
+    @UI("#ColumnChipGroup .v-chip-group")
+    public static ChipGroup columnChipGroup;
 
-    @UI("#FilterResultsChipGroup .v-chip")
-    public static List<ChipGroups> filterResultsChipGroup;
+    @UI("#FilterResultsChipGroup .v-chip-group")
+    public static ChipGroup filterResultsChipGroup;
 
-    @UI("#MandatoryChipGroup .v-chip")
-    public static List<ChipGroups> mandatoryChipGroup;
+    @UI("#MandatoryChipGroup .v-chip-group")
+    public static ChipGroup mandatoryChipGroup;
 
     @UI("#MandatoryChipGroup .v-slide-group__next")
     public static Tabs mandatoryGroupButtonNext;
@@ -24,8 +23,8 @@ public class ChipGroupsPage extends VuetifyPage {
     @UI("#MandatoryChipGroup .v-slide-group__prev")
     public static Tabs mandatoryGroupButtonBack;
 
-    @UI("#MultipleChipGroup .v-slide-group__wrapper .v-chip")
-    public static List<ChipGroups> multipleChipGroup;
+    @UI("#MultipleChipGroup .v-chip-group")
+    public static ChipGroup multipleChipGroup;
 
     @UI("#MultipleChipGroup .v-slide-group__next")
     public static Tabs multipleGroupButtonNext;
@@ -33,8 +32,8 @@ public class ChipGroupsPage extends VuetifyPage {
     @UI("#MultipleChipGroup .v-slide-group__prev")
     public static Tabs multipleGroupButtonBack;
 
-    @UI("#ProductCardChipGroup .v-chip")
-    public static List<ChipGroups> productCardChipGroup;
+    @UI("#ProductCardChipGroup .v-chip-group")
+    public static ChipGroup productCardChipGroup;
 
     @UI("#ProductCardChipGroup .v-btn")
     public static Button shirtBlouseAddToCartButton;
@@ -42,8 +41,8 @@ public class ChipGroupsPage extends VuetifyPage {
     @UI("#ProductCardChipGroup")
     public static Card shirtBlouseText;
 
-    @UI("#ToothbrushCardChipGroup .v-chip")
-    public static List<ChipGroups> toothbrushCardChipGroup;
+    @UI("#ToothbrushCardChipGroup .v-chip-group")
+    public static ChipGroup toothbrushCardChipGroup;
 
     @UI("#ToothbrushCardChipGroup .v-btn")
     public static Button toothbrushAddToCardButton;
