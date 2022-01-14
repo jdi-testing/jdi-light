b93a55ac3341c1b5d28612d35a9f799565c48d82
Kratlis <kwork@fduch.ru>
2022-01-14 19:58:12 +0300

    badge checks added too AvatarTests

--- jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/displaydata/AvatarTests.java
+++ jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/displaydata/AvatarTests.java
@@ -1,16 +1,26 @@
 package io.github.epam.material.tests.displaydata;
 
-import com.epam.jdi.light.material.elements.displaydata.Avatar;
-import io.github.epam.TestsInit;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
 import static io.github.com.StaticSite.avatarPage;
 import static io.github.com.pages.displaydata.AvatarPage.avatarsWithIcon;
 import static io.github.com.pages.displaydata.AvatarPage.avatarsWithPhoto;
 import static io.github.com.pages.displaydata.AvatarPage.avatarsWithText;
 
+import com.epam.jdi.light.material.elements.displaydata.Avatar;
+import com.epam.jdi.light.material.elements.displaydata.Badge;
+import io.github.epam.TestsInit;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 public class AvatarTests extends TestsInit {
+
+    @DataProvider
+    public static Object[][] avatarsWithBadge() {
+        return new Object[][]{
+                {avatarsWithPhoto.get(1), true, ""},
+                {avatarsWithPhoto.get(2), false, "R"}
+        };
+    }
 
     @BeforeMethod
     public void beforeTest() {
@@ -19,7 +29,7 @@ public class AvatarTests extends TestsInit {
 
     @Test
     public void avatarsWithTextTests() {
-        for(Avatar avatar : avatarsWithText) {
+        for (Avatar avatar : avatarsWithText) {
             avatar.is().displayed();
         }
         avatarsWithText.get(1).is().text("L");
@@ -28,16 +38,27 @@ public class AvatarTests extends TestsInit {
 
     @Test
     public void avatarsWithPhotoTests() {
-        for(Avatar avatar : avatarsWithPhoto) {
+        for (Avatar avatar : avatarsWithPhoto) {
             avatar.is().displayed();
             avatar.image().is().displayed();
             avatar.badge().is().displayed();
         }
     }
 
+    @Test(dataProvider = "avatarsWithBadge")
+    public void avatarsWithBadgeTests(Avatar avatar, boolean isDot, String badgeText) {
+        avatar.is().displayed();
+        Badge badge = avatar.badge();
+        badge.is().displayed();
+        if (isDot) {
+            badge.is().dot();
+        }
+        badge.has().text(badgeText).and().position("BottomRightCircle");
+    }
+
     @Test
     public void avatarsWithIconTests() {
-        for(Avatar avatar : avatarsWithIcon) {
+        for (Avatar avatar : avatarsWithIcon) {
             avatar.is().displayed();
             avatar.icon().is().displayed();
             avatar.image().is().hidden();
