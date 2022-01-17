2e1f40733010edde47c3d61e0503d970e7363ea9
Evgenii Kuznetsov <vgenii_Kuznetsov3@epam.com>
2022-01-17 14:47:03 +0300

    Deleted unused import

--- jdi-light-material-ui-tests/src/main/java/io/github/com/pages/utils/PopperPage.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/pages/utils/PopperPage.java
@@ -2,7 +2,6 @@ package io.github.com.pages.utils;
 
 import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
-import com.epam.jdi.light.material.elements.displaydata.Typography;
 import com.epam.jdi.light.material.elements.utils.Popper;
 import com.epam.jdi.light.ui.html.elements.common.Text;
 import io.github.com.custom.TooltipButton;
