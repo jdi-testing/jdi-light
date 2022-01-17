ac24cb84ed6a6564b1b4949fa53d050326dbe0ae
Evgenii Kuznetsov <vgenii_Kuznetsov3@epam.com>
2022-01-17 13:06:13 +0300

    Tests were refactored. JDIActions were added in elements

--- jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/inputs/TransferListTests.java
+++ jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/inputs/TransferListTests.java
@@ -1,65 +1,61 @@
 package io.github.epam.material.tests.inputs;
 
+import com.epam.jdi.light.material.elements.inputs.transferlist.TransferList;
 import static io.github.com.StaticSite.enhancedTransferListPage;
 import static io.github.com.StaticSite.simpleTransferListPage;
 import static io.github.com.pages.inputs.EnhancedTransferListPage.enhancedTransferList;
 import static io.github.com.pages.inputs.SimpleTransferListPage.simpleTransferList;
-
 import io.github.epam.TestsInit;
+import java.util.Arrays;
+import java.util.List;
 import org.testng.annotations.Test;
 
 public class TransferListTests extends TestsInit {
+    List<String> items = Arrays.asList("List item 1", "List item 2",
+            "List item 3", "List item 4", "List item 5", "List item 6", "List item 7", "List item 8");
 
     @Test
     public void simpleTransferListTest() {
         simpleTransferListPage.open();
-        simpleTransferList.is().isMoveRightButtonDisable();
-        simpleTransferList.is().isMoveLeftButtonDisable();
+        simpleTransferList.assertThat().isMoveRightButtonDisable().and().isMoveLeftButtonDisable();
 
-        simpleTransferList.check("List item 1");
-        simpleTransferList.is().checked("List item 1");
+        simpleTransferList.check(items.get(0));
+        simpleTransferList.is().checked(items.get(0));
 
         simpleTransferList.is().isMoveRightButtonEnable();
         simpleTransferList.moveItemsRight();
-        simpleTransferList.is().itemsMovedRight("List item 1");
+        simpleTransferList.is().itemsMovedRight(items.get(0));
 
-        simpleTransferList.check("List item 5");
-        simpleTransferList.check("List item 6");
-        simpleTransferList.is().checked("List item 5");
-        simpleTransferList.is().checked("List item 6");
+        simpleTransferList.check(items.get(4));
+        simpleTransferList.check(items.get(5));
+        simpleTransferList.is().checked(items.get(4));
+        simpleTransferList.is().checked(items.get(5));
         simpleTransferList.is().isMoveLeftButtonEnable();
         simpleTransferList.moveItemsLeft();
-        simpleTransferList.is().itemsMovedLeft("List item 5", "List item 6");
+        simpleTransferList.is().itemsMovedLeft(items.get(4), items.get(5));
 
-        simpleTransferList.moveAllElementsRight();
-        simpleTransferList.is().itemsMovedRight("List item 1", "List item 2",
-            "List item 3", "List item 4", "List item 5", "List item 6", "List item 7", "List item 8");
-
-        simpleTransferList.moveAllElementsLeft();
-        simpleTransferList.is().itemsMovedLeft("List item 1", "List item 2",
-          "List item 3", "List item 4", "List item 5", "List item 6", "List item 7", "List item 8");
+        checkMoveElementsFunction(simpleTransferList, items.toArray(new String[0]));
     }
 
     @Test
     public void enhancedTransferListTest() {
         enhancedTransferListPage.open();
-        enhancedTransferList.is().isMoveRightButtonDisable();
-        enhancedTransferList.is().isMoveLeftButtonDisable();
+        enhancedTransferList.assertThat().isMoveRightButtonDisable().and().isMoveLeftButtonDisable();
 
-        enhancedTransferList.check("List item 1");
-        enhancedTransferList.is().checked("List item 1");
+        enhancedTransferList.check(items.get(0));
+        enhancedTransferList.is().checked(items.get(0));
         enhancedTransferList.is().isMoveRightButtonEnable();
 
-        enhancedTransferList.uncheck("List item 1");
-        enhancedTransferList.is().unchecked("List item 1");
+        enhancedTransferList.uncheck(items.get(0));
+        enhancedTransferList.is().unchecked(items.get(0));
 
-        enhancedTransferList.moveAllElementsRight();
-        enhancedTransferList.is().itemsMovedRight("List item 1", "List item 2",
-            "List item 3", "List item 4", "List item 5", "List item 6", "List item 7", "List item 8");
-
-        enhancedTransferList.moveAllElementsLeft();
-        enhancedTransferList.is().itemsMovedLeft("List item 1", "List item 2",
-            "List item 3", "List item 4", "List item 5", "List item 6", "List item 7", "List item 8");
+        checkMoveElementsFunction(enhancedTransferList, items.toArray(new String[0]));
     }
 
+    private static void checkMoveElementsFunction(TransferList transferList, String... checkingItems) {
+        transferList.moveAllElementsRight();
+        transferList.has().itemsMovedRight(checkingItems);
+        transferList.moveAllElementsLeft();
+        transferList.has().itemsMovedLeft(checkingItems);
+    }
 }
--- jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/elements/inputs/transferlist/SimpleTransferList.java
+++ jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/elements/inputs/transferlist/SimpleTransferList.java
@@ -1,5 +1,7 @@
 package com.epam.jdi.light.material.elements.inputs.transferlist;
 
+import com.epam.jdi.light.common.JDIAction;
+
 /**
  * To see an example of Simple Transfer List web element please visit
  * https://mui.com/components/transfer-list/#basic-transfer-list
@@ -7,11 +9,13 @@ package com.epam.jdi.light.material.elements.inputs.transferlist;
 
 public class SimpleTransferList extends TransferList {
 
+  @JDIAction("Moves all {name}'s elements left")
   @Override
   public void moveAllElementsLeft() {
     this.find(moveAllLeftButton).click();
   }
 
+  @JDIAction("Moves all {name}'s elements right")
   @Override
   public void moveAllElementsRight() {
     this.find(moveAllRightButton).click();
