d4500f893e71b1935686f75fa01ed1815fd05e0e
Vladislav_Prokopenko <daark_light@mail.ru>
2022-01-21 09:26:23 +0300

    WindowsTests: waitCondition was added to improve test stability

--- jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/complex/WindowsTests.java
+++ jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/complex/WindowsTests.java
@@ -108,8 +108,12 @@ public class WindowsTests extends TestsInit {
 
         AccountCreatedWindow accountCreatedWindow = accountCreationWindows.getActive(AccountCreatedWindow.class);
         accountCreatedWindow.image().has().attr("style", "height: 128px;");
-        accountCreatedWindow.image().find(".v-image__image")
-                .has().attr("style", containsString("background-image: url(\"https://cdn.vuetifyjs.com/images/logos/v.svg\");"));
+        waitCondition(() -> accountCreatedWindow.image()
+                .find("div.v-image__image.v-image__image--contain").attr("syle")
+                .contains("background-image: url(\"https://cdn.vuetifyjs.com/images/logos/v.svg\")"));
+        accountCreatedWindow.image().find("div.v-image__image.v-image__image--contain")
+                .has().attr("style", containsString("background-image: " +
+                        "url(\"https://cdn.vuetifyjs.com/images/logos/v.svg\")"));
         accountCreatedWindow.welcome().has().text("Welcome to Vuetify");
         accountCreatedWindow.caption().has().text("Thanks for signing up!");
         accountNext.isDisabled();
