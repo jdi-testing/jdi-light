f8817dafd8d0f4349c8e6216c2c887cd9ba2d603
Kratlis <kwork@fduch.ru>
2022-02-11 14:22:05 +0300

    added elements on page

--- jdi-light-vuetify-tests/src/main/java/io/github/com/pages/ItemGroupsPage.java
+++ jdi-light-vuetify-tests/src/main/java/io/github/com/pages/ItemGroupsPage.java
@@ -1,7 +1,10 @@
 package io.github.com.pages;
 
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
+import com.epam.jdi.light.vuetify.elements.common.VuetifyButton;
 import com.epam.jdi.light.vuetify.elements.complex.ItemGroup;
+import com.epam.jdi.light.vuetify.elements.complex.TextArea;
+import com.epam.jdi.light.vuetify.elements.complex.TextField;
 import io.github.com.custom.itemGroups.PrimaryItemGroup;
 import io.github.com.custom.itemGroups.SelectionItemGroup;
 
@@ -12,6 +15,21 @@ public class ItemGroupsPage extends VuetifyPage {
 
     @UI("#MandatoryItemGroup .v-card")
     public static ItemGroup mandatoryItemGroup;
+
+    @UI("#MultipleItemGroup .v-card")
+    public static ItemGroup multipleItemGroup;
+
+    @UI("#ChipsItemGroup .v-text-field")
+    public static TextField textField;
+
+    @UI("#ChipsItemGroup .v-textarea")
+    public static TextArea textArea;
+
+    @UI("#ChipsItemGroup .v-chip")
+    public static ItemGroup chipsItemGroup;
+
+    @UI("#ChipsItemGroup button")
+    public static VuetifyButton button;
 
     @UI("#SelectionItemGroup .v-image")
     public static SelectionItemGroup selectionItemGroup;
