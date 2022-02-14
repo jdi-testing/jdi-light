e170dea0d01a5968ef5a602baaeca3aa26d9f325
Maksim_Gudovich <Maksim_Gudovich@epam.com>
2022-02-14 18:01:05 +0300

    getIcon() moved from CustomAlert to Alert class. getValue() method and HasValue interface removed from Alert class.

--- jdi-light-vuetify-tests/src/main/java/io/github/com/custom/CustomAlert.java
+++ jdi-light-vuetify-tests/src/main/java/io/github/com/custom/CustomAlert.java
@@ -6,13 +6,7 @@ import com.epam.jdi.light.vuetify.elements.common.Alert;
 
 public class CustomAlert extends Alert {
 
-    protected static final String DEFAULT_ICON_LOCATOR = ".v-alert__icon";
     protected static final String DEFAULT_BUTTON_LOCATOR = "button";
-
-    @JDIAction("Get icon from '{name}'")
-    public UIElement getIcon() {
-        return linked(DEFAULT_ICON_LOCATOR, String.format("%s.icon", getName()));
-    }
 
     @JDIAction("Get button from '{name}'")
     public UIElement getButton() {
