6dc03f13f42f62c457544bbbb6e95114c8446359
ermekbaeva <ermekbaevameerim@gmail.com>
2022-02-24 14:59:11 +0300

    check if expandButton idDisplayed was added for CollapsedBreadcrumbs

--- jdi-light-material-ui-tests/src/main/java/io/github/com/custom/elements/CollapsedBreadcrumbs.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/custom/elements/CollapsedBreadcrumbs.java
@@ -1,14 +1,23 @@
 package io.github.com.custom.elements;
 
+import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.material.elements.navigation.Breadcrumbs;
 import com.epam.jdi.light.ui.html.elements.common.Button;
 
 public class CollapsedBreadcrumbs extends Breadcrumbs {
+
     @UI(".MuiButtonBase-root")
     protected Button expandButton;
 
+    @JDIAction("Get '{name}'s 'expand' button")
     public void expand() {
-        expandButton.click();
+        if (isDisplayed()) {
+            expandButton.click();
+        }
+    }
+
+    public boolean isDisplayed() {
+        return expandButton.isDisplayed();
     }
 }
