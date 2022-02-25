7ad25889244ce7909889db093a97f833aaf90aaf
IvKardapolov <Hx5Z3CWdYKT3agq>
2022-02-25 14:48:56 +0400

    Изменения по код-ревью. Удалил has() в Menu, поправил ContextMenuPage, изменил проверку contextMenuTest

--- jdi-light-material-ui-tests/src/main/java/io/github/com/pages/navigation/ContextMenuPage.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/pages/navigation/ContextMenuPage.java
@@ -3,7 +3,6 @@ package io.github.com.pages.navigation;
 import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.material.elements.displaydata.List;
-import com.epam.jdi.light.material.elements.navigation.Menu;
 import com.epam.jdi.light.ui.html.elements.common.Button;
 
 public class ContextMenuPage extends WebPage {
