a9f96bc829b86d5bad3c9030929d6097247ec76e
Kratlis <kwork@fduch.ru>
2022-02-28 09:12:06 +0300

    refactored menu

---
+++ jdi-light-material-ui-tests/src/test/java/io/github/epam/test/data/MenuDataProvider.java
@@ -0,0 +1,23 @@
+package io.github.epam.test.data;
+
+import static io.github.com.pages.navigation.SimpleMenuPage.iconMenuButton;
+import static io.github.com.pages.navigation.SimpleMenuPage.scrollMenuButton;
+import static io.github.com.pages.navigation.SimpleMenuPage.selectedSelectedMenuButton;
+import static io.github.com.pages.navigation.SimpleMenuPage.simpleMenuButton;
+
+import java.util.Arrays;
+import org.testng.annotations.DataProvider;
+
+public class MenuDataProvider {
+
+    @DataProvider
+    public Object[][] simpleMenuItemsTestData() {
+        return new Object[][]{
+            {simpleMenuButton, Arrays.asList("Profile", "My account", "Logout")},
+            {iconMenuButton, Arrays.asList("Text with send icon", "Text with priority icon", "Text with drafts icon")},
+            {selectedSelectedMenuButton, Arrays.asList("Profile", "My account", "Logout")},
+            {scrollMenuButton, Arrays.asList("None", "Atria", "Callisto", "Dione", "Ganymede", "Hangouts Call", "Luna",
+                "Oberon", "Phobos", "Pyxis", "Sedna", "Titania", "Triton", "Umbriel")},
+        };
+    }
+}
