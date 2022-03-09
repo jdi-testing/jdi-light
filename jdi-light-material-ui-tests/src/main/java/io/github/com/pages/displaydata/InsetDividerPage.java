e4b0c0a068038760331e55437545050b97c0e6d2
ermekbaeva <ermekbaevameerim@gmail.com>
2022-03-09 15:28:34 +0300

    locators was edited

--- jdi-light-material-ui-tests/src/main/java/io/github/com/pages/displaydata/InsetDividerPage.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/pages/displaydata/InsetDividerPage.java
@@ -3,6 +3,8 @@ package io.github.com.pages.displaydata;
 import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.material.elements.displaydata.Divider;
+import com.epam.jdi.light.material.elements.displaydata.Icon;
+import com.epam.jdi.light.ui.html.elements.common.Text;
 
 import java.util.List;
 
@@ -11,6 +13,12 @@ public class InsetDividerPage extends WebPage {
     @UI("li.MuiDivider-root")
     public static List<Divider> insetDividers;
 
-    @UI("ul.MuiList-root")
-    public static List itemWithTextList;
+    @UI("//div[@class='MuiListItemAvatar-root']")
+    public static List<Icon> iconList;
+
+    @UI("//div[@class='MuiListItemText-root MuiListItemText-multiline']//span")
+    public static List<Text> titleTextList;
+
+    @UI("//div[@class='MuiListItemText-root MuiListItemText-multiline']//p")
+    public static List<Text> dateTextList;
 }
