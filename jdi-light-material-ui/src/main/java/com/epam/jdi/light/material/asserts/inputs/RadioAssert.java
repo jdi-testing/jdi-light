812df3af87dfc1cd38b211a3ddc419ac77adb7e0
Roman_Vasilev1 <Roman_Vasilev1@epam.com>
2022-03-25 12:06:07 +0300

    Compilation error fixed.

--- jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/asserts/inputs/RadioAssert.java
+++ jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/asserts/inputs/RadioAssert.java
@@ -9,14 +9,14 @@ public class RadioAssert extends UISelectAssert<RadioAssert, RadioButtons> {
 
     @JDIAction("Assert that '{name}' radio button {0} label has position '{1}'")
     public RadioAssert position(int index, Position labelPosition) {
-        element().labels().get(index).hasClass(labelPosition.getPosition());
+        element().labels().get(index).hasClass(labelPosition.toString());
         return this;
     }
 
     @JDIAction("Assert that '{name}' radio button '{0}' label has position '{1}'")
     public RadioAssert position(String value, Position labelPosition) {
         int index = element().values().indexOf(value);
-        element().labels().get(index).hasClass(labelPosition.getPosition());
+        element().labels().get(index).hasClass(labelPosition.toString());
         return this;
     }
 }
