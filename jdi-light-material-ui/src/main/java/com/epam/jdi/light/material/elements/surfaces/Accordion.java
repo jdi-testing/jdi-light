71cf1eb8a1b068cba75dec7fe8ca49959a19cb13
Iuliia_Liubavina <iuliia_liubavina@epam.com>
2022-03-25 15:35:49 +0300

    Created javadoc for accordion and button group.

--- jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/elements/surfaces/Accordion.java
+++ jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/elements/surfaces/Accordion.java
@@ -9,24 +9,40 @@ import com.epam.jdi.light.material.interfaces.base.CanBeDisabled;
 import com.epam.jdi.light.ui.html.elements.common.Button;
 
 /**
- * To see an example of Accordion web element please visit
- * https://mui.com/components/accordion/
+ * Represents accordion MUI component on GUI.
+ *
+ * @see <a href="https://mui.com/components/accordion/">Accordion MUI documentation</a>
+ * @see <a href="https://jdi-testing.github.io/jdi-light/material">MUI test page</a>
  */
-
 public class Accordion extends UIBaseElement<AccordionAssert> implements CanBeDisabled {
 
+    /**
+     * Button that expands this accordion.
+     */
     @UI(".MuiAccordionSummary-expandIcon")
     public Button expandButton;
 
+    /**
+     * Content of this accordion.
+     */
     @UI(".MuiAccordionDetails-root .MuiTypography-root")
     public Typography content;
 
+    /**
+     * First header of this accordion.
+     */
     @UI(".MuiAccordionSummary-content .MuiTypography-root[1]")
     public Typography firstHeader;
 
+    /**
+     * Second header of this accordion.
+     */
     @UI(".MuiAccordionSummary-content .MuiTypography-root[2]")
     public Typography secondHeader;
 
+    /**
+     * Expands this accordion, if possible. If not possible, does nothing.
+     */
     @JDIAction("Expand '{name}'")
     public void expand() {
         if (isCollapsed()) {
@@ -34,6 +50,9 @@ public class Accordion extends UIBaseElement<AccordionAssert> implements CanBeDi
         }
     }
 
+    /**
+     * Collapses this accordion, if possible. If not possible, does nothing.
+     */
     @JDIAction("Collapse '{name}'")
     public void collapse() {
         if (isExpanded()) {
@@ -41,11 +60,21 @@ public class Accordion extends UIBaseElement<AccordionAssert> implements CanBeDi
         }
     }
 
+    /**
+     * Checks if this accordion is expanded or not.
+     *
+     * @return {@code true} if this accordion is expanded, otherwise {@code false}
+     */
     @JDIAction("Check that '{name}' is expanded")
     public boolean isExpanded() {
         return core().hasClass("Mui-expanded");
     }
 
+    /**
+     * Checks if this accordion is collapsed or not.
+     *
+     * @return {@code true} if this accordion is collapsed, otherwise {@code false}
+     */
     @JDIAction("Check that '{name}' is collapsed")
     public boolean isCollapsed() {
         return !isExpanded();
