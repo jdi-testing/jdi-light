6912ae210edb64ab2ea1cf53dc4cde3073300b57
Iuliia_Liubavina <iuliia_liubavina@epam.com>
2022-03-29 15:10:35 +0300

    Javadoc for annotation JDIButtonGroup added

--- jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/annotations/JDIButtonGroup.java
+++ jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/annotations/JDIButtonGroup.java
@@ -1,14 +1,23 @@
 package com.epam.jdi.light.material.annotations;
 
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.MarkupLocator;
+import com.epam.jdi.light.material.elements.inputs.ButtonGroup;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation for setting locators for buttons in {@link ButtonGroup}
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.FIELD})
 public @interface JDIButtonGroup {
+    /**
+     * Locator for buttons in {@link ButtonGroup}.
+     *
+     * @return locator for buttons in {@link ButtonGroup}
+     */
     @MarkupLocator String list() default "";
 }
