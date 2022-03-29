0a843a7d796b97e5bbd6cbc950fc11392b399b2e
Mary <mariyakat@yandex.ru>
2022-03-29 13:20:18 +0300

    Gh pages material (#3925)
    * Update Lists page with element IDs
    
    * Update auxiliary js files
    
    * add alerts page and fix badge with switch, 3506, 3615 (#3784)
    
    
    
    Co-authored-by: Mariia Katkova <mariia_katkova@epam.com>
    
    * 3922 add nested list, 3883 add indeterminate state for checkboxes
    
    Co-authored-by: AndreySinelnikov <55104290+AndreySinelnikov@users.noreply.github.com>
    Co-authored-by: Mariia Katkova <mariia_katkova@epam.com>

--- material/_next/static/chunks/webpack.js
+++ material/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3d49e89247e92ef83fdd";
+/******/ 	var hotCurrentHash = "4355e6cb8ea44a5644e6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
