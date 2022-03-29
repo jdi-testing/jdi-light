3b811ca7441f9228eba3bc4aca062db1aa3481bf
Iuliia_Liubavina <iuliia_liubavina@epam.com>
2022-03-29 12:42:10 +0300

    Created javadoc for assertions for accordion and button group.

--- jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/asserts/inputs/ButtonGroupAssert.java
+++ jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/asserts/inputs/ButtonGroupAssert.java
@@ -10,14 +10,29 @@ import java.util.List;
 
 import static com.epam.jdi.light.asserts.core.SoftAssert.jdiAssert;
 
+/**
+ * Assertions for {@link ButtonGroup}
+ */
 public class ButtonGroupAssert extends UISelectAssert<ButtonGroupAssert, ButtonGroup> {
 
+    /**
+     * Checks that button group has given number of buttons.
+     *
+     * @param number number of buttons to compare
+     * @return {@link ButtonGroupAssert}
+     */
     @JDIAction("Number of grouped buttons in '{name}' is '{0}'")
     public ButtonGroupAssert numberOfGroupedButtons(Integer number) {
         jdiAssert(element().getAllButtons().size(), Matchers.is(number));
         return this;
     }
 
+    /**
+     * Checks that button group contains given texts in any order.
+     *
+     * @param texts to compare
+     * @return {@link ButtonGroupAssert}
+     */
     @JDIAction("Number of grouped buttons in '{name}' is '{0}'")
     public ButtonGroupAssert buttonsTextsInAnyOrder(List<String> texts) {
         jdiAssert(element().getAllButtons().stream().map(Button::getText).toArray(),
