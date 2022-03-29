f69f8518c426e839e6a509d8694469521d6ef02c
IvKardapolov <Hx5Z3CWdYKT3agq>
2022-03-29 15:17:27 +0400

    Changes according code review

--- jdi-light-material-ui-tests/src/main/java/io/github/com/custom/elements/table/CollapsingTable.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/custom/elements/table/CollapsingTable.java
@@ -3,10 +3,19 @@ package io.github.com.custom.elements.table;
 import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.elements.complex.WebList;
 import com.epam.jdi.light.elements.complex.table.Table;
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
+import com.epam.jdi.light.ui.html.elements.common.Button;
 import com.jdiai.tools.map.MapArray;
 import org.openqa.selenium.By;
 
+import java.util.List;
+
 public class CollapsingTable extends Table {
+
+    @UI("button")
+    private List<Button> buttons;
+
+    private final int tableRowMultiplayer = 2;
 
     protected MuiCacheAll<MapArray<String, WebList>> rows
             = new MuiCacheAll<>(MapArray::new);
@@ -16,10 +25,12 @@ public class CollapsingTable extends Table {
     @Override
     @JDIAction("Get '{name}' rows as array")
     protected MapArray<String, WebList> getRows() {
-        if (rows.isGotAll()) { return rows.get(); }
+        if (rows.isGotAll()) {
+            return rows.get();
+        }
         MapArray<String, WebList> result = new MapArray<>();
         int count = count() + getStartIndex();
-        for (int i = getStartIndex(); i < count; i += 2) {
+        for (int i = getStartIndex(); i < count; i += tableRowMultiplayer) {
             result.add(i + "", webRow(i));
         }
         rows.gotAll();
@@ -37,14 +48,14 @@ public class CollapsingTable extends Table {
             innerTables = new InnerTable[rows().size()];
         }
 
-        core().finds(By.cssSelector("button")).get(rowNum).click();
-        By tableLocator = By.xpath(String.format("//tbody/tr[%d]//table", rowNum * 2));
+        buttons.get(rowNum).click();
+        By tableLocator = By.xpath(String.format("//tbody/tr[%d]//table", rowNum * tableRowMultiplayer));
         InnerTable innerTable = new InnerTable().setCore(InnerTable.class, find(tableLocator));
         innerTables[rowNum - 1] = innerTable;
     }
 
     @JDIAction("Collapse '{name}' row '{0}'")
     public void collapseRow(int rowNum) {
-        core().finds(By.cssSelector("button")).get(rowNum).click();
+        buttons.get(rowNum).click();
     }
 }
