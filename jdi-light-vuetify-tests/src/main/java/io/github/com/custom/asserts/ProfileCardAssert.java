4098f81de599953ad5a349280d1d76f5202a7cb6
b14ckster <b1ackcter@gmail.com>
2022-03-30 17:16:11 +0300

    add javadocs for asserts

--- jdi-light-vuetify-tests/src/main/java/io/github/com/custom/asserts/ProfileCardAssert.java
+++ jdi-light-vuetify-tests/src/main/java/io/github/com/custom/asserts/ProfileCardAssert.java
@@ -7,14 +7,29 @@ import org.hamcrest.Matchers;
 
 import static com.epam.jdi.light.asserts.core.SoftAssert.jdiAssert;
 
+/**
+ * Assertions for {@link ProfileCard}.
+ */
 public class ProfileCardAssert extends UIAssert<ProfileCardAssert, ProfileCard> {
 
+    /**
+     * Checks that {@link ProfileCard} has expected username.
+     *
+     * @param text expected username.
+     * @return this {@link ProfileCardAssert} instance.
+     */
     @JDIAction("Assert that '{name}' has username '{0}'")
     public ProfileCardAssert userName(String text) {
         jdiAssert(element().getUserName(), Matchers.is(text));
         return this;
     }
 
+    /**
+     * Checks that {@link ProfileCard} has expected job function.
+     *
+     * @param text expected job function.
+     * @return this {@link ProfileCardAssert} instance.
+     */
     @JDIAction("Assert that '{name}' has job function '{0}'")
     public ProfileCardAssert userJobFunction(String text) {
         jdiAssert(element().getUserJobFunction(), Matchers.is(text));
--- jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/asserts/AvatarAssert.java
+++ jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/asserts/AvatarAssert.java
@@ -10,14 +10,16 @@ import org.openqa.selenium.Dimension;
 
 import static com.epam.jdi.light.asserts.core.SoftAssert.jdiAssert;
 
+/**
+ * Assertions for {@link Avatar}.
+ */
 public class AvatarAssert extends UIAssert<AvatarAssert, Avatar> implements ITextAssert<AvatarAssert> {
 
     /**
-     * Assert for check text in {@link Avatar}.
-     * It's a helper method for the default method "text" of interface ITextAssert.
+     * Checks text in {@link Avatar}.
      *
-     * @param condition - Matcher for checking text
-     * @return AvatarAssert
+     * @param condition Matcher for checking text.
+     * @return this {@link AvatarAssert} instance.
     */
     @Override
     @JDIAction("Assert that '{name}' text {0}")
@@ -27,11 +29,11 @@ public class AvatarAssert extends UIAssert<AvatarAssert, Avatar> implements ITex
     }
 
     /**
-     * Assert for check {@link Avatar} size
-     * All avatars have resolution size * size (for example, 48x48 px)
+     * Checks {@link Avatar} size.
+     * All avatars have resolution (size * size) pixels (for example, 48x48 pixels).
      *
-     * @param size size of avatar in px.
-     * @return AvatarAssert
+     * @param size size of avatar in pixels.
+     * @return this {@link AvatarAssert} instance.
      */
     @JDIAction("Assert that '{name}' size is {int} px")
     public AvatarAssert size(Integer size) {
