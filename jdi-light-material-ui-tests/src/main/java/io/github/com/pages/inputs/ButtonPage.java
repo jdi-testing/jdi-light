8710448b23c191762c81dbcfae67220c8ee14215
Duyen Nguyen <Duyen_Nguyen@epam.com>
2022-04-13 06:04:56 +0700

    Task 3977: Refactor ButtonPage

--- jdi-light-material-ui-tests/src/main/java/io/github/com/pages/inputs/ButtonPage.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/pages/inputs/ButtonPage.java
@@ -4,6 +4,7 @@ import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.ui.html.elements.common.Button;
 import com.epam.jdi.light.ui.html.elements.common.Icon;
+import com.epam.jdi.light.ui.html.elements.common.Text;
 
 import java.util.List;
 
@@ -12,8 +13,14 @@ public class ButtonPage extends WebPage {
     @UI("//h2[text()='Contained buttons']/parent::div/div[1]/*")
     public static List<Button> containedButtons;
 
+    @UI("#simpleLastClick")
+    public static Text simpleLastClick;
+
     @UI("//h2[text()='Text buttons']/parent::div/div[2]/*")
     public static List<Button> textButtons;
+
+    @UI("#textLastClick")
+    public static Text textLastClick;
 
     @UI("//h2[text()='Buttons with icons and label']/parent::div/div[3]/*")
     public static List<Button> iconLabelButtons;
@@ -21,15 +28,27 @@ public class ButtonPage extends WebPage {
     @UI("//span[contains(@class,'MuiButton-icon')]")
     public static List<Icon> iconLabelIcons;
 
+    @UI("#labeledLastClick")
+    public static Text labeledLastClick;
+
     @UI("//h2[text()='Icon buttons']/parent::div/div[4]/*")
     public static List<Button> iconButtons;
 
     @UI("//span[@class='MuiIconButton-label']")
     public static List<Icon> iconButtonIcons;
 
+    @UI("#iconLastClick")
+    public static Text iconLastClick;
+
     @UI("//h2[text()='Customized buttons']/parent::div/div[5]/*")
     public static List<Button> customizedButtons;
 
+    @UI("#customizedLastClick")
+    public static Text customizedLastClick;
+
     @UI("//h2[text()='Complex buttons']/parent::div/div[6]/*")
     public static List<Button> complexButtons;
+
+    @UI("#complexLastClick")
+    public static Text complexLastClick;
 }
--- jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/inputs/ButtonTests.java
+++ jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/inputs/ButtonTests.java
@@ -5,15 +5,20 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static io.github.com.StaticSite.buttonPage;
-import static io.github.com.pages.inputs.ButtonPage.complexButtons;
 import static io.github.com.pages.inputs.ButtonPage.containedButtons;
-import static io.github.com.pages.inputs.ButtonPage.customizedButtons;
-import static io.github.com.pages.inputs.ButtonPage.iconButtonIcons;
-import static io.github.com.pages.inputs.ButtonPage.iconButtons;
+import static io.github.com.pages.inputs.ButtonPage.simpleLastClick;
+import static io.github.com.pages.inputs.ButtonPage.textButtons;
+import static io.github.com.pages.inputs.ButtonPage.textLastClick;
 import static io.github.com.pages.inputs.ButtonPage.iconLabelButtons;
 import static io.github.com.pages.inputs.ButtonPage.iconLabelIcons;
-import static io.github.com.pages.inputs.ButtonPage.textButtons;
-
+import static io.github.com.pages.inputs.ButtonPage.labeledLastClick;
+import static io.github.com.pages.inputs.ButtonPage.iconButtons;
+import static io.github.com.pages.inputs.ButtonPage.iconButtonIcons;
+import static io.github.com.pages.inputs.ButtonPage.iconLastClick;
+import static io.github.com.pages.inputs.ButtonPage.customizedButtons;
+import static io.github.com.pages.inputs.ButtonPage.customizedLastClick;
+import static io.github.com.pages.inputs.ButtonPage.complexButtons;
+import static io.github.com.pages.inputs.ButtonPage.complexLastClick;
 /**
  * To see an example of Button web element please visit
  * https://material-ui.com/components/buttons/
@@ -31,24 +36,24 @@ public class ButtonTests extends TestsInit {
     public void defaultButtonTest() {
 
         containedButtons.get(1).click();
-        containedButtons.get(1).is().text("DEFAULT");
-        containedButtons.get(6).is().text("Last click: Default");
+        containedButtons.get(1).has().text("DEFAULT");
+        simpleLastClick.is().text("Last click: Default");
 
         containedButtons.get(2).click();
-        containedButtons.get(2).is().text("PRIMARY");
-        containedButtons.get(6).is().text("Last click: Primary");
+        containedButtons.get(2).has().text("PRIMARY");
+        simpleLastClick.is().text("Last click: Primary");
 
         containedButtons.get(3).click();
-        containedButtons.get(3).is().text("SECONDARY");
-        containedButtons.get(6).is().text("Last click: Secondary");
+        containedButtons.get(3).has().text("SECONDARY");
+        simpleLastClick.is().text("Last click: Secondary");
 
-        containedButtons.get(4).is().text("DISABLED");
-        containedButtons.get(4).is().attr("disabled");
+        containedButtons.get(4).has().text("DISABLED");
+        containedButtons.get(4).has().attr("disabled");
         containedButtons.get(4).is().disabled();
 
         containedButtons.get(5).click();
-        containedButtons.get(5).is().text("LINK");
-        containedButtons.get(6).is().text("Last click: Link");
+        containedButtons.get(5).has().text("LINK");
+        simpleLastClick.is().text("Last click: Link");
         containedButtons.get(5).is().notVisible();
     }
 
@@ -56,24 +61,24 @@ public class ButtonTests extends TestsInit {
     public void textButtonTest() {
 
         textButtons.get(1).click();
-        textButtons.get(1).is().text("DEFAULT");
-        textButtons.get(6).is().text("Last click: Default");
+        textButtons.get(1).has().text("DEFAULT");
+        textLastClick.is().text("Last click: Default");
 
         textButtons.get(2).click();
-        textButtons.get(2).is().text("PRIMARY");
-        textButtons.get(6).is().text("Last click: Primary");
+        textButtons.get(2).has().text("PRIMARY");
+        textLastClick.is().text("Last click: Primary");
 
         textButtons.get(3).click();
-        textButtons.get(3).is().text("SECONDARY");
-        textButtons.get(6).is().text("Last click: Secondary");
+        textButtons.get(3).has().text("SECONDARY");
+        textLastClick.is().text("Last click: Secondary");
 
-        textButtons.get(4).is().text("DISABLED");
-        textButtons.get(4).is().attr("disabled");
+        textButtons.get(4).has().text("DISABLED");
+        textButtons.get(4).has().attr("disabled");
         textButtons.get(4).is().disabled();
 
         textButtons.get(5).click();
-        textButtons.get(5).is().text("LINK");
-        textButtons.get(6).is().text("Last click: Link");
+        textButtons.get(5).has().text("LINK");
+        textLastClick.is().text("Last click: Link");
         textButtons.get(5).is().notVisible();
     }
 
@@ -81,32 +86,32 @@ public class ButtonTests extends TestsInit {
     public void iconLabelButtonTest() {
 
         iconLabelButtons.get(1).click();
-        iconLabelButtons.get(1).is().text("DELETE");
-        iconLabelButtons.get(7).is().text("Last click: Delete");
+        iconLabelButtons.get(1).has().text("DELETE");
+        labeledLastClick.is().text("Last click: Delete");
         iconLabelIcons.get(1).is().displayed();
 
         iconLabelButtons.get(2).click();
-        iconLabelButtons.get(2).is().text("SEND\nSEND");
-        iconLabelButtons.get(7).is().text("Last click: Sendsend");
+        iconLabelButtons.get(2).has().text("SEND\nSEND");
+        labeledLastClick.is().text("Last click: Sendsend");
         iconLabelIcons.get(2).is().displayed();
 
         iconLabelButtons.get(3).click();
-        iconLabelButtons.get(3).is().text("UPLOAD");
-        iconLabelButtons.get(7).is().text("Last click: Upload");
+        iconLabelButtons.get(3).has().text("UPLOAD");
+        labeledLastClick.is().text("Last click: Upload");
         iconLabelIcons.get(3).is().displayed();
 
-        iconLabelButtons.get(4).is().attr("disabled");
-        iconLabelButtons.get(4).is().text("TALK");
+        iconLabelButtons.get(4).has().attr("disabled");
+        iconLabelButtons.get(4).has().text("TALK");
         iconLabelIcons.get(4).is().displayed();
 
         iconLabelButtons.get(5).click();
-        iconLabelButtons.get(5).is().text("SAVE");
-        iconLabelButtons.get(7).is().text("Last click: Save");
+        iconLabelButtons.get(5).has().text("SAVE");
+        labeledLastClick.is().text("Last click: Save");
         iconLabelIcons.get(5).is().displayed();
 
         iconLabelButtons.get(6).click();
-        iconLabelButtons.get(6).is().text("SAVE ALL");
-        iconLabelButtons.get(7).is().text("Last click: Save all");
+        iconLabelButtons.get(6).has().text("SAVE ALL");
+        labeledLastClick.is().text("Last click: Save all");
         iconLabelIcons.get(6).is().displayed();
     }
 
@@ -114,19 +119,19 @@ public class ButtonTests extends TestsInit {
     public void iconButtonTest() {
 
         iconButtons.get(1).click();
-        iconButtons.get(5).is().text("Last click: Delete enabled");
+        iconLastClick.is().text("Last click: Delete enabled");
         iconButtonIcons.get(1).is().displayed();
 
-        iconButtons.get(2).is().attr("disabled");
+        iconButtons.get(2).has().attr("disabled");
         iconButtons.get(2).is().disabled();
         iconButtonIcons.get(2).is().displayed();
 
         iconButtons.get(3).click();
-        iconButtons.get(5).is().text("Last click: Alarm");
+        iconLastClick.is().text("Last click: Alarm");
         iconButtonIcons.get(3).is().displayed();
 
         iconButtons.get(4).click();
-        iconButtons.get(5).is().text("Last click: Shopping cart");
+        iconLastClick.is().text("Last click: Shopping cart");
         iconButtonIcons.get(4).is().displayed();
     }
 
@@ -134,31 +139,31 @@ public class ButtonTests extends TestsInit {
     public void customizedButtonTest() {
 
         customizedButtons.get(1).click();
-        customizedButtons.get(1).is().text("CUSTOM CSS");
-        customizedButtons.get(4).is().text("Last click: Custom CSS");
+        customizedButtons.get(1).has().text("CUSTOM CSS");
+        customizedLastClick.is().text("Last click: Custom CSS");
 
         customizedButtons.get(2).click();
-        customizedButtons.get(2).is().text("THEME PROVIDER");
-        customizedButtons.get(4).is().text("Last click: Theme Provider");
+        customizedButtons.get(2).has().text("THEME PROVIDER");
+        customizedLastClick.is().text("Last click: Theme Provider");
 
         customizedButtons.get(3).click();
-        customizedButtons.get(3).is().text("Bootstrap");
-        customizedButtons.get(4).is().text("Last click: Bootstrap");
+        customizedButtons.get(3).has().text("Bootstrap");
+        customizedLastClick.is().text("Last click: Bootstrap");
     }
 
     @Test
     public void complexButtonTest() {
 
         complexButtons.get(1).click();
-        complexButtons.get(1).is().text("Breakfast");
-        complexButtons.get(4).is().text("Last click:Breakfast");
+        complexButtons.get(1).has().text("Breakfast");
+        complexLastClick.is().text("Last click:Breakfast");
 
         complexButtons.get(2).click();
-        complexButtons.get(2).is().text("Burgers");
-        complexButtons.get(4).is().text("Last click:Burgers");
+        complexButtons.get(2).has().text("Burgers");
+        complexLastClick.is().text("Last click:Burgers");
 
         complexButtons.get(3).click();
-        complexButtons.get(3).is().text("Camera");
-        complexButtons.get(4).is().text("Last click:Camera");
+        complexButtons.get(3).has().text("Camera");
+        complexLastClick.is().text("Last click:Camera");
     }
 }
