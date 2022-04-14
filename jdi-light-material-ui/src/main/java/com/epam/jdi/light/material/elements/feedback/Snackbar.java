f7f9e98f520d70a0bcb28302448186a843ea572c
Evgenii_Liashenko <evgenii_liashenko@epam.com>
2022-04-14 15:32:35 +0300

    Javadocs are ready for: - FloatingAnimatedButtonsSection - Snackbar - Backdrop

--- jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/elements/feedback/Snackbar.java
+++ jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/elements/feedback/Snackbar.java
@@ -11,17 +11,28 @@ import com.epam.jdi.light.ui.html.elements.common.Button;
 import static com.epam.jdi.light.settings.WebSettings.logger;
 
 /**
- * To see an example of Snackbar web element please visit
- * https://material-ui.com/components/snackbars/
+ * Represents Snackbar MUI component on GUI.
+ *
+ * @see <a href="https://mui.com/components/snackbars/">Snackbar MUI documentation</a>
+ * @see <a href="https://jdi-testing.github.io/jdi-light/material">MUI test page</a>
  */
-
 public class Snackbar extends UIBaseElement<SnackbarAssert> implements IsText, HasLabel {
 
+    /**
+     * Gets a specific snackbar by its name.
+     *
+     * @param name the partial text content of the snackbar to be found.
+     * @return the found snackbar as {@link Button}
+     */
     @JDIAction("Get {name} {0} button")
     public Button snackbarButton(String name) {
         return new Button().setCore(Button.class, find("//*[contains(text(), '" + name + "')]"));
     }
 
+    /**
+     * Gets the text content of this snackbar.
+     * @return the text content of this snackbar as {@link String}
+     */
     @JDIAction("Get {name} text")
     @Override
     public String text() {
@@ -33,6 +44,9 @@ public class Snackbar extends UIBaseElement<SnackbarAssert> implements IsText, H
         }
     }
 
+    /**
+     * Closes this snackbar.
+     */
     @JDIAction("Close {name}")
     public void close() {
         UIElement close = find("[aria-label='close']");
@@ -43,55 +57,65 @@ public class Snackbar extends UIBaseElement<SnackbarAssert> implements IsText, H
         }
     }
 
+    /**
+     * Checks if the snackbar message is of the specified type (e.g. error message, success message).
+     * @param messageType the message type to check for matching.
+     * @return {@code true} if the message type of this snackbar matches the specified message type.
+     */
     @JDIAction("Show that the {name} message has {0} type")
     public boolean messageType(String messageType) {
         boolean result = false;
         String msgType = messageType.toLowerCase();
         switch (msgType) {
-            case "error":
-                result = core().hasClass("MuiAlert-filledError");
-                break;
-            case "warning":
-                result =  core().hasClass("MuiAlert-filledWarning");
-                break;
-            case "info":
-                result = core().hasClass("MuiAlert-filledInfo");
-                break;
-            case "success":
-                result = core().hasClass("MuiAlert-filledSuccess");
-                break;
-            default:
-                logger.error("Unknown message type %s", msgType);
-                break;
+        case "error":
+            result = core().hasClass("MuiAlert-filledError");
+            break;
+        case "warning":
+            result = core().hasClass("MuiAlert-filledWarning");
+            break;
+        case "info":
+            result = core().hasClass("MuiAlert-filledInfo");
+            break;
+        case "success":
+            result = core().hasClass("MuiAlert-filledSuccess");
+            break;
+        default:
+            logger.error("Unknown message type %s", msgType);
+            break;
         }
         return result;
     }
 
+    /**
+     * Checks if this snackbar is aligned in the specified way (e.g. TOP-LEFT, BOTTOM-CENTER).
+     * @param align the position to check for matching.
+     * @return true if the position of this snackbar matches the specified position.
+     */
     @JDIAction("Show that {name} has {0} position")
     public boolean hasPosition(Position align) {
         boolean result = true;
         String[] positions = align.name().toLowerCase().split("_");
-        for (String position: positions) {
+        for (String position : positions) {
             switch (position) {
-                case "top":
-                    result = result && attr("class").contains("Top");
-                    break;
-                case "right":
-                    result = result && attr("class").contains("Right");
-                    break;
-                case "center":
-                    result = result && attr("class").contains("Center");
-                    break;
-                case "left":
-                    result = result && attr("class").contains("Left");
-                    break;
-                case "bottom":
-                    result = result && attr("class").contains("Bottom");
-                    break;
-                default:
-                    logger.error("Unknown position %s", position);
-                    result = false;
-                    break;
+            case "top":
+                result = result && attr("class").contains("Top");
+                break;
+            case "right":
+                result = result && attr("class").contains("Right");
+                break;
+            case "center":
+                result = result && attr("class").contains("Center");
+                break;
+            case "left":
+                result = result && attr("class").contains("Left");
+                break;
+            case "bottom":
+                result = result && attr("class").contains("Bottom");
+                break;
+            default:
+                logger.error("Unknown position %s", position);
+                result = false;
+                break;
             }
         }
         return result;
