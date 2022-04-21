78c416ccf70fd716c55b3cc344e349681b7008f3
Anna Vasileva <AnnaVasileva2@epam.com>
2022-04-21 14:43:01 +0300

    Minor changes.

--- jdi-light-material-ui-tests/src/main/java/io/github/com/pages/surfaces/BackToTopAppBarPage.java
+++ jdi-light-material-ui-tests/src/main/java/io/github/com/pages/surfaces/BackToTopAppBarPage.java
@@ -7,7 +7,7 @@ import com.epam.jdi.light.ui.html.elements.common.Button;
 
 public class BackToTopAppBarPage extends WebPage {
 
-    @UI("(.MuiBox-root[1]")
+    @UI(".MuiBox-root[1]")
     public static AppBar backToTopAppBar;
 
     @UI("div[role=presentation]")
