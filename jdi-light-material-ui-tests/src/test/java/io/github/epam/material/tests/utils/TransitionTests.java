37f4f19decea3dc1a8e037a4564adab2d06eebd1
Anna Vasileva <AnnaVasileva2@epam.com>
2022-05-05 10:14:44 +0300

    Removed the specialTransitionOnThePage() method and made related changes.

--- jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/utils/TransitionTests.java
+++ jdi-light-material-ui-tests/src/test/java/io/github/epam/material/tests/utils/TransitionTests.java
@@ -2,28 +2,16 @@ package io.github.epam.material.tests.utils;
 
 import com.epam.jdi.light.material.elements.inputs.Switch;
 import com.epam.jdi.light.material.elements.utils.Transition;
-import com.epam.jdi.light.material.elements.utils.enums.TransitionType;
 import io.github.epam.TestsInit;
 import io.github.epam.test.data.TransitionDataProvider;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.Collections;
-import java.util.List;
-
 import static com.jdiai.tools.Timer.waitCondition;
 import static io.github.com.StaticSite.transitionPage;
 import static io.github.com.pages.utils.TransitionPage.collapseTransitions;
-import static io.github.com.pages.utils.TransitionPage.fadeTransitions;
-import static io.github.com.pages.utils.TransitionPage.growTransitions;
-import static io.github.com.pages.utils.TransitionPage.slideTransitions;
 import static io.github.com.pages.utils.TransitionPage.switches;
-import static io.github.com.pages.utils.TransitionPage.zoomTransitions;
 
-/**
- * To see an example of Transitions web element please visit
- * https://material-ui.com/components/transitions/
- */
 public class TransitionTests extends TestsInit {
 
     @BeforeMethod
@@ -33,8 +21,7 @@ public class TransitionTests extends TestsInit {
     }
 
     @Test(dataProvider = "transitionTestData", dataProviderClass = TransitionDataProvider.class)
-    public void basicFunctionalityTest(TransitionType transitionType, int switchIndex, int transitionIndex) {
-        Transition transition = specialTransitionOnThePage(transitionType).get(transitionIndex);
+    public void basicFunctionalityTest(Transition transition, int switchIndex) {
         Switch transitionSwitch = switches.get(switchIndex);
         transition.show();
 
@@ -55,30 +42,5 @@ public class TransitionTests extends TestsInit {
         switches.get(1).uncheck();
         collapseTransitions.get(1).is().hidden();
         collapseTransitions.get(2).is().displayed();
-    }
-
-    private List<Transition> specialTransitionOnThePage(TransitionType type) {
-        List<Transition> transitions;
-        switch (type) {
-        case COLLAPSE:
-            transitions = collapseTransitions;
-            break;
-        case FADE:
-            transitions = fadeTransitions;
-            break;
-        case GROW:
-            transitions = growTransitions;
-            break;
-        case SLIDE:
-            transitions = slideTransitions;
-            break;
-        case ZOOM:
-            transitions = zoomTransitions;
-            break;
-        default:
-            transitions = Collections.emptyList();
-            break;
-        }
-        return transitions;
     }
 }
--- jdi-light-material-ui-tests/src/test/java/io/github/epam/test/data/TransitionDataProvider.java
+++ jdi-light-material-ui-tests/src/test/java/io/github/epam/test/data/TransitionDataProvider.java
@@ -2,26 +2,26 @@ package io.github.epam.test.data;
 
 import org.testng.annotations.DataProvider;
 
-import static com.epam.jdi.light.material.elements.utils.enums.TransitionType.COLLAPSE;
-import static com.epam.jdi.light.material.elements.utils.enums.TransitionType.FADE;
-import static com.epam.jdi.light.material.elements.utils.enums.TransitionType.GROW;
-import static com.epam.jdi.light.material.elements.utils.enums.TransitionType.SLIDE;
-import static com.epam.jdi.light.material.elements.utils.enums.TransitionType.ZOOM;
+import static io.github.com.pages.utils.TransitionPage.collapseTransitions;
+import static io.github.com.pages.utils.TransitionPage.fadeTransitions;
+import static io.github.com.pages.utils.TransitionPage.growTransitions;
+import static io.github.com.pages.utils.TransitionPage.slideTransitions;
+import static io.github.com.pages.utils.TransitionPage.zoomTransitions;
 
 public class TransitionDataProvider {
 
     @DataProvider
     public Object[][] transitionTestData() {
         return new Object[][] {
-            {COLLAPSE, 1, 1},
-            {COLLAPSE, 1, 2},
-            {FADE, 2, 1},
-            {FADE, 2, 2},
-            {GROW, 3, 1},
-            {GROW, 3, 2},
-            {SLIDE, 4, 1},
-            {ZOOM, 5, 1},
-            {ZOOM, 5, 2},
+            {collapseTransitions.get(1), 1},
+            {collapseTransitions.get(2), 1},
+            {fadeTransitions.get(1), 2},
+            {fadeTransitions.get(2), 2},
+            {growTransitions.get(1), 3},
+            {growTransitions.get(2), 3},
+            {slideTransitions.get(1), 4},
+            {zoomTransitions.get(1), 5},
+            {zoomTransitions.get(2), 5},
         };
     }
 }
