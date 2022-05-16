cee876f15410eae8b37bf18cbf0695a843b5f562
Duyen Nguyen <Duyen_Nguyen@epam.com>
2022-05-16 14:50:51 +0700

    remove unneeded method

--- jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/elements/inputs/MUIButton.java
+++ jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/elements/inputs/MUIButton.java
@@ -3,18 +3,11 @@ package com.epam.jdi.light.material.elements.inputs;
 import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.elements.common.Label;
 import com.epam.jdi.light.elements.interfaces.base.HasLabel;
-import com.epam.jdi.light.material.elements.displaydata.Icon;
 import com.epam.jdi.light.material.interfaces.displaydata.HasIcon;
 import com.epam.jdi.light.ui.html.elements.common.Button;
 
 public class MUIButton extends Button
         implements HasIcon, HasLabel {
-
-    @Override
-    @JDIAction("Get '{name}' icon")
-    public Icon icon() {
-        return new Icon().setCore(Icon.class, core().find(".MuiSvgIcon-root, .MuiIcon-root"));
-    }
 
     @Override
     @JDIAction("Get '{name}' label")
