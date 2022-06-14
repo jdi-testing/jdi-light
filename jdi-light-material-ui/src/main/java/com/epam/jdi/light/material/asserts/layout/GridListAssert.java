0b3563b868cc9b8317290d713017fff8190c1f99
Kirill Chebulaev <chebulaev@gmail.com>
2022-06-14 15:59:52 +0300

    Grid list update

--- jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/asserts/layout/GridListAssert.java
+++ jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/asserts/layout/GridListAssert.java
@@ -12,21 +12,41 @@ import org.hamcrest.Matchers;
  */
 public class GridListAssert extends UISelectAssert<GridListAssert, GridList> implements HasAssert<GridListAssert> {
 
+    /**
+     * Check if element has required title or not
+     *
+     * @param index item to check
+     * @param reqTitle required title
+     * @return {@link GridListAssert}
+     */
     @JDIAction("Assert that '{name}' element {0} has title '{1}'")
-    public GridListAssert title(int elNum, String reqTitle) {
-        jdiAssert(element().title(elNum), Matchers.is(reqTitle));
+    public GridListAssert title(int index, String reqTitle) {
+        jdiAssert(element().title(index), Matchers.is(reqTitle));
         return this;
     }
 
+    /**
+     * Checks if element item has image or not
+     *
+     * @param index item to check
+     * @return {@link GridListAssert}
+     */
     @JDIAction("Assert that '{name}' element {0} has image")
-    public GridListAssert image(int elNum) {
-        jdiAssert(element().hasImage(elNum) ? "has image" : "hasn't image", Matchers.is("has image"));
+    public GridListAssert image(int index) {
+        jdiAssert(element().hasImage(index) ? "has image" : "hasn't image", Matchers.is("has image"));
         return this;
     }
 
+    /**
+     * Checks if element item has alternative image text or not
+     *
+     * @param index item to check
+     * @param altText required text
+     * @return {@link GridListAssert}
+     */
     @JDIAction("Assert that '{name}' image {0} has alternative name '{1}'")
-    public GridListAssert altImgName(int elNum, String reqAltName) {
-        jdiAssert(element().altImgName(elNum), Matchers.is(reqAltName));
+    public GridListAssert alternativeImageText(int index, String altText) {
+        jdiAssert(element().image(index).alt(), Matchers.is(altText));
         return this;
     }
 }
