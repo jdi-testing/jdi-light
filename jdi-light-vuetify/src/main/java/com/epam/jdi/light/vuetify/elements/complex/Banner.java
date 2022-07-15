3fbb7185e56c061123ec4f66ee5d0234d3114c64
kseniia-mironova <kseniia.mironova@prt.manomano.com>
2022-07-15 17:07:23 +0400

    Add methods for actions and content in Banners class

--- jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/elements/complex/Banner.java
+++ jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/elements/complex/Banner.java
@@ -2,6 +2,7 @@ package com.epam.jdi.light.vuetify.elements.complex;
 
 import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.elements.base.UIBaseElement;
+import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.light.elements.interfaces.common.IsText;
 import com.epam.jdi.light.vuetify.asserts.BannerAssert;
 import com.epam.jdi.light.vuetify.elements.common.Icon;
@@ -11,6 +12,8 @@ import com.epam.jdi.light.vuetify.elements.common.Icon;
  */
 
 public class Banner extends UIBaseElement<BannerAssert> implements IsText {
+
+    private static final String ICON_LOCATOR = ".v-banner__icon";
 
     @JDIAction("Get '{name}' button group")
     public ButtonGroup buttons() {
@@ -25,6 +28,16 @@ public class Banner extends UIBaseElement<BannerAssert> implements IsText {
     @JDIAction("Get '{name}' text content")
     public String getText() {
         return core().find(".v-banner__text").getText();
+    }
+
+    @JDIAction("Get '{name}' content element")
+    public UIElement content() {
+        return core().find(".v-banner__content");
+    }
+
+    @JDIAction("Get '{name}' actions element")
+    public UIElement actions() {
+        return core().find(".v-banner__actions");
     }
 
     public BannerAssert is() {
