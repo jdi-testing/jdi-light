0ae3ff3ed48387108417b15a305db77dc576768f
Anna Vasileva <AnnaVasileva2@epam.com>
2022-07-25 09:22:55 +0300

    Changed according the Codacy

--- jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/asserts/ListItemAssert.java
+++ jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/asserts/ListItemAssert.java
@@ -3,7 +3,6 @@ package com.epam.jdi.light.vuetify.asserts;
 import com.epam.jdi.light.asserts.generic.ITextAssert;
 import com.epam.jdi.light.asserts.generic.UIAssert;
 import com.epam.jdi.light.common.JDIAction;
-import com.epam.jdi.light.vuetify.elements.common.Avatar;
 import com.epam.jdi.light.vuetify.elements.common.ListItem;
 import com.jdiai.tools.Timer;
 import org.hamcrest.Matcher;
