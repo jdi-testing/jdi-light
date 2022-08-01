0d69c5a04e0156923415b3471f2badb221568240
Duc Le <duc_le@epam.com>
2022-08-01 10:11:09 +0700

    remove import

--- jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/elements/common/CancelButton.java
+++ jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/elements/common/CancelButton.java
@@ -1,9 +1,7 @@
 package com.epam.jdi.light.vuetify.elements.common;
 
 import com.epam.jdi.light.elements.base.UIBaseElement;
-import com.epam.jdi.light.ui.html.elements.common.Button;
 import com.epam.jdi.light.vuetify.asserts.CancelButtonAssert;
-import com.epam.jdi.light.vuetify.asserts.OkButtonAssert;
 
 public class CancelButton extends UIBaseElement<CancelButtonAssert> {
     public void clickCancel() {
