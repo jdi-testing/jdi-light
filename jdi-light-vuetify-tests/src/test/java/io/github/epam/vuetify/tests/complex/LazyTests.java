68dd4e5ea2ad5a1486eb72c1e1ac8a1fbfc875b3
Tuan_Pham <totonro.nigma@gmail.com>
2022-08-04 16:41:28 +0700

    Finish refactoring the lazy component

--- jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/complex/LazyTests.java
+++ jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/complex/LazyTests.java
@@ -1,16 +1,14 @@
 package io.github.epam.vuetify.tests.complex;
 
 import io.github.epam.TestsInit;
-import org.hamcrest.Matchers;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static com.jdiai.tools.Timer.waitCondition;
 import static io.github.com.StaticSite.lazyPage;
-import static io.github.com.pages.LazyPage.ITEM_TEXT;
-import static io.github.com.pages.LazyPage.ITEM_TITLE;
-import static io.github.com.pages.LazyPage.lazy;
-import static io.github.com.pages.LazyPage.lazyContent;
+import static io.github.com.pages.LazyPage.lazyCard;
+import static io.github.com.pages.LazyPage.scrollDownText;
+import static org.hamcrest.Matchers.containsString;
 
 
 public class LazyTests extends TestsInit {
@@ -24,16 +22,20 @@ public class LazyTests extends TestsInit {
 
     @Test
     public void itemIsHiddenTest() {
-        lazyContent.is().hidden();
+        lazyCard.has().notExistContent();
     }
 
     @Test
     public void itemIsLoadedTest() {
-        lazy.scrollIntoView();
-        lazy.is().displayed();
-        lazy.hiddenItem().is().displayed();
-        lazy.core().find(ITEM_TITLE).has().text("Card title");
-        lazy.core().find(ITEM_TEXT).is().displayed();
-        lazy.core().find(ITEM_TEXT).has().text(Matchers.containsString("Aliquam lobortis"));
+        String title = "Card title";
+        String partOfTextContent = "Aliquam lobortis";
+        lazyCard.scrollIntoView();
+        lazyCard.is().existContent();
+        lazyCard.title().is().text(title);
+        lazyCard.contentText().is().text(containsString(partOfTextContent));
+        scrollDownText.scrollIntoView();
+        lazyCard.is().existContent();
+        lazyCard.title().is().text(title);
+        lazyCard.contentText().is().text(containsString(partOfTextContent));
     }
 }
