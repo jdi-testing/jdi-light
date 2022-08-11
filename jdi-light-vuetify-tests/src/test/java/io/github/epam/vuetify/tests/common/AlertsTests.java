55516a752863f6ed5c852df42285ab555e19b8ec
kseniia-mironova <kseniia.mironova@prt.manomano.com>
2022-08-11 17:28:45 +0300

    Added test for shape and elevation;

--- jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/common/AlertsTests.java
+++ jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/common/AlertsTests.java
@@ -5,13 +5,19 @@ import com.epam.jdi.light.asserts.generic.UIAssert;
 import com.epam.jdi.light.vuetify.elements.common.Alert;
 import io.github.epam.TestsInit;
 import io.github.epam.vuetify.tests.data.AlertsTestsDataProvider;
+import org.hamcrest.Matchers;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import javax.annotation.Nullable;
+
+import static com.epam.jdi.light.elements.base.Conditions.visible;
 import static com.jdiai.tools.Timer.waitCondition;
 import static io.github.com.StaticSite.alertsPage;
 import static io.github.com.pages.AlertsPage.dismissibleAlert;
 import static io.github.com.pages.AlertsPage.dismissibleAlertResetButton;
+import static io.github.com.pages.AlertsPage.transitionAlertToggleButton;
+import static io.github.com.pages.AlertsPage.transitionAlert;
 import static org.hamcrest.Matchers.containsString;
 
 
@@ -25,11 +31,12 @@ public class AlertsTests extends TestsInit {
 
     @Test(dataProvider = "basicAlertsTestsData", dataProviderClass = AlertsTestsDataProvider.class)
     public void basicAlertsHaveProperTextAndIcon(Alert basicAlertType, String alertText, String cssClassText,
-                                                 String colorCssStyle, String color) {
+                                                 String colorCssStyle, String color, @Nullable String shape) {
         basicAlertType.has().text(alertText);
         basicAlertType.icon().has().cssClass(cssClassText);
         basicAlertType.has().css(colorCssStyle, color);
         checkBorderColor(basicAlertType, color);
+        if (shape != null) basicAlertType.has().classValue(Matchers.containsString(shape));
     }
 
     @Test(dataProvider = "dismissibleAlertsTestsData", dataProviderClass = AlertsTestsDataProvider.class)
@@ -44,6 +51,50 @@ public class AlertsTests extends TestsInit {
 
         dismissibleAlert.is().displayed();
         dismissibleAlert.has().text(containsString(alertText));
+    }
+
+    @Test(dataProvider = "prominentAlertsTestsData", dataProviderClass = AlertsTestsDataProvider.class)
+    public void prominentAlertsTest(Alert prominentAlertType, String cssClassText, String iconCssText) {
+        prominentAlertType.show();
+        prominentAlertType.shouldBe(visible);
+        prominentAlertType.has().classValue(Matchers.containsString(cssClassText));
+        prominentAlertType.icon().has().classValue(Matchers.containsString(iconCssText));
+    }
+
+    @Test(dataProvider = "denseAlertsTestsData", dataProviderClass = AlertsTestsDataProvider.class)
+    public void denseAlertsTest(Alert denseAlertType, String cssText, String cssClassText, String cssType, String cssColor) {
+        denseAlertType.show();
+        denseAlertType.shouldBe(visible);
+        denseAlertType.has().text(cssText);
+        denseAlertType.icon().has().classValue(Matchers.containsString(cssClassText));
+        denseAlertType.has().css(cssType, cssColor);
+    }
+
+    @Test(dataProvider = "outlinedAlertsTestsData", dataProviderClass = AlertsTestsDataProvider.class)
+    public void outlinedAlertsTest(Alert outlinedAlertType, String cssClassText, String cssType, String cssColor) {
+        outlinedAlertType.show();
+        outlinedAlertType.shouldBe(visible);
+        outlinedAlertType.has().classValue(Matchers.containsString(cssClassText));
+        outlinedAlertType.has().css(cssType, cssColor);
+        if (outlinedAlertType.hasIcon()) outlinedAlertType.icon().has().classValue(Matchers.containsString(cssClassText));
+    }
+
+    @Test(dataProvider = "borderAlertsTestsData", dataProviderClass = AlertsTestsDataProvider.class)
+    public void borderAlertsTest(Alert borderAlertType, String cssText, String cssBorder, String cssType, String cssColor) {
+        borderAlertType.show();
+        borderAlertType.shouldBe(visible);
+        borderAlertType.has().classValue(Matchers.containsString(cssBorder));
+        borderAlertType.has().text(cssText);
+        borderAlertType.has().css(cssType, cssColor);
+    }
+
+    @Test(dataProvider = "transitionAlertsTestsData", dataProviderClass = AlertsTestsDataProvider.class)
+    public void transitionAlertsTest(String attrName, String attrValue, String buttonName) {
+        transitionAlertToggleButton.show();
+        transitionAlertToggleButton.has().text(buttonName);
+        transitionAlertToggleButton.click();
+        waitCondition(() -> transitionAlert.isNotVisible());
+        transitionAlert.has().attr(attrName, Matchers.containsString(attrValue));
     }
 
     private void checkBorderColor(HasAssert<? extends UIAssert<?, ?>> element, String color) {
--- jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/data/AlertsTestsDataProvider.java
+++ jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/data/AlertsTestsDataProvider.java
@@ -7,16 +7,31 @@ import static io.github.com.pages.AlertsPage.errorAlert;
 import static io.github.com.pages.AlertsPage.infoAlert;
 import static io.github.com.pages.AlertsPage.successAlert;
 import static io.github.com.pages.AlertsPage.warningAlert;
+import static io.github.com.pages.AlertsPage.prominentErrorAlert;
+import static io.github.com.pages.AlertsPage.prominentSchoolAlert;
+import static io.github.com.pages.AlertsPage.prominentLockAlert;
+import static io.github.com.pages.AlertsPage.denseInfoAlert;
+import static io.github.com.pages.AlertsPage.denseSuccessAlert;
+import static io.github.com.pages.AlertsPage.denseWarningAlert;
+import static io.github.com.pages.AlertsPage.denseErrorAlert;
+import static io.github.com.pages.AlertsPage.purpleOutlinedAlert;
+import static io.github.com.pages.AlertsPage.successOutlinedAlert;
+import static io.github.com.pages.AlertsPage.warningOutlinedAlert;
+import static io.github.com.pages.AlertsPage.redAlert;
+import static io.github.com.pages.AlertsPage.blueGreyAlert;
+import static io.github.com.pages.AlertsPage.pinkAlert;
+import static io.github.com.pages.AlertsPage.indigoAlert;
+
 
 public class AlertsTestsDataProvider {
 
     @DataProvider(name = "basicAlertsTestsData")
     public static Object[][] basicAlertsTestsData() {
         return new Object[][] {
-                {successAlert, "I'm a success alert.", "mdi-check-circle", "background-color", Colors.GREEN.value()},
-                {infoAlert, "I'm an info alert.", "mdi-information", "background-color", Colors.BLUE.value()},
-                {warningAlert, "I'm a warning alert.", "mdi-exclamation", "background-color", Colors.ORANGE_DARKEN_1.value()},
-                {errorAlert, "I'm an error alert.", "mdi-alert", "background-color", Colors.RED_ACCENT_2.value()}
+                {successAlert, "I'm a success alert.", "mdi-check-circle", "background-color", Colors.GREEN.value(), null},
+                {infoAlert, "I'm an info alert.", "mdi-information", "background-color", Colors.BLUE.value(), null},
+                {warningAlert, "I'm a warning alert.", "mdi-exclamation", "background-color", Colors.ORANGE_DARKEN_1.value(), "shaped"},
+                {errorAlert, "I'm an error alert.", "mdi-alert", "background-color", Colors.RED_ACCENT_2.value(), "elevation"}
         };
     }
 
@@ -24,6 +39,51 @@ public class AlertsTestsDataProvider {
     public static Object[][] dismissibleAlertsTestsData() {
         return new Object[][]{
                 {"Aenean imperdiet. Quisque id odio."}
+        };
+    }
+
+    @DataProvider(name = "prominentAlertsTestsData")
+    public static Object[][] prominentAlertsTestsData() {
+        return new Object[][]{
+                {prominentErrorAlert, "error", "mdi-alert"},
+                {prominentSchoolAlert, "blue-grey", "mdi-school"},
+                {prominentLockAlert, "info--text", "mdi-shield-lock-outline"}
+        };
+    }
+
+    @DataProvider(name = "denseAlertsTestsData")
+    public static Object[][] denseAlertsTestsData() {
+        return new Object[][]{
+                {denseInfoAlert, "I'm a dense alert with a type of info", "mdi-information", "background-color", Colors.BLUE.value()},
+                {denseSuccessAlert, "I'm a dense alert with the text prop and a type of success", "mdi-check-circle", "caret-color", Colors.GREEN_RGB.value()},
+                {denseWarningAlert, "I'm a dense alert with the border prop and a type of warning", "mdi-exclamation", "background-color", Colors.ORANGE_DARKEN_1.value()},
+                {denseErrorAlert, "I'm a dense alert with the outlined prop and a type of error", "mdi-alert", "caret-color", Colors.RED_ACCENT_2_RGB.value()}
+        };
+    }
+
+    @DataProvider(name = "outlinedAlertsTestsData")
+    public static Object[][] outlinedAlertsTestsData() {
+        return new Object[][]{
+                {purpleOutlinedAlert,"purple--text", "caret-color", Colors.PURPLE_RGB.value()},
+                {successOutlinedAlert, "success--text", "caret-color", Colors.GREEN_RGB.value()},
+                {warningOutlinedAlert, "warning--text", "caret-color", Colors.ORANGE_DARKEN_1_RGB.value()}
+        };
+    }
+
+    @DataProvider(name = "borderAlertsTestsData")
+    public static Object[][] borderAlertsTestsData() {
+        return new Object[][]{
+                {redAlert, "I'm an alert with a top border and red color", "top", "background-color", Colors.RED_LIGHTEN_2.value()},
+                {blueGreyAlert, "I'm an alert with a right border and blue-grey color", "right", "background-color", Colors.BLUE_GREY.value()},
+                {pinkAlert, "I'm an alert with a bottom border and pink color", "bottom","background-color", Colors.PINK_DARKEN_1.value()},
+                {indigoAlert, "I'm an alert with a border left type info", "left", "background-color", Colors.INDIGO.value()}
+        };
+    }
+
+    @DataProvider(name = "transitionAlertsTestsData")
+    public static Object[][] transitionAlertsTestsData() {
+        return new Object[][]{
+                {"style", "display: none;", "TOGGLE"}
         };
     }
 }
