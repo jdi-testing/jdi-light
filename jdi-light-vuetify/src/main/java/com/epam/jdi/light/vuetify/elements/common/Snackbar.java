4e65473d9488c78a992ed0503b163605caa916a1
ElenaP <34471412+omega790@users.noreply.github.com>
2022-09-22 16:32:36 +0400

    4317 add tests for different views of snackbars (#4323)

--- jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/elements/common/Snackbar.java
+++ jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/elements/common/Snackbar.java
@@ -4,12 +4,13 @@ import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.elements.base.UIBaseElement;
 import com.epam.jdi.light.elements.interfaces.common.IsText;
 import com.epam.jdi.light.vuetify.asserts.SnackbarAssert;
+import com.epam.jdi.light.vuetify.interfaces.HasTheme;
 
 /**
  * To see an example of Checkbox web element please visit https://vuetifyjs.com/en/components/snackbars/
  */
 
-public class Snackbar extends UIBaseElement<SnackbarAssert> implements IsText {
+public class Snackbar extends UIBaseElement<SnackbarAssert> implements IsText, HasTheme {
 
     @Override
     public SnackbarAssert is() {
@@ -47,8 +48,69 @@ public class Snackbar extends UIBaseElement<SnackbarAssert> implements IsText {
         return core().attr("class").contains("--centered");
     }
 
+    @JDIAction("Check that {name} is top")
+    public boolean isTop() {
+        return core().attr("class").contains("--top");
+    }
+
+    @JDIAction("Check that {name} is bottom")
+    public boolean isBottom() {
+        return core().attr("class").contains("--bottom");
+    }
+
     @JDIAction("Check that {name} is vertical")
     public boolean isVertical() {
         return core().attr("class").contains("--vertical");
     }
+
+    @JDIAction("Check that {name} is multi-line")
+    public boolean isMultiLine() {
+        return core().attr("class").contains("--multi-line");
+    }
+
+    @JDIAction("Check that {name} is shaped")
+    public boolean isShaped() {
+        return core().find(".v-snack__wrapper").attr("class").contains("--shaped");
+    }
+
+    @JDIAction("Check that {name} is rounded")
+    public boolean isRounded() {
+        return core().find(".v-snack__wrapper").attr("class").matches(".* rounded-[^0][\\S]* .*");
+    }
+
+    @JDIAction("Check that {name} is elevated")
+    public boolean isElevated() {
+        return core().find(".v-snack__wrapper").attr("class").contains("elevation-");
+    }
+
+    @JDIAction("Check that {name} is text")
+    public boolean isText() {
+        return core().attr("class").contains("--text");
+    }
+
+    @JDIAction("Check that {name} is outlined")
+    public boolean isOutlined() {
+        return core().find(".v-snack__wrapper").attr("class").contains("--outlined");
+    }
+
+    @Override
+    @JDIAction("Check that {name} has light theme")
+    public boolean isLightTheme() {
+        return core().find(".v-snack__wrapper").hasClass("theme--light");
+    }
+
+    @Override
+    @JDIAction("Check that {name} has dark theme")
+    public boolean isDarkTheme() {
+        return core().find(".v-snack__wrapper").hasClass("theme--dark");
+    }
+
+    @JDIAction("Get '{name}' color")
+    public String color() { return core().find(".v-snack__wrapper").getCssValue("background-color"); }
+
+    @JDIAction("Get '{name}' height")
+    public int height() { return core().find(".v-snack__wrapper").getRect().getHeight(); }
+
+    @JDIAction("Get '{name}' width")
+    public int width() { return core().find(".v-snack__wrapper").getRect().getWidth(); }
 }
