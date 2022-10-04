44451e42593d40d93d8d1c544988ee2c2ed5d445
ElenaP <34471412+omega790@users.noreply.github.com>
2022-10-04 19:41:13 +0400

    4343 sheets refactor and add new tests for different views (#4361)
    * 4343 sheets refactor and add new tests for different views
    
    * 4343 sheets codestyle fix
    
    * 4343 sheets codestyle fix
    
    * 4343 sheets codestyle fix

--- jdi-light-vuetify-tests/src/main/java/io/github/com/pages/SheetsPage.java
+++ jdi-light-vuetify-tests/src/main/java/io/github/com/pages/SheetsPage.java
@@ -3,16 +3,13 @@ package io.github.com.pages;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.vuetify.elements.composite.Sheet;
 
-public class SheetsPage extends VuetifyPage {
-    // elevation prop
-    @UI("(//*[@id = 'ElevationSheet']//*[contains(@class, 'mx-auto')])[1]")
-    public static Sheet elevation6Sheet;
-    @UI("(//*[@id = 'ElevationSheet']//*[contains(@class, 'mx-auto')])[2]")
-    public static Sheet elevation12Sheet;
-    @UI("(//*[@id = 'ElevationSheet']//*[contains(@class, 'mx-auto')])[3]")
-    public static Sheet elevation18Sheet;
+import java.util.List;
 
-    // rounded prop
+public class SheetsPage extends VuetifyPage {
+
+    @UI("#ElevationSheet .v-sheet div")
+    public static List<Sheet> elevationSheets;
+
     @UI("(//*[@id = 'RoundedSheet']//*[contains(@class, 'mx-auto')])[1]")
     public static Sheet defaultRoundedSheet;
     @UI("(//*[@id = 'RoundedSheet']//*[contains(@class, 'mx-auto')])[2]")
@@ -20,17 +17,15 @@ public class SheetsPage extends VuetifyPage {
     @UI("(//*[@id = 'RoundedSheet']//*[contains(@class, 'mx-auto')])[3]")
     public static Sheet xlRoundedSheet;
 
-    // color sheets
-    @UI("//*[@id = 'ColorSheet']//*[contains(@class, 'theme--light') and .//*[contains(text(), '#1')]]")
-    public static Sheet firstColoredSheet;
-    @UI("//*[@id = 'ColorSheet']//*[contains(@class, 'theme--light') and .//*[contains(text(), '#2')]]")
-    public static Sheet secondColoredSheet;
-    @UI("//*[@id = 'ColorSheet']//*[contains(@class, 'theme--light') and .//*[contains(text(), '#3')]]")
-    public static Sheet thirdColoredSheet;
-    @UI("//*[@id = 'ColorSheet']//*[contains(@class, 'theme--light') and .//*[contains(text(), '#4')]]")
-    public static Sheet fourthColoredSheet;
-    @UI("//*[@id = 'ColorSheet']//*[contains(@class, 'theme--light') and .//*[contains(text(), '#5')]]")
-    public static Sheet fifthColoredSheet;
-    @UI("//*[@id = 'ColorSheet']//*[contains(@class, 'theme--light') and .//*[contains(text(), '#6')]]")
-    public static Sheet sixthColoredSheet;
+    @UI("#ColorSheet .v-sheet")
+    public static List<Sheet> coloredSheets;
+
+    @UI("#OutlinedSheet .v-sheet")
+    public static Sheet outlinedSheet;
+
+    @UI("#ShapedSheet .v-sheet")
+    public static Sheet shapedSheet;
+
+    @UI("#TileSheet .v-sheet")
+    public static Sheet tileSheet;
 }
---
+++ jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/interfaces/IsOutlined.java
@@ -0,0 +1,18 @@
+package com.epam.jdi.light.vuetify.interfaces;
+
+import com.epam.jdi.light.common.JDIAction;
+import com.epam.jdi.light.elements.interfaces.base.ICoreElement;
+
+public interface IsOutlined extends ICoreElement {
+
+    /**
+     * Checks if element is outlined or not.
+     *
+     * @return {@code true} if element is outlined, otherwise {@code false}
+     */
+
+    @JDIAction("Check that {name} is outlined")
+    default boolean isOutlined() {
+        return core().attr("class").contains("--outlined");
+    }
+}
