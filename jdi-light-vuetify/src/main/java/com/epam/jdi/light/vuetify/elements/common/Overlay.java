a0ee8ed7e172883d15892614c0aa791f8c0b7051
omega790 <prikhodko.olena@gmail.com>
2022-10-10 11:26:03 +0400

    4787 navigation-drawers delete unused import

--- jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/elements/common/Overlay.java
+++ jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/elements/common/Overlay.java
@@ -3,7 +3,6 @@ package com.epam.jdi.light.vuetify.elements.common;
 import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.elements.base.UIBaseElement;
 import com.epam.jdi.light.elements.common.UIElement;
-import com.epam.jdi.light.elements.interfaces.base.HasClick;
 import com.epam.jdi.light.vuetify.asserts.OverlayAssert;
 
 import static com.epam.jdi.light.elements.init.UIFactory.$;
