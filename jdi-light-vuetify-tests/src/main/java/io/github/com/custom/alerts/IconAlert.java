fc341476b0b1742c583854d796b0652da62db6ec
omega790 <prikhodko.olena@gmail.com>
2022-10-13 15:42:41 +0400

    4223 timelines add asserts for vuetify text

--- jdi-light-vuetify-tests/src/main/java/io/github/com/custom/alerts/IconAlert.java
+++ jdi-light-vuetify-tests/src/main/java/io/github/com/custom/alerts/IconAlert.java
@@ -1,7 +1,7 @@
 package io.github.com.custom.alerts;
 
-import com.epam.jdi.light.vuetify.elements.complex.Text;
+import com.epam.jdi.light.vuetify.elements.common.Alert;
 import com.epam.jdi.light.vuetify.interfaces.HasIcon;
 
-public class IconAlert extends Text implements HasIcon {
+public class IconAlert extends Alert implements HasIcon {
 }
--- jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/elements/common/Alert.java
+++ jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/elements/common/Alert.java
@@ -1,17 +1,28 @@
 package com.epam.jdi.light.vuetify.elements.common;
 
-import com.epam.jdi.light.asserts.generic.TextAssert;
+import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.elements.base.UIBaseElement;
 import com.epam.jdi.light.elements.interfaces.common.IsText;
+import com.epam.jdi.light.vuetify.asserts.AlertAssert;
 import com.epam.jdi.light.vuetify.interfaces.HasIcon;
 
 /**
  * To see an example of Alert web element please visit https://vuetifyjs.com/en/components/alerts/
  */
-public class Alert extends UIBaseElement<TextAssert> implements IsText, HasIcon {
+public class Alert extends UIBaseElement<AlertAssert> implements IsText, HasIcon {
+
+    @JDIAction("Get 'name' color")
+    public String color() {
+        return core().css("color");
+    }
+
+    @JDIAction("Get 'name' background color")
+    public String backgroundColor() {
+        return core().css("background-color");
+    }
 
     @Override
-    public TextAssert is() {
-        return new TextAssert().set(this);
+    public AlertAssert is() {
+        return new AlertAssert().set(this);
     }
 }
