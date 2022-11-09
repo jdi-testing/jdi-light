4af3270855dccb8f6562bfb92860eae384b310e2
Liliia Iskanderova <liliia.iskanderova@ukg.com>
2022-11-09 08:21:55 +0100

    4269 refactor fileinputs remove unused import

--- jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/complex/FileInputsTests.java
+++ jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/complex/FileInputsTests.java
@@ -5,7 +5,6 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.awt.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
