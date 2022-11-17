bd753f29c453d86f1326bde58e20f4ee1495d668
ElenaP <34471412+omega790@users.noreply.github.com>
2022-11-17 22:06:36 +0400

    4404 vuetify lazy refactor tests (#4501)
    * 4404 vuetify lazy: refactor tests
    
    * 4404 vuetify lazy: refactor tests

--- jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/complex/LazyTests.java
+++ jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/complex/LazyTests.java
@@ -6,10 +6,9 @@ import org.testng.annotations.Test;
 
 import static com.jdiai.tools.Timer.waitCondition;
 import static io.github.com.StaticSite.lazyPage;
+import static io.github.com.pages.LazyPage.lazyContent;
 import static io.github.com.pages.LazyPage.lazyCard;
-import static io.github.com.pages.LazyPage.scrollDownText;
 import static org.hamcrest.Matchers.containsString;
-
 
 public class LazyTests extends TestsInit {
 
@@ -21,21 +20,23 @@ public class LazyTests extends TestsInit {
     }
 
     @Test
-    public void itemIsHiddenTest() {
-        lazyCard.has().notExistContent();
+    public void itemIsLoadedTest() {
+        lazyCard.is().notVisible();
+        lazyContent.scrollIntoView();
+        lazyCard.is().displayed();
+        lazyCard.title().is().text("Card title");
+        lazyCard.content().has().text(containsString("Aliquam lobortis"));
     }
 
     @Test
-    public void itemIsLoadedTest() {
-        String title = "Card title";
-        String partOfTextContent = "Aliquam lobortis";
-        lazyCard.scrollIntoView();
-        lazyCard.is().existContent();
-        lazyCard.title().is().text(title);
-        lazyCard.contentText().is().text(containsString(partOfTextContent));
-        scrollDownText.scrollIntoView();
-        lazyCard.is().existContent();
-        lazyCard.title().is().text(title);
-        lazyCard.contentText().is().text(containsString(partOfTextContent));
+    public void measurementLazyText() {
+        lazyContent.scrollIntoView();
+        lazyCard.is().displayed();
+        lazyContent.has().height(350);
+        lazyContent.has().heightGreaterThan(300);
+        lazyContent.has().heightLessThan(400);
+        lazyContent.has().width(300);
+        lazyContent.has().widthGreaterThan(250);
+        lazyContent.has().widthLessThan(350);
     }
 }
