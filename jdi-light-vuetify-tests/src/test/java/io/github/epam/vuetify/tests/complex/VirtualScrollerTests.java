fa8b90e6855a23920186a8fe69b805baa005ddd4
ElenaP <34471412+omega790@users.noreply.github.com>
2022-11-17 22:08:47 +0400

    4233 vuetify virtualscroller refactor tests (#4497)
    * 4233 vuetify virtual scroller test refactor
    
    * 4233 vuetify virtual scroller refactor tests
    
    * 4233 vuetify virtual scroller exception fix

--- jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/complex/VirtualScrollerTests.java
+++ jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/complex/VirtualScrollerTests.java
@@ -4,129 +4,56 @@ import static com.jdiai.tools.Timer.waitCondition;
 import static io.github.com.StaticSite.virtualScrollerPage;
 import static io.github.com.pages.VirtualScrollerPage.benchScroller;
 import static io.github.com.pages.VirtualScrollerPage.totalBenched;
-import static io.github.com.pages.VirtualScrollerPage.userScroller;
-import static org.openqa.selenium.Keys.BACK_SPACE;
 
-import io.github.com.dataproviders.VirtualScrollerDataProvider;
 import io.github.epam.TestsInit;
-import org.hamcrest.Matchers;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class VirtualScrollerTests extends TestsInit {
     private static final int DEFAULT_NUMBER_BENCH_ITEMS = 5;
-    private static final int DEFAULT_NUMBER_USER_ITEMS = 6;
-    private static final int DEFAULT_BENCH_NUMBER = 0;
 
     @BeforeClass
-    public static void before() {
+    public void before() {
         virtualScrollerPage.open();
         waitCondition(() -> virtualScrollerPage.isOpened());
         virtualScrollerPage.checkOpened();
     }
 
     @Test
-    public static void defaultRenderedBenchItemsTest() {
+    public void renderedItemsVirtualScrollTest() {
         benchScroller.is().displayed().and().has().items();
-        clearTextField();
-
-        totalBenched.setValue(String.valueOf(DEFAULT_BENCH_NUMBER));
-        benchScroller.has().renderedItems(DEFAULT_NUMBER_BENCH_ITEMS);
-    }
-
-    @Test(dataProvider = "bench number",
-            dataProviderClass = VirtualScrollerDataProvider.class)
-    public static void changeRenderedBenchItemsTest(Integer benchNumber) {
-        benchScroller.is().displayed().and().has().items();
-        benchScroller.scrollToPosition(0);
-        clearTextField();
-
-        totalBenched.sendKeys(String.valueOf(benchNumber));
-        benchScroller.has().renderedItems(DEFAULT_NUMBER_BENCH_ITEMS + benchNumber);
-    }
-
-    @Test(dataProvider = "bench number with scroll",
-            dataProviderClass = VirtualScrollerDataProvider.class)
-    public static void changeRenderedBenchItemsWithScrollTest(Integer benchNumber, String itemText) {
-        benchScroller.is().displayed().and().has().items();
-        clearTextField();
-
-        totalBenched.sendKeys(String.valueOf(benchNumber));
-        benchScroller.scrollToElementWithText(itemText);
-        benchScroller.has().renderedItems(DEFAULT_NUMBER_BENCH_ITEMS + 2 * benchNumber);
-    }
-
-    @Test(dataProvider = "pixels to scroll down",
-            dataProviderClass = VirtualScrollerDataProvider.class)
-    public static void benchScrollDownToPositionTest(Integer pixelsToScrollDown) {
-        benchScroller.is().displayed().and().has().items();
-        benchScroller.scrollToPosition(0);
-
-        int startPosition = benchScroller.position();
-        benchScroller.scrollToPosition(startPosition + pixelsToScrollDown);
-        benchScroller.has().positionBetween(startPosition, startPosition + pixelsToScrollDown);
-    }
-
-    @Test(dataProvider = "pixels to scroll down and up",
-            dataProviderClass = VirtualScrollerDataProvider.class)
-    public static void benchScrollDownAndUpToPositionTest(Integer pixelsToScrollDown, Integer pixelsToScrollUp) {
-        benchScroller.is().displayed().and().has().items();
-        benchScroller.scrollToPosition(0);
-
-        int startPosition = benchScroller.position();
-        benchScroller.scrollToPosition(pixelsToScrollDown);
-        int endPosition = benchScroller.position();
-        benchScroller.has().position(Matchers.greaterThan(startPosition));
-
-        startPosition = endPosition;
-        benchScroller.scrollToPosition(startPosition - pixelsToScrollUp);
-        benchScroller.has().position(Matchers.lessThan(startPosition));
-    }
-
-    @Test(dataProvider = "bench items text",
-            dataProviderClass = VirtualScrollerDataProvider.class)
-    public static void benchScrollToItemTest(String itemText) {
-        benchScroller.is().displayed().and().has().items();
-
-        benchScroller.scrollToElementWithText(itemText);
-        benchScroller.items().get(3).find(".v-list-item__content").has().text(itemText);
+        totalBenched.input("0");
+        benchScroller.has().itemsCount(DEFAULT_NUMBER_BENCH_ITEMS);
+        totalBenched.input("6");
+        benchScroller.has().itemsCount(DEFAULT_NUMBER_BENCH_ITEMS + 6);
     }
 
     @Test
-    public static void defaultRenderedUserItemsTest() {
-        userScroller.is().displayed()
-                .and().has().items()
-                .and().renderedItems(DEFAULT_NUMBER_USER_ITEMS);
+    public void textVirtualScrollerTest() {
+        String[] virtualScrollerItems = {"User Database Record ID 1", "User Database Record ID 2",
+                "User Database Record ID 3", "User Database Record ID 4", "User Database Record ID 5"};
+        benchScroller.scrollToTop();
+        benchScroller.show();
+        benchScroller.has().text(virtualScrollerItems);
     }
 
-    @Test(dataProvider = "pixels to scroll down",
-            dataProviderClass = VirtualScrollerDataProvider.class)
-    public static void userScrollDownToPositionTest(Integer pixelsToScrollDown) {
-        userScroller.is().displayed().and().has().items();
-
-        int startPosition = userScroller.position();
-        userScroller.scrollToPosition(startPosition + pixelsToScrollDown);
-        userScroller.has().positionBetween(startPosition, startPosition + pixelsToScrollDown);
+    @Test
+    public void measurementVirtualScrollerTest() {
+        benchScroller.show();
+        benchScroller.has().height(300);
+        benchScroller.has().heightGreaterThan(200);
+        benchScroller.has().heightLessThan(400);
+        benchScroller.has().width(400);
+        benchScroller.has().widthGreaterThan(300);
+        benchScroller.has().widthLessThan(500);
+        benchScroller.has().itemsHeight(64);
     }
 
-    @Test(dataProvider = "pixels to scroll down and up",
-            dataProviderClass = VirtualScrollerDataProvider.class)
-    public static void userScrollDownAndUpToPositionTest(Integer pixelsToScrollDown, Integer pixelsToScrollUp) {
-        userScroller.is().displayed().and().has().items();
-
-        int startPosition = userScroller.position();
-        userScroller.scrollToPosition(startPosition + pixelsToScrollDown);
-        int endPosition = userScroller.position();
-        userScroller.has().position(Matchers.greaterThan(startPosition));
-
-        startPosition = endPosition;
-        userScroller.scrollToPosition(startPosition - pixelsToScrollUp);
-        userScroller.has().position(Matchers.lessThan(startPosition));
-    }
-
-    private static void clearTextField() {
-        while (!totalBenched.getText().equals("")) {
-            totalBenched.sendKeys(BACK_SPACE);
-        }
+    @Test
+    public void scrollToItemVirtualScrollerTest() {
+        String itemText = "User Database Record ID 14";
+        benchScroller.show();
+        benchScroller.scrollToElement(itemText);
+        benchScroller.item(itemText).is().displayed();
     }
 }
--- jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/asserts/VirtualScrollerAssert.java
+++ jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/asserts/VirtualScrollerAssert.java
@@ -5,36 +5,47 @@ import static com.epam.jdi.light.asserts.core.SoftAssert.jdiAssert;
 import com.epam.jdi.light.asserts.generic.UIAssert;
 import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.vuetify.elements.complex.VirtualScroller;
+import com.epam.jdi.light.vuetify.interfaces.asserts.MeasurementAssert;
 import com.jdiai.tools.Timer;
-import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 
-public class VirtualScrollerAssert extends UIAssert<VirtualScrollerAssert, VirtualScroller> {
+import java.util.Arrays;
+import java.util.List;
 
-    @JDIAction("Assert that '{name}' position is between {0} and {1}")
-    public VirtualScrollerAssert positionBetween(int lowerValue, int upperValue) {
-        position(Matchers.greaterThan(lowerValue))
-                .and().position(Matchers.lessThanOrEqualTo(upperValue));
-        return this;
-    }
-
-    @JDIAction("Assert that '{name}' position is {0}")
-    public VirtualScrollerAssert position(Matcher<Integer> condition) {
-        jdiAssert(element().position(), condition);
-        return this;
-    }
+public class VirtualScrollerAssert extends UIAssert<VirtualScrollerAssert, VirtualScroller>
+        implements MeasurementAssert<VirtualScrollerAssert, VirtualScroller> {
 
     @JDIAction("Assert that number of rendered items of '{name}' are {0}")
-    public VirtualScrollerAssert renderedItems(int number) {
-        Timer.waitCondition(element().items()::isExist);
-        jdiAssert(element().items().size(), Matchers.is(number));
+    public VirtualScrollerAssert itemsCount(int expectedCount) {
+        Timer.waitCondition(() -> !element().items().isEmpty());
+        int actualCount = element().items().size();
+        jdiAssert(actualCount, Matchers.is(expectedCount),
+                String.format("Number of actual rendered items is '%d', but expected '%d'", actualCount, expectedCount));
         return this;
     }
 
     @JDIAction("Assert that '{name}' items are exist")
     public VirtualScrollerAssert items() {
-        Timer.waitCondition(element().items()::isExist);
+        Timer.waitCondition(()-> !element().items().isEmpty());
         jdiAssert(element().items(), Matchers.notNullValue());
+        return this;
+    }
+
+    @JDIAction("Assert that '{name}' items has text {0}")
+    public VirtualScrollerAssert text(String... expectedText) {
+        Timer.waitCondition(()-> !element().items().isEmpty());
+        List<String> actualItemsText = element().itemsText();
+        jdiAssert(actualItemsText.containsAll(Arrays.asList(expectedText)), Matchers.is(true),
+                String.format("Virtual scroller has '%s' items, but expected '%s'",
+                        String.join(", ", actualItemsText), String.join(", ", expectedText)));
+        return this;
+    }
+
+    @JDIAction("Assert that '{name}' has {0} items height")
+    public VirtualScrollerAssert itemsHeight(int expectedItemsHeight) {
+        int actualItemsHeight = element().itemHeight();
+        jdiAssert(actualItemsHeight, Matchers.is(expectedItemsHeight),
+                String.format("Actual item height is '%d', but expected '%d'", actualItemsHeight, expectedItemsHeight));
         return this;
     }
 }
