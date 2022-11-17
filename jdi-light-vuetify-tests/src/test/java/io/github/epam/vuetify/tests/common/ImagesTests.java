b76db3d6c3185660a10757ad82d2a187eed8e82a
omega790 <prikhodko.olena@gmail.com>
2022-11-17 16:54:55 +0400

    4350 vuetify images: refactor tests

--- jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/common/ImagesTests.java
+++ jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/common/ImagesTests.java
@@ -1,28 +1,20 @@
 package io.github.epam.vuetify.tests.common;
 
-import com.epam.jdi.light.vuetify.elements.common.Image;
 import io.github.epam.TestsInit;
-import io.github.epam.vuetify.tests.data.ImagesTestsData;
-import org.hamcrest.Matchers;
+import io.github.epam.vuetify.tests.data.ImagesTestsDataProvider;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.List;
-
-import static com.epam.jdi.light.asserts.core.SoftAssert.jdiAssert;
 import static com.jdiai.tools.Timer.waitCondition;
 import static io.github.com.StaticSite.imagesPage;
+import static io.github.com.pages.ImagesPage.altImage;
 import static io.github.com.pages.ImagesPage.aspectRatioImage;
 import static io.github.com.pages.ImagesPage.containImages;
 import static io.github.com.pages.ImagesPage.gradientsImages;
-import static io.github.com.pages.ImagesPage.gridImages;
-import static io.github.com.pages.ImagesPage.heightImages;
 import static io.github.com.pages.ImagesPage.placeholderImage;
 import static io.github.com.pages.ImagesPage.slider;
 
 public class ImagesTests extends TestsInit {
-
-    private final ImagesTestsData imagesTestsData = new ImagesTestsData();
 
     @BeforeClass
     public void before() {
@@ -31,65 +23,54 @@ public class ImagesTests extends TestsInit {
         imagesPage.checkOpened();
     }
 
-    @Test
-    public void aspectRatioImageTests() {
+    @Test(dataProvider = "measurementImagesDataProvider", dataProviderClass = ImagesTestsDataProvider.class)
+    public void measurementImagesTest(int sliderValue, int width, int height) {
         aspectRatioImage.is().displayed();
-        aspectRatioImage.has().sourcePath("https://cdn.vuetifyjs.com/images/parallax/material.jpg");
-        for (ImagesTestsData.AspectRatioImageTestDataObject dataObject : imagesTestsData.aspectRatioImageTestData()) {
-            slider.slideHorizontalTo(String.valueOf(dataObject.getSliderPosition()));
-            aspectRatioImage.has().width(dataObject.getWidth());
-            aspectRatioImage.has().height(dataObject.getHeight());
-        }
+        slider.slideHorizontalTo(String.valueOf(sliderValue));
+        aspectRatioImage.has().width(width);
+        aspectRatioImage.has().height(height);
     }
 
     @Test
-    public void containImagesTests() {
-        for (Image image : containImages) {
-            image.show();
-            waitCondition(() -> !image.hasSourcePath().equals("")); //because images uploading after image frame is shown
-            jdiAssert(image.hasSourcePath(), Matchers.containsString("https://jdi-testing.github.io/jdi-light/vuetify/pictures/"));
-        }
+    public void altImageTest() {
+        altImage.show();
+        altImage.has().altText("Empty Subway");
     }
 
     @Test
-    public void gradientsImagesTests() {
-        for (Image image : gradientsImages) {
-            image.is().displayed();
-            image.has().sourcePath("https://cdn.vuetifyjs.com/images/parallax/material2.jpg");
-            image.has().gradient();
-        }
+    public void containImageTest() {
+        containImages.get(4).show();
+        containImages.get(4).is().contain();
+        containImages.get(1).is().notContain();
     }
 
     @Test
-    public void heightImagesTests() {
-        for (Image image : heightImages) {
-            image.show();
-            image.is().displayed();
-            waitCondition(() -> !image.hasSourcePath().equals(""));
-            jdiAssert(image.hasSourcePath(), Matchers.containsString("https://jdi-testing.github.io/jdi-light/vuetify/pictures/"));
-            image.has().limitedHeight();
-            image.has().height(125);
-        }
+    public void themeImagesTest() {
+        aspectRatioImage.show();
+        aspectRatioImage.has().lightTheme();
     }
 
     @Test
-    public void placeholderImageTests() {
-        placeholderImage.is().displayed();
-        placeholderImage.has().sourcePath("https://jdi-testing.github.io/jdi-light/vuetify/pictures/placeholder_60.jpeg");
-        placeholderImage.has().width(500);
-        placeholderImage.has().height(300);
-        placeholderImage.is().loading();
+    public void sourceImageTest() {
+        gradientsImages.forEach(gradientsImage -> {
+            gradientsImage.show();
+            gradientsImage.has().sourcePath("https://cdn.vuetifyjs.com/images/parallax/material2.jpg");
+        });
     }
 
     @Test
-    public void gridImagesTests() {
-        final List<String> lazySrc = imagesTestsData.gridImagesLazySrcTestData();
-        final List<String> src = imagesTestsData.gridImagesSrcTestData();
-        int imageIndex = 0;
-        for (Image image : gridImages) {
-            image.is().displayed();
-            image.has().sourcePath(lazySrc.get(imageIndex), src.get(imageIndex));
-            imageIndex++;
-        }
+    public void gradientsImagesTest() {
+        gradientsImages.forEach(gradientsImage -> {
+            gradientsImage.show();
+            gradientsImage.has().gradient();
+        });
+        aspectRatioImage.has().noGradient();
+    }
+
+    @Test
+    public void placeholderImageTest() {
+        placeholderImage.show();
+        placeholderImage.has().placeholder();
+        aspectRatioImage.has().noPlaceholder();
     }
 }
