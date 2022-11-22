c51edcb1d84816c962829124a4e61dd2105b17f5
omega790 <prikhodko.olena@gmail.com>
2022-11-22 17:47:07 +0400

    4370 vuetify overlay: refactor tests

--- jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/common/OverlaysTests.java
+++ jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/common/OverlaysTests.java
@@ -1,19 +1,23 @@
 package io.github.epam.vuetify.tests.common;
 
-import com.epam.jdi.light.vuetify.elements.common.Overlay;
 import io.github.epam.TestsInit;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static com.jdiai.tools.Timer.waitCondition;
 import static io.github.com.StaticSite.overlaysPage;
+import static io.github.com.enums.Colors.CYAN_DARKEN_5;
 import static io.github.com.pages.OverlaysPage.absoluteOverlay;
 import static io.github.com.pages.OverlaysPage.absoluteOverlayButton;
+import static io.github.com.pages.OverlaysPage.advancedOverlay;
 import static io.github.com.pages.OverlaysPage.advancedOverlayCard;
+import static io.github.com.pages.OverlaysPage.closeButton;
 import static io.github.com.pages.OverlaysPage.loaderOverlay;
 import static io.github.com.pages.OverlaysPage.loaderOverlayButton;
 import static io.github.com.pages.OverlaysPage.opacityOverlay;
 import static io.github.com.pages.OverlaysPage.opacityOverlayButton;
+import static io.github.com.pages.OverlaysPage.progressCircular;
 import static io.github.com.pages.OverlaysPage.zIndexOverlay;
 import static io.github.com.pages.OverlaysPage.zIndexOverlayButton;
 
@@ -26,71 +30,62 @@ public class OverlaysTests extends TestsInit {
         overlaysPage.checkOpened();
     }
 
-    @Test
-    public void absoluteOverlayTest() {
-        absoluteOverlay.is().absolute();
-        absoluteOverlay.is().notActive();
-        absoluteOverlay.is().opacity(0);
+    @AfterMethod()
+    public void afterTest() {
+        if (closeButton.isDisplayed()) {
+            closeButton.click();
+            waitCondition(closeButton::isHidden);
+        } else if (progressCircular.isVisible()) {
+            waitCondition(progressCircular::isHidden);
+        }
+    }
 
+    @Test(description = "Check that overlay position is absolute")
+    public void absolutePositionOverlaysTest() {
+        absoluteOverlayButton.show();
         absoluteOverlayButton.click();
-        absoluteOverlay.is().absolute();
-        absoluteOverlay.is().active();
-        absoluteOverlay.is().opacity(0.46);
-
-        absoluteOverlay.content().find("button").click();
-        absoluteOverlay.is().notActive();
-        absoluteOverlay.is().opacity(0);
+        absoluteOverlay.has().absolutePosition();
     }
 
-    @Test
-    public void opacityOverlayTest() {
-        opacityOverlay.is().opacity(0);
-        opacityOverlayButton.click();
-        opacityOverlay.is().opacity(1);
-        opacityOverlay.content().find("button").click();
-        opacityOverlay.is().opacity(0);
-    }
-
-    @Test
-    public void zIndexOverlayTest() {
-        zIndexOverlay.is().zIndex(0);
-        zIndexOverlay.is().opacity(0);
-        zIndexOverlay.is().notAbsolute();
-
-        absoluteOverlay.is().zIndex(5);
-        absoluteOverlayButton.click();
-        absoluteOverlay.is().active();
-
-        zIndexOverlayButton.click();
-        zIndexOverlay.is().active();
-
-        absoluteOverlay.content().find("button").click();
-        absoluteOverlay.is().notActive();
-
-        zIndexOverlay.content().find("button").click();
-        zIndexOverlay.is().notActive();
-    }
-
-    @Test
-    public void advancedOverlayTest() {
-        advancedOverlayCard.finds(".v-overlay").is().empty();
-        advancedOverlayCard.hover();
-        advancedOverlayCard.finds(".v-overlay").is().notEmpty();
-        Overlay advancedOverlay = new Overlay(advancedOverlayCard.find(".v-overlay"));
-        advancedOverlay.is().active();
-        advancedOverlay.is().absolute();
-        advancedOverlay.is().contentExist();
-        zIndexOverlayButton.hover();
-        advancedOverlayCard.finds(".v-overlay").is().empty();
-    }
-
-    @Test
-    public void loaderOverlayTest() {
-        loaderOverlay.is().contentNotExist();
+    @Test(description = "Check that overlay position is not absolute")
+    public void notAbsolutePositionOverlaysTest() {
+        loaderOverlayButton.show();
         loaderOverlayButton.click();
-        loaderOverlay.is().active();
-        loaderOverlay.is().contentExist();
-        loaderOverlay.content().firstChild().is().cssClass("v-progress-circular");
-        loaderOverlay.is().contentNotExist();
+        loaderOverlay.has().notAbsolutePosition();
+    }
+
+    @Test(description = "Check overlay color")
+    public void colorOverlaysTest() {
+        advancedOverlayCard.show();
+        advancedOverlayCard.hover();
+        advancedOverlay.has().backgroundColor(CYAN_DARKEN_5.value());
+    }
+
+    @Test(description = "Check that overlay theme is dark")
+    public void darkThemeOverlaysTest() {
+        absoluteOverlayButton.show();
+        absoluteOverlayButton.click();
+        absoluteOverlay.has().darkTheme();
+    }
+
+    @Test(description = "Check overlay opacity value")
+    public void opacityOverlaysTest() {
+        opacityOverlayButton.show();
+        opacityOverlayButton.click();
+        opacityOverlay.has().opacity(1);
+    }
+
+    @Test(description = "Check that overlay is displayed or not")
+    public void displayedOverlaysTest() {
+        absoluteOverlay.is().hidden();
+        absoluteOverlayButton.click();
+        absoluteOverlay.is().displayed();
+    }
+
+    @Test(description = "Check overlay z-index value")
+    public void zIndexOverlaysTest() {
+        zIndexOverlayButton.show();
+        zIndexOverlayButton.click();
+        zIndexOverlay.has().zIndex(0);
     }
 }
--- jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/elements/common/Overlay.java
+++ jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/elements/common/Overlay.java
@@ -2,45 +2,37 @@ package com.epam.jdi.light.vuetify.elements.common;
 
 import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.elements.base.UIBaseElement;
-import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.light.vuetify.asserts.OverlayAssert;
+import com.epam.jdi.light.vuetify.interfaces.HasColor;
+import com.epam.jdi.light.vuetify.interfaces.HasTheme;
 
 import static com.epam.jdi.light.elements.init.UIFactory.$;
-import static com.epam.jdi.light.elements.init.UIFactory.$$;
 
 /**
  * To see an example of Overlay web element please visit https://vuetifyjs.com/en/components/overlays
  */
-public class Overlay extends UIBaseElement<OverlayAssert> {
+public class Overlay extends UIBaseElement<OverlayAssert> implements HasColor, HasTheme {
 
     private static final double DEFAULT_OPACITY = 0.46;
     private static final int DEFAULT_Z_INDEX = 5;
+    private static final String OVERLAY_SCRIM_LOCATOR = ".v-overlay__scrim";
 
-    public Overlay() {
-    }
-
-    public Overlay(UIElement element) {
-        core().setCore(element);
-    }
-
-    @JDIAction("Get content '{name}'")
-    public UIElement content() {
-        String contentLocator = ".v-overlay__content";
-        return $$(contentLocator, core()).stream().findFirst().orElse(null);
-    }
-
-    public boolean isActive() {
+    @Override
+    @JDIAction("Check that '{name}' is displayed")
+    public boolean isDisplayed() {
         return core().hasClass("v-overlay--active");
     }
 
+    @JDIAction("Check that '{name}' is absolute")
     public boolean isAbsolute() {
         return core().hasClass("v-overlay--absolute");
     }
 
-    @JDIAction("Get '{name}' z-index")
+    @JDIAction("Get '{name}' z-index value")
     public int zIndex() {
-        if (!core().getAttribute("style").replaceAll("^\\D*?(\\d+).*$", "$1").isEmpty()) {
-            return Integer.parseInt(core().getAttribute("style").replaceAll("^\\D*?(\\d+).*$", "$1"));
+        String zIndex = core().getAttribute("style").replaceAll("^\\D*?(\\d+).*$", "$1");
+        if (!zIndex.isEmpty()) {
+            return Integer.parseInt(zIndex);
         } else {
             return DEFAULT_Z_INDEX;
         }
@@ -48,17 +40,18 @@ public class Overlay extends UIBaseElement<OverlayAssert> {
 
     @JDIAction("Get '{name}' opacity")
     public double opacity() {
-        String scrimLocator = ".v-overlay__scrim";
-        if (!$(scrimLocator, core()).css("opacity").isEmpty()) {
-            return Double.parseDouble($(scrimLocator, core()).css("opacity"));
+        String opacity = $(OVERLAY_SCRIM_LOCATOR, core()).css("opacity");
+        if (!opacity.isEmpty()) {
+            return Double.parseDouble(opacity);
         } else {
             return DEFAULT_OPACITY;
         }
     }
 
-    @JDIAction("Get '{name}' color")
-    public String color() {
-        return this.css("background-color");
+    @Override
+    @JDIAction("Get '{name}' background color")
+    public String backgroundColor() {
+        return core().find(OVERLAY_SCRIM_LOCATOR).css("background-color");
     }
 
     @Override
