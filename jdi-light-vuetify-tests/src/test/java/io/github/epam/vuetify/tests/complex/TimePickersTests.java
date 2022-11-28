509c3f6af570b2118b4777ffbde9ecb5ac8fff93
Liliia Iskanderova <61730255+Liliya-web@users.noreply.github.com>
2022-11-28 16:41:20 +0300

    4336 refactor timepickers (#4508)
    Co-authored-by: Liliia Iskanderova <liliia.iskanderova@ukg.com>

--- jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/complex/TimePickersTests.java
+++ jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/complex/TimePickersTests.java
@@ -1,6 +1,5 @@
 package io.github.epam.vuetify.tests.complex;
 
-import com.jdiai.tools.Timer;
 import io.github.epam.TestsInit;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -15,37 +14,29 @@ import static io.github.com.pages.TimePickersPage.allowedTimesTimePicker;
 import static io.github.com.pages.TimePickersPage.amPmInTitleTimePicker;
 import static io.github.com.pages.TimePickersPage.disabledTimePicker;
 import static io.github.com.pages.TimePickersPage.elevatedTimePicker;
-import static io.github.com.pages.TimePickersPage.endEventTimePicker;
 import static io.github.com.pages.TimePickersPage.firstColorTimePicker;
 import static io.github.com.pages.TimePickersPage.firstWidthTimePicker;
 import static io.github.com.pages.TimePickersPage.formatTimePicker;
 import static io.github.com.pages.TimePickersPage.noTitleTimePicker;
 import static io.github.com.pages.TimePickersPage.pickerInDialogTimePicker;
-import static io.github.com.pages.TimePickersPage.pickerInMenuTimePicker;
 import static io.github.com.pages.TimePickersPage.readOnlyTimePicker;
+import static io.github.com.pages.TimePickersPage.scrollableTimePicker;
 import static io.github.com.pages.TimePickersPage.secondAmPmInTitleTimePicker;
-import static io.github.com.pages.TimePickersPage.secondColorTimePicker;
-import static io.github.com.pages.TimePickersPage.startEventTimePicker;
-import static io.github.com.pages.TimePickersPage.useSecondsTimePicker;
+import static io.github.com.pages.TimePickersPage.useSecondsTimePicker1;
+import static io.github.com.pages.TimePickersPage.useSecondsTimePicker2;
 
 public class TimePickersTests extends TestsInit {
 
     private static final String GREEN_COLOR_HEX = "#66bb6a";
-    private static final String BLUE_COLOR_HEX = "#1976d2";
-    private static final String ELEVATION = "15px";
+    private static final int ELEVATION = 15;
     private static final int TIME_PICKER_WIDTH = 290;
-
-    private static final String CHOSEN_AVAILABLE_HOURS = "15";
+    private static final int TIME_PICKER_HEIGHT = 392;
     private static final String CHOSEN_TIME = "7:15:45 PM";
     private static final String CHOSEN_TIME_AM = "7:15 AM";
     private static final String CHOSEN_TIME_TWO_DIGITS = "07:15";
     private static final String CHOSEN_HOURS = "7";
     private static final String CHOSEN_MINUTES = "15";
     private static final String CHOSEN_SECONDS = "45";
-    private static final String CHOSEN_HOURS_TWO = "7";
-    private static final String CHOSEN_MINUTES_TWO = "15";
-    private static final String CHOSEN_HOURS_THREE = "5";
-    private static final String CHOSEN_MINUTES_THREE = "25";
     private static final String CHOSEN_HOURS_FOUR = "3";
     private static final String CHOSEN_MINUTES_FOUR = "55";
     private static final String CHOSEN_TIME_FOUR = "3:55 PM";
@@ -55,16 +46,6 @@ public class TimePickersTests extends TestsInit {
     private static final List<String> CHECKED_ENABLED_HOURS = Arrays.asList("9", "11", "13", "15", "17", "19", "21");
     private static final List<String> CHECKED_DISABLED_HOURS = Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7",
             "8", "10", "12", "14", "16", "18", "20", "22", "23");
-    private static final List<String> CHECKED_ENABLED_MINUTES = Arrays.asList("10", "15", "20", "25", "30", "35",
-            "40", "45", "50");
-    private static final List<String> CHECKED_DISABLED_MINUTES = Arrays.asList("00", "05", "55");
-
-    private static final List<String> ALL_ENABLED_HOURS = Arrays.asList("12", "1", "2", "3", "4", "5", "6",
-            "7", "8", "9", "10", "11");
-    private static final List<String> ALL_ENABLED_MINUTES = Arrays.asList("00", "05", "10", "15", "20",
-            "25", "30", "35", "40", "45", "50", "55");
-    private static final List<String> END_EVENT_DISABLED_HOURS = Arrays.asList("12", "1", "2", "3", "4", "5", "6");
-    private static final List<String> END_EVENT_DISABLED_MINUTES = Arrays.asList("00", "05", "10");
 
     @BeforeClass
     public void before() {
@@ -73,23 +54,21 @@ public class TimePickersTests extends TestsInit {
         timePickersPage.checkOpened();
     }
 
-    @Test
+    @Test(description = "Test checks that enabled and disabled hours or minutes are proper")
     public void allowedTimesTimePickerTest() {
-        Timer.waitCondition(() -> allowedTimesTimePicker.isVisible());
-        allowedTimesTimePicker.has().twentyFourHourFormat();
+        allowedTimesTimePicker.show();
         allowedTimesTimePicker.has().disabledHoursOrMinutesNonEmptyList();
         allowedTimesTimePicker.has().clickableEnabledHoursOrMinutes();
         allowedTimesTimePicker.has().nonClickableDisabledHoursOrMinutes();
         allowedTimesTimePicker.has().properEnabledHoursOrMinutes(CHECKED_ENABLED_HOURS);
         allowedTimesTimePicker.has().properDisabledHoursOrMinutes(CHECKED_DISABLED_HOURS);
-        allowedTimesTimePicker.selectHours(CHOSEN_AVAILABLE_HOURS);
-        allowedTimesTimePicker.has().properEnabledHoursOrMinutes(CHECKED_ENABLED_MINUTES);
-        allowedTimesTimePicker.has().properDisabledHoursOrMinutes(CHECKED_DISABLED_MINUTES);
+        allowedTimesTimePicker.has().activeHoursOrMinutes("11");
+        allowedTimesTimePicker.has().nonActiveHoursOrMinutes("0");
     }
 
-    @Test
+    @Test (description = "Test checks switching to AM/PM periods : am/pm in title")
     public void amPmInTitleTimePickerTest() {
-        Timer.waitCondition(() -> amPmInTitleTimePicker.isVisible());
+        amPmInTitleTimePicker.show();
         amPmInTitleTimePicker.switchToPM();
         amPmInTitleTimePicker.has().pmPeriod();
         amPmInTitleTimePicker.switchToAM();
@@ -100,116 +79,89 @@ public class TimePickersTests extends TestsInit {
         secondAmPmInTitleTimePicker.has().pmPeriod();
     }
 
-    @Test
+    @Test (description = "Test checks element's background color")
     public void colorTimePickerTest() {
-        Timer.waitCondition(() -> firstColorTimePicker.isVisible());
-        firstColorTimePicker.has().twelveHourFormat();
-        firstColorTimePicker.selectHours(CHOSEN_HOURS);
-        firstColorTimePicker.selectMinutes(CHOSEN_MINUTES);
-        firstColorTimePicker.has().hours(CHOSEN_HOURS);
-        firstColorTimePicker.has().minutes(CHOSEN_MINUTES);
-        firstColorTimePicker.has().amPeriod();
-        firstColorTimePicker.switchToPM();
-        firstColorTimePicker.has().pmPeriod();
-        firstColorTimePicker.switchToAM();
-        firstColorTimePicker.has().amPeriod();
-        firstColorTimePicker.has().color(GREEN_COLOR_HEX);
-        secondColorTimePicker.has().color(BLUE_COLOR_HEX);
+        firstColorTimePicker.show();
+        firstColorTimePicker.has().backgroundColor(GREEN_COLOR_HEX);
     }
 
-    @Test
+    @Test (description = "Test checks that timepicker is disabled or not")
     public void disabledTimePickerTest() {
-        Timer.waitCondition(() -> disabledTimePicker.isVisible());
+        disabledTimePicker.show();
         disabledTimePicker.is().disabled();
-        disabledTimePicker.has().activeHoursOrMinutes("12");
+        elevatedTimePicker.show();
+        elevatedTimePicker.is().enabled();
     }
 
-    @Test
+    @Test (description = "Test checks that element has elevation or not")
     public void elevationTimePickerTest() {
-        Timer.waitCondition(() -> elevatedTimePicker.isVisible());
+        elevatedTimePicker.show();
+        elevatedTimePicker.is().elevated();
         elevatedTimePicker.has().elevation(ELEVATION);
     }
 
-    @Test
-    public void formatTimePickerTest() {
-        Timer.waitCondition(() -> formatTimePicker.isVisible());
-        formatTimePicker.has().twentyFourHourFormat();
-        formatTimePicker.selectHours(CHOSEN_HOURS);
-        formatTimePicker.selectMinutes(CHOSEN_MINUTES);
+    @Test (description = "Test checks that AM/PM, hours, minutes, seconds can be selected correctly")
+    public void selectHoursMinutesSecondsTimePickerTest() {
+        useSecondsTimePicker1.show();
+        useSecondsTimePicker1.switchToPM();
+        useSecondsTimePicker1.selectHours(CHOSEN_HOURS);
+        useSecondsTimePicker1.selectMinutes(CHOSEN_MINUTES);
+        useSecondsTimePicker1.selectSeconds(CHOSEN_SECONDS);
+        useSecondsTimePicker1.has().hours(CHOSEN_HOURS);
+        useSecondsTimePicker1.has().minutes(CHOSEN_MINUTES);
+        useSecondsTimePicker1.has().seconds(CHOSEN_SECONDS);
+        useSecondsTimePicker1.has().time(CHOSEN_TIME);
     }
 
-    @Test
-    public void noTitleTimePickerTest() {
-        Timer.waitCondition(() -> noTitleTimePicker.isVisible());
+    @Test (description = "Test selects time and checks that it is selected")
+    public void setTimeWithSecondsTimePickerTest() {
+        useSecondsTimePicker1.show();
+        useSecondsTimePicker1.selectTime(CHOSEN_TIME);
+        useSecondsTimePicker1.has().time(CHOSEN_TIME);
+    }
+
+    @Test (description = "Test checks if timepicker has title or not")
+    public void titleTimePickerTest() {
+        noTitleTimePicker.show();
         noTitleTimePicker.has().noTitle();
+        formatTimePicker.show();
+        formatTimePicker.has().title();
     }
 
-    @Test
-    public void planEventTimePickerTest() {
-        Timer.waitCondition(() -> startEventTimePicker.isVisible());
-        startEventTimePicker.switchToAM();
-        startEventTimePicker.selectHours(CHOSEN_HOURS);
-        startEventTimePicker.selectMinutes(CHOSEN_MINUTES);
-        startEventTimePicker.has().hours(CHOSEN_HOURS);
-        startEventTimePicker.has().minutes(CHOSEN_MINUTES);
-        endEventTimePicker.has().properDisabledHoursOrMinutes(END_EVENT_DISABLED_HOURS);
-        endEventTimePicker.selectHours(CHOSEN_HOURS_TWO);
-        endEventTimePicker.has().properDisabledHoursOrMinutes(END_EVENT_DISABLED_MINUTES);
-        endEventTimePicker.selectMinutes(CHOSEN_MINUTES_TWO);
-        endEventTimePicker.has().hours(CHOSEN_HOURS_TWO);
-        endEventTimePicker.has().minutes(CHOSEN_MINUTES_TWO);
-        startEventTimePicker.selectHours(CHOSEN_HOURS_THREE);
-        startEventTimePicker.selectMinutes(CHOSEN_MINUTES_THREE);
-        startEventTimePicker.has().hours(CHOSEN_HOURS_THREE);
-        startEventTimePicker.has().minutes(CHOSEN_MINUTES_THREE);
-        endEventTimePicker.switchToPM();
-        endEventTimePicker.clickTitleHours();
-        endEventTimePicker.has().properEnabledHoursOrMinutes(ALL_ENABLED_HOURS);
-        endEventTimePicker.selectHours(CHOSEN_HOURS_FOUR);
-        endEventTimePicker.has().properEnabledHoursOrMinutes(ALL_ENABLED_MINUTES);
-        endEventTimePicker.selectMinutes(CHOSEN_MINUTES_FOUR);
-        endEventTimePicker.has().hours(CHOSEN_HOURS_FOUR);
-        endEventTimePicker.has().minutes(CHOSEN_MINUTES_FOUR);
-    }
-
-    @Test
+    @Test (description = "Test checks if timepicker is readonly or not : readonly")
     public void readOnlyTimePickerTest() {
-        readOnlyTimePicker.selectHours(CHOSEN_HOURS);
-        readOnlyTimePicker.has().nonActiveHoursOrMinutes(CHOSEN_HOURS);
-        readOnlyTimePicker.selectMinutes(CHOSEN_MINUTES);
-        readOnlyTimePicker.has().nonActiveHoursOrMinutes(CHOSEN_MINUTES);
+        readOnlyTimePicker.show();
+        readOnlyTimePicker.is().readonly();
+        scrollableTimePicker.show();
+        scrollableTimePicker.is().notReadonly();
     }
 
-    @Test
-    public void useSecondsTimePickerTest() {
-        Timer.waitCondition(() -> useSecondsTimePicker.isVisible());
-        useSecondsTimePicker.selectTime(CHOSEN_TIME);
-        useSecondsTimePicker.has().hours(CHOSEN_HOURS);
-        useSecondsTimePicker.has().minutes(CHOSEN_MINUTES);
-        useSecondsTimePicker.has().seconds(CHOSEN_SECONDS);
-        useSecondsTimePicker.has().time(CHOSEN_TIME);
+    @Test (description = "Test checks theme of timepicker : theme (dark/light)")
+    public void themeTimePickerTest() {
+        scrollableTimePicker.show();
+        scrollableTimePicker.has().darkTheme();
+        readOnlyTimePicker.show();
+        readOnlyTimePicker.has().lightTheme();
     }
 
-    @Test
+    @Test (description = "Test checks that timepicker is landscape or not : title position")
+    public void landscapeTimePickerTest() {
+        useSecondsTimePicker1.show();
+        useSecondsTimePicker1.is().notLandscape();
+        useSecondsTimePicker2.show();
+        useSecondsTimePicker2.is().landscape();
+    }
+
+    @Test (description = "Test checks timepicker's measurements")
     public void widthTimePickerTest() {
-        Timer.waitCondition(() -> firstWidthTimePicker.isVisible());
-        firstWidthTimePicker.has().pickerWidth(TIME_PICKER_WIDTH);
+        firstWidthTimePicker.show();
+        firstWidthTimePicker.has().width(TIME_PICKER_WIDTH);
+        firstWidthTimePicker.has().height(TIME_PICKER_HEIGHT);
     }
 
-    @Test
-    public void menuTimePickerTest() {
-        Timer.waitCondition(() -> pickerInMenuTimePicker.isVisible());
-        pickerInMenuTimePicker.expand();
-        pickerInMenuTimePicker.switchToAM();
-        pickerInMenuTimePicker.selectHours(CHOSEN_HOURS);
-        pickerInMenuTimePicker.has().hours(CHOSEN_HOURS);
-        pickerInMenuTimePicker.selectMinutes(CHOSEN_MINUTES);
-        pickerInDialogTimePicker.has().resultTime(CHOSEN_TIME_TWO_DIGITS);
-    }
-
-    @Test
+    @Test (description = "Test checks time changes in dialog timepicker")
     public void dialogTimePickerTest() {
-        Timer.waitCondition(() -> pickerInDialogTimePicker.isVisible());
+        pickerInDialogTimePicker.show();
         pickerInDialogTimePicker.expand();
         pickerInDialogTimePicker.clickCancel();
         pickerInDialogTimePicker.expand();
