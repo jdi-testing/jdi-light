70bda92b4b5f7f22dcdaeb60607667a3498f6706
Liliia Iskanderova <61730255+Liliya-web@users.noreply.github.com>
2022-12-08 20:20:13 +0300

    4138-refactor-toolbars (#4530)
    Co-authored-by: Liliia Iskanderova <liliia.iskanderova@ukg.com>

--- jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/elements/complex/bars/ToolBar.java
+++ jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/elements/complex/bars/ToolBar.java
@@ -3,25 +3,27 @@ package com.epam.jdi.light.vuetify.elements.complex.bars;
 import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.vuetify.asserts.bars.ToolBarAssert;
 import com.epam.jdi.light.vuetify.elements.common.VuetifyButton;
-import com.epam.jdi.light.vuetify.interfaces.IsFlat;
 
 /**
  * To see examples of Tool Bar web elements please visit https://vuetifyjs.com/en/components/toolbars
  */
 
-public class ToolBar extends BasicBar<ToolBar, ToolBarAssert> implements IsFlat {
-
-    @JDIAction("Get '{name}'s 'close' button")
+public class ToolBar extends BasicBar<ToolBar, ToolBarAssert> {
+    public ToolBarAssert is() {
+        return new ToolBarAssert().set(this);
+    }
+    @JDIAction("Get '{name}' 'close' button")
     public VuetifyButton closeButton() {
         return findIconButton("mdi-close");
     }
 
-    @JDIAction("Get '{name}'s 'delete' button")
+    @JDIAction("Get '{name}' 'delete' button")
     public VuetifyButton deleteButton() {
         return findIconButton("mdi-delete");
     }
 
-    public ToolBarAssert is() {
-        return new ToolBarAssert().set(this);
+    @JDIAction("Check that '{name}' has background image")
+    public boolean hasBackgroundImage() {
+        return core().find("div.v-toolbar__image .v-image").isExist();
     }
 }
