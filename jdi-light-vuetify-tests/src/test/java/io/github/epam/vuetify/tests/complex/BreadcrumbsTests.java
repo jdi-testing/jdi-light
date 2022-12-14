5051199c7748f7995fc85a6bcb9158ebff2b5416
Liliia Iskanderova <61730255+Liliya-web@users.noreply.github.com>
2022-12-14 10:26:56 +0300

    4378 refactor breadcrumbs (#4549)
    * 4378 refactor breadcrumbs
    
    * 4378 refactor breadcrumbs fix failing test
    
    Co-authored-by: Liliia Iskanderova <liliia.iskanderova@ukg.com>

--- jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/complex/BreadcrumbsTests.java
+++ jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/complex/BreadcrumbsTests.java
@@ -1,19 +1,14 @@
 package io.github.epam.vuetify.tests.complex;
 
-import com.epam.jdi.light.common.JDIAction;
-import com.epam.jdi.light.elements.common.UIElement;
-import com.epam.jdi.light.vuetify.elements.complex.Breadcrumbs;
 import io.github.epam.TestsInit;
-import org.hamcrest.Matchers;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.function.Predicate;
-
-import static com.epam.jdi.light.asserts.core.SoftAssert.jdiAssert;
 import static com.jdiai.tools.Timer.waitCondition;
 import static io.github.com.StaticSite.breadcrumbsPage;
-import static io.github.com.pages.BreadcrumbsPage.backwardSlashedBreadcrumbs;
+import static io.github.com.enums.MdiIcons.CHEVRON_RIGHT;
+import static io.github.com.enums.MdiIcons.FORWARD;
+import static io.github.com.pages.BreadcrumbsPage.darkThemeRippleBreadcrumbs;
 import static io.github.com.pages.BreadcrumbsPage.dashedBreadcrumbs;
 import static io.github.com.pages.BreadcrumbsPage.dottedBreadcrumbs;
 import static io.github.com.pages.BreadcrumbsPage.forwardSlashedBreadcrumbs;
@@ -25,7 +20,6 @@ import static io.github.com.pages.BreadcrumbsPage.mdiForwardBreadcrumbs;
 import static io.github.com.pages.BreadcrumbsPage.semicolonBreadcrumbs;
 
 public class BreadcrumbsTests extends TestsInit {
-
     @BeforeClass
     public void before() {
         breadcrumbsPage.open();
@@ -33,57 +27,78 @@ public class BreadcrumbsTests extends TestsInit {
         breadcrumbsPage.checkOpened();
     }
 
-    @Test
-    public void dividersTypeTest() {
+    @Test(description = "Test checks that breadcrumbs are displayed")
+    public void displayedBreadcrumbsTest() {
+        dashedBreadcrumbs.show();
         dashedBreadcrumbs.is().displayed();
+    }
+
+    @Test(description = "Test checks breadcrumbs, items and dividers size")
+    public void itemsSizeBreadcrumbsTest() {
+        dashedBreadcrumbs.show();
         dashedBreadcrumbs.has().size(3);
-        dashedBreadcrumbs.items().has().size(3);
+        dashedBreadcrumbs.list().has().size(3);
         dashedBreadcrumbs.dividers().has().size(2);
-
-        assertDivider(dashedBreadcrumbs, correctSymbol("-"));
-        assertDivider(forwardSlashedBreadcrumbs, correctSymbol("/"));
-        assertDivider(backwardSlashedBreadcrumbs, correctSymbol("\\"));
-        assertDivider(dottedBreadcrumbs, correctSymbol("."));
-        assertDivider(semicolonBreadcrumbs, correctSymbol(";"));
-        assertDivider(greaterSignBreadcrumbs, correctSymbol(">"));
     }
 
-    @Test
+    @Test(description = "Test checks dividers types")
+    public void dividersTypeTest() {
+        dashedBreadcrumbs.show();
+        dashedBreadcrumbs.has().dividerType("-");
+        forwardSlashedBreadcrumbs.show();
+        forwardSlashedBreadcrumbs.has().dividerType("/");
+        dottedBreadcrumbs.show();
+        dottedBreadcrumbs.has().dividerType(".");
+        semicolonBreadcrumbs.show();
+        semicolonBreadcrumbs.has().dividerType(";");
+        greaterSignBreadcrumbs.show();
+        greaterSignBreadcrumbs.has().dividerType(">");
+    }
+
+    @Test(description = "Test checks if breadcrumbs are large or not")
     public void largeBreadcrumbsTest() {
-        largeBreadcrumbs.is().displayed();
-        largeBreadcrumbs.has().cssClass("v-breadcrumbs--large");
-        largeBreadcrumbs.has().css("font-size", "16px");
+        largeBreadcrumbs.show();
+        largeBreadcrumbs.is().large();
+        mdiForwardBreadcrumbs.show();
+        mdiForwardBreadcrumbs.is().notLarge();
     }
 
-    @Test
+    @Test(description = "Test checks breadcrumbs icon dividers")
     public void iconBreadcrumbsTest() {
-        assertDivider(mdiForwardBreadcrumbs, correctIcon("mdi-forward"));
-        assertDivider(mdiChevronRightBreadcrumbs, correctIcon("mdi-chevron-right"));
+        mdiForwardBreadcrumbs.show();
+        mdiForwardBreadcrumbs.has().iconDivider(FORWARD.mdi());
+        mdiChevronRightBreadcrumbs.show();
+        mdiChevronRightBreadcrumbs.has().iconDivider(CHEVRON_RIGHT.mdi());
     }
 
-    @Test
-    public void itemSlotBreadcrumbsTest() {
-        itemSlotsBreadcrumbs.is().displayed();
-        itemSlotsBreadcrumbs.has().size(3);
-        itemSlotsBreadcrumbs.dividers().has().size(2);
-        itemSlotsBreadcrumbs.items().has().values("DASHBOARD", "LINK 1", "LINK 2");
-        jdiAssert(itemSlotsBreadcrumbs.selected("DASHBOARD"), Matchers.is(false));
-        jdiAssert(itemSlotsBreadcrumbs.selected("LINK 2"), Matchers.is(true));
+    @Test(description = "Test checks breadcrumbs item text")
+    public void itemTextBreadcrumbsTest() {
+        itemSlotsBreadcrumbs.show();
+        itemSlotsBreadcrumbs.items().get(0).has().text("DASHBOARD");
+        itemSlotsBreadcrumbs.items().get(1).has().text("LINK 1");
+        itemSlotsBreadcrumbs.items().get(2).has().text("LINK 2");
+        itemSlotsBreadcrumbs.list().has().values("DASHBOARD", "LINK 1", "LINK 2");
     }
 
-    protected Predicate<UIElement> correctSymbol(String symbol) {
-        // checks that inner text is equal to the symbol
-        return e -> e.text().equals(symbol);
+    @Test(description = "Test checks if breadcrumbs values, checks if value is selected or not")
+    public void selectionBreadcrumbsTest() {
+        itemSlotsBreadcrumbs.show();
+        itemSlotsBreadcrumbs.has().selected("LINK 2");
+        itemSlotsBreadcrumbs.has().notSelected("DASHBOARD");
     }
 
-    protected Predicate<UIElement> correctIcon(String iconClass) {
-        // finds icon tag and checks that the element has the following iconClass
-        return e -> e.find("i").hasClass(iconClass);
+    @Test(description = "Test checks breadcrumbs theme")
+    public void themeBreadcrumbsTest() {
+        itemSlotsBreadcrumbs.show();
+        itemSlotsBreadcrumbs.has().lightTheme();
+        darkThemeRippleBreadcrumbs.show();
+        darkThemeRippleBreadcrumbs.has().darkTheme();
     }
 
-    @JDIAction("Assert that '{0}' dividers satisfy a predicate '{1}'")
-    private void assertDivider(Breadcrumbs breadcrumbs, Predicate<? super UIElement> predicate) {
-        // checks if all dividers of the breadcrumbs are satisfied the following predicate
-        jdiAssert(breadcrumbs.dividers().all(predicate::test), Matchers.is(true));
+    @Test(description = "Test checks if breadcrumbs item is disabled or not")
+    public void disabledBreadcrumbsTest() {
+        itemSlotsBreadcrumbs.show();
+        itemSlotsBreadcrumbs.items().get(2).is().disabled();
+        itemSlotsBreadcrumbs.items().get(0).is().enabled();
     }
 }
