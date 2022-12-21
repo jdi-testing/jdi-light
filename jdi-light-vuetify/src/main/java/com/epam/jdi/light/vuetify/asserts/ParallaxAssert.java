a19f974dbf487919b1e5e36c793121cd68193740
Liliia Iskanderova <61730255+Liliya-web@users.noreply.github.com>
2022-12-21 09:36:53 +0300

    4372 refactor parallax (#4561)
    * 4372 refactor parallax
    
    * 4372 refactor parallax fix issues
    
    Co-authored-by: Liliia Iskanderova <liliia.iskanderova@ukg.com>

--- jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/asserts/ParallaxAssert.java
+++ jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/asserts/ParallaxAssert.java
@@ -9,22 +9,23 @@ import static com.epam.jdi.light.asserts.core.SoftAssert.jdiAssert;
 
 public class ParallaxAssert extends UIAssert<ParallaxAssert, Parallax> {
 
-    @JDIAction("Assert that '{name}' parallax container height is {0} pixels")
-    public ParallaxAssert height(int expectedHeight) {
-        int elemHeight = Integer.parseInt(element().height().replace("px", ""));
-        jdiAssert(elemHeight, Matchers.equalTo(expectedHeight));
+    @JDIAction("Assert that '{name}' container height is '{0} px'")
+    public ParallaxAssert heightPx(int height) {
+        int actualHeight = element().heightPx();
+        jdiAssert(actualHeight, Matchers.equalTo(height), String.format("Actual element's height '%s px' is not " +
+                "equal to expected '%s px'", actualHeight, height));
         return this;
     }
 
-    @JDIAction("Assert that there are elements in '{name}' content section")
+    @JDIAction("Assert that '{name}' has elements in content section")
     public ParallaxAssert content() {
-        jdiAssert(element().hasContent() ? "has content" : "has no content", Matchers.is("has content"));
+        jdiAssert(element().hasContent(), Matchers.is(true), "Element has no content");
         return this;
     }
 
-    @JDIAction("Assert that there are no elements in '{name}' content section")
+    @JDIAction("Assert that '{name}' has no elements in content section")
     public ParallaxAssert noContent() {
-        jdiAssert(element().hasContent() ? "has content" : "has no content", Matchers.is("has no content"));
+        jdiAssert(element().hasContent(), Matchers.is(false), "Element has content");
         return this;
     }
 }
