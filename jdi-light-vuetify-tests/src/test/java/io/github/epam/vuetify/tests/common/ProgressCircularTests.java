d63c23e2b61fafbbf241dbdef42bda9ed09cae81
pavelpostnikov1 <84065866+pavelpostnikov1@users.noreply.github.com>
2022-12-23 10:03:54 +0100

    Progress Circular: add methods, asserts and checks (#4556)
    * add methods, asserts and checks
    
    * add text checks

--- jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/common/ProgressCircularTests.java
+++ jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/common/ProgressCircularTests.java
@@ -23,40 +23,48 @@ public class ProgressCircularTests extends TestsInit {
 
     @Test(dataProvider = "colorProgressCircularsTestsDataProvider",
             dataProviderClass = ProgressCircularDataProvider.class)
-    public void colorProgressCircularsTests(int index, String color, String height, String width) {
-        colorProgressCirculars.get(index).is().displayed();
-        colorProgressCirculars.get(index).is().notSpinning();
-        colorProgressCirculars.get(index).has().color(color);
-        colorProgressCirculars.get(index).has().height(height);
-        colorProgressCirculars.get(index).has().width(width);
+    public void colorProgressCircularsTests(int index, String color, String height, String width, int value) {
+        colorProgressCirculars.get(index).is()
+                .displayed()
+                .notSpinning()
+                .color(color)
+                .height(height)
+                .width(width)
+                .value(value)
+                .text(String.valueOf(index));
     }
 
     @Test(dataProvider = "indeterminateProgressCircularsTestsDataProvider",
             dataProviderClass = ProgressCircularDataProvider.class)
     public void indeterminateProgressCircularsTests(int index, String color, String height, String width) {
-        indeterminateProgressCirculars.get(index).is().displayed();
-        indeterminateProgressCirculars.get(index).is().spinning();
-        indeterminateProgressCirculars.get(index).has().color(color);
-        indeterminateProgressCirculars.get(index).has().height(height);
-        indeterminateProgressCirculars.get(index).has().width(width);
+        indeterminateProgressCirculars.get(index).is()
+                .displayed()
+                .spinning()
+                .color(color)
+                .height(height)
+                .width(width);
     }
 
     @Test(dataProvider = "rotateProgressCircularsTestsDataProvider",
             dataProviderClass = ProgressCircularDataProvider.class)
-    public void rotateProgressCircularsTests(int index, String color, String height, String width) {
-        rotateProgressCirculars.get(index).is().displayed();
-        rotateProgressCirculars.get(index).has().color(color);
-        rotateProgressCirculars.get(index).has().height(height);
-        rotateProgressCirculars.get(index).has().width(width);
+    public void rotateProgressCircularsTests(int index, String color, String height, String width, int rotate) {
+        rotateProgressCirculars.get(index).is()
+                .displayed()
+                .color(color)
+                .height(height)
+                .width(width)
+                .rotate(rotate);
     }
 
     @Test(dataProvider = "sizeWidthProgressCircularsTestsDataProvider",
             dataProviderClass = ProgressCircularDataProvider.class)
-    public void sizeWidthProgressCircularsTests(int index, String color, String height, String width) {
-        sizeWidthProgressCirculars.get(index).is().displayed();
-        sizeWidthProgressCirculars.get(index).is().spinning();
-        sizeWidthProgressCirculars.get(index).has().color(color);
-        sizeWidthProgressCirculars.get(index).has().height(height);
-        sizeWidthProgressCirculars.get(index).has().width(width);
+    public void sizeWidthProgressCircularsTests(int index, String color, String height, String width, int thickness) {
+        sizeWidthProgressCirculars.get(index).is()
+                .displayed()
+                .spinning()
+                .color(color)
+                .height(height)
+                .width(width)
+                .thickness(thickness);
     }
 }
