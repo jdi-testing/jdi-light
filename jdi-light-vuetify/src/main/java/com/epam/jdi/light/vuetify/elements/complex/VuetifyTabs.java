2956ebd3b6482d85838a0950178cfe9882045fcc
Liliia Iskanderova <61730255+Liliya-web@users.noreply.github.com>
2022-12-23 21:48:03 +0300

    4223 refactor tabs (#4570)
    * 4223 refactor tabs
    
    * 4223 refactor tabs fix failing test
    
    * 4223 refactor tabs disable fix failing test
    
    Co-authored-by: Liliia Iskanderova <liliia.iskanderova@ukg.com>

--- jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/elements/complex/VuetifyTabs.java
+++ jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/elements/complex/VuetifyTabs.java
@@ -1,65 +1,146 @@
 package com.epam.jdi.light.vuetify.elements.complex;
 
-import static com.epam.jdi.light.elements.init.UIFactory.$$;
-
+import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.light.elements.complex.WebList;
 import com.epam.jdi.light.ui.html.elements.complex.Tabs;
 import com.epam.jdi.light.vuetify.asserts.VuetifyTabsAssert;
+import com.epam.jdi.light.vuetify.elements.common.Icon;
+import com.epam.jdi.light.vuetify.elements.common.VuetifyButton;
+import com.epam.jdi.light.vuetify.interfaces.HasColor;
+import com.epam.jdi.light.vuetify.interfaces.HasTheme;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.epam.jdi.light.elements.init.UIFactory.$$;
 
 /**
  * To see an example of Tabs web element please visit https://vuetifyjs.com/en/components/tabs/
  */
 
-public class VuetifyTabs extends Tabs {
-
+public class VuetifyTabs extends Tabs implements HasColor, HasTheme {
+    private UIElement tabList() {
+        return find("[role='tablist']");
+    }
     private UIElement tabs() {
         return find(".v-tabs");
     }
+    private UIElement tabContent() {
+        return find(".v-card__text");
+    }
+    private UIElement tabsSlider() {
+        return tabs().find(".v-tabs-slider");
+    }
 
     @Override
+    @JDIAction("Get '{name}' list of tabs")
     public WebList list() {
         return finds("//*[@role='tab']");
     }
 
+    @JDIAction("Get '{name}' list of menu items")
     public WebList menu() {
         return $$("[role='menu'] [role='menuitem']");
     }
 
-    public WebList icons() {
-        return finds(".v-icon");
+    @JDIAction("Get '{name}' list of icons")
+    public List<Icon> icons() {
+        return finds(".v-icon").stream()
+                .map((e) -> new Icon().setCore(Icon.class, e))
+                .collect(Collectors.toList());
     }
 
-    public UIElement next() {
-        return find(".v-slide-group__next");
+    @JDIAction("Get '{name}' next button")
+    public SlideGroup navigation() {
+        return new SlideGroup().setCore(SlideGroup.class, tabs().find(".v-slide-group"));
     }
 
-    public UIElement prev() {
-        return find(".v-slide-group__prev");
+    @JDIAction("Get '{name}' menu buttons")
+    public List<VuetifyButton> menuButton() {
+        return finds(".mdi-menu-down").stream()
+                .map((e) -> new VuetifyButton().setCore(VuetifyButton.class, e))
+                .collect(Collectors.toList());
     }
 
-    public UIElement menuButton() {
-        return find(".mdi-menu-down");
-    }
-
+    @JDIAction("Check that '{name}' is selected")
     public boolean isSelected(int index) {
         return list().get(index).attr("aria-selected").equals("true");
     }
 
+    @JDIAction("Check that '{name}' is fixed")
     public boolean isFixed() {
         return attr("class").contains("fixed-tabs");
     }
 
+    @JDIAction("Check that '{name}' is grow")
     public boolean isGrow() {
         return tabs().attr("class").contains("grow");
     }
 
+    @JDIAction("Check that '{name}' is right")
     public boolean isRight() {
         return tabs().attr("class").contains("right");
     }
 
+    @JDIAction("Check that '{name}' is vertical")
     public boolean isVertical() {
         return tabs().attr("class").contains("vertical");
+    }
+
+    @JDIAction("Check that '{name}' is align with title")
+    public boolean isAlignWithTitle() {
+        return tabs().hasClass("v-tabs--align-with-title");
+    }
+
+    @JDIAction("Get '{name}' tab text content")
+    public String getTabTextContent() {
+        return tabContent().getText();
+    }
+
+    @JDIAction("Get '{name}' color")
+    public String color() {
+        return tabList().css("color");
+    }
+
+    @JDIAction("Get '{name}' background color")
+    public String backgroundColor() {
+        return tabList().css("background-color");
+    }
+
+    @JDIAction("Get '{name}' theme")
+    public String theme() {
+        return tabs().classLike("theme--");
+    }
+
+    @JDIAction("Get '{name}' theme")
+    public int tabsSliderSize() {
+        return Integer.parseInt(tabsSlider().css("tab-size"));
+    }
+
+    @JDIAction("Get '{name}' theme")
+    public String tabsSliderColor() {
+        return tabsSlider().css("color");
+    }
+
+    @JDIAction("Check that '{name}' tab '{0}' is disabled")
+    public boolean isTabDisabled(int index) {
+        return get(index).hasClass("v-tab--disabled");
+    }
+
+    @JDIAction("Check that '{name}' tab '{0}' has href")
+    public boolean tabHasHref(int index) {
+        return !getTabHref(index).isEmpty();
+    }
+
+    @JDIAction("Get '{name}' tab '{0}' href")
+    public String getTabHref(int index) {
+        return get(index).getAttribute("href");
+    }
+
+    @JDIAction("Check that '{name}' shows arrows on hover")
+    public boolean showsArrows() {
+        return tabList().hasClass("v-tabs-bar--show-arrows");
     }
 
     @Override
@@ -71,5 +152,4 @@ public class VuetifyTabs extends Tabs {
     public VuetifyTabsAssert has() {
         return is();
     }
-
 }
