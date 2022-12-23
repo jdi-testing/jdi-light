cc3e5f0eec6eab4743efd54bfa4f774f70e92710
Maiia_Afanaseva <Maiia_Afanaseva@epam.com>
2022-12-23 16:48:05 +0300

    Task4298_slideGroupsTestsRefFix

--- jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/complex/SlideGroupsTests.java
+++ jdi-light-vuetify-tests/src/test/java/io/github/epam/vuetify/tests/complex/SlideGroupsTests.java
@@ -1,11 +1,14 @@
 package io.github.epam.vuetify.tests.complex;
 
 import io.github.epam.TestsInit;
+import io.github.epam.vuetify.tests.data.SlideGroupTestsData;
+import java.util.List;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static com.jdiai.tools.Timer.waitCondition;
 import static io.github.com.StaticSite.slideGroupsPage;
+import static io.github.com.pages.SlideGroupsPage.mandatorySlideGroup;
 import static io.github.com.pages.SlideGroupsPage.multipleSlideGroup;
 import static io.github.com.pages.SlideGroupsPage.pseudoCarouselSlideGroup;
 import static io.github.com.pages.SlideGroupsPage.centerActiveSlideGroup;
@@ -28,8 +31,10 @@ public class SlideGroupsTests extends TestsInit {
         waitCondition(() -> slideGroupsPage.isOpened());
     }
 
-    @Test
+    @Test(description="Test checks 'single' selection of slide ")
     public void activeClassSlideGroupTests() {
+        // to refactor after changes on test-site. It should not be mandatory
+        //to check active-class
         activeClassSlideGroup.show();
         activeClassSlideGroup.startTest();
         activeClassSlideGroup.is().displayed();
@@ -42,28 +47,37 @@ public class SlideGroupsTests extends TestsInit {
         activeClassSlideGroup.has().slidesNotSelectedExcept("#ActiveClassSlideGroup", 2);
     }
 
-    @Test
+    @Test(description="Test checks slide group feature 'center-active'")
     public void centerActiveSlideGroupTests() {
+        //center-active is reflected in attribute 'style' via 'transform:translateX(*px)'
+
+        List<Integer> slidesPositions = new SlideGroupTestsData().centerActiveSlideGroupTestData();
         centerActiveSlideGroup.show();
+        centerActiveSlideGroup.hasAttribute("style");
         centerActiveSlideGroup.startTest();
         centerActiveSlideGroup.is().displayed();
-        centerActiveSlideGroup.slideByIndex(2).click();
-        centerActiveSlideGroup.has().centerActiveSlide();
 
-        centerActiveSlideGroup.slideByIndex(5).click();
-        centerActiveSlideGroup.has().centerActiveSlide();
-
-        centerActiveSlideGroup.getNextButton().click();
-        centerActiveSlideGroup.slideByIndex(5).click();
-        centerActiveSlideGroup.has().centerActiveSlide();
-
-        centerActiveSlideGroup.getNextButton().click();
-        centerActiveSlideGroup.slideByIndex(5).click();
-        centerActiveSlideGroup.has().centerActiveSlide();
+        for (int i = 1; i <= 3; i++) {
+            centerActiveSlideGroup.slideByIndex(i).click();
+            centerActiveSlideGroup.has().slideSelected(i);
+        }
+        for (Integer slidesPosition : slidesPositions) {
+            centerActiveSlideGroup.slideByIndex(4).click();
+            centerActiveSlideGroup.has().visibleSlidesPosition(slidesPosition);
+            centerActiveSlideGroup.has().slideSelected(3);
+        }
+        for (int i = 4; i <= 6; i++) {
+            centerActiveSlideGroup.slideByIndex(i).click();
+            centerActiveSlideGroup.has().slideSelected(i);
+            centerActiveSlideGroup.has().visibleSlidesPosition(-1316);
+        }
     }
 
-    @Test
+    @Test(description="Test checks slide group feature: 'icon' and theme = 'light'")
     public void customIconsSlideGroupTests() {
+        //On our test-site we have the following custom icons:
+        // prev-icon="mdi-minus"
+        // next-icon="mdi-plus"
         customIconsSlideGroup.show();
         customIconsSlideGroup.startTest();
         customIconsSlideGroup.is().displayed();
@@ -73,29 +87,55 @@ public class SlideGroupsTests extends TestsInit {
         customIconsSlideGroup.slideByIndex(1).click();
         customIconsSlideGroup.has().slideSelected(1);
         customIconsSlideGroup.has().slidesNotSelectedExcept("#CustomIconsSlideGroup", 1);
+
+        customIconsSlideGroup.is().lightTheme();
     }
 
-    @Test
+    @Test(description="Test checks slide group feature: 'multiple' and max selections")
     public void multipleSlideGroupTests() {
+        //On our test-site we have the following max=3 selections
         multipleSlideGroup.show();
         multipleSlideGroup.startTest();
         multipleSlideGroup.is().displayed();
+
+        //Check that on selecting 2 slides we have 2 active slides
         multipleSlideGroup.slideByIndex(1).click();
         multipleSlideGroup.slideByIndex(3).click();
         multipleSlideGroup.has().slidesNotSelectedExcept("#MultipleSlideGroup", multipleSelectedIndexes);
+        multipleSlideGroup.slideByIndex(1).click();
+        multipleSlideGroup.slideByIndex(3).click();
+
+        //Check that on selecting 4 slides we have only 3 selected slides as it is our max
+        for (int i = 1; i <= 4; i++) {
+            multipleSlideGroup.slideByIndex(i).click();
+        }
+        int selectedSlides = multipleSlideGroup.allSelectedIndexes("#MultipleSlideGroup").size();
+        assertEquals(selectedSlides, 3);
     }
 
-    @Test
+    @Test(description="Test checks slide group version: 'pseudocarousel' and max selections")
     public void pseudoCarouselSlideGroupTests() {
         pseudoCarouselSlideGroup.show();
         pseudoCarouselSlideGroup.startTest();
         pseudoCarouselSlideGroup.is().displayed();
         pseudoCarouselSlideGroup.slideByIndex(1).click();
         pseudoCarouselCount.is().displayed();
-        assertEquals(pseudoCarouselCount.getText(), "Selected " + pseudoCarouselSlideGroup.selectedIndex("#PseudoCarouselSlideGroup"));
+        assertEquals(pseudoCarouselCount.getText(),
+            "Selected " + pseudoCarouselSlideGroup.selectedIndex("#PseudoCarouselSlideGroup"));
         pseudoCarouselSlideGroup.slideByIndex(3).click();
-        assertEquals(pseudoCarouselCount.getText(), "Selected " + pseudoCarouselSlideGroup.selectedIndex("#PseudoCarouselSlideGroup"));
+        assertEquals(pseudoCarouselCount.getText(),
+            "Selected " + pseudoCarouselSlideGroup.selectedIndex("#PseudoCarouselSlideGroup"));
         pseudoCarouselSlideGroup.slideByIndex(5).click();
-        assertEquals(pseudoCarouselCount.getText(), "Selected " + pseudoCarouselSlideGroup.selectedIndex("#PseudoCarouselSlideGroup"));
+        assertEquals(pseudoCarouselCount.getText(),
+            "Selected " + pseudoCarouselSlideGroup.selectedIndex("#PseudoCarouselSlideGroup"));
+    }
+
+    @Test(description="Test checks slide group feature: 'mandatory' and theme dark")
+    public void mandatorySlideGroupTests() {
+        mandatorySlideGroup.show();
+        mandatorySlideGroup.startTest();
+        mandatorySlideGroup.is().displayed();
+
+        mandatorySlideGroup.is().darkTheme();
     }
 }
