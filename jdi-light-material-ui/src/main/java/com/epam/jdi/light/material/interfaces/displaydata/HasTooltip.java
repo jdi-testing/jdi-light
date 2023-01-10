085a4592222d88433fbb3cd242e65be941e1176c
Natalia Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2023-01-10 17:45:23 -0500

    Fix Tooltip

--- jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/interfaces/displaydata/HasTooltip.java
+++ jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/interfaces/displaydata/HasTooltip.java
@@ -3,9 +3,7 @@ package com.epam.jdi.light.material.interfaces.displaydata;
 import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.elements.interfaces.base.ICoreElement;
 import com.epam.jdi.light.material.elements.displaydata.Tooltip;
-import org.openqa.selenium.By;
 
-import static com.epam.jdi.light.driver.WebDriverFactory.getDriver;
 import static com.epam.jdi.light.elements.init.JDITalk.NAME_TO_ELEMENT;
 
 /**
