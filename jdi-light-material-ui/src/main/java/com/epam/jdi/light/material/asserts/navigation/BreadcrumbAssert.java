876213ba105eb7fe5a01184dbb0e5ee8d1e091b7
Natalia Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2023-01-12 23:28:51 -0500

    Remove Portal

--- jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/asserts/navigation/BreadcrumbAssert.java
+++ jdi-light-material-ui/src/main/java/com/epam/jdi/light/material/asserts/navigation/BreadcrumbAssert.java
@@ -1,11 +1,7 @@
 package com.epam.jdi.light.material.asserts.navigation;
 
 import com.epam.jdi.light.asserts.generic.UISelectAssert;
-import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.material.elements.navigation.Breadcrumbs;
-import org.hamcrest.Matcher;
-
-import static com.epam.jdi.light.asserts.core.SoftAssert.jdiAssert;
 
 /**
  * Assertions for {@link Breadcrumbs}.
