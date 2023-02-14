912bb5c390d96ee501fda613d25c8266748f6d54
antonfilichkin <41286257+antonfilichkin@users.noreply.github.com>
2023-02-14 11:15:38 +0200

    4735 refactor tests bottom navigation leftovers (#4772)
    * isScrollable interface
    Card test scrollable fixed
    
    * Naming fixes
    
    * Test dependencies fixed
    
    * CR fixes
    
    * Codacy fix

---
+++ jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/interfaces/IsScrollable.java
@@ -0,0 +1,22 @@
+package com.epam.jdi.light.vuetify.interfaces;
+
+import com.epam.jdi.light.common.JDIAction;
+import com.epam.jdi.light.elements.interfaces.base.ICoreElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.WheelInput.ScrollOrigin;
+
+public interface IsScrollable extends ICoreElement {
+
+    /**
+     * Mouse over the element and emulate mouse wheel scroll
+     *
+     * @param px number of pixels to scroll. Negative value scrolls up.
+     */
+    @JDIAction("Scroll over '{name}' for '{0}' px")
+    default void scroll(int px) {
+        ScrollOrigin scrollOrigin = ScrollOrigin.fromElement(core().get());
+        Actions actions = new Actions(core().driver());
+        actions.scrollFromOrigin(scrollOrigin, 0, px);
+        actions.perform();
+    }
+}
