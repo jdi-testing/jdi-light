b2ce4b465bc2850e1383cda4420a13f073b8e745
Natalia Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2023-03-28 16:16:54 -0500

    Remove code duplicate

--- jdi-light-selenium3/src/main/java/com/epam/jdi/light/common/CheckTypes.java
+++ jdi-light-selenium3/src/main/java/com/epam/jdi/light/common/CheckTypes.java
@@ -8,24 +8,21 @@ public enum CheckTypes {
     DEFAULT, NONE, EQUALS, MATCH, CONTAINS;
 
     public static CheckTypes parseUrlCheck(String value) {
-        String check = value.trim().toLowerCase().replaceAll("[^a-z]", "");
-        switch (check) {
-            case "none": return NONE;
-            case "equals": return EQUALS;
-            case "match": return MATCH;
-            case "contains": return CONTAINS;
-            default: return CONTAINS;
-        }
+        return getCheckTypes(value, CONTAINS);
     }
 
     public static CheckTypes parseTitleCheck(String value) {
+        return getCheckTypes(value, NONE);
+    }
+
+    private static CheckTypes getCheckTypes(String value, CheckTypes defaultValue) {
         String check = value.trim().toLowerCase().replaceAll("[^a-z]", "");
         switch (check) {
             case "none": return NONE;
             case "equals": return EQUALS;
             case "match": return MATCH;
             case "contains": return CONTAINS;
-            default: return NONE;
+            default: return defaultValue;
         }
     }
 }
