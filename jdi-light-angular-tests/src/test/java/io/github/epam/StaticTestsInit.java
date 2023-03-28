8b8042036b25d251fcecfe48c78f29b47e652e06
Natalia Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2023-03-28 18:35:02 -0500

    Fix style error - imports

--- jdi-light-angular-tests/src/test/java/io/github/epam/StaticTestsInit.java
+++ jdi-light-angular-tests/src/test/java/io/github/epam/StaticTestsInit.java
@@ -3,7 +3,6 @@ package io.github.epam;
 import io.github.com.StaticSite;
 import org.testng.annotations.BeforeSuite;
 
-import static com.epam.jdi.light.driver.WebDriverUtils.killAllSeleniumDrivers;
 import static com.epam.jdi.light.elements.init.PageFactory.initSite;
 import static com.epam.jdi.light.settings.WebSettings.logger;
 
--- jdi-light-mobile/src/main/java/com/epam/jdi/light/mobile/elements/common/MobileFileManager.java
+++ jdi-light-mobile/src/main/java/com/epam/jdi/light/mobile/elements/common/MobileFileManager.java
@@ -1,6 +1,5 @@
 package com.epam.jdi.light.mobile.elements.common;
 
-import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.ios.IOSDriver;
 import org.openqa.selenium.WebDriver;
