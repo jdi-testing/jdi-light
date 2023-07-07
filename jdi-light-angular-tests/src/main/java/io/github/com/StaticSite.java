1613d466c89e6dbbd47a799757dc6b4c3728fc57
Natalia Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2023-07-07 00:15:19 -0500

    Remove unused import

--- jdi-light-angular-tests/src/main/java/io/github/com/StaticSite.java
+++ jdi-light-angular-tests/src/main/java/io/github/com/StaticSite.java
@@ -1,13 +1,7 @@
 package io.github.com;
 
-import com.epam.jdi.light.elements.composite.Form;
 import com.epam.jdi.light.elements.pageobjects.annotations.JSite;
-import com.epam.jdi.light.elements.pageobjects.annotations.Url;
-import com.epam.jdi.light.elements.pageobjects.annotations.locators.Css;
-import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
-import com.epam.jdi.light.ui.html.elements.common.Button;
-import com.epam.jdi.light.ui.html.elements.common.Text;
-import com.jdiai.tools.DataClass;
+import com.epam.jdi.light.elements.pageobjects.annotations.Url;;
 import io.github.com.pages.AngularPage;
 import io.github.com.pages.ProgressBarPage;
 
