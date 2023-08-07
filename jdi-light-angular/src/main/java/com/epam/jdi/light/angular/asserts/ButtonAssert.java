1b49658a493b8a18686832e85826c8527a5ccab7
b14ckster <b1ackcter@gmail.com>
2023-08-07 15:36:15 +0300

    refactor button assert

--- jdi-light-angular/src/main/java/com/epam/jdi/light/angular/asserts/ButtonAssert.java
+++ jdi-light-angular/src/main/java/com/epam/jdi/light/angular/asserts/ButtonAssert.java
@@ -13,37 +13,55 @@ public class ButtonAssert extends UIAssert<ButtonAssert, Button> {
 
     @JDIAction("Assert that '{name}' has text '{0}'")
     public ButtonAssert text(String expectedText) {
-        jdiAssert(element().text(), Matchers.is(expectedText));
+        String actualText = element().text();
+        jdiAssert(actualText, Matchers.is(expectedText),
+            String.format("Actual element's text '%s'" +
+                "is not equal to expected '%s'", actualText, expectedText));
         return this;
     }
 
     @JDIAction("Assert that '{name}' is focused")
     public ButtonAssert focused() {
-        jdiAssert(element().focused(), Matchers.is(true));
+        boolean actualFocused = element().focused();
+        jdiAssert(actualFocused, Matchers.is(true),
+            String.format("Element is not focused. Actual result '%b' " +
+                "is not equal to expected '%b'", actualFocused, true));
         return this;
     }
 
     @JDIAction("Assert that label of '{name}' of type '{0}' has text '{1}'")
     public ButtonAssert buttonLabelText(ButtonsTypes buttonType, String expectedText) {
-        jdiAssert(element().buttonLabelText(buttonType), Matchers.containsString(expectedText));
+        String actualText = element().buttonLabelText(buttonType);
+        jdiAssert(actualText, Matchers.containsString(expectedText),
+            String.format("Actual element's label text '%s'" +
+                "is not equal to expected '%s'", actualText, expectedText));
         return this;
     }
 
     @JDIAction("Assert that '{name}' color is '{0}'")
     public ButtonAssert color(ButtonsColors expectedColor) {
-        jdiAssert(element().color(), Matchers.is(expectedColor.getColor()));
+        String actualColor = element().color();
+        jdiAssert(actualColor, Matchers.is(expectedColor.getColor()),
+            String.format("Actual element's color '%s'" +
+                "is not equal to expected '%s'", actualColor, expectedColor.getColor()));
         return this;
     }
 
     @JDIAction("Assert that '{name}' has default color")
     public ButtonAssert defaultColor() {
-        jdiAssert(element().defaultColor(), Matchers.is(true));
+        boolean actualDefaultColor = element().defaultColor();
+        jdiAssert(actualDefaultColor, Matchers.is(true),
+            String.format("Element doesn't have default color. Actual result '%b' " +
+                "is not equal to expected '%b'", actualDefaultColor, true));
         return this;
     }
 
     @JDIAction("Assert that '{name}' has visual type '{0}'")
     public ButtonAssert visualTypeOfButton(ButtonsTypes expectedType) {
-        jdiAssert(element().visualTypeOfButton(), Matchers.is(expectedType.getType()));
+        String actualVisualType = element().visualTypeOfButton();
+        jdiAssert(actualVisualType, Matchers.is(expectedType.getType()),
+            String.format("Actual element's visual type '%s'" +
+                "is not equal to expected '%s'", actualVisualType, expectedType.getType()));
         return this;
     }
 }
