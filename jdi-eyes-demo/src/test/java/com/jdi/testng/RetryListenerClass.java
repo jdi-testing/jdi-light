4d04054a3d880ca5acfe30fbd706cef5d3c66649
Natalia Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2023-08-17 23:09:00 -0500

    Update TestNg version

--- jdi-eyes-demo/src/test/java/com/jdi/testng/RetryListenerClass.java
+++ jdi-eyes-demo/src/test/java/com/jdi/testng/RetryListenerClass.java
@@ -1,8 +1,8 @@
 package com.jdi.testng;
 
-/**
- * Created by Roman Iovlev on 10.11.2018
- * Email: roman.iovlev.jdi@gmail.com; Skype: roman.iovlev
+/*
+  Created by Roman Iovlev on 10.11.2018
+  Email: roman.iovlev.jdi@gmail.com; Skype: roman.iovlev
  */
 
 import org.testng.IAnnotationTransformer;
