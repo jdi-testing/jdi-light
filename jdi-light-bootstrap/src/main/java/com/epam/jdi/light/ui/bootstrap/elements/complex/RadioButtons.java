f975959d5d3b308dc6250e1ecc4578819f4a19d7
Maya Afanaseva <afanasevaml@yandex.ru>
2023-10-09 15:48:02 +0300

    4911-refactoringRadioButtonPreliminary

--- jdi-light-bootstrap/src/main/java/com/epam/jdi/light/ui/bootstrap/elements/complex/RadioButtons.java
+++ jdi-light-bootstrap/src/main/java/com/epam/jdi/light/ui/bootstrap/elements/complex/RadioButtons.java
@@ -15,16 +15,16 @@ import static com.epam.jdi.light.common.TextTypes.LABEL;
 public class RadioButtons extends com.epam.jdi.light.ui.html.elements.complex.RadioButtons {
 
     @Override
-    public WebList<W> list() {
-        WebList<W> radioBtnWebList = new WebList<W>(base()).setup(b -> b.setSearchRule("Any", ANY_ELEMENT))
+    public WebList list() {
+        WebList radioBtnWebList = new WebList(base()).setup(b -> b.setSearchRule("Any", ANY_ELEMENT))
                                                            .setUIElementName(LABEL);
         // TODO REFACTOR
         //radioBtnWebList.setClickArea(ACTION_CLICK);
         return radioBtnWebList;
     }
 
-    public WebList<W> list(JFunc1<WebElement, Boolean> searchRule, ElementArea elementArea) {
-        WebList<W> radioBtnWebList = new WebList<W>(base()).setup(jdiB -> jdiB.setSearchRule("UNDEFINED", searchRule))
+    public WebList list(JFunc1<WebElement, Boolean> searchRule, ElementArea elementArea) {
+        WebList radioBtnWebList = new WebList(base()).setup(jdiB -> jdiB.setSearchRule("UNDEFINED", searchRule))
                                                            .setUIElementName(LABEL);
         // TODO REFACTOR
         //radioBtnWebList.setClickArea(elementArea);
