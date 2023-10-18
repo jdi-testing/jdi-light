b2f8717ee3968848bad815eb7541a613a55d1ba7
Maya Afanaseva <afanasevaml@yandex.ru>
2023-10-18 17:15:30 +0300

    4952-64-refactoringGridListAfterRebase

--- jdi-light-angular-tests/src/main/java/io/github/com/StaticSite.java
+++ jdi-light-angular-tests/src/main/java/io/github/com/StaticSite.java
@@ -2,12 +2,6 @@ package io.github.com;
 
 import com.epam.jdi.light.elements.pageobjects.annotations.JSite;
 import com.epam.jdi.light.elements.pageobjects.annotations.Url;
-import com.epam.jdi.light.elements.pageobjects.annotations.Url;
-import com.epam.jdi.light.elements.pageobjects.annotations.locators.Css;
-import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
-import com.epam.jdi.light.ui.html.elements.common.Button;
-import com.epam.jdi.light.ui.html.elements.common.Text;
-import com.jdiai.tools.DataClass;
 import io.github.com.pages.AngularPage;
 import io.github.com.pages.AutocompletePage;
 import io.github.com.pages.BadgePage;
@@ -16,7 +10,6 @@ import io.github.com.pages.ButtonsPage;
 import io.github.com.pages.ProgressBarPage;
 import io.github.com.pages.ProgressSpinnerPage;
 import io.github.com.pages.GridListPage;
-import io.github.com.pages.ProgressBarPage;
 
 @JSite("https://jdi-testing.github.io/jdi-light/angular-page/#/")
 public class StaticSite {
