634f62914a4054589f900543563b01b6d42cce82
Maya Afanaseva <afanasevaml@yandex.ru>
2023-10-25 13:14:24 +0300

    4911-refactoringRadioTestsBootstrapFix

--- jdi-light-bootstrap/src/main/java/com/epam/jdi/light/ui/bootstrap/elements/complex/RadioButtons.java
+++ jdi-light-bootstrap/src/main/java/com/epam/jdi/light/ui/bootstrap/elements/complex/RadioButtons.java
@@ -17,7 +17,7 @@ public class RadioButtons extends com.epam.jdi.light.ui.html.elements.complex.Ra
     @Override
     public WebList list() {
         WebList radioBtnWebList = new WebList(base()).setup(b -> b.setSearchRule("Any", ANY_ELEMENT))
-                                                     .setUIElementName(LABEL);
+                .setUIElementName(LABEL);
         // TODO REFACTOR
         //radioBtnWebList.setClickArea(ACTION_CLICK);
         return radioBtnWebList;
@@ -25,7 +25,7 @@ public class RadioButtons extends com.epam.jdi.light.ui.html.elements.complex.Ra
 
     public WebList list(JFunc1<WebElement, Boolean> searchRule, ElementArea elementArea) {
         WebList radioBtnWebList = new WebList(base()).setup(jdiB -> jdiB.setSearchRule("UNDEFINED", searchRule))
-                                                     .setUIElementName(LABEL);
+                .setUIElementName(LABEL);
         // TODO REFACTOR
         //radioBtnWebList.setClickArea(elementArea);
         return radioBtnWebList;
