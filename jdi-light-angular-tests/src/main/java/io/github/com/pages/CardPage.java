26462e0c0cf58bb055fc2c1408e60ef3309c8eb8
Maya Afanaseva <afanasevaml@yandex.ru>
2023-11-24 12:28:39 +0200

    4914_cardTestsRefRebased2

--- jdi-light-angular-tests/src/main/java/io/github/com/pages/CardPage.java
+++ jdi-light-angular-tests/src/main/java/io/github/com/pages/CardPage.java
@@ -3,7 +3,7 @@ package io.github.com.pages;
 import com.epam.jdi.light.angular.elements.complex.Card;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 
-public class CardPage extends NewAngularPage{
+public class CardPage extends NewAngularPage {
     @UI("#simple-card")
     public static Card simpleCard;
 
