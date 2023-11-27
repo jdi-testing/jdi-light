54ae13b78ef4249efb780107d108c9c062d63b43
Maya Afanaseva <afanasevaml@yandex.ru>
2023-11-27 20:04:16 +0200

    4914_cardTestsRefAlignTestAdded2

--- jdi-light-angular/src/main/java/com/epam/jdi/light/angular/elements/complex/Card.java
+++ jdi-light-angular/src/main/java/com/epam/jdi/light/angular/elements/complex/Card.java
@@ -5,7 +5,6 @@ import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.elements.base.UIBaseElement;
 import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.light.elements.complex.WebList;
-import org.openqa.selenium.By;
 
 /**
  * To see an example of Card web element please visit https://material.angular.io/components/card/overview.
