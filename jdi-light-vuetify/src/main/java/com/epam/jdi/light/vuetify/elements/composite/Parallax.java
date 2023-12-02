9ddb642216675e13f80fa7688cef9bd8f58651f7
Natalia Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2023-12-02 00:36:37 -0500

    Fix Parallax

--- jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/elements/composite/Parallax.java
+++ jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/elements/composite/Parallax.java
@@ -5,6 +5,7 @@ import com.epam.jdi.light.elements.base.UIBaseElement;
 import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import com.epam.jdi.light.vuetify.asserts.ParallaxAssert;
+import com.epam.jdi.light.vuetify.interfaces.HasImage;
 import com.epam.jdi.light.vuetify.interfaces.IsContainer;
 import org.openqa.selenium.By;
 import com.epam.jdi.light.vuetify.elements.common.Image;
@@ -17,12 +18,9 @@ import com.epam.jdi.light.vuetify.elements.common.Image;
  * To see an example of Parallax web element please visit https://vuetifyjs.com/en/components/parallax
  */
 
-public class Parallax extends UIBaseElement<ParallaxAssert> implements IsContainer {
+public class Parallax extends UIBaseElement<ParallaxAssert> implements IsContainer, HasImage {
 
     protected String parallaxContent = "v-parallax__content";
-
-    @UI(".v-parallax__image-container > img")
-    public Image parallaxImage;
 
     @JDIAction("Get '{name}' parallax container height")
     public int height() {
@@ -48,8 +46,8 @@ public class Parallax extends UIBaseElement<ParallaxAssert> implements IsContain
      * for parallax Image, while html Image methods are working fine
      */
     @JDIAction("Get '{name}' parallax image")
-    public com.epam.jdi.light.ui.html.elements.common.Image image() {
-        return parallaxImage.getJDIImage();
+    public Image image() {
+        return new Image().setCore(Image.class, core());
     }
 
     @Override
