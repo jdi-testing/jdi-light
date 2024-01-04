9db59c71d49e7b463c65dd7640a239d729474271
Natalia Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2024-01-04 17:00:36 -0500

    Fix asserts OverflowButtonAssert

--- jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/asserts/OverflowButtonAssert.java
+++ jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/asserts/OverflowButtonAssert.java
@@ -31,75 +31,69 @@ public class OverflowButtonAssert extends UIAssert<OverflowButtonAssert, Overflo
 
     @JDIAction(value = "Assert that '{name}' is expanded", isAssert = true)
     public OverflowButtonAssert expanded() {
-        jdiAssert(element().isExpanded(), Matchers.is(true), "Element is not expanded");
+        jdiAssert(element().isExpanded(), Matchers.is(true), "OverflowButton is not expanded");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' is closed", isAssert = true)
     public OverflowButtonAssert closed() {
-        jdiAssert(element().isClosed(), Matchers.is(true), "Element is expanded");
+        jdiAssert(element().isClosed(), Matchers.is(true), "OverflowButton is not closed");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' selected value is '{0}'", isAssert = true)
     public OverflowButtonAssert selected(String value) {
-        String actualSelectedValue = element().selected();
-        jdiAssert(actualSelectedValue, Matchers.is(value), String.format("Actual selected value '%s' is not equal to " +
-                "expected '%s'", actualSelectedValue, value));
+        jdiAssert(element().selected(), Matchers.is(value));
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' placeholder has text '{0}'", isAssert = true)
     public OverflowButtonAssert placeholder(String text) {
-        String actualPlaceholderText = element().placeholder();
-        jdiAssert(actualPlaceholderText, Matchers.is(text), String.format("Element's actual placeholder text '%s' " +
-                "is not equal to expected '%s'", actualPlaceholderText, text));
+        jdiAssert(element().placeholder(), Matchers.is(text));
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' has counter", isAssert = true)
     public OverflowButtonAssert counter() {
-        jdiAssert(element().hasCounter(), Matchers.is(true), "Element has not counter");
+        jdiAssert(element().hasCounter(), Matchers.is(true), "OverflowButton has not counter");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' has not counter", isAssert = true)
     public OverflowButtonAssert notCounter() {
-        jdiAssert(element().hasCounter(), Matchers.is(false), "Element has counter");
+        jdiAssert(element().hasCounter(), Matchers.is(false), "OverflowButton has counter");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' counter is '{0}'", isAssert = true)
     public OverflowButtonAssert counter(int value) {
-        int actualCounterValue = element().counterValue();
-        jdiAssert(actualCounterValue, Matchers.equalTo(value), String.format("Actual counter value '%s' is not equal " +
-                "to expected '%s'", actualCounterValue, value));
+        jdiAssert(element().counterValue(), Matchers.equalTo(value));
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' is editable", isAssert = true)
     public OverflowButtonAssert editable() {
-        jdiAssert(element().isEditable(), Matchers.is(true), "Element is not editable");
+        jdiAssert(element().isEditable(), Matchers.is(true), "OverflowButton is not editable");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' is not editable", isAssert = true)
     public OverflowButtonAssert notEditable() {
-        jdiAssert(element().isEditable(), Matchers.is(false), "Element is editable");
+        jdiAssert(element().isEditable(), Matchers.is(false), "OverflowButton is editable");
         return this;
     }
 
     @Override
     @JDIAction(value = "Assert that '{name}' is enabled", isAssert = true)
     public OverflowButtonAssert enabled() {
-        jdiAssert(element().isEnabled(), Matchers.is(true), "Element is disabled");
+        jdiAssert(element().isEnabled(), Matchers.is(true), "OverflowButton is disabled");
         return this;
     }
 
     @Override
     @JDIAction(value = "Assert that '{name}' is disabled", isAssert = true)
     public OverflowButtonAssert disabled() {
-        jdiAssert(element().isDisabled(), Matchers.is(true), "Element is enabled");
+        jdiAssert(element().isDisabled(), Matchers.is(true), "OverflowButton is enabled");
         return this;
     }
 
@@ -117,37 +111,37 @@ public class OverflowButtonAssert extends UIAssert<OverflowButtonAssert, Overflo
 
     @JDIAction(value = "Assert that '{name}' is segmented", isAssert = true)
     public OverflowButtonAssert segmented() {
-        jdiAssert(element().isSegmented(), Matchers.is(true), "Element is not segmented");
+        jdiAssert(element().isSegmented(), Matchers.is(true), "OverflowButton is not segmented");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' is not segmented", isAssert = true)
     public OverflowButtonAssert notSegmented() {
-        jdiAssert(element().isSegmented(), Matchers.is(false), "Element is segmented");
+        jdiAssert(element().isSegmented(), Matchers.is(false), "OverflowButton is segmented");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' has chips", isAssert = true)
     public OverflowButtonAssert chips() {
-        jdiAssert(element().hasChips(), Matchers.is(true), "Element has no chips");
+        jdiAssert(element().hasChips(), Matchers.is(true), "OverflowButton has no chips");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' has no chips", isAssert = true)
     public OverflowButtonAssert noChips() {
-        jdiAssert(element().hasChips(), Matchers.is(false), "Element has chips");
+        jdiAssert(element().hasChips(), Matchers.is(false), "OverflowButton has chips");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' has small chips", isAssert = true)
     public OverflowButtonAssert smallChips() {
-        jdiAssert(element().hasSmallChips(), Matchers.is(true), "Element has no small chips");
+        jdiAssert(element().hasSmallChips(), Matchers.is(true), "OverflowButton has no small chips");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' has no small chips", isAssert = true)
     public OverflowButtonAssert noSmallChips() {
-        jdiAssert(element().hasSmallChips(), Matchers.is(false), "Element has small chips");
+        jdiAssert(element().hasSmallChips(), Matchers.is(false), "OverflowButton has small chips");
         return this;
     }
 }
