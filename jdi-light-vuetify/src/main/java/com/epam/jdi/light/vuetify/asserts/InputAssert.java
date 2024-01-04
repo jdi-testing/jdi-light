6862f61651dca8f2415ee34e112ad5ff226a1055
Natalia Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2024-01-04 16:43:15 -0500

    Fix asserts Input

--- jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/asserts/InputAssert.java
+++ jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/asserts/InputAssert.java
@@ -22,20 +22,21 @@ public class InputAssert extends UIAssert<InputAssert, Input>
     @Override
     @JDIAction(value = "Assert that '{name}' is disabled", isAssert = true)
     public InputAssert disabled() {
-        jdiAssert(element().isDisabled(), Matchers.is(true), "Element is enabled");
+        jdiAssert(element().isDisabled(), Matchers.is(true), "Input is enabled");
         return this;
     }
 
     @Override
     @JDIAction(value = "Assert that '{name}' is enabled", isAssert = true)
     public InputAssert enabled() {
-        jdiAssert(element().isDisabled(), Matchers.is(false), "Element is disabled");
+        jdiAssert(element().isDisabled(), Matchers.is(false), "Input is disabled");
         return this;
     }
 
+    // TODO check this method
     @JDIAction(value = "Assert that '{name}' has text in slot", isAssert = true)
     public InputAssert textInSlot() {
-        jdiAssert(element().hasTextInSlot(), Matchers.is(true), "Element hasn't text in slot");
+        jdiAssert(element().hasTextInSlot(), Matchers.is(true), "Input hasn't text in slot");
         return this;
     }
 
@@ -49,129 +50,109 @@ public class InputAssert extends UIAssert<InputAssert, Input>
 
     @JDIAction(value = "Assert that '{name}' has text field", isAssert = true)
     public InputAssert textField() {
-        jdiAssert(element().hasTextField(), Matchers.is(true), "Element hasn't text field");
+        jdiAssert(element().hasTextField(), Matchers.is(true), "Input hasn't text field");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' has text field", isAssert = true)
     public InputAssert notTextField() {
-        jdiAssert(element().hasTextField(), Matchers.is(false), "Element text field");
+        jdiAssert(element().hasTextField(), Matchers.is(false), "Input text field");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' has typed text in input field", isAssert = true)
     public InputAssert typedText() {
-        jdiAssert(element().hasTypedText(), Matchers.is(true), "Element hasn't typed text");
+        jdiAssert(element().hasTypedText(), Matchers.is(true), "Input hasn't typed text");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' has typed text '{0}' in input field", isAssert = true)
     public InputAssert typedText(String text) {
-        String actualTypedText = element().getTypedText();
-        jdiAssert(actualTypedText, Matchers.equalTo(text), String.format("Actual typed text '%s' is not equal to " +
-                "expected '%s'", actualTypedText, text));
+        jdiAssert(element().getTypedText(), Matchers.equalTo(text));
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' has not typed text in input field", isAssert = true)
     public InputAssert notTypedText() {
-        jdiAssert(element().hasTypedText(), Matchers.is(true), "Element has typed text");
+        jdiAssert(element().hasTypedText(), Matchers.is(false), "Input has typed text");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' has empty input field", isAssert = true)
     public InputAssert empty() {
-        String actualTypedText = element().getTypedText();
-        String errorMsg = String.format("Element should not have text, but was '%s'", actualTypedText);
-        jdiAssert(actualTypedText, Matchers.emptyString(), errorMsg);
+        jdiAssert(element().getTypedText(), Matchers.emptyString());
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' has label", isAssert = true)
     public InputAssert label() {
-        jdiAssert(element().hasLabel(), Matchers.is(true), "Element has no label");
+        jdiAssert(element().hasLabel(), Matchers.is(true), "Input has no label");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' has no label", isAssert = true)
     public InputAssert noLabel() {
-        jdiAssert(element().hasLabel(), Matchers.is(false), "Element has label");
+        jdiAssert(element().hasLabel(), Matchers.is(false), "Input has label");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' has label '{0}'", isAssert = true)
     public InputAssert label(String text) {
-        String actualLabelText = element().getLabel();
-        jdiAssert(actualLabelText, Matchers.equalTo(text), String.format("Actual label text '%s' is not equal to " +
-                "expected '%s'", actualLabelText, text));
+        jdiAssert(element().getLabel(), Matchers.equalTo(text));
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' is focused", isAssert = true)
     public InputAssert focused() {
-        jdiAssert(element().isFocused(), Matchers.is(true), "Element is not focused");
+        jdiAssert(element().isFocused(), Matchers.is(true), "Input is not focused");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' is not focused", isAssert = true)
     public InputAssert notFocused() {
-        jdiAssert(element().isFocused(), Matchers.is(false), "Element is focused");
+        jdiAssert(element().isFocused(), Matchers.is(false), "Input is focused");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' has prepend outer icon", isAssert = true)
     public InputAssert prependOuterIcon() {
-        jdiAssert(element().hasPrependOuterIcon(), Matchers.is(true), "Element hasn't prepend outer icon");
+        jdiAssert(element().hasPrependOuterIcon(), Matchers.is(true), "Input hasn't prepend outer icon");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' has prepend inner icon", isAssert = true)
     public InputAssert prependInnerIcon() {
-        jdiAssert(element().hasPrependInnerIcon(), Matchers.is(true), "Element hasn't prepend inner icon");
+        jdiAssert(element().hasPrependInnerIcon(), Matchers.is(true), "Input hasn't prepend inner icon");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' has append outer icon", isAssert = true)
     public InputAssert appendOuterIcon() {
-        jdiAssert(element().hasAppendOuterIcon(), Matchers.is(true), "Element hasn't append outer icon");
+        jdiAssert(element().hasAppendOuterIcon(), Matchers.is(true), "Input hasn't append outer icon");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' has append inner icon", isAssert = true)
     public InputAssert appendInnerIcon() {
-        jdiAssert(element().hasAppendInnerIcon(), Matchers.is(true), "Element hasn't append inner icon");
+        jdiAssert(element().hasAppendInnerIcon(), Matchers.is(true), "Input hasn't append inner icon");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' has switch", isAssert = true)
     public InputAssert switchInput() {
-        jdiAssert(element().hasSwitch(), Matchers.is(true), "Element hasn't switch");
+        jdiAssert(element().hasSwitch(), Matchers.is(true), "Input hasn't switch");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' switch is checked", isAssert = true)
     public InputAssert checkedSwitch() {
-        jdiAssert(element().switchIsChecked(), Matchers.is(true), "Element's switch is unchecked");
+        jdiAssert(element().switchIsChecked(), Matchers.is(true), "Input's switch is unchecked");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' switch is unchecked", isAssert = true)
     public InputAssert uncheckedSwitch() {
-        jdiAssert(element().switchIsChecked(), Matchers.is(false), "Element's switch is checked");
-        return this;
-    }
-
-    @JDIAction(value = "Assert that '{name}' has class attribute '{0}'", isAssert = true)
-    public InputAssert className(String className) {
-        jdiAssert(element().hasClass(className), Matchers.is(true), String.format("Element hasn't class " +
-                "attribute '%s'", className));
-        return this;
-    }
-
-    @JDIAction(value = "Assert that '{name}' has not class attribute '{0}'", isAssert = true)
-    public InputAssert notClassName(String className) {
-        jdiAssert(element().hasAttribute(className), Matchers.is(false), String.format("Element has class " +
-                "attribute '%s'", className));
+        jdiAssert(element().switchIsChecked(), Matchers.is(false), "Input's switch is checked");
         return this;
     }
 }
