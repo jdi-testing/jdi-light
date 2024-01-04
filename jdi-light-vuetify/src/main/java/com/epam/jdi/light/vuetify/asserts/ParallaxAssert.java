fb4dfce5e4906e481b138a2163d8a65896b91f5b
Natalia Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2024-01-04 17:04:51 -0500

    Fix asserts ParallaxAssert

--- jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/asserts/ParallaxAssert.java
+++ jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/asserts/ParallaxAssert.java
@@ -12,20 +12,19 @@ public class ParallaxAssert extends UIAssert<ParallaxAssert, Parallax> implement
 
     @JDIAction(value = "Assert that '{name}' container height is '{0} px'", isAssert = true)
     public ParallaxAssert height(int height) {
-        int actualHeight = element().height();
-        jdiAssert(actualHeight, Matchers.equalTo(height));
+        jdiAssert(element().height(), Matchers.equalTo(height));
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' has elements in content section", isAssert = true)
     public ParallaxAssert content() {
-        jdiAssert(element().hasContent(), Matchers.is(true), "Element has no content");
+        jdiAssert(element().hasContent(), Matchers.is(true), "Parallax has no content");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' has no elements in content section", isAssert = true)
     public ParallaxAssert noContent() {
-        jdiAssert(element().hasContent(), Matchers.is(false), "Element has content");
+        jdiAssert(element().hasContent(), Matchers.is(false), "Parallax has content");
         return this;
     }
 }
