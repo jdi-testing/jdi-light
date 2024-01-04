5ab050cc1e081483dd5be8902fefbf04af7d5c59
Natalia Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2024-01-04 16:55:43 -0500

    Fix asserts ListItemGroup

--- jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/asserts/ListItemGroupsAssert.java
+++ jdi-light-vuetify/src/main/java/com/epam/jdi/light/vuetify/asserts/ListItemGroupsAssert.java
@@ -17,81 +17,79 @@ public class ListItemGroupsAssert extends UIAssert<ListItemGroupsAssert, ListIte
 
     @JDIAction(value = "Assert that '{name}' is active", isAssert = true)
     public ListItemGroupsAssert active() {
-        jdiAssert(element().isActive(), Matchers.is(true), "Element is not active");
+        jdiAssert(element().isActive(), Matchers.is(true), "ListItemGroup is not active");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' is not active", isAssert = true)
     public ListItemGroupsAssert notActive() {
-        jdiAssert(element().isActive(), Matchers.is(false), "Element is active");
+        jdiAssert(element().isActive(), Matchers.is(false), "ListItemGroup is active");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' has title", isAssert = true)
     public ListItemGroupsAssert title() {
-        jdiAssert(element().hasTitle(), Matchers.is(true), "Element has no title");
+        jdiAssert(element().hasTitle(), Matchers.is(true), "ListItemGroup has no title");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' has no title", isAssert = true)
     public ListItemGroupsAssert noTitle() {
-        jdiAssert(element().hasTitle(), Matchers.is(false), "Element has title");
+        jdiAssert(element().hasTitle(), Matchers.is(false), "ListItemGroup has title");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' has border", isAssert = true)
     public ListItemGroupsAssert border() {
-        jdiAssert(element().hasBorder(), Matchers.is(true), "Element has no border");
+        jdiAssert(element().hasBorder(), Matchers.is(true), "ListItemGroup has no border");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' has not border", isAssert = true)
     public ListItemGroupsAssert noBorder() {
-        jdiAssert(element().hasBorder(), Matchers.is(false), "Element has border");
+        jdiAssert(element().hasBorder(), Matchers.is(false), "ListItemGroup has border");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' has icon", isAssert = true)
     public ListItemGroupsAssert icon() {
-        jdiAssert(element().hasIcon(), Matchers.is(true), "Element has no icon");
+        jdiAssert(element().hasIcon(), Matchers.is(true), "ListItemGroup does not have icon");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' has no icon", isAssert = true)
     public ListItemGroupsAssert noIcon() {
-        jdiAssert(element().hasIcon(), Matchers.is(false), "Element has icon");
+        jdiAssert(element().hasIcon(), Matchers.is(false), "ListItemGroup has icon");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' contains text '{0}'", isAssert = true)
     public ListItemGroupsAssert containsText(String text) {
-        String actualText = element().getText();
-        jdiAssert(actualText, Matchers.containsString(text), String.format("Element's actual text '%s' doesn't " +
-                "contain expected '%s'", actualText, text));
+        jdiAssert(element().getText(), Matchers.containsString(text));
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' is a sub-group", isAssert = true)
     public ListItemGroupsAssert subGroup() {
-        jdiAssert(element().isSubgroup(), Matchers.is(true), "Element is not a sub-group");
+        jdiAssert(element().isSubgroup(), Matchers.is(true), "ListItemGroup is not a sub-group");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' is not a sub-group", isAssert = true)
     public ListItemGroupsAssert notSubGroup() {
-        jdiAssert(element().isSubgroup(), Matchers.is(false), "Element is sub-group");
+        jdiAssert(element().isSubgroup(), Matchers.is(false), "ListItemGroup is sub-group");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' is no-action", isAssert = true)
     public ListItemGroupsAssert noAction() {
-        jdiAssert(element().isNoAction(), Matchers.is(true), "Element is not no-action");
+        jdiAssert(element().isNoAction(), Matchers.is(true), "ListItemGroup is not no-action");
         return this;
     }
 
     @JDIAction(value = "Assert that '{name}' is not no-action", isAssert = true)
     public ListItemGroupsAssert notNoAction() {
-        jdiAssert(element().isNoAction(), Matchers.is(false), "Element is no-action");
+        jdiAssert(element().isNoAction(), Matchers.is(false), "ListItemGroup is no-action");
         return this;
     }
 }
