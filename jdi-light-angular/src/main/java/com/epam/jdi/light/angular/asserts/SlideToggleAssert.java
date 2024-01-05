dff581e4c97a4889b574a52c56566a06346553c7
Natalia Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2024-01-04 21:27:35 -0500

    Fix asserts

--- jdi-light-angular/src/main/java/com/epam/jdi/light/angular/asserts/SlideToggleAssert.java
+++ jdi-light-angular/src/main/java/com/epam/jdi/light/angular/asserts/SlideToggleAssert.java
@@ -10,40 +10,40 @@ import org.hamcrest.Matchers;
 public class SlideToggleAssert extends UIAssert<SlideToggleAssert, SlideToggle> {
 
     @Override
-    @JDIAction("Assert that '{name}' is disabled")
+    @JDIAction(value = "Assert that '{name}' is disabled", isAssert = true)
     public SlideToggleAssert disabled() {
         jdiAssert(element().isDisabled(), Matchers.is(true), "Slide toggle is enabled");
         return this;
     }
 
     @Override
-    @JDIAction("Assert that '{name}' is disabled")
+    @JDIAction(value = "Assert that '{name}' is disabled", isAssert = true)
     public SlideToggleAssert enabled() {
         jdiAssert(element().isDisabled(), Matchers.is(false), "Slide toggle is disabled");
         return this;
     }
 
-    @JDIAction("'{name}' element label is in before position")
+    @JDIAction(value = "'{name}' element label is in before position", isAssert = true)
     public SlideToggleAssert labelBeforePosition() {
         jdiAssert(element().hasLabelBeforePosition(), Matchers.is(true),
             "Slide toggle label is not in before position");
         return this;
     }
 
-    @JDIAction("'{name}' element label is in after position")
+    @JDIAction(value = "'{name}' element label is in after position", isAssert = true)
     public SlideToggleAssert labelAfterPosition() {
         jdiAssert(element().hasLabelBeforePosition(), Matchers.is(false),
             "Slide toggle label is not in after position");
         return this;
     }
 
-    @JDIAction("'{name}' is selected")
+    @JDIAction(value = "'{name}' is selected", isAssert = true)
     public SlideToggleAssert selected() {
         jdiAssert(element().isSelected(), Matchers.is(true), "Slide toggle is not selected");
         return this;
     }
 
-    @JDIAction("'{name}' is not selected")
+    @JDIAction(value = "'{name}' is not selected", isAssert = true)
     public SlideToggleAssert notSelected() {
         jdiAssert(element().isSelected(), Matchers.is(false), "Slide toggle is selected");
         return this;
