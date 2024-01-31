5bcaab1a6a0a6f715759ebe87363d3c4a3c0a2e7
igor-korost <74544655+igor-korost@users.noreply.github.com>
2024-01-31 19:47:03 +0600

    Update jdi-light-angular/src/main/java/com/epam/jdi/light/angular/elements/complex/Snackbar.java
    Co-authored-by: Natalia Pozhidaeva <Natalia_Pozhidaeva@epam.com>

--- jdi-light-angular/src/main/java/com/epam/jdi/light/angular/elements/complex/Snackbar.java
+++ jdi-light-angular/src/main/java/com/epam/jdi/light/angular/elements/complex/Snackbar.java
@@ -25,7 +25,7 @@ public class Snackbar extends UIBaseElement<SnackbarAssert> {
     }
 
     @JDIAction("Get '{name}' message")
-    public String getMessageText() {
+    public String messageText() {
         return message.getText();
     }
 
