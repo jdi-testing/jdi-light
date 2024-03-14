169b3a9709c37633a3d28f03ccd4e05b3cbe2fd9
Aleksandr_Vorobev <aleksandr.vorobev@tvh.com>
2024-03-14 16:07:35 +0300

    rework Angular element: Button

--- jdi-light-angular/src/main/java/com/epam/jdi/light/angular/elements/common/Button.java
+++ jdi-light-angular/src/main/java/com/epam/jdi/light/angular/elements/common/Button.java
@@ -1,15 +1,12 @@
 package com.epam.jdi.light.angular.elements.common;
 
 import com.epam.jdi.light.angular.asserts.ButtonAssert;
-import com.epam.jdi.light.angular.elements.enums.AngularColors;
-import com.epam.jdi.light.angular.elements.enums.ButtonsTypes;
 import com.epam.jdi.light.angular.elements.interfaces.HasBadge;
 import com.epam.jdi.light.angular.elements.interfaces.IsButton;
 import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.elements.base.UIBaseElement;
 import com.epam.jdi.light.elements.interfaces.base.HasClick;
 import com.epam.jdi.light.elements.interfaces.common.IsText;
-import com.jdiai.tools.map.MapArray;
 
 public class Button extends UIBaseElement<ButtonAssert> implements HasBadge, HasClick, IsText, IsButton {
 
